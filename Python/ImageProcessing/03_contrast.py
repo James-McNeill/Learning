a6ab9b2f08f2a150e8dcde88c6877c540d89a286
James_McNeill <jmcneill06@outlook.com>
2021-08-22 09:06:57 +0100

    Create 03_contrast.py

---
+++ Python/ImageProcessing/03_contrast.py
@@ -0,0 +1,27 @@
+# Import the required module
+from skimage import exposure
+
+# Show original x-ray image and its histogram
+show_image(chest_xray_image, 'Original x-ray')
+
+plt.title('Histogram of image')
+plt.hist(chest_xray_image.ravel(), bins=256)
+plt.show()
+
+# Use histogram equalization to improve the contrast
+xray_image_eq =  exposure.equalize_hist(chest_xray_image)
+
+# Show the resulting image
+show_image(xray_image_eq, 'Resulting image')
+# Import the necessary modules
+from skimage import data, exposure
+
+# Load the image
+original_image = data.coffee()
+
+# Apply the adaptive equalization on the original image
+adapthist_eq_image = exposure.equalize_adapthist(original_image, clip_limit=0.03)
+
+# Compare the original image to the equalized
+show_image(original_image)
+show_image(adapthist_eq_image, '#ImageProcessingDatacamp')
