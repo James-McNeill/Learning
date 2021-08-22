da2843aa987fec1126a5e4c1a08b772103fd4a28
James_McNeill <jmcneill06@outlook.com>
2021-08-22 09:09:24 +0100

    Create 07_noise.py

---
+++ Python/ImageProcessing/07_noise.py
@@ -0,0 +1,29 @@
+# Import the module and function
+from skimage.util import random_noise
+
+# Add noise to the image
+noisy_image = random_noise(fruit_image)
+
+# Show original and resulting image
+show_image(fruit_image, 'Original')
+show_image(noisy_image, 'Noisy image')
+# Import the module and function
+from skimage.restoration import denoise_tv_chambolle
+
+# Apply total variation filter denoising
+denoised_image = denoise_tv_chambolle(noisy_image, 
+                                      multichannel=True)
+
+# Show the noisy and denoised images
+show_image(noisy_image, 'Noisy')
+show_image(denoised_image, 'Denoised image')
+# Import bilateral denoising function
+from skimage.restoration import denoise_bilateral
+
+# Apply bilateral filter denoising
+denoised_image = denoise_bilateral(landscape_image, 
+                                   multichannel=True)
+
+# Show original and resulting images
+show_image(landscape_image, 'Noisy image')
+show_image(denoised_image, 'Denoised image')
