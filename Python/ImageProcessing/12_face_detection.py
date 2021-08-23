86f3788a75abbdc00f2e1f53c40c246b2876bb39
James_McNeill <jmcneill06@outlook.com>
2021-08-23 10:04:18 +0100

    Create 12_face_detection.py

---
+++ Python/ImageProcessing/12_face_detection.py
@@ -0,0 +1,54 @@
+# Import the classifier class
+from skimage.feature import Cascade
+# Import the data module
+from skimage import data
+
+# Load the trained file from data - xml file format, detecting one face within the image
+trained_file = data.lbp_frontal_face_cascade_filename()
+
+# Initialize the detector cascade
+detector = Cascade(trained_file)
+
+# Detect faces with min and max size of searching window
+detected = detector.detect_multi_scale(img = night_image,
+                                       scale_factor=1.2,
+                                       step_ratio=1,
+                                       min_size=(10, 10),
+                                       max_size=(200, 200))
+
+# Show the detected faces
+show_detected_face(night_image, detected)
+
+# Load the trained file from data - detecting multiple faces within an image
+trained_file = data.lbp_frontal_face_cascade_filename()
+
+# Initialize the detector cascade
+detector = Cascade(trained_file)
+
+# Detect faces with scale factor to 1.2 and step ratio to 1
+detected = detector.detect_multi_scale(img=friends_image,
+                                       scale_factor=1.2,
+                                       step_ratio=1,
+                                       min_size=(10, 10),
+                                       max_size=(200, 200))
+# Show the detected faces
+show_detected_face(friends_image, detected)
+
+# Import the modules
+from skimage.segmentation import slic
+from skimage.color import label2rgb
+
+# Obtain the segmentation with default 100 regions
+segments = slic(profile_image, n_segments=100)
+
+# Obtain segmented image using label2rgb
+segmented_image = label2rgb(segments, profile_image, kind='avg')
+
+# Detect the faces with multi scale method
+detected = detector.detect_multi_scale(img=segmented_image, 
+                                       scale_factor=1.2, 
+                                       step_ratio=1, 
+                                       min_size=(10, 10), max_size=(1000, 1000))
+
+# Show the detected faces
+show_detected_face(segmented_image, detected)
