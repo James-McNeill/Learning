8224e1da683241a5c0c85eb8f2c307b590269654
James_McNeill <jmcneill06@outlook.com>
2021-08-25 15:02:45 +0100

    Create boto_client.py

---
+++ Python/AWS_Boto/boto_client.py
@@ -0,0 +1,31 @@
+# Connection credentials when setting up the connection script
+# AWS_KEY_ID: username
+# AWS_SECRET: password
+
+# Generate the boto3 client for interacting with S3
+s3 = boto3.client('s3', region_name='us-east-1', 
+                        # Set up AWS credentials 
+                        aws_access_key_id=AWS_KEY_ID, 
+                        aws_secret_access_key=AWS_SECRET)
+# List the buckets
+buckets = s3.list_buckets()
+
+# Print the buckets
+print(buckets)
+
+# Setting up multiple client services
+# Generate the boto3 client for interacting with S3 and SNS
+s3 = boto3.client('s3', region_name='us-east-1', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+sns = boto3.client('sns', region_name='us-east-1', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+# List S3 buckets and SNS topics
+buckets = s3.list_buckets()
+topics = sns.list_topics()
+
+# Print out the list of SNS topics
+print(topics)
