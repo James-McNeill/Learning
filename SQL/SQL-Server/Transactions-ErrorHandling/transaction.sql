c9c31ff2235ff486de6b5e160909703ec9bdf01d
James_McNeill <jmcneill06@outlook.com>
2021-08-30 16:36:52 +0100

    Update transaction.sql

--- SQL/SQL-Server/Transactions-ErrorHandling/transaction.sql
+++ SQL/SQL-Server/Transactions-ErrorHandling/transaction.sql
@@ -4,7 +4,7 @@
 -- transaction process if the code has no errors. If there are errors within the TRAN then the 
 -- ROLLBACK statement will reverse any code within the TRAN statements.
 BEGIN TRY  
-	BEGIN TRAN;
+	BEGIN TRAN; -- Everything within this section needs to work correctly to function
 		UPDATE accounts SET current_balance = current_balance - 100 WHERE account_id = 1;
 		INSERT INTO transactions VALUES (1, -100, GETDATE());
         
@@ -13,5 +13,5 @@ BEGIN TRY
 	COMMIT TRAN;
 END TRY
 BEGIN CATCH  
-	ROLLBACK TRAN;
+	ROLLBACK TRAN; -- If there are errors in the TRAN block then this statement will ROLLBACK
 END CATCH
