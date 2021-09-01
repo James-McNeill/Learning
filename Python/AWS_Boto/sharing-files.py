550c43e2ba5d1de9ba0adb9d596030ea1128ee2f
James_McNeill <jmcneill06@outlook.com>
2021-09-01 10:47:52 +0100

    Update sharing-files.py

--- Python/AWS_Boto/sharing-files.py
+++ Python/AWS_Boto/sharing-files.py
@@ -11,3 +11,31 @@ share_url = s3.generate_presigned_url(
 
 # Print out the presigned URL
 print(share_url)
+
+
+# Loading multiple files into a DataFrame for analysis. Objective is to take multiple
+# files that are stored within a S3 bucket and concat them into one DataFrame.
+df_list =  [ ] 
+
+# All objects contained in the Bucket are stored within the response variable
+for file in response['Contents']:
+    # For each file in response load the object from S3
+    obj = s3.get_object(Bucket='gid-requests', Key=file['Key'])
+    # Load the object's StreamingBody with pandas
+    obj_df = pd.read_csv(obj['Body'])
+    # Append the resulting DataFrame to list
+    df_list.append(obj_df)
+
+# Concat all the DataFrames with pandas
+df = pd.concat(df_list)
+
+# Preview the resulting DataFrame
+df.head()
+
+# Review accessing private objects in S3
+# download then open
+s3.download_file() 
+# Open directly
+s3.get_object()
+# Generate presigned URL
+s3.generate_presigned_url()
