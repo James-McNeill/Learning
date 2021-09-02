3feacc4b02ddc4c9d1ffe27bd697aadf17eeec3a
James_McNeill <jmcneill06@outlook.com>
2021-09-02 17:05:58 +0100

    Create sns_multiple_topics.py

---
+++ Python/AWS_Boto/sns_multiple_topics.py
@@ -0,0 +1,26 @@
+# Create list of departments
+departments = ['trash', 'streets', 'water']
+
+# Create multiple sns topics using the following for loop
+for dept in departments:
+  	# For every department, create a general topic
+    sns.create_topic(Name="{}_general".format(dept))
+    
+    # For every department, create a critical topic
+    sns.create_topic(Name="{}_critical".format(dept))
+
+# Print all the topics in SNS
+response = sns.list_topics()
+print(response['Topics'])
+
+# Removing the topics that are not critical from the messaging service
+# Get the current list of topics
+topics = sns.list_topics()['Topics']
+
+for topic in topics:
+  # For each topic, if it is not marked critical, delete it
+  if "critical" not in topic['TopicArn']:
+    sns.delete_topic(TopicArn=topic['TopicArn'])
+    
+# Print the list of remaining critical topics
+print(sns.list_topics()['Topics'])
