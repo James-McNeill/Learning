6ea932c327f48054bc3924731cf8a1a6ac64b20f
James_McNeill <jmcneill06@outlook.com>
2021-09-06 12:39:54 +0100

    Create comprehend.py

---
+++ Python/AWS_Boto/comprehend.py
@@ -0,0 +1,33 @@
+# Creating a comprehend client (AWS Comprehend)
+# The dumping_df contains all of the responses to the app
+# For each dataframe row
+for index, row in dumping_df.iterrows():
+    # Get the public description field
+    description =dumping_df.loc[index, 'public_description']
+    if description != '':
+        # Detect language in the field content
+        resp = comprehend.detect_dominant_language(Text=description)
+        # Assign the top choice language to the lang column.
+        dumping_df.loc[index, 'lang'] = resp['Languages'][0]['LanguageCode']
+        
+# Count the total number of spanish posts
+spanish_post_ct = len(dumping_df[dumping_df.lang == 'es'])
+# Print the result
+print("{} posts in Spanish".format(spanish_post_ct))
+
+# 2. Translating text from one langage to another
+for index, row in dumping_df.iterrows():
+  	# Get the public_description into a variable
+    description = dumping_df.loc[index, 'public_description']
+    if description != '':
+      	# Translate the public description
+        resp = translate.translate_text(
+            Text=description, 
+            SourceLanguageCode='auto', TargetLanguageCode='en')
+        # Store original language in original_lang column
+        dumping_df.loc[index, 'original_lang'] = resp['SourceLanguageCode']
+        # Store the translation in the translated_desc column
+        dumping_df.loc[index, 'translated_desc'] = resp['TranslatedText']
+# Preview the resulting DataFrame
+dumping_df = dumping_df[['service_request_id', 'original_lang', 'translated_desc']]
+dumping_df.head()
