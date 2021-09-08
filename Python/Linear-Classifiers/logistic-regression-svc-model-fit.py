1012807210de6446ecb5c00d11f9ed6c99347053
James_McNeill <jmcneill06@outlook.com>
2021-09-08 17:29:57 +0100

    Create logistic-regression-svc-model-fit.py

---
+++ Python/Linear-Classifiers/logistic-regression-svc-model-fit.py
@@ -0,0 +1,18 @@
+# Working with Logistic Regression and Linear SVC classifiers
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn import datasets
+digits = datasets.load_digits()
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target)
+
+# Apply logistic regression and print scores
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+print(lr.score(X_train, y_train))
+print(lr.score(X_test, y_test))
+
+# Apply SVM and print scores
+svm = SVC()
+svm.fit(X_train, y_train)
+print(svm.score(X_train, y_train))
+print(svm.score(X_test, y_test))
