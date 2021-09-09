d79ca66b2873b74378cd0e517b98e338598d5433
James_McNeill <jmcneill06@outlook.com>
2021-09-09 17:03:27 +0100

    Create logistic-hinge-loss.py

---
+++ Python/Linear-Classifiers/logistic-hinge-loss.py
@@ -0,0 +1,29 @@
+# Mathematical functions for logistic and hinge losses
+def log_loss(raw_model_output):
+   return np.log(1+np.exp(-raw_model_output))
+def hinge_loss(raw_model_output):
+   return np.maximum(0,1-raw_model_output)
+
+# Create a grid of values and plot
+grid = np.linspace(-2,2,1000)
+plt.plot(grid, log_loss(grid), label='logistic')
+plt.plot(grid, hinge_loss(grid), label='hinge')
+plt.legend()
+plt.show()
+
+# Example of the log loss using the Breast Cancer dataset from sklearn
+# The logistic loss, summed over training examples
+def my_loss(w):
+    s = 0
+    for i in range(X.shape[0]):
+        raw_model_output = w@X[i]
+        s = s + log_loss(raw_model_output * y[i])
+    return s
+
+# Returns the w that makes my_loss(w) smallest
+w_fit = minimize(my_loss, X[0]).x
+print(w_fit)
+
+# Compare with scikit-learn's LogisticRegression
+lr = LogisticRegression(fit_intercept=False, C=1000000).fit(X,y)
+print(lr.coef_)
