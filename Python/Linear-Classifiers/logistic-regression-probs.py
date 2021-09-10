5b8702d3534570ee0813fe549855299130def7d2
James_McNeill <jmcneill06@outlook.com>
2021-09-10 15:44:05 +0100

    Update logistic-regression-probs.py

--- Python/Linear-Classifiers/logistic-regression-probs.py
+++ Python/Linear-Classifiers/logistic-regression-probs.py
@@ -24,3 +24,19 @@ plot_classifier(X,y,model,proba=True)
 # Predict probabilities on training points
 prob = model.predict_proba(X)
 print("Maximum predicted probability", np.max(prob))
+
+# Example: review the handwritten digit dataset and compare the most and least predictive digits
+lr = LogisticRegression()
+lr.fit(X,y)
+
+# Get predicted probabilities
+proba = lr.predict_proba(X)
+
+# Sort the example indices by their maximum probability
+proba_inds = np.argsort(np.max(proba,axis=1))
+
+# Show the most confident (least ambiguous) digit - shows the highest probability prediction
+show_digit(proba_inds[-1], lr)
+
+# Show the least confident (most ambiguous) digit - shows the lowest probability prediction
+show_digit(proba_inds[0], lr)
