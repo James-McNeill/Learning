74bcbb3eea57d33ff96352b09a18ae886ba981f7
James_McNeill <jmcneill06@outlook.com>
2021-09-10 16:25:15 +0100

    Update svms.py

--- Python/Linear-Classifiers/svms.py
+++ Python/Linear-Classifiers/svms.py
@@ -15,3 +15,15 @@ y_small = y[svm.support_]
 svm_small = SVC(kernel="linear")
 svm_small.fit(X_small, y_small)
 plot_classifier(X_small, y_small, svm_small, lims=(11,15,0,6))
+
+# A higher value for Gamma results in overfitting so need to take care
+# Instantiate an RBF SVM
+svm = SVC()
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(svm, parameters)
+searcher.fit(X, y)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
