7a34fcdf58949c2262bf66c78c7154184c393ecf
James_McNeill <jmcneill06@outlook.com>
2021-09-13 17:10:41 +0100

    Update scalar-udf.sql

--- SQL/SQL-Server/Functions-Procedures/scalar-udf.sql
+++ SQL/SQL-Server/Functions-Procedures/scalar-udf.sql
@@ -1,3 +1,4 @@
+-- UDF that returns yesterdays date, no input parameters required
 -- Create GetYesterday()
 CREATE FUNCTION GetYesterday()
 -- Specify return data type
@@ -8,4 +9,21 @@ BEGIN
 RETURN (
     SELECT DATEADD(day, -1, GETDATE())
     )
+END
+
+-- UDF that contains an input parameter
+-- Create SumRideHrsSingleDay
+CREATE FUNCTION SumRideHrsSingleDay (@DateParm date)
+-- Specify return data type
+RETURNS numeric
+AS
+-- Begin
+BEGIN
+RETURN
+-- Add the difference between StartDate and EndDate
+(SELECT SUM(DATEDIFF(second, StartDate, EndDate))/3600
+FROM CapitalBikeShare
+ -- Only include transactions where StartDate = @DateParm
+WHERE CAST(StartDate AS date) = @DateParm)
+-- End
 END
