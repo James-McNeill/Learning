acff0274e393550457aeb3de7884cfc84921db0e
James_McNeill <jmcneill06@outlook.com>
2021-09-13 10:55:04 +0100

    Update EDA-dates.sql

--- SQL/Repo/EDA-dates.sql
+++ SQL/Repo/EDA-dates.sql
@@ -9,3 +9,16 @@ FROM CapitalBikeShare
 GROUP BY CONVERT(DATE, StartDate)
 -- Sort the results by the date portion of StartDate
 ORDER BY CONVERT(DATE, StartDate);
+
+-- Create a grouping using the CASE statement
+SELECT
+	-- Count the number of IDs
+	COUNT(ID) AS Count,
+    -- Use DATEPART() to evaluate the SECOND part of StartDate
+    "StartDate" = CASE WHEN DATEPART(SECOND, StartDate) = 0 THEN 'SECONDS = 0'
+					   WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
+FROM CapitalBikeShare
+GROUP BY
+    -- Use DATEPART() to Group By the CASE statement
+	CASE WHEN DATEPART(SECOND, StartDate) = 0 THEN 'SECONDS = 0'
+		 WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
