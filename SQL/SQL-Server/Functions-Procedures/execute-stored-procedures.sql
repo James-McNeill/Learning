5aaadfb4e1f4f0020d07614ef1d4b24f032657e3
James_McNeill <jmcneill06@outlook.com>
2021-09-14 12:32:22 +0100

    Update execute-stored-procedures.sql

--- SQL/SQL-Server/Functions-Procedures/execute-stored-procedures.sql
+++ SQL/SQL-Server/Functions-Procedures/execute-stored-procedures.sql
@@ -9,3 +9,21 @@ EXEC dbo.cuspSumRideHrsSingleDay
 	@RideHrsOut = @RideHrs OUTPUT
 -- Select @RideHrs
 SELECT @RideHrs AS RideHours
+
+-- 2. Execute stored procedure with return value
+-- Create @ReturnStatus
+DECLARE @ReturnStatus AS int
+-- Execute the SP, storing the result in @ReturnStatus
+EXEC @ReturnStatus = dbo.cuspRideSummaryUpdate
+    -- Specify @DateParm
+	@DateParm = '3/1/2018',
+    -- Specify @RideHrs
+	@RideHrs = 300
+
+-- Select the columns of interest
+SELECT
+	@ReturnStatus AS ReturnStatus,
+    Date,
+    RideHours
+FROM dbo.RideSummary 
+WHERE Date = '3/1/2018';
