6273f7626a8d30dddaa49c9da13ad7b6879431ac
James_McNeill <jmcneill06@outlook.com>
2021-09-14 10:10:11 +0100

    Update executing-udfs.sql

--- SQL/SQL-Server/Functions-Procedures/executing-udfs.sql
+++ SQL/SQL-Server/Functions-Procedures/executing-udfs.sql
@@ -2,6 +2,7 @@
 -- examples the dbo schema has been used. However if a value is not provided for the schema
 -- then the users default schema will be used unless a specific value was assigned.
 
+-- 1. Execute the UDF within a SELECT statement
 -- Create @BeginDate
 DECLARE @BeginDate AS date = '3/1/2018'
 -- Create @EndDate
@@ -13,3 +14,12 @@ SELECT
   @EndDate AS EndDate,
   -- Execute SumRideHrsDateRange()
   dbo.SumRideHrsDateRange(@BeginDate, @EndDate) AS TotalRideHrs
+
+-- 2. Use the EXEC keyword to assign a value to the declared variable
+-- Create @RideHrs
+DECLARE @RideHrs AS numeric
+-- Execute SumRideHrsSingleDay function and store the result in @RideHrs
+EXEC @RideHrs = dbo.SumRideHrsSingleDay @DateParm = '3/5/2018' 
+SELECT 
+  'Total Ride Hours for 3/5/2018:', 
+  @RideHrs
