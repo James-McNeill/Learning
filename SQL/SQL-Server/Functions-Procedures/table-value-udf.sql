a5252159daa0a5985bdebf174bcaf398eff8c02b
James_McNeill <jmcneill06@outlook.com>
2021-09-14 09:56:01 +0100

    Create table-value-udf.sql

---
+++ SQL/SQL-Server/Functions-Procedures/table-value-udf.sql
@@ -0,0 +1,17 @@
+-- 1. Creating an Inline Table Value Function (TVF)
+-- Create the function
+CREATE FUNCTION SumStationStats(@StartDate AS datetime)
+-- Specify return data type
+RETURNS TABLE
+AS
+RETURN
+SELECT
+	StartStation,
+    -- Use COUNT() to select RideCount
+	COUNT(ID) AS RideCount,
+    -- Use SUM() to calculate TotalDuration
+    SUM(DURATION) AS TotalDuration
+FROM CapitalBikeShare
+WHERE CAST(StartDate as Date) = @StartDate
+-- Group by StartStation
+GROUP BY StartStation;
