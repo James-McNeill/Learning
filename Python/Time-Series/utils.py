eefb9820e0a7d78d6e07871079a9d4095a89a18f
James_McNeill <jmcneill06@outlook.com>
2021-09-14 16:52:26 +0100

    Create utils.py

---
+++ Python/Time-Series/utils.py
@@ -0,0 +1,27 @@
+"""
+#----------------------------------------------------------------------------------+
+#      This python client gives you an ability to:                                 |
+#       - A utils module that connects to all of the input modules. Allows for     |
+#         this master module to be imported and the methods to be used from it.    |
+#----------------------------------------------------------------------------------+
+"""
+__author__ = "James Mc Neill"
+__version__ = "1.0"
+__maintainer__ = "James Mc Neill"
+__email__ = "jmcneill06@outlook.com"
+__status__ = "Test"
+
+# Example: import utils as utl
+# This code will import all of the modules connected to this package
+
+# Import sub packages
+from ._awsathena import (create_cursor)
+from ._transformations import (transformations)
+from ._stationarity import (stationarity)
+# creating the dependent variable transformation python code
+from ._correlation import (Correlation)
+from ._single_factor import (SingleFactor)
+from ._multi_factor import (MultiFactor)
+
+from ._methods import (dict_to_list, column_exist, check_var_list, create_date_list)
+from ._historicdata import (HistoricData)
