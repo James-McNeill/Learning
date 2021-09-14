19846f3f1a457e5a183a00d269085f2f80d6d9e4
James_McNeill <jmcneill06@outlook.com>
2021-09-14 12:12:13 +0100

    Update crud-stored-procedure.sql

--- SQL/SQL-Server/Functions-Procedures/crud-stored-procedure.sql
+++ SQL/SQL-Server/Functions-Procedures/crud-stored-procedure.sql
@@ -20,3 +20,23 @@ FROM dbo.RideSummary
 -- Check whether Date equals @DateParm
 WHERE Date = @DateParm
 END;
+
+-- 2. Perform an UPDATE statement to the table being updated
+-- Create the stored procedure
+CREATE PROCEDURE dbo.cuspRideSummaryUpdate
+	-- Specify @Date input parameter
+	(@Date date,
+     -- Specify @RideHrs input parameter
+     @RideHrs numeric(18,0))
+AS
+BEGIN
+SET NOCOUNT ON
+-- Update RideSummary
+UPDATE dbo.RideSummary
+-- Set
+SET
+	Date = @Date,
+    RideHours = @RideHrs
+-- Include records where Date equals @Date
+WHERE Date = @Date
+END;
