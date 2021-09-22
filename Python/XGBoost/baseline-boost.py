c10c36377faff05cf2a62118041516cc86647bab
James_McNeill <jmcneill06@outlook.com>
2021-09-22 12:39:28 +0100

    Create baseline-boost.py

---
+++ Python/XGBoost/baseline-boost.py
@@ -0,0 +1,21 @@
+# Make use of the inbuild CV method from the xgb API to perform the boosting review
+
+# Create arrays for the features and the target: X, y
+X, y = churn_data.iloc[:,:-1], churn_data.iloc[:,-1]
+
+# Create the DMatrix from X and y: churn_dmatrix
+churn_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params. NOTE: have to explicitly provide the objective param
+params = {"objective":"reg:logistic", "max_depth":3}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=churn_dmatrix, params=params, 
+                  nfold=3, num_boost_round=5, 
+                  metrics="error", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Print the accuracy
+print(((1-cv_results["test-error-mean"]).iloc[-1]))
