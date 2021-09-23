d1c7fd4669cd47773910aaf844b471afc0572956
James_McNeill <jmcneill06@outlook.com>
2021-09-23 16:49:21 +0100

    Update evaluate-model-quality.py

--- Python/XGBoost/evaluate-model-quality.py
+++ Python/XGBoost/evaluate-model-quality.py
@@ -1,3 +1,4 @@
+# Example 1: Showing the Root Mean Squared Error (RMSE) metric. Penalises large residuals
 # Create the DMatrix: housing_dmatrix
 housing_dmatrix = xgb.DMatrix(data=X, label=y)
 
@@ -13,3 +14,19 @@ print(cv_results)
 # Extract and print final boosting round metric
 print((cv_results["test-rmse-mean"]).tail(1))
 
+
+# Example 2: Showing the Mean Absolute Error (MAE) metric
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics='mae', as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Extract and print final boosting round metric
+print((cv_results["test-mae-mean"]).tail(1))
