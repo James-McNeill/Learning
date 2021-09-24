abb9718faf6a87e00843d7ac6ae76f80b4b97ee9
James_McNeill <jmcneill06@outlook.com>
2021-09-24 12:42:45 +0100

    Create tuning-hyper-maxdepth.py

---
+++ Python/XGBoost/tuning-hyper-maxdepth.py
@@ -0,0 +1,28 @@
+# Max_depth relates to the depth that the trees can grow too
+
+# Create your housing DMatrix
+housing_dmatrix = xgb.DMatrix(data=X,label=y)
+
+# Create the parameter dictionary
+params = {"objective":"reg:linear"}
+
+# Create list of max_depth values
+max_depths = [2, 5, 10, 20]
+best_rmse = []
+
+# Systematically vary the max_depth
+for curr_val in max_depths:
+
+    params["max_depth"] = curr_val
+    
+    # Perform cross-validation
+    cv_results = xgb.cv(
+        dtrain=housing_dmatrix, params=params, nfold=2, num_boost_round=10,
+        early_stopping_rounds=5, metrics="rmse", seed=123, as_pandas=True
+    )
+    
+    # Append the final round rmse to best_rmse
+    best_rmse.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+print(pd.DataFrame(list(zip(max_depths, best_rmse)),columns=["max_depth","best_rmse"]))
