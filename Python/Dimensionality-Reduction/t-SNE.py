0936275ddad0e87bd4a3cc9b248003fdcaf65642
James_McNeill <jmcneill06@outlook.com>
2021-09-24 16:10:25 +0100

    Create t-SNE.py

---
+++ Python/Dimensionality-Reduction/t-SNE.py
@@ -0,0 +1,18 @@
+# t-SNE is a great technique for visual exploration of high dimensional datasets
+# Should be applied to non-numeric features
+
+# Import module
+from sklearn.manifold import TSNE
+
+# Non-numerical columns in the dataset
+non_numeric = ['Branch', 'Gender', 'Component']
+
+# Drop the non-numerical columns from df
+df_numeric = df.drop(non_numeric, axis=1)
+
+# Create a t-SNE model with learning rate 50
+m = TSNE(learning_rate=50)
+
+# Fit and transform the t-SNE model on the numeric dataset
+tsne_features = m.fit_transform(df_numeric)
+print(tsne_features.shape)
