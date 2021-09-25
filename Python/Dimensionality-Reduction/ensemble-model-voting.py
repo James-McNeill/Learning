b7c9b46998bab6d660e4d39c75321902c345e63f
James_McNeill <jmcneill06@outlook.com>
2021-09-25 09:37:52 +0100

    Update ensemble-model-voting.py

--- Python/Dimensionality-Reduction/ensemble-model-voting.py
+++ Python/Dimensionality-Reduction/ensemble-model-voting.py
@@ -47,3 +47,19 @@ print('The model can explain {0:.1%} of the variance in the test set'.format(r_s
 
 # Assign the support array to gb_mask
 rf_mask = rfe_rf.support_
+
+# 4. Use the votes to fit a simple Linear Regression model
+# Sum the votes of the three models
+votes = np.sum([lcv_mask, rf_mask, gb_mask], axis=0)
+
+# Create a mask for features selected by all 3 models
+meta_mask = votes >= 3
+
+# Apply the dimensionality reduction on X
+X_reduced = X.loc[:, meta_mask]
+
+# Plug the reduced dataset into a linear regression pipeline
+X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.3, random_state=0)
+lm.fit(scaler.fit_transform(X_train), y_train)
+r_squared = lm.score(scaler.transform(X_test), y_test)
+print('The model can explain {0:.1%} of the variance in the test set using {1:} features.'.format(r_squared, len(lm.coef_)))
