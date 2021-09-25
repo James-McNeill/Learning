f54693225f671b967c8527680169f07145edf2d3
James_McNeill <jmcneill06@outlook.com>
2021-09-25 08:54:07 +0100

    Update feature-select-rfe.py

--- Python/Dimensionality-Reduction/feature-select-rfe.py
+++ Python/Dimensionality-Reduction/feature-select-rfe.py
@@ -17,3 +17,33 @@ y_pred = lr.predict(X_test_std)
 # Prints accuracy metrics and feature coefficients
 print("{0:.1%} accuracy on test set.".format(accuracy_score(y_test, y_pred))) 
 print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
+
+# Performing manual recursive feature elimination - run the model and then remove features with the lowest co-efficient
+
+# 1. Remove the feature with the lowest model coefficient - initial run
+X = diabetes_df[['pregnant', 'glucose', 'diastolic', 'triceps', 'insulin', 'bmi', 'family', 'age']]
+
+# Performs a 25-75% train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Scales features and fits the logistic regression model
+lr.fit(scaler.fit_transform(X_train), y_train)
+
+# Calculates the accuracy on the test set and prints coefficients
+acc = accuracy_score(y_test, lr.predict(scaler.transform(X_test)))
+print("{0:.1%} accuracy on test set.".format(acc)) 
+print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
+
+# 2. Only keep the feature with the highest coefficient
+X = diabetes_df[['glucose']]
+
+# Performs a 25-75% train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Scales features and fits the logistic regression model to the data
+lr.fit(scaler.fit_transform(X_train), y_train)
+
+# Calculates the accuracy on the test set and prints coefficients
+acc = accuracy_score(y_test, lr.predict(scaler.transform(X_test)))
+print("{0:.1%} accuracy on test set.".format(acc)) 
+print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
