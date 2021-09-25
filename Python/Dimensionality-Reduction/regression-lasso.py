947caf9f2b527cd9ba3f654260d44d325d2147d4
James_McNeill <jmcneill06@outlook.com>
2021-09-25 09:24:06 +0100

    Update regression-lasso.py

--- Python/Dimensionality-Reduction/regression-lasso.py
+++ Python/Dimensionality-Reduction/regression-lasso.py
@@ -24,3 +24,16 @@ zero_coef = la.coef_ == 0
 # Calculate how many features have a zero coefficient
 n_ignored = sum(zero_coef)
 print("The model has ignored {} out of {} features.".format(n_ignored, len(la.coef_)))
+
+# Adjusting the aplha parameter for model regularization
+# Find the highest alpha value with R-squared above 98%
+la = Lasso(alpha=0.1, random_state=0)
+
+# Fits the model and calculates performance stats
+la.fit(X_train_std, y_train)
+r_squared = la.score(X_test_std, y_test)
+n_ignored_features = sum(la.coef_ == 0)
+
+# Print peformance stats 
+print("The model can predict {0:.1%} of the variance in the test set.".format(r_squared))
+print("{} out of {} features were ignored.".format(n_ignored_features, len(la.coef_)))
