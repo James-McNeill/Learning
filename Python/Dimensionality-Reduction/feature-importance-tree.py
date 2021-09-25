0197a2f7cc51dc52dda78d7b7944212ff69566e5
James_McNeill <jmcneill06@outlook.com>
2021-09-25 09:12:49 +0100

    Update feature-importance-tree.py

--- Python/Dimensionality-Reduction/feature-importance-tree.py
+++ Python/Dimensionality-Reduction/feature-importance-tree.py
@@ -22,3 +22,19 @@ reduced_X = X.loc[:, mask]
 
 # prints out the selected column names
 print(reduced_X.columns)
+
+
+# Set the feature eliminator to remove 2 features on each step
+# n_features_to_select: number of features to retain in the model
+# step: number of features that are removed during each iteration
+rfe = RFE(estimator=RandomForestClassifier(), n_features_to_select=2, step=2, verbose=1)
+
+# Fit the model to the training data
+rfe.fit(X_train, y_train)
+
+# Create a mask - retains the features that are selected from the RFE wrapper
+mask = rfe.support_
+
+# Apply the mask to the feature dataset X and print the result
+reduced_X = X.loc[:, mask]
+print(reduced_X.columns)
