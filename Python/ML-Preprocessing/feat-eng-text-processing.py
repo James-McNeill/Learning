e6834c897fc559113de137e5dfd4bd30b4699fc7
James_McNeill <jmcneill06@outlook.com>
2021-09-28 15:30:36 +0100

    Update feat-eng-text-processing.py

--- Python/ML-Preprocessing/feat-eng-text-processing.py
+++ Python/ML-Preprocessing/feat-eng-text-processing.py
@@ -13,3 +13,24 @@ def return_mileage(length):
 # Apply the function to the Length column and take a look at both columns
 hiking["Length_num"] = hiking["Length"].apply(lambda row: return_mileage(row))
 print(hiking[["Length", "Length_num"]].head())
+
+# Tf/idf string vectorization
+# Take the title text
+title_text = volunteer["title"]
+
+# Create the vectorizer method
+tfidf_vec = TfidfVectorizer()
+
+# Transform the text into tf-idf vectors
+text_tfidf = tfidf_vec.fit_transform(title_text)
+
+# Using tf/idf within ML model to classify
+# Split the dataset according to the class distribution of category_desc
+y = volunteer["category_desc"]
+X_train, X_test, y_train, y_test = train_test_split(text_tfidf.toarray(), y, stratify=y)
+
+# Fit the model to the training data
+nb.fit(X_train, y_train)
+
+# Print out the model's accuracy
+print(nb.score(X_test, y_test))
