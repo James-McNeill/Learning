eae8d480943a75efd6634b26a3e83f9a41b2e298
James_McNeill <jmcneill06@outlook.com>
2021-09-28 12:56:41 +0100

    Update pca-selection.py

--- Python/Dimensionality-Reduction/pca-selection.py
+++ Python/Dimensionality-Reduction/pca-selection.py
@@ -7,3 +7,18 @@ pipe = Pipeline([('scaler', StandardScaler()),
 pipe.fit(ansur_df)
 
 print('{} components selected'.format(len(pipe.steps[1][1].components_)))
+
+# Use a visual plot to help select the number of components to include - making use of an elbow section of the plot as the initial cut-off point
+# Pipeline a scaler and pca selecting 10 components
+pipe = Pipeline([('scaler', StandardScaler()),
+        		 ('reducer', PCA(n_components=10))])
+
+# Fit the pipe to the data
+pipe.fit(ansur_df)
+
+# Plot the explained variance ratio
+plt.plot(pipe.steps[1][1].explained_variance_ratio_)
+
+plt.xlabel('Principal component index')
+plt.ylabel('Explained variance ratio')
+plt.show()
