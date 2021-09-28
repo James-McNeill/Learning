999998c8db4a5b599f2e55ecb5eda6cfe784caea
James_McNeill <jmcneill06@outlook.com>
2021-09-28 11:08:11 +0100

    Create pca-model-pipeline.py

---
+++ Python/Dimensionality-Reduction/pca-model-pipeline.py
@@ -0,0 +1,28 @@
+# Build the pipeline
+pipe = Pipeline([
+        ('scaler', StandardScaler()),
+        ('reducer', PCA(n_components=2)),
+        ('classifier', RandomForestClassifier(random_state=0))])
+
+# Fit the pipeline to the training data
+pipe.fit(X_train, y_train)
+
+# Prints the explained variance ratio
+print(pipe.steps[1][1].explained_variance_ratio_)
+
+# Adding additional component didn't result in an improved accuracy metric
+# Build the pipeline
+pipe = Pipeline([
+        ('scaler', StandardScaler()),
+        ('reducer', PCA(n_components=3)),
+        ('classifier', RandomForestClassifier(random_state=0))])
+
+# Fit the pipeline to the training data
+pipe.fit(X_train, y_train)
+
+# Score the accuracy on the test set
+accuracy = pipe.score(X_test, y_test)
+
+# Prints the explained variance ratio and accuracy
+print(pipe.steps[1][1].explained_variance_ratio_)
+print('{0:.1%} test set accuracy'.format(accuracy))
