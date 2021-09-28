4333c5ed4e6526f57a3ef5f7014f53fc9a57626b
James_McNeill <jmcneill06@outlook.com>
2021-09-28 11:04:56 +0100

    Update pca-applications.py

--- Python/Dimensionality-Reduction/pca-applications.py
+++ Python/Dimensionality-Reduction/pca-applications.py
@@ -9,3 +9,19 @@ vectors = pipe.steps[1][1].components_.round(2)
 # Print feature effects
 print('PC 1 effects = ' + str(dict(zip(poke_df.columns, vectors[0]))))
 print('PC 2 effects = ' + str(dict(zip(poke_df.columns, vectors[1]))))
+
+# Feature exploration
+pipe = Pipeline([('scaler', StandardScaler()),
+                 ('reducer', PCA(n_components=2))])
+
+# Fit the pipeline to poke_df and transform the data
+pc = pipe.fit_transform(poke_df)
+
+# Add the 2 components to poke_cat_df
+poke_cat_df['PC 1'] = pc[:, 0]
+poke_cat_df['PC 2'] = pc[:, 1]
+
+# Use the Legendary feature to color the PC 1 vs PC 2 scatterplot
+sns.scatterplot(data=poke_cat_df, 
+                x='PC 1', y='PC 2', hue='Legendary')
+plt.show()
