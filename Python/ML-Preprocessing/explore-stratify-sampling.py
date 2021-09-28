e4e99315ddff12ed34360a37a60468f13874e5e4
James_McNeill <jmcneill06@outlook.com>
2021-09-28 14:38:54 +0100

    Create explore-stratify-sampling.py

---
+++ Python/ML-Preprocessing/explore-stratify-sampling.py
@@ -0,0 +1,14 @@
+# When working with an imbalanced class label it makes sense to retain the label distribution between the train and test set.
+# This is where the parameter stratify within the train_test_split module can be used.
+
+# Create a data with all columns except category_desc
+volunteer_X = volunteer.drop("category_desc", axis=1)
+
+# Create a category_desc labels dataset
+volunteer_y = volunteer[["category_desc"]]
+
+# Use stratified sampling to split up the dataset according to the volunteer_y dataset
+X_train, X_test, y_train, y_test = train_test_split(volunteer_X, volunteer_y, stratify=volunteer_y)
+
+# Print out the category_desc counts on the training y labels
+print(y_train["category_desc"].value_counts())
