d1d4e06339d1d0e013b9880e586d57052a6b329d
James_McNeill <jmcneill06@outlook.com>
2021-09-28 15:01:34 +0100

    Update standardizing-methods.py

--- Python/ML-Preprocessing/standardizing-methods.py
+++ Python/ML-Preprocessing/standardizing-methods.py
@@ -23,3 +23,27 @@ wine_subset = wine[["Ash", "Alcalinity of ash", "Magnesium"]]
 
 # Apply the scaler to the DataFrame subset - using the method fit_transform will fit and transform the data in one step.
 wine_subset_scaled = ss.fit_transform(wine_subset)
+
+
+# ML model performance - comparing non-scaled and scaled model accuracy
+# Split the dataset and labels into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# Fit the k-nearest neighbors model to the training data
+knn.fit(X_train, y_train)
+
+# Score the model on the test data
+print(knn.score(X_test, y_test))
+
+# Create the scaling method.
+ss = StandardScaler()
+
+# Apply the scaling method to the dataset used for modeling.
+X_scaled = ss.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y)
+
+# Fit the k-nearest neighbors model to the training data.
+knn.fit(X_train, y_train)
+
+# Score the model on the test data.
+print(knn.score(X_test, y_test))
