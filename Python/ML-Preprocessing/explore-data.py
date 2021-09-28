e514e8b1fd43acdb437c3a6d7ad03d3ea68a38c5
James_McNeill <jmcneill06@outlook.com>
2021-09-28 14:42:47 +0100

    Update explore-data.py

--- Python/ML-Preprocessing/explore-data.py
+++ Python/ML-Preprocessing/explore-data.py
@@ -3,6 +3,7 @@
 # Drop columns that have at least three missing values
 volunteer.dropna(axis=1, thresh=3)
 
+# Review missing values by Row
 # Check how many values are missing in the category_desc column
 print(volunteer['category_desc'].isnull().sum())
 
@@ -12,6 +13,7 @@ volunteer_subset = volunteer[volunteer['category_desc'].notnull()]
 # Print out the shape of the subset
 print(volunteer_subset.shape)
 
+# Converting a feature type
 # Print the head of the hits column
 print(volunteer["hits"].head())
 
