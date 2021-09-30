91aaba41d8bc4a9b199ea2696a8f7f9ed547839a
James_McNeill <jmcneill06@outlook.com>
2021-09-30 17:17:07 +0100

    Create feat-select-text.py

---
+++ Python/ML-Preprocessing/feat-select-text.py
@@ -0,0 +1,32 @@
+# Exploring Text vectors
+# Creating a function to return the top_n words from text vectors
+def return_weights(vocab, original_vocab, vector, vector_index, top_n):
+    zipped = dict(zip(vector[vector_index].indices, vector[vector_index].data))
+    
+    # Let's transform that zipped dict into a series
+    zipped_series = pd.Series({vocab[i]:zipped[i] for i in vector[vector_index].indices})
+    
+    # Let's sort the series to pull out the top n weighted words
+    zipped_index = zipped_series.sort_values(ascending=False)[:top_n].index
+    return [original_vocab[i] for i in zipped_index]
+
+# Print out the weighted words
+print(return_weights(vocab, tfidf_vec.vocabulary_, text_tfidf, 8, 3))
+
+# Extract the top words from each document in the text vector, return a list of the word indices, 
+# and use that list to filter the text vector down to those top words
+def words_to_filter(vocab, original_vocab, vector, top_n):
+    filter_list = []
+    for i in range(0, vector.shape[0]):
+    
+        # Here we'll call the function from the previous exercise, and extend the list we're creating
+        filtered = return_weights(vocab, original_vocab, vector, i, top_n)
+        filter_list.extend(filtered)
+    # Return the list in a set, so we don't get duplicate word indices
+    return set(filter_list)
+
+# Call the function to get the list of word indices
+filtered_words = words_to_filter(vocab, tfidf_vec.vocabulary_, text_tfidf, 3)
+
+# By converting filtered_words back to a list, we can use it to filter the columns in the text vector
+filtered_text = text_tfidf[:, list(filtered_words)]
