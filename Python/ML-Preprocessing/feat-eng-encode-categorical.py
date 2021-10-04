5e13bb83906d6010c34b59267ec501556ef1ba3e
James_McNeill <jmcneill06@outlook.com>
2021-10-04 14:37:50 +0100

    Update feat-eng-encode-categorical.py

--- Python/ML-Preprocessing/feat-eng-encode-categorical.py
+++ Python/ML-Preprocessing/feat-eng-encode-categorical.py
@@ -12,3 +12,15 @@ category_enc = pd.get_dummies(volunteer["category_desc"])
 
 # Take a look at the encoded columns
 print(category_enc.head())
+
+# Use Pandas to encode us values as 1 and others as 0
+ufo["country_enc"] = ufo["country"].apply(lambda x: 1 if x == "us" else 0)
+
+# Print the number of unique type values
+print(len(ufo.type.unique()))
+
+# Create a one-hot encoded set of the type values
+type_set = pd.get_dummies(ufo["type"])
+
+# Concatenate this set back to the ufo DataFrame
+ufo = pd.concat([ufo, type_set], axis=1)
