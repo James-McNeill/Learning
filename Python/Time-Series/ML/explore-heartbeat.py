385b2bde5ad105c02ca2e9b35be1b1f8332fd277
James_McNeill <jmcneill06@outlook.com>
2021-10-05 10:06:46 +0100

    Update explore-heartbeat.py

--- Python/Time-Series/ML/explore-heartbeat.py
+++ Python/Time-Series/ML/explore-heartbeat.py
@@ -12,3 +12,17 @@ stacked_audio = np.hstack([normal, abnormal]).T
 for iaudio, ax in zip(stacked_audio, axs.T.ravel()):
     ax.plot(time, iaudio)
 show_plot_and_make_titles()
+
+# Invariance of the time series. We are aware that there will be noise within the data. Therefore this initial
+# step aims to remove some of the noise by applying an average
+# Average across the audio files of each DataFrame
+mean_normal = np.mean(normal, axis=1)
+mean_abnormal = np.mean(abnormal, axis=1)
+
+# Plot each average over time
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 3), sharey=True)
+ax1.plot(time, mean_normal)
+ax1.set(title="Normal Data")
+ax2.plot(time, mean_abnormal)
+ax2.set(title="Abnormal Data")
+plt.show()
