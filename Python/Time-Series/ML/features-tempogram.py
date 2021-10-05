e7ee2bebeb1c49a80f4b2a309a86668173e2c35a
James_McNeill <jmcneill06@outlook.com>
2021-10-05 12:39:03 +0100

    Create features-tempogram.py

---
+++ Python/Time-Series/ML/features-tempogram.py
@@ -0,0 +1,26 @@
+# Working with the audio data requires the user to review the tempo and rhythm features. This is were
+# the python library librosa can be used
+
+# Import package
+import librosa as lr
+
+# Calculate the tempo of the sounds
+tempos = []
+for col, i_audio in audio.items():
+    tempos.append(lr.beat.tempo(i_audio.values, sr=sfreq, hop_length=2**6, aggregate=None))
+
+# Convert the list to an array so you can manipulate it more easily
+tempos = np.array(tempos)
+
+# Calculate statistics of each tempo
+tempos_mean = tempos.mean(axis=-1)
+tempos_std = tempos.std(axis=-1)
+tempos_max = tempos.max(axis=-1)
+
+# Create the X and y arrays - the other array details are taken from the initial feature development of the raw data series
+X = np.column_stack([means, stds, maxs, tempos_mean, tempos_std, tempos_max])
+y = labels.reshape([-1, 1])
+
+# Fit the model and score on testing data
+percent_score = cross_val_score(model, X, y, cv=5)
+print(np.mean(percent_score))
