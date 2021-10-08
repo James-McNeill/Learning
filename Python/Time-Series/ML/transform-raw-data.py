c7f06e450897d82c851230e0a5d113972159c92a
James_McNeill <jmcneill06@outlook.com>
2021-10-08 12:21:40 +0100

    Create transform-raw-data.py

---
+++ Python/Time-Series/ML/transform-raw-data.py
@@ -0,0 +1,14 @@
+# Perform percentage change analysis - aims to remove the temporal nature of the time series from the raw data
+def percent_change(series):
+    # Collect all *but* the last value of this window, then the final value
+    previous_values = series[:-1]
+    last_value = series[-1]
+
+    # Calculate the % difference between the last value and the mean of earlier values
+    percent_change = (last_value - np.mean(previous_values)) / np.mean(previous_values)
+    return percent_change
+
+# Apply your custom function and plot
+prices_perc = prices.rolling(20).apply(percent_change)
+prices_perc.loc["2014":"2015"].plot()
+plt.show()
