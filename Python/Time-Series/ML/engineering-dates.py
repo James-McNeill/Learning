490cfe80dd54c7e77e95107b7b8e71173972981b
James_McNeill <jmcneill06@outlook.com>
2021-10-08 16:23:40 +0100

    Create engineering-dates.py

---
+++ Python/Time-Series/ML/engineering-dates.py
@@ -0,0 +1,9 @@
+# Engineering additional information about the date feature
+
+# Extract date features from the data, add them as columns
+prices_perc['day_of_week'] = prices_perc.index.weekday
+prices_perc['week_of_year'] = prices_perc.index.week
+prices_perc['month_of_year'] = prices_perc.index.month
+
+# Print prices_perc
+print(prices_perc)
