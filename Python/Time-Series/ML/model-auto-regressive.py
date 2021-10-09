5345364657206b8812b4c2a215eb229494a1e24f
James_McNeill <jmcneill06@outlook.com>
2021-10-09 17:53:39 +0100

    Create model-auto-regressive.py

---
+++ Python/Time-Series/ML/model-auto-regressive.py
@@ -0,0 +1,28 @@
+# Working with Auto-regressive models to predict time series data points
+
+# Replace missing values with the median for each column
+X = prices_perc_shifted.fillna(np.nanmedian(prices_perc_shifted))
+y = prices_perc.fillna(np.nanmedian(prices_perc))
+
+# Fit the model
+model = Ridge()
+model.fit(X, y)
+
+# Create a method to visualize the regression coefficients that are being produced
+def visualize_coefficients(coefs, names, ax):
+    # Make a bar plot for the coefficients, including their names on the x-axis
+    ax.bar(names, coefs)
+    ax.set(xlabel='Coefficient name', ylabel='Coefficient value')
+    
+    # Set formatting so it looks nice
+    plt.setp(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
+    return ax
+
+# Run the method on the time shifted DataFrame
+# Visualize the output data up to "2011-01"
+fig, axs = plt.subplots(2, 1, figsize=(10, 5))
+y.loc[:'2011-01'].plot(ax=axs[0])
+
+# Run the function to visualize model's coefficients
+visualize_coefficients(model.coef_, prices_perc_shifted.columns, ax=axs[1])
+plt.show()
