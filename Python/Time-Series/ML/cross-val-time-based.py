4874f333f61efa309b01ade690f050f0ad8b0178
James_McNeill <jmcneill06@outlook.com>
2021-10-09 18:15:14 +0100

    Create cross-val-time-based.py

---
+++ Python/Time-Series/ML/cross-val-time-based.py
@@ -0,0 +1,17 @@
+# Using the time series cross-validation iterator to iterate through the time series data. 
+# The module will stack previous data points together to perform the train / test split.
+
+# Import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit
+
+# Create time-series cross-validation object
+cv = TimeSeriesSplit(n_splits=10)
+
+# Iterate through CV splits
+fig, ax = plt.subplots()
+for ii, (tr, tt) in enumerate(cv.split(X, y)):
+    # Plot the training data on each iteration, to see the behavior of the CV
+    ax.plot(tr, ii + y[tr])
+
+ax.set(title='Training data on each CV iteration', ylabel='CV iteration')
+plt.show()
