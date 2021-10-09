33428e7cd20c95b0965fc390d0c1d05b9f7a7be2
James_McNeill <jmcneill06@outlook.com>
2021-10-09 18:50:58 +0100

    Create bootstrap-model-performance.py

---
+++ Python/Time-Series/ML/bootstrap-model-performance.py
@@ -0,0 +1,14 @@
+# Visualizing model score variability across time
+
+# Import module
+from sklearn.model_selection import cross_val_score
+
+# Generate scores for each split to see how the model performs over time - my_pearsonr is custom function
+scores = cross_val_score(model, X, y, cv=cv, scoring=my_pearsonr)
+
+# Convert to a Pandas Series object
+scores_series = pd.Series(scores, index=times_scores, name='score')
+
+# Bootstrap a rolling confidence interval for the mean score
+scores_lo = scores_series.rolling(20).aggregate(partial(bootstrap_interval, percentiles=2.5))
+scores_hi = scores_series.rolling(20).aggregate(partial(bootstrap_interval, percentiles=97.5))
