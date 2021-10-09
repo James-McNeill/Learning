cd1b725856e2767679f3a0cf2d9438ac1df429a0
James_McNeill <jmcneill06@outlook.com>
2021-10-09 18:51:17 +0100

    Update bootstrap-model-performance.py

--- Python/Time-Series/ML/bootstrap-model-performance.py
+++ Python/Time-Series/ML/bootstrap-model-performance.py
@@ -12,3 +12,10 @@ scores_series = pd.Series(scores, index=times_scores, name='score')
 # Bootstrap a rolling confidence interval for the mean score
 scores_lo = scores_series.rolling(20).aggregate(partial(bootstrap_interval, percentiles=2.5))
 scores_hi = scores_series.rolling(20).aggregate(partial(bootstrap_interval, percentiles=97.5))
+
+# Plot the results
+fig, ax = plt.subplots()
+scores_lo.plot(ax=ax, label="Lower confidence interval")
+scores_hi.plot(ax=ax, label="Upper confidence interval")
+ax.legend()
+plt.show()
