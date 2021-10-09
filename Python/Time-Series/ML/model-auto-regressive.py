004c8f2f3893450aac7205a132f2853043a01518
James_McNeill <jmcneill06@outlook.com>
2021-10-09 17:56:56 +0100

    Update model-auto-regressive.py

--- Python/Time-Series/ML/model-auto-regressive.py
+++ Python/Time-Series/ML/model-auto-regressive.py
@@ -26,3 +26,14 @@ y.loc[:'2011-01'].plot(ax=axs[0])
 # Run the function to visualize model's coefficients
 visualize_coefficients(model.coef_, prices_perc_shifted.columns, ax=axs[1])
 plt.show()
+
+# The time series shifted data has been smoothed even more than the original model build. The aim was to use
+# 40 data points instead of 20 to understand the impact on the co-efficient values when more smoothing took
+# place in the adjusted dataset
+# Visualize the output data up to "2011-01"
+fig, axs = plt.subplots(2, 1, figsize=(10, 5))
+y.loc[:'2011-01'].plot(ax=axs[0])
+
+# Run the function to visualize model's coefficients
+visualize_coefficients(model.coef_, prices_perc_shifted.columns, ax=axs[1])
+plt.show()
