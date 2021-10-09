492a8ae07e3f819d361dbbc4a423f681d2e8e891
James_McNeill <jmcneill06@outlook.com>
2021-10-09 19:32:07 +0100

    Create binning-values.py

---
+++ Python/ML-Preprocessing/Feature-Engineering/binning-values.py
@@ -0,0 +1,24 @@
+# Convert numeric features to bins
+
+# Bin the continuous variable ConvertedSalary into 5 bins
+so_survey_df['equal_binned'] = pd.cut(so_survey_df['ConvertedSalary'], 5)
+
+# Print the first 5 rows of the equal_binned column
+print(so_survey_df[['equal_binned', 'ConvertedSalary']].head())
+
+# Creating customised bins
+# Import numpy
+import numpy as np
+
+# Specify the boundaries of the bins
+bins = [-np.inf, 10000, 50000, 100000, 150000, np.inf]
+
+# Bin labels
+labels = ['Very low', 'Low', 'Medium', 'High', 'Very high']
+
+# Bin the continuous variable ConvertedSalary using these boundaries
+so_survey_df['boundary_binned'] = pd.cut(so_survey_df['ConvertedSalary'], 
+                                         bins=bins, labels=labels)
+
+# Print the first 5 rows of the boundary_binned column
+print(so_survey_df[['boundary_binned', 'ConvertedSalary']].head())
