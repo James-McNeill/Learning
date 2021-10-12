8cad2da5d0a8c47933d09f9122ad20b467f67d41
James_McNeill <jmcneill06@outlook.com>
2021-10-12 14:55:31 +0100

    Update text-data-encode.py

--- Python/ML-Preprocessing/Feature-Engineering/text-data-encode.py
+++ Python/ML-Preprocessing/Feature-Engineering/text-data-encode.py
@@ -8,3 +8,15 @@ speech_df['text_clean'] = speech_df['text_clean'].str.lower()
 
 # Print the first 5 rows of the text_clean column
 print(speech_df['text_clean'].head())
+
+# Find the length of each text
+speech_df['char_cnt'] = speech_df['text_clean'].str.len()
+
+# Count the number of words in each text
+speech_df['word_cnt'] = speech_df['text_clean'].str.split().str.len()
+
+# Find the average length of word
+speech_df['avg_word_length'] = speech_df['char_cnt'] / speech_df['word_cnt']
+
+# Print the first 5 rows of these columns
+print(speech_df[['text_clean', 'char_cnt', 'word_cnt', 'avg_word_length']])
