a0526aa987970f1e70808e8f2e15114b7f97c929
James_McNeill <jmcneill06@outlook.com>
2021-10-12 14:28:16 +0100

    Update scaling-transforming.py

--- Python/ML-Preprocessing/Feature-Engineering/scaling-transforming.py
+++ Python/ML-Preprocessing/Feature-Engineering/scaling-transforming.py
@@ -29,3 +29,20 @@ so_numeric_df['Age_SS'] = SS_scaler.transform(so_numeric_df[['Age']])
 
 # Compare the origional and transformed column
 print(so_numeric_df[['Age_SS', 'Age']].head())
+
+# 3. Log transformation
+# Import PowerTransformer
+from sklearn.preprocessing import PowerTransformer
+
+# Instantiate PowerTransformer
+pow_trans = PowerTransformer()
+
+# Train the transform on the data
+pow_trans.fit(so_numeric_df[['ConvertedSalary']])
+
+# Apply the power transform to the data
+so_numeric_df['ConvertedSalary_LG'] = pow_trans.transform(so_numeric_df[['ConvertedSalary']])
+
+# Plot the data before and after the transformation
+so_numeric_df[['ConvertedSalary', 'ConvertedSalary_LG']].hist()
+plt.show()
