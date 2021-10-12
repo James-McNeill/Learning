01dfead13b121e5feab76c370323e3e62906e63a
James_McNeill <jmcneill06@outlook.com>
2021-10-12 16:22:32 +0100

    Update text-data-tf-idf.py

--- Python/ML-Preprocessing/Feature-Engineering/text-data-tf-idf.py
+++ Python/ML-Preprocessing/Feature-Engineering/text-data-tf-idf.py
@@ -1,6 +1,7 @@
 # Term frequency-inverse document frequency (Tf-Idf)
 # Tf-idf has the effect of reducing the value of common words, while increasing the weight of words that do not occur in many documents
 
+# 1. Building a baseline
 # Import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -14,3 +15,25 @@ tv_transformed = tv.fit_transform(speech_df['text_clean'])
 tv_df = pd.DataFrame(tv_transformed.toarray(), 
                      columns=tv.get_feature_names()).add_prefix('TFIDF_')
 print(tv_df.head())
+
+# 2. Inspecting the transformed results
+# Isolate the row to be examined
+sample_row = tv_df.iloc[0]
+
+# Print the top 5 words of the sorted output
+print(sample_row.sort_values(ascending=False).head())
+
+# 3. Transforming unseen (test) data
+# Instantiate TfidfVectorizer
+tv = TfidfVectorizer(max_features=100, stop_words='english')
+
+# Fit the vectroizer and transform the data
+tv_transformed = tv.fit_transform(train_speech_df['text_clean'])
+
+# Transform test data
+test_tv_transformed = tv.transform(test_speech_df['text_clean'])
+
+# Create new features for the test set
+test_tv_df = pd.DataFrame(test_tv_transformed.toarray(), 
+                          columns=tv.get_feature_names()).add_prefix('TFIDF_')
+print(test_tv_df.head())
