d9cabbb893d11cf89924a0cf8638474b496419a5
James_McNeill <jmcneill06@outlook.com>
2021-10-12 15:53:03 +0100

    Update text-data-counting-words.py

--- Python/ML-Preprocessing/Feature-Engineering/text-data-counting-words.py
+++ Python/ML-Preprocessing/Feature-Engineering/text-data-counting-words.py
@@ -39,3 +39,12 @@ cv_array = cv_transformed.toarray()
 
 # Print the array shape
 print(cv_array.shape)
+
+# Text to DataFrame
+# Create a DataFrame with these features
+cv_df = pd.DataFrame(cv_array, 
+                     columns=cv.get_feature_names()).add_prefix('Counts_')
+
+# Add the new columns to the original DataFrame
+speech_df_new = pd.concat([speech_df, cv_df], axis=1, sort=False)
+print(speech_df_new.head())
