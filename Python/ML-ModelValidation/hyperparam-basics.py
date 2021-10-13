240aed250c23ef7e50809908b84bc0f2349b58b2
James_McNeill <jmcneill06@outlook.com>
2021-10-13 13:50:20 +0100

    Update hyperparam-basics.py

--- Python/ML-ModelValidation/hyperparam-basics.py
+++ Python/ML-ModelValidation/hyperparam-basics.py
@@ -12,3 +12,16 @@ min_samples_split = [2, 5, 10]
 
 # Max features 
 max_features = [4, 6, 8, 10]
+
+# Randomly select the hyperparameters to use
+from sklearn.ensemble import RandomForestRegressor
+
+# Fill in rfr using your variables
+rfr = RandomForestRegressor(
+    n_estimators=100,
+    max_depth=random.choice(max_depth),
+    min_samples_split=random.choice(min_samples_split),
+    max_features=random.choice(max_features))
+
+# Print out the parameters
+print(rfr.get_params())
