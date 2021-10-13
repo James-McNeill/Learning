d20444f6f915dbbdb35f0f8631e51078d3f64b13
James_McNeill <jmcneill06@outlook.com>
2021-10-13 10:04:26 +0100

    Update basics-reg-metrics.py

--- Python/ML-ModelValidation/basics-reg-metrics.py
+++ Python/ML-ModelValidation/basics-reg-metrics.py
@@ -1,6 +1,6 @@
 # Regression model accuracy metrics
 
-# Mean Absolute Error
+# 1. Mean Absolute Error
 from sklearn.metrics import mean_absolute_error
 
 # Manually calculate the MAE
@@ -11,3 +11,30 @@ print('With a manual calculation, the error is {}'.format(mae_one))
 # Use scikit-learn to calculate the MAE
 mae_two = mean_absolute_error(y_test, predictions)
 print('Using scikit-lean, the error is {}'.format(mae_two))
+
+# 2. Mean Squared Error - large errors (poorer predictions) will have more of an impact. 
+from sklearn.metrics import mean_squared_error
+
+n = len(predictions)
+# Finish the manual calculation of the MSE
+mse_one = sum((y_test - predictions)**2) / n
+print('With a manual calculation, the error is {}'.format(mse_one))
+
+# Use the scikit-learn function to calculate MSE
+mse_two = mean_squared_error(y_test, predictions)
+print('Using scikit-lean, the error is {}'.format(mse_two))
+
+# 3. Example to compare the mae by NBA conference
+# Find the East conference teams
+east_teams = labels == "E"
+
+# Create arrays for the true and predicted values
+true_east = y_test[east_teams]
+preds_east = predictions[east_teams]
+
+# Print the accuracy metrics
+print('The MAE for East teams is {}'.format(
+    mae(true_east, preds_east)))
+
+# Print the West accuracy
+print('The MAE for West conference is {}'.format(west_error))
