18f2980b7eebb88e3aeeeb2cc9dd92d6006321b3
James_McNeill <jmcneill06@outlook.com>
2021-10-13 10:32:20 +0100

    Update basics-classification-metrics.py

--- Python/ML-ModelValidation/basics-classification-metrics.py
+++ Python/ML-ModelValidation/basics-classification-metrics.py
@@ -19,3 +19,27 @@ print("The precision is {0: 0.2f}".format(precision))
 # Calculate and print the recall
 recall = (TP) / (FN + TP)
 print("The recall is {0: 0.2f}".format(recall))
+
+# 1. Method from sklearn module
+from sklearn.metrics import confusion_matrix
+
+# Create predictions
+test_predictions = rfc.predict(X_test)
+
+# Create and print the confusion matrix
+cm = confusion_matrix(y_test, test_predictions)
+print(cm)
+
+# Print the true positives (actual 1s that were predicted 1s)
+print("The number of true positives is: {}".format(cm[1, 1]))
+
+# 2. Precision score
+from sklearn.metrics import precision_score
+
+test_predictions = rfc.predict(X_test)
+
+# Create precision or recall score based on the metric you imported
+score = precision_score(y_test, test_predictions)
+
+# Print the final result
+print("The precision value is {0:.2f}".format(score))
