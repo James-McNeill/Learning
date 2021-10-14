dc894de5269b7f5fe2e30fdc1cc6a822e26a595b
James_McNeill <jmcneill06@outlook.com>
2021-10-14 10:41:57 +0100

    Update nltk-advanced.py

--- Python/NLP/Intro/nltk-advanced.py
+++ Python/NLP/Intro/nltk-advanced.py
@@ -23,3 +23,16 @@ print(mentions_hashtags)
 tknzr = TweetTokenizer()
 all_tokens = [tknzr.tokenize(t) for t in tweets]
 print(all_tokens)
+
+# 4. Finding capital words in German Text
+# Tokenize and print all words in german_text
+all_words = word_tokenize(german_text)
+print(all_words)
+
+# Tokenize and print only capital words
+capital_words = r"[A-ZÜ]\w+"
+print(regexp_tokenize(german_text, capital_words))
+
+# Tokenize and print only emoji
+emoji = "['\U0001F300-\U0001F5FF'|'\U0001F600-\U0001F64F'|'\U0001F680-\U0001F6FF'|'\u2600-\u26FF\u2700-\u27BF']"
+print(regexp_tokenize(german_text, emoji))
