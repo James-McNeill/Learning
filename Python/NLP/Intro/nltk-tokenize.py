a82fe466ca97cceb119377b5f2692e49a7b54070
James_McNeill <jmcneill06@outlook.com>
2021-10-14 10:09:58 +0100

    Update nltk-tokenize.py

--- Python/NLP/Intro/nltk-tokenize.py
+++ Python/NLP/Intro/nltk-tokenize.py
@@ -16,3 +16,10 @@ unique_tokens = set(word_tokenize(scene_one))
 
 # Print the unique tokens result
 print(unique_tokens)
+
+# Search
+# Search for the first occurrence of "coconuts" in scene_one: match
+match = re.search("coconuts", scene_one)
+
+# Print the start and end indexes of match
+print(match.start(), match.end())
