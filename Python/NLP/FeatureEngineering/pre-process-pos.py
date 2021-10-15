09addd1a17b857aa1c1c6b1e35102dc40b7063b7
James_McNeill <jmcneill06@outlook.com>
2021-10-15 16:10:20 +0100

    Update pre-process-pos.py

--- Python/NLP/FeatureEngineering/pre-process-pos.py
+++ Python/NLP/FeatureEngineering/pre-process-pos.py
@@ -34,3 +34,24 @@ def nouns(text, model=nlp):
     return pos.count('NOUN')
 
 print(nouns("Abdul, Bill and Cathy went to the market to buy apples.", nlp))
+
+# 1. Using the functions to review the difference between Real and Fake news for Propn and Noun
+# a. Reviewing proper noun's
+headlines['num_propn'] = headlines['title'].apply(proper_nouns)
+
+# Compute mean of proper nouns
+real_propn = headlines[headlines['label'] == 'REAL']['num_propn'].mean()
+fake_propn = headlines[headlines['label'] == 'FAKE']['num_propn'].mean()
+
+# Print results - fake group has higher number
+print("Mean no. of proper nouns in real and fake headlines are %.2f and %.2f respectively"%(real_propn, fake_propn))
+
+# b. Reviewing noun's
+headlines['num_noun'] = headlines['title'].apply(nouns)
+
+# Compute mean of other nouns
+real_noun = headlines[headlines['label'] == 'REAL']['num_noun'].mean()
+fake_noun = headlines[headlines['label'] == 'FAKE']['num_noun'].mean()
+
+# Print results - real group has higher number
+print("Mean no. of other nouns in real and fake headlines are %.2f and %.2f respectively"%(real_noun, fake_noun))
