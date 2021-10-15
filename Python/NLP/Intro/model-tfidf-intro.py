e6f1a30d6e9e4f6d1b5ba12e7725ce8fd499ec29
James_McNeill <jmcneill06@outlook.com>
2021-10-15 14:25:22 +0100

    Create model-tfidf-intro.py

---
+++ Python/NLP/Intro/model-tfidf-intro.py
@@ -0,0 +1,19 @@
+# Working with TfidfVectorizer
+
+# Import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Initialize a TfidfVectorizer object: tfidf_vectorizer
+tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
+
+# Transform the training data: tfidf_train 
+tfidf_train = tfidf_vectorizer.fit_transform(X_train.values)
+
+# Transform the test data: tfidf_test 
+tfidf_test = tfidf_vectorizer.transform(X_test.values)
+
+# Print the first 10 features
+print(tfidf_vectorizer.get_feature_names()[:10])
+
+# Print the first 5 vectors of the tfidf training data
+print(tfidf_train.A[:5])
