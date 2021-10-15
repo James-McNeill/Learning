d722c8788ace2c2340b52618364a9cf6159af56a
James_McNeill <jmcneill06@outlook.com>
2021-10-15 15:39:53 +0100

    Update pre-process-clean-blog.py

--- Python/NLP/FeatureEngineering/pre-process-clean-blog.py
+++ Python/NLP/FeatureEngineering/pre-process-clean-blog.py
@@ -15,3 +15,20 @@ a_lemmas = [lemma for lemma in lemmas
 
 # Print string after text cleaning
 print(' '.join(a_lemmas))
+
+# 1. Working with the TED talk DataFrame of speeches
+# Function to preprocess text
+def preprocess(text):
+  	# Create Doc object
+    doc = nlp(text, disable=['ner', 'parser'])
+    # Generate lemmas
+    lemmas = [token.lemma_ for token in doc]
+    # Remove stopwords and non-alphabetic characters
+    a_lemmas = [lemma for lemma in lemmas 
+            if lemma.isalpha() and lemma not in stopwords]
+    
+    return ' '.join(a_lemmas)
+  
+# Apply preprocess to ted['transcript']
+ted['transcript'] = ted['transcript'].apply(preprocess)
+print(ted['transcript'])
