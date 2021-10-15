cadf1ba95909d26ea8c22ef28b1cd529fe5e6bb9
James_McNeill <jmcneill06@outlook.com>
2021-10-15 12:25:25 +0100

    Create ner-intro.py

---
+++ Python/NLP/Intro/ner-intro.py
@@ -0,0 +1,23 @@
+# Named-entity recognition
+
+# Import modules
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+# Tokenize the article into sentences: sentences
+sentences = sent_tokenize(article)
+
+# Tokenize each sentence into words: token_sentences
+token_sentences = [word_tokenize(sent) for sent in sentences]
+
+# Tag each tokenized sentence into parts of speech: pos_sentences
+pos_sentences = [nltk.pos_tag(sent) for sent in token_sentences] 
+
+# Create the named entity chunks: chunked_sentences
+chunked_sentences = nltk.ne_chunk_sents(pos_sentences, binary=True)
+
+# Test for stems of the tree with 'NE' tags
+for sent in chunked_sentences:
+    for chunk in sent:
+        if hasattr(chunk, "label") and chunk.label() == "NE":
+            print(chunk)
