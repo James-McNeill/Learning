526e02bc27cdc06c91c30db9b64ca60a93ee4d4b
James_McNeill <jmcneill06@outlook.com>
2021-10-15 15:30:40 +0100

    Create pre-process-token-lemma.py

---
+++ Python/NLP/FeatureEngineering/pre-process-token-lemma.py
@@ -0,0 +1,15 @@
+# Tokenization and Lemmatizing text
+# Text is taken from the gettysburg speech
+
+# Import module
+import spacy
+
+# Load the en_core_web_sm model
+nlp = spacy.load('en_core_web_sm')
+
+# Create a Doc object
+doc = nlp(gettysburg)
+
+# Generate the tokens
+tokens = [token.text for token in doc]
+print(tokens)
