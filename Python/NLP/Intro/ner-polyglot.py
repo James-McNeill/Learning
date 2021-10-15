6a6151fbba98286f542fc0ca108bcf26829104e9
James_McNeill <jmcneill06@outlook.com>
2021-10-15 14:05:43 +0100

    Update ner-polyglot.py

--- Python/NLP/Intro/ner-polyglot.py
+++ Python/NLP/Intro/ner-polyglot.py
@@ -16,3 +16,27 @@ for ent in txt.entities:
     
 # Print the type of ent
 print(type(ent))
+
+# Create the list of tuples: entities
+entities = [(ent.tag, ' '.join(ent)) for ent in txt.entities]
+
+# Print entities
+print(entities)
+
+# Check if two entity words are in the text and count the proportion of times
+# Initialize the count variable: count
+count = 0
+
+# Iterate over all the entities
+for ent in txt.entities:
+    # Check whether the entity contains 'Márquez' or 'Gabo'
+    if "Márquez" in ent or "Gabo" in ent:
+        # Increment count
+        count += 1
+
+# Print count
+print(count)
+
+# Calculate the percentage of entities that refer to "Gabo": percentage
+percentage = count / len(txt.entities)
+print(percentage)
