12eeb6253a4c5abe412d46a03a578437bd7ed078
James_McNeill <jmcneill06@outlook.com>
2021-10-15 14:41:56 +0100

    Update and rename model-text-countv.py to model-text-countv-tfidfv.py

--- Python/NLP/Intro/model-text-countv-tfidfv.py
+++ Python/NLP/Intro/model-text-countv-tfidfv.py
@@ -20,3 +20,21 @@ print(score)
 # Calculate the confusion matrix: cm
 cm = metrics.confusion_matrix(y_test, pred, labels=["FAKE", "REAL"])
 print(cm)
+
+# TfidfVectorizer model predictions
+# Create a Multinomial Naive Bayes classifier: nb_classifier
+nb_classifier = MultinomialNB()
+
+# Fit the classifier to the training data
+nb_classifier.fit(tfidf_train, y_train)
+
+# Create the predicted tags: pred
+pred = nb_classifier.predict(tfidf_test)
+
+# Calculate the accuracy score: score
+score = metrics.accuracy_score(y_test, pred)
+print(score)
+
+# Calculate the confusion matrix: cm
+cm = metrics.confusion_matrix(y_test, pred, labels=["FAKE", "REAL"])
+print(cm)
