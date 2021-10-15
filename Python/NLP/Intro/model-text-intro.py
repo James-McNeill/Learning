80c0276ef8ec72a99da2d39921a633fb8cb351ac
James_McNeill <jmcneill06@outlook.com>
2021-10-15 14:31:04 +0100

    Update model-text-intro.py

--- Python/NLP/Intro/model-text-intro.py
+++ Python/NLP/Intro/model-text-intro.py
@@ -45,3 +45,22 @@ print(tfidf_vectorizer.get_feature_names()[:10])
 # Print the first 5 vectors of the tfidf training data
 print(tfidf_train.A[:5])
 
+# Inspecting the vectors
+# Create the CountVectorizer DataFrame: count_df
+count_df = pd.DataFrame(count_train.A, columns=count_vectorizer.get_feature_names())
+
+# Create the TfidfVectorizer DataFrame: tfidf_df
+tfidf_df = pd.DataFrame(tfidf_train.A, columns=tfidf_vectorizer.get_feature_names())
+
+# Print the head of count_df
+print(count_df.head())
+
+# Print the head of tfidf_df
+print(tfidf_df.head())
+
+# Calculate the difference in columns: difference
+difference = set(tfidf_df.columns) - set(count_df.columns)
+print(difference)
+
+# Check whether the DataFrames are equal
+print(count_df.equals(tfidf_df))
