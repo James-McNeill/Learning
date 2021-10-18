47cf20bf3ee52e53ece73c24335dbf9efa9341af
James_McNeill <jmcneill06@outlook.com>
2021-10-18 15:44:25 +0100

    Create dataframes-basics.r

---
+++ R/DataAnalyst/Basics/dataframes-basics.r
@@ -0,0 +1,8 @@
+# Dataframes follow a similar construct to tabular data seen in other programming languages.
+# Initial methods are shown below that provide details on the dataframes being reviewed
+
+# Head of the dataframe
+head(mtcars)
+
+# Investigate the structure of mtcars - provides high level insights of dataframe structure
+str(mtcars)
