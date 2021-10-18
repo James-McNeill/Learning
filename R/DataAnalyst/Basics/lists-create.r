7cb781cc448d017eaa410ed06d2b44ac378c8772
James_McNeill <jmcneill06@outlook.com>
2021-10-18 16:47:41 +0100

    Create lists-create.r

---
+++ R/DataAnalyst/Basics/lists-create.r
@@ -0,0 +1,14 @@
+# Create a list
+# Allows the user the gather a range of objects within a list
+
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+
+# Construct list with these different elements:
+my_list <- list(my_vector, my_matrix, my_df)
