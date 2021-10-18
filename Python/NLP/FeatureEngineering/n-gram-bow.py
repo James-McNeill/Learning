285b6bc76f2ae41d51ecf2fd6cad053222cf5e6c
James_McNeill <jmcneill06@outlook.com>
2021-10-18 09:40:59 +0100

    Create n-gram-bow.py

---
+++ Python/NLP/FeatureEngineering/n-gram-bow.py
@@ -0,0 +1,13 @@
+# Building a bag of words vectorizer
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Generate matrix of word vectors
+bow_matrix = vectorizer.fit_transform(corpus)
+
+# Print the shape of bow_matrix
+print(bow_matrix.shape)
