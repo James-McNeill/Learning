580e813b56405fd3362902a8f3589b0beab34305
James_McNeill <jmcneill06@outlook.com>
2021-10-18 15:35:59 +0100

    Create factors-ordered.r

---
+++ R/DataAnalyst/Basics/factors-ordered.r
@@ -0,0 +1,20 @@
+# Factors ordered
+
+# Create speed_vector
+speed_vector <- c("medium", "slow", "slow", "medium", "fast")
+
+# Convert speed_vector to ordered factor vector
+factor_speed_vector <- factor(speed_vector, ordered = TRUE, levels = c("slow", "medium", "fast"))
+
+# Print factor_speed_vector
+factor_speed_vector
+summary(factor_speed_vector)
+
+# Factor value for second data analyst
+da2 <- factor_speed_vector[2]
+
+# Factor value for fifth data analyst
+da5 <- factor_speed_vector[5]
+
+# Is data analyst 2 faster than data analyst 5?
+da2 > da5
