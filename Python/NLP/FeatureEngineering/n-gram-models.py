40526ca88164e338ab4b02b773ca38a6e315b2ef
James_McNeill <jmcneill06@outlook.com>
2021-10-18 09:59:39 +0100

    Update n-gram-models.py

--- Python/NLP/FeatureEngineering/n-gram-models.py
+++ Python/NLP/FeatureEngineering/n-gram-models.py
@@ -29,3 +29,21 @@ print("The accuracy of the classifier on the test set is %.3f" % accuracy)
 review = "The movie was not good. The plot had several holes and the acting lacked panache."
 prediction = clf_ng.predict(ng_vectorizer.transform([review]))[0]
 print("The sentiment predicted by the classifier is %i" % (prediction))
+
+
+# Comparison of uni-gram and n-gram (3) models. Overall model performance not to different even with much higer dimensionality
+start_time = time.time()
+# Splitting the data into training and test sets
+train_X, test_X, train_y, test_y = train_test_split(df['review'], df['sentiment'], test_size=0.5, random_state=42, stratify=df['sentiment'])
+
+# Generating ngrams
+vectorizer = CountVectorizer(ngram_range=(1, 3))
+train_X = vectorizer.fit_transform(train_X)
+test_X = vectorizer.transform(test_X)
+
+# Fit classifier
+clf = MultinomialNB()
+clf.fit(train_X, train_y)
+
+# Print accuracy, time and number of dimensions
+print("The program took %.3f seconds to complete. The accuracy on the test set is %.2f. The ngram representation had %i features." % (time.time() - start_time, clf.score(test_X, test_y), train_X.shape[1]))
