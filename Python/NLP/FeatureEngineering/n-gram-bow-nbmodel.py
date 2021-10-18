afc51af6468ae796d76fbaef785e58cced02c38d
James_McNeill <jmcneill06@outlook.com>
2021-10-18 09:50:28 +0100

    Update n-gram-bow-nbmodel.py

--- Python/NLP/FeatureEngineering/n-gram-bow-nbmodel.py
+++ Python/NLP/FeatureEngineering/n-gram-bow-nbmodel.py
@@ -15,3 +15,18 @@ X_test_bow = vectorizer.transform(X_test)
 # Print shape of X_train_bow and X_test_bow
 print(X_train_bow.shape)
 print(X_test_bow.shape)
+
+# Create a MultinomialNB object - classifier is taken from sklearn
+clf = MultinomialNB()
+
+# Fit the classifier
+clf.fit(X_train_bow, y_train)
+
+# Measure the accuracy
+accuracy = clf.score(X_test_bow, y_test)
+print("The accuracy of the classifier on the test set is %.3f" % accuracy)
+
+# Predict the sentiment of a negative review
+review = "The movie was terrible. The music was underwhelming and the acting mediocre."
+prediction = clf.predict(vectorizer.transform([review]))[0]
+print("The sentiment predicted by the classifier is %i" % (prediction))
