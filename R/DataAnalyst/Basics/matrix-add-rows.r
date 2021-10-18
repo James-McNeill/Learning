9d4df013c5e77b193f14355dabc3c27dc0b6ffce
James_McNeill <jmcneill06@outlook.com>
2021-10-18 14:39:08 +0100

    Create matrix-add-rows.r

---
+++ R/DataAnalyst/Basics/matrix-add-rows.r
@@ -0,0 +1,10 @@
+# Similar to the column binding
+
+# star_wars_matrix and star_wars_matrix2 are available in your workspace
+star_wars_matrix  
+star_wars_matrix2 
+
+# Combine both Star Wars trilogies in one matrix
+all_wars_matrix <- rbind(star_wars_matrix, star_wars_matrix2)
+
+# rbind(): can add matrices and vectors, similar to cbind()
