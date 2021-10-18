1f44a2913e226a689840e02deaa88e312a9c3cbb
James_McNeill <jmcneill06@outlook.com>
2021-10-18 14:06:13 +0100

    Update vectors-calc-totals.r

--- R/DataAnalyst/Basics/vectors-calc-totals.r
+++ R/DataAnalyst/Basics/vectors-calc-totals.r
@@ -18,7 +18,7 @@ names(roulette_vector) <- days_vector
 # Assign to total_daily how much you won/lost on each day
 total_daily <- poker_vector + roulette_vector
 
-# Review the total weekly movements
+# 1. Review the total weekly movements
 # Poker and roulette winnings from Monday to Friday:
 poker_vector <- c(140, -50, 20, -120, 240)
 roulette_vector <- c(-24, -50, 100, -350, 10)
@@ -37,3 +37,18 @@ total_week <- sum(total_poker, total_roulette)
 
 # Print out total_week
 total_week
+
+# 2. Check to see are the returns better in poker compared to roulette
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Calculate total gains for poker and roulette
+total_poker <- sum(poker_vector)
+total_roulette <- sum(roulette_vector)
+
+# Check if you realized higher total gains in poker than in roulette - prints message of TRUE
+total_poker > total_roulette
