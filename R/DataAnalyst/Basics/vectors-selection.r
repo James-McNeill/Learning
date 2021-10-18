9b39a3b9fa3fbc67f7ddb4b817916e4fbaffec1d
James_McNeill <jmcneill06@outlook.com>
2021-10-18 14:08:17 +0100

    Create vectors-selection.r

---
+++ R/DataAnalyst/Basics/vectors-selection.r
@@ -0,0 +1,12 @@
+# Vector selection operations
+# Note that R begins indexing from 1, instead of 0.
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Define a new variable based on a selection
+poker_wednesday <- poker_vector[3]
