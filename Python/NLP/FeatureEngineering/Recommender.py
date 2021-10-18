89615d2e790af0767427ba81be751b556af5671d
James_McNeill <jmcneill06@outlook.com>
2021-10-18 10:31:41 +0100

    Update Recommender.py

--- Python/NLP/FeatureEngineering/Recommender.py
+++ Python/NLP/FeatureEngineering/Recommender.py
@@ -1,4 +1,4 @@
-# Working to create a recommender algoritm for movies
+# Working to create a recommender algorithm for movies
 
 # Initialize the TfidfVectorizer 
 tfidf = TfidfVectorizer(stop_words='english')
@@ -11,3 +11,16 @@ cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
  
 # Generate recommendations 
 print(get_recommendations('The Dark Knight Rises', cosine_sim, indices))
+
+# TED talk recommender
+# Initialize the TfidfVectorizer 
+tfidf = TfidfVectorizer(stop_words='english')
+
+# Construct the TF-IDF matrix
+tfidf_matrix = tfidf.fit_transform(transcripts)
+
+# Generate the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+ 
+# Generate recommendations 
+print(get_recommendations('5 ways to kill your dreams', cosine_sim, indices))
