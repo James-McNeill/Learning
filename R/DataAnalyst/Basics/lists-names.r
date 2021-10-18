52d106cf37d2e6f2fef571be9a5010e1f1044d92
James_McNeill <jmcneill06@outlook.com>
2021-10-18 16:49:33 +0100

    Create lists-names.r

---
+++ R/DataAnalyst/Basics/lists-names.r
@@ -0,0 +1,16 @@
+# Assigning names to the list for each of the objects
+
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+
+# Adapt list() call to give the components names
+my_list <- list(vec = my_vector, mat = my_matrix, df = my_df)
+
+# Print out my_list
+my_list
