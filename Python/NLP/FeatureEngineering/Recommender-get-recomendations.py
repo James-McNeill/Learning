4beee549943432dab89c346dc1cb4e54f2897e46
James_McNeill <jmcneill06@outlook.com>
2021-10-18 10:29:04 +0100

    Create Recommender-get-recomendations.py

---
+++ Python/NLP/FeatureEngineering/Recommender-get-recomendations.py
@@ -0,0 +1,17 @@
+# Method for get_recommendations
+
+# Generate mapping between titles and index
+indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
+
+def get_recommendations(title, cosine_sim, indices):
+    # Get index of movie that matches title
+    idx = indices[title]
+    # Sort the movies based on the similarity scores
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    # Get the scores for 10 most similar movies
+    sim_scores = sim_scores[1:11]
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+    # Return the top 10 most similar movies
+    return metadata['title'].iloc[movie_indices]
