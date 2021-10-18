c3dce054188b4d37e6303b3403c5d4db532211e2
James_McNeill <jmcneill06@outlook.com>
2021-10-18 14:20:30 +0100

    Create vectors-selection-comparison.r

---
+++ R/DataAnalyst/Basics/vectors-selection-comparison.r
@@ -0,0 +1,23 @@
+# Selection by comparison
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Which days did you make money on poker?
+selection_vector <- c(poker_vector) > 0
+  
+# Print out selection_vector - returns TRUE and FALSE values for each element reviewed
+selection_vector
+
+# Select from poker_vector these days - only select the elements that match
+poker_winning_days <- poker_vector[selection_vector]
+
+# Which days did you make money on roulette?
+selection_vector <- roulette_vector > 0
+
+# Select from roulette_vector these days
+roulette_winning_days <- roulette_vector[selection_vector]
