cafd81521f38cf7656b23fa8a5bed8c8db6bfd7a
James_McNeill <jmcneill06@outlook.com>
2021-10-18 10:24:56 +0100

    Update and rename recommender.py to Recommender.py

--- Python/NLP/FeatureEngineering/Recommender.py
+++ Python/NLP/FeatureEngineering/Recommender.py
@@ -6,7 +6,7 @@ tfidf = TfidfVectorizer(stop_words='english')
 # Construct the TF-IDF matrix
 tfidf_matrix = tfidf.fit_transform(movie_plots)
 
-# Generate the cosine similarity matrix
+# Generate the cosine similarity matrix - linear_kernel is similar to cosine_similarity but can be much quicker depending on tabular data size
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
  
 # Generate recommendations 
