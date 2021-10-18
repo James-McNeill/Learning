7bf3c40e613baff680cae2b3862c638e4c5b6dd4
James_McNeill <jmcneill06@outlook.com>
2021-10-18 16:06:34 +0100

    Delete methods.r

--- R/DataAnalyst/Basics/methods.r
+++
@@ -1,7 +0,0 @@
-# order(): is a function that gives you the ranked position of each element when it is applied on a variable
-
-# Create a vector
-a <- c(1000, 99, 400000)
-order(a)
-
-# console will output the rank position of each element in the vector
