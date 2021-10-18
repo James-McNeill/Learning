1a4c8b50a2ae8712adeec9a4d98c8f028579a0c1
James_McNeill <jmcneill06@outlook.com>
2021-10-18 15:53:56 +0100

    Update dataframes-selection.r

--- R/DataAnalyst/Basics/dataframes-selection.r
+++ R/DataAnalyst/Basics/dataframes-selection.r
@@ -8,3 +8,9 @@ planets_df[4,]
 
 # Select first 5 values of diameter column - making use of the column name
 planets_df[1:5, "diameter"]
+
+# Select the rings variable from planets_df - the $variable_name allows us to subset the dataframe for all values in this column
+rings_vector <- planets_df$rings
+  
+# Print out rings_vector
+rings_vector
