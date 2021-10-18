a01c135660c55b55bd855a7deb63fa31d4eab312
James_McNeill <jmcneill06@outlook.com>
2021-10-18 09:56:35 +0100

    Update n-gram-models.py

--- Python/NLP/FeatureEngineering/n-gram-models.py
+++ Python/NLP/FeatureEngineering/n-gram-models.py
@@ -14,3 +14,18 @@ ng3 = vectorizer_ng3.fit_transform(corpus)
 
 # Print the number of features for each model
 print("ng1, ng2 and ng3 have %i, %i and %i features respectively" % (ng1.shape[1], ng2.shape[1], ng3.shape[1]))
+
+# Define an instance of MultinomialNB 
+clf_ng = MultinomialNB()
+
+# Fit the classifier 
+clf_ng.fit(X_train_ng, y_train)
+
+# Measure the accuracy 
+accuracy = clf_ng.score(X_test_ng, y_test)
+print("The accuracy of the classifier on the test set is %.3f" % accuracy)
+
+# Predict the sentiment of a negative review
+review = "The movie was not good. The plot had several holes and the acting lacked panache."
+prediction = clf_ng.predict(ng_vectorizer.transform([review]))[0]
+print("The sentiment predicted by the classifier is %i" % (prediction))
