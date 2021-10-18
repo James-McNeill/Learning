5ef1a8fd1e077845a40c86b8ba98d6708b540f03
James_McNeill <jmcneill06@outlook.com>
2021-10-18 15:23:49 +0100

    Create factors-nominal-ordinal.r

---
+++ R/DataAnalyst/Basics/factors-nominal-ordinal.r
@@ -0,0 +1,14 @@
+# Nominal and Ordinal categorical variables
+
+# Animals - by default the order does not matter
+animals_vector <- c("Elephant", "Giraffe", "Donkey", "Horse")
+factor_animals_vector <- factor(animals_vector)
+factor_animals_vector
+
+# Temperature
+temperature_vector <- c("High", "Low", "High","Low", "Medium")
+factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c("Low", "Medium", "High"))
+factor_temperature_vector
+
+# order: outlines that the order matters
+# levels: order of importance
