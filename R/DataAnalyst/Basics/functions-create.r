6db99028c63ea1b500de840d6b20465a8278df1b
James_McNeill <jmcneill06@outlook.com>
2021-10-19 16:03:44 +0100

    Update functions-create.r

--- R/DataAnalyst/Basics/functions-create.r
+++ R/DataAnalyst/Basics/functions-create.r
@@ -36,3 +36,13 @@ hello <- function() {
 
 # Call the function hello()
 hello()
+
+# 3. Adding in default keyword parameters
+# Finish the pow_two() function
+pow_two <- function(x, print_info = TRUE) {
+  y <- x ^ 2
+  if (print_info == TRUE) {
+    print(paste(x, "to the power two equals", y))
+  }
+  return(y)
+}
