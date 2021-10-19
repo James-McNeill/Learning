9d233f9504ea0688b297a0e84e716bfe88792ceb
James_McNeill <jmcneill06@outlook.com>
2021-10-19 12:27:07 +0100

    Update loops-while.r

--- R/DataAnalyst/Basics/loops-while.r
+++ R/DataAnalyst/Basics/loops-while.r
@@ -48,3 +48,16 @@ while (speed > 30) {
     speed <- speed - 6
   }
 }
+
+# 4. Working with reciprocals
+# Initialize i as 1 
+i <- 1
+
+# Code the while loop
+while (i <= 10) {
+  print(3 * i)
+  if (3 * i %% 8 == 0) {
+    break
+  }
+  i <- i + 1
+}
