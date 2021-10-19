4c336e7794e84a040f69e38f2edbfd5e415836c9
James_McNeill <jmcneill06@outlook.com>
2021-10-19 11:02:30 +0100

    Update conditional.r

--- R/DataAnalyst/Basics/conditional.r
+++ R/DataAnalyst/Basics/conditional.r
@@ -30,3 +30,22 @@ if (num_views > 15) {
 } else {
   print("Try to be more visible!")
 }
+
+# 3. Add else if statement
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else if (medium == "Facebook") {
+  print("Showing Facebook information")
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+} else if (num_views <= 15 & num_views > 10) {
+  print("Your number of views is average")
+} else {
+  print("Try to be more visible!")
+}
