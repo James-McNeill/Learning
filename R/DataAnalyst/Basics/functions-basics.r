3af7e79ba48352801ed2a183a42020a98c09b128
James_McNeill <jmcneill06@outlook.com>
2021-10-19 14:25:10 +0100

    Create functions-basics.r

---
+++ R/DataAnalyst/Basics/functions-basics.r
@@ -0,0 +1,10 @@
+# Basics of working with functions
+
+# 1. Function documentation
+# Consult the documentation on the mean() function
+help(mean)
+# ?mean
+
+# Inspect the arguments of the mean() function
+args(mean)
+
