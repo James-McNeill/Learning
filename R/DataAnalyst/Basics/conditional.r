01dd6118588da824a6cdacabf0087b934675172c
James_McNeill <jmcneill06@outlook.com>
2021-10-19 10:58:41 +0100

    Create conditional.r

---
+++ R/DataAnalyst/Basics/conditional.r
@@ -0,0 +1,16 @@
+# Conditional statements
+# Reviewing the conditional operators (if statements)
+
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 20
+
+# Examine the if statement for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+}
+
+# Write the if statement for num_views
+if (num_views > 15) {
+  print("You are popular!")
+}
