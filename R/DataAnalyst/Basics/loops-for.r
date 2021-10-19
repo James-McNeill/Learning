03f913144268d8fd043439441ce03fdfcddc3924
James_McNeill <jmcneill06@outlook.com>
2021-10-19 12:43:50 +0100

    Update loops-for.r

--- R/DataAnalyst/Basics/loops-for.r
+++ R/DataAnalyst/Basics/loops-for.r
@@ -30,3 +30,13 @@ for (n in nyc) {
 for (n in 1:length(nyc)) {
     print(nyc[[n]])
 }
+
+# 3. Looping over matrix
+# The tic-tac-toe matrix ttt has already been defined for you
+
+# define the double for loop
+for (i in 1:nrow(ttt)) {
+  for (j in 1:ncol(ttt)) {
+    print(paste("On row ", i, " and column ", j, " the board contains ", ttt[i, j]))
+  }
+}
