5775a2366bb80aaab2d035333c3afc30800823ef
James_McNeill <jmcneill06@outlook.com>
2021-10-19 16:27:44 +0100

    Update functions-create.r

--- R/DataAnalyst/Basics/functions-create.r
+++ R/DataAnalyst/Basics/functions-create.r
@@ -46,3 +46,21 @@ pow_two <- function(x, print_info = TRUE) {
   }
   return(y)
 }
+
+# 4. Define control flow constructs
+# The linkedin and facebook vectors have already been created for you
+
+# Define the interpret function
+interpret <- function(num_views) {
+  if (num_views > 15) {
+    print("You're popular!")
+    return (num_views)
+  } else {
+    print("Try to be more visible!")
+    return (0)
+  }
+}
+
+# Call the interpret function twice
+interpret(linkedin[1])
+interpret(facebook[2])
