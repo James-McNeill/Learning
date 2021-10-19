7920f07b99011947127b8d9c983e9473dca78836
James_McNeill <jmcneill06@outlook.com>
2021-10-19 11:24:59 +0100

    Create loops-while.r

---
+++ R/DataAnalyst/Basics/loops-while.r
@@ -0,0 +1,15 @@
+# Loops - while
+# Working with the while condition looping
+
+# 1. Simple while loop
+# Initialize the speed variable
+speed <- 64
+
+# Code the while loop
+while (speed > 30) {
+  print("Slow down!")
+  speed <- speed - 7
+}
+
+# Print out the speed variable
+speed
