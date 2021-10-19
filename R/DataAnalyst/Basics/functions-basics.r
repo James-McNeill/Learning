2d329b9f5732dd0e4f3bfae949ae11f19f185960
James_McNeill <jmcneill06@outlook.com>
2021-10-19 14:27:16 +0100

    Update functions-basics.r

--- R/DataAnalyst/Basics/functions-basics.r
+++ R/DataAnalyst/Basics/functions-basics.r
@@ -8,3 +8,15 @@ help(mean)
 # Inspect the arguments of the mean() function
 args(mean)
 
+# 2. Calling a function
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# Calculate average number of views
+avg_li <- mean(linkedin)
+avg_fb <- mean(facebook)
+
+# Inspect avg_li and avg_fb
+avg_li
+avg_fb
