597fa54e97591358622c2623e61dc63b55acab5b
James_McNeill <jmcneill06@outlook.com>
2021-10-19 12:45:47 +0100

    Update loops-for.r

--- R/DataAnalyst/Basics/loops-for.r
+++ R/DataAnalyst/Basics/loops-for.r
@@ -40,3 +40,17 @@ for (i in 1:nrow(ttt)) {
     print(paste("On row ", i, " and column ", j, " the board contains ", ttt[i, j]))
   }
 }
+
+# 4. Looping over vector with if statements
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Code the for loop with conditionals
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  print(li)
+}
