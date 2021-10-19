971bcaeca3062620b610058769cf15f2cb428bf0
James_McNeill <jmcneill06@outlook.com>
2021-10-19 15:57:46 +0100

    Update functions-create.r

--- R/DataAnalyst/Basics/functions-create.r
+++ R/DataAnalyst/Basics/functions-create.r
@@ -10,3 +10,19 @@
 # arg1, arg2: two positional parameter arguments used to define the function
 # body: element of the function where an operation takes place
 
+# 1. Create basic functions
+# Create a function pow_two()
+pow_two <- function(x) {
+    x * x
+}
+
+# Use the function
+pow_two(12)
+
+# Create a function sum_abs()
+sum_abs <- function(x, y) {
+    abs(x) + abs(y)
+}
+
+# Use the function
+sum_abs(-2, 3)
