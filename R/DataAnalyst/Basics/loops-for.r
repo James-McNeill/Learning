71fafde605783867e3d8d4109338ca7d57021ed4
James_McNeill <jmcneill06@outlook.com>
2021-10-19 12:38:12 +0100

    Create loops-for.r

---
+++ R/DataAnalyst/Basics/loops-for.r
@@ -0,0 +1,16 @@
+# Loops - for
+# Working with the for loop method
+
+# 1. Basic for loop
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1
+for (l in linkedin) {
+    print(l)
+}
+
+# Loop version 2
+for (l in 1:length(linkedin)) {
+    print(linkedin[l])
+}
