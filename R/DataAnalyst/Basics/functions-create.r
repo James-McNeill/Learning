0d172a4c9008382c7b67fcb0d6761be054c45b6f
James_McNeill <jmcneill06@outlook.com>
2021-10-19 15:55:30 +0100

    Create functions-create.r

---
+++ R/DataAnalyst/Basics/functions-create.r
@@ -0,0 +1,12 @@
+# Creating functions
+
+# Basic structure of a function
+# my_fun <- function(arg1, arg2) {
+#  body
+# }
+
+# my_fun: variable that is assigned the function
+# function: method that is provided
+# arg1, arg2: two positional parameter arguments used to define the function
+# body: element of the function where an operation takes place
+
