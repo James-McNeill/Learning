823f8d73d04322d8057d390888246931c8280cba
James_McNeill <jmcneill06@outlook.com>
2021-10-20 17:18:44 +0100

    Update utilities-basics.r

--- R/DataAnalyst/Basics/utilities-basics.r
+++ R/DataAnalyst/Basics/utilities-basics.r
@@ -8,3 +8,28 @@ errors <- c(1.9, -2.6, 4.0, -9.5, -3.4, 7.3)
 
 # Sum of absolute rounded values of errors
 sum(abs(round(errors)))
+
+# 2. Data
+# seq(): Generate sequences, by specifying the from, to, and by arguments.
+# rep(): Replicate elements of vectors and lists.
+# sort(): Sort a vector in ascending order. Works on numerics, but also on character strings and logicals.
+# rev(): Reverse the elements in a data structures for which reversal is defined.
+# str(): Display the structure of any R object.
+# append(): Merge vectors or lists.
+# is.*(): Check for the class of an R object.
+# as.*(): Convert an R object from one class to another.
+# unlist(): Flatten (possibly embedded) lists to produce a vector.
+
+# The linkedin and facebook lists have already been created for you
+linkedin <- list(16, 9, 13, 5, 2, 17, 14)
+facebook <- list(17, 7, 5, 16, 8, 13, 14)
+
+# Convert linkedin and facebook to a vector: li_vec and fb_vec
+li_vec <- unlist(linkedin)
+fb_vec <- unlist(facebook)
+
+# Append fb_vec to li_vec: social_vec
+social_vec <- append(li_vec, fb_vec)
+
+# Sort social_vec
+rev(sort(social_vec))
