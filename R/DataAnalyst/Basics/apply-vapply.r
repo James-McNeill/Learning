47399740b20b4b226df9d6ffc5fcb0548570589e
James_McNeill <jmcneill06@outlook.com>
2021-10-20 17:03:52 +0100

    Update apply-vapply.r

--- R/DataAnalyst/Basics/apply-vapply.r
+++ R/DataAnalyst/Basics/apply-vapply.r
@@ -3,7 +3,7 @@
 # vapply(X, FUN, FUN.VALUE, ..., USE.NAMES = TRUE)
 # FUN.VALUE: argument expects a template for the return argument of this function
 
-# 1. 
+# 1. Basic overview of the method
 # Definition of basics()
 basics <- function(x) {
   c(min = min(x), mean = mean(x), max = max(x))
@@ -11,3 +11,12 @@ basics <- function(x) {
 
 # Apply basics() over temp using vapply() - returns matrix with the variable names outlined in the vector
 vapply(temp, basics, numeric(3))
+
+# 2. Additional method added to the function
+# Definition of the basics() function
+basics <- function(x) {
+  c(min = min(x), mean = mean(x), median = median(x), max = max(x))
+}
+
+# Fix the error:
+vapply(temp, basics, numeric(4))
