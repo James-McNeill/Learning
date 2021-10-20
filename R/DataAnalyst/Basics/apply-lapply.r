f2f541086fe6671293bee7f09a4ec633bedd5316
James_McNeill <jmcneill06@outlook.com>
2021-10-20 09:47:12 +0100

    Update apply-lapply.r

--- R/DataAnalyst/Basics/apply-lapply.r
+++ R/DataAnalyst/Basics/apply-lapply.r
@@ -5,7 +5,7 @@
 # x: element to review
 # FUN: function to apply to element(s)
 
-
+# 1. Perform initial review
 # The vector pioneers has already been created for you
 pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
 
@@ -17,3 +17,25 @@ split_low <- lapply(split_math, tolower)
 
 # Take a look at the structure of split_low
 str(split_low)
+
+# 2. Extract elements from the vector with two functions
+# Code from previous exercise:
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+split <- strsplit(pioneers, split = ":")
+split_low <- lapply(split, tolower)
+
+# Write function select_first()
+select_first <- function(x) {
+  x[1]
+}
+
+# Apply select_first() over split_low: names
+names <- lapply(split_low, select_first)
+
+# Write function select_second()
+select_second <- function(x) {
+  x[2]
+}
+
+# Apply select_second() over split_low: years
+years <- lapply(split_low, select_second)
