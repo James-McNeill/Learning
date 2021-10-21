96540352f4a737b734a225f469a1f816ddd2d431
James_McNeill <jmcneill06@outlook.com>
2021-10-21 10:47:41 +0100

    Create utilities-date-time.r

---
+++ R/DataAnalyst/Basics/utilities-date-time.r
@@ -0,0 +1,14 @@
+# Working with date time objects
+# Start date for R is 1st January 1970
+
+# Get the current date: today
+today <- Sys.Date()
+
+# See what today looks like under the hood
+unclass(today)
+
+# Get the current time: now
+now <- Sys.time()
+
+# See what now looks like under the hood
+unclass(now)
