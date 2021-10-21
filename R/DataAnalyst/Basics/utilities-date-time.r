6e1fa1fabbf40491682925dc74b7a30f5a44cc70
James_McNeill <jmcneill06@outlook.com>
2021-10-21 11:03:19 +0100

    Update utilities-date-time.r

--- R/DataAnalyst/Basics/utilities-date-time.r
+++ R/DataAnalyst/Basics/utilities-date-time.r
@@ -73,3 +73,17 @@ day_diff <- diff(pizza)
 
 # Average period between two consecutive pizza days
 mean(day_diff)
+
+# 4. Calculations with Times
+# login and logout are already defined in the workspace
+# Calculate the difference between login and logout: time_online
+time_online <- logout - login
+
+# Inspect the variable time_online
+time_online
+
+# Calculate the total time online
+sum(time_online)
+
+# Calculate the average time online
+mean(time_online)
