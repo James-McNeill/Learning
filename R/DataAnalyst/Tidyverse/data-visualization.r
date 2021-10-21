8173287d9714c4aa13411537b1b91347e45ffe11
James_McNeill <jmcneill06@outlook.com>
2021-10-21 17:12:08 +0100

    Update data-visualization.r

--- R/DataAnalyst/Tidyverse/data-visualization.r
+++ R/DataAnalyst/Tidyverse/data-visualization.r
@@ -46,3 +46,10 @@ ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
   geom_point() +
   scale_x_log10() +
   facet_wrap(~ continent)
+
+# Scatter plot comparing gdpPercap and lifeExp, with color representing continent
+# and size representing population, faceted by year
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
+    geom_point() +
+    scale_x_log10() +
+    facet_wrap(~ year)
