f1d39f940c752f7881bb08d6b4854e9211316f0e
James_McNeill <jmcneill06@outlook.com>
2021-10-21 16:14:26 +0100

    Update data-wrangle-basics.r

--- R/DataAnalyst/Tidyverse/data-wrangle-basics.r
+++ R/DataAnalyst/Tidyverse/data-wrangle-basics.r
@@ -15,3 +15,7 @@ library(dplyr)
 
 # Filter the gapminder dataset for the year 1957. %>%: represents a pipe
 gapminder %>% filter(year == 1957)
+
+# Filter for China in 2002
+gapminder %>%
+    filter(country == "China", year == 2002)
