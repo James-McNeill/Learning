ab9b30b9c099a2ae135d1e68c0805c6ed728c758
James_McNeill <jmcneill06@outlook.com>
2021-10-21 16:57:59 +0100

    Update data-visualization.r

--- R/DataAnalyst/Tidyverse/data-visualization.r
+++ R/DataAnalyst/Tidyverse/data-visualization.r
@@ -8,3 +8,10 @@ library(ggplot2)
 
 # Create gapminder_1952
 gapminder_1952 <- gapminder %>% filter(year == 1952)
+
+# 1. Produce scattergraph
+# aes: relates to the axis
+# geom_point(): added to signify that a geometric point layer is requested for the plot
+# Change to put pop on the x-axis and gdpPercap on the y-axis
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
+  geom_point()
