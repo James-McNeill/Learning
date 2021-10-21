7f093632c00e12c34aeda4c007c878e9a807028c
James_McNeill <jmcneill06@outlook.com>
2021-10-21 17:01:49 +0100

    Update data-visualization.r

--- R/DataAnalyst/Tidyverse/data-visualization.r
+++ R/DataAnalyst/Tidyverse/data-visualization.r
@@ -15,3 +15,10 @@ gapminder_1952 <- gapminder %>% filter(year == 1952)
 # Change to put pop on the x-axis and gdpPercap on the y-axis
 ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
   geom_point()
+
+# 2. Add log scale to the axis
+# scale_x_log10(): method converts the x axis variable to log base 10 scale. For y axis conversion just switch x with y
+# Change this plot to put the x-axis on a log scale
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
+  geom_point() +
+  scale_x_log10()
