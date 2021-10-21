ace695bc5bdff18bf7c7c7a01eade5413efa8ceb
James_McNeill <jmcneill06@outlook.com>
2021-10-21 16:44:48 +0100

    Update data-wrangle-basics.r

--- R/DataAnalyst/Tidyverse/data-wrangle-basics.r
+++ R/DataAnalyst/Tidyverse/data-wrangle-basics.r
@@ -33,3 +33,12 @@ gapminder %>%
 gapminder %>%
     filter(year == 1957) %>%
         arrange(desc(pop))
+
+# 3. Verb - mutate, used to alter or create a column
+# Use mutate to change lifeExp to be in months
+gapminder %>%
+    mutate(lifeExp = lifeExp * 12)
+
+# Use mutate to create a new column called lifeExpMonths
+gapminder %>%
+    mutate(lifeExpMonths = lifeExp * 12)
