108c37a245b37b970018eaf2dbcbf0fc6bc63260
James_McNeill <jmcneill06@outlook.com>
2021-10-21 16:08:21 +0100

    Create wrangle-basics.r

---
+++ R/DataAnalyst/Tidyverse/wrangle-basics.r
@@ -0,0 +1,10 @@
+# Performing basic data wrangling tasks
+
+# Load the gapminder package
+library(gapminder)
+
+# Load the dplyr package
+library(dplyr)
+
+# Look at the gapminder dataset
+gapminder
