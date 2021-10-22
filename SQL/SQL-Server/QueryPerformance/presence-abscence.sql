83ffe85ef392b7c03f0ae6ed85ab0e93c59e9507
James_McNeill <jmcneill06@outlook.com>
2021-10-22 17:06:33 +0100

    Update presence-abscence.sql

--- SQL/SQL-Server/QueryPerformance/presence-abscence.sql
+++ SQL/SQL-Server/QueryPerformance/presence-abscence.sql
@@ -18,3 +18,25 @@ EXCEPT -- Add the operator to compare the two queries
 
 SELECT Country 
 FROM Earthquakes; -- Table with country codes
+
+-- 3. IN and EXISTS
+-- First attempt
+SELECT CountryName,
+       Pop2017, -- 2017 country population
+        Capital, -- Capital city	   
+       WorldBankRegion
+FROM Nations
+WHERE Capital IN -- Add the operator to compare queries
+        (SELECT NearestPop 
+	     FROM Earthquakes);
+       
+-- Second attempt
+SELECT CountryName,   
+	   Capital,
+       Pop2016, -- 2016 country population
+       WorldBankRegion
+FROM Nations AS n
+WHERE EXISTS -- Add the operator to compare queries
+	  (SELECT 1
+	   FROM Earthquakes AS e
+	   WHERE n.Capital = e.NearestPop); -- Columns being compared
