ba48b47a2f7c67499ac03686cf41b653146955ca
James_McNeill <jmcneill06@outlook.com>
2021-10-22 14:23:04 +0100

    Create classification-logon.sql

---
+++ SQL/SQL-Server/Triggers/classification-logon.sql
@@ -0,0 +1,21 @@
+-- Trigger classification logon
+-- These triggers are only fired during the user logon phase. Therefore after the user has logged on but prior to the session instance being setup
+
+-- 1. Track database logins to enhance security
+-- Save user details in the audit table
+INSERT INTO ServerLogonLog (LoginName, LoginDate, SessionID, SourceIPAddress)
+SELECT ORIGINAL_LOGIN(), GETDATE(), @@SPID, client_net_address
+-- The user details can be found in SYS.DM_EXEC_CONNECTIONS
+FROM SYS.DM_EXEC_CONNECTIONS WHERE session_id = @@SPID;
+
+-- Create a trigger firing when users log on to the server
+CREATE TRIGGER LogonAudit
+-- Use ALL SERVER to create a server-level trigger
+ON ALL SERVER WITH EXECUTE AS 'sa'
+-- The trigger should fire after a logon
+FOR LOGON
+AS
+	-- Save user details in the audit table
+	INSERT INTO ServerLogonLog (LoginName, LoginDate, SessionID, SourceIPAddress)
+	SELECT ORIGINAL_LOGIN(), GETDATE(), @@SPID, client_net_address
+	FROM SYS.DM_EXEC_CONNECTIONS WHERE session_id = @@SPID;
