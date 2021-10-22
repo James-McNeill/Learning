48d564cd6bd8d407fedc326d705298e015f85340
James_McNeill <jmcneill06@outlook.com>
2021-10-22 15:00:32 +0100

    Update optimise-manage-overview.sql

--- SQL/SQL-Server/Triggers/optimise-manage-overview.sql
+++ SQL/SQL-Server/Triggers/optimise-manage-overview.sql
@@ -20,7 +20,12 @@ INSTEAD OF DELETE
 AS
 	PRINT 'You are not allowed to remove data from the Discounts table.';
 
--- 3. Disable the trigger
+-- 3. Disable the trigger. If the trigger is not required for a certain period of time it can be paused instead of deleted
 -- Pause the trigger execution
 DISABLE TRIGGER PreventOrdersUpdate
+ON Orders;
+
+-- 4. Re-enable a disabled trigger
+-- Resume the trigger execution
+ENABLE TRIGGER PreventOrdersUpdate
 ON Orders;
