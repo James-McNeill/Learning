5009bc7e2b7ebcb697a52e9d3b21382c4318346c
James_McNeill <jmcneill06@outlook.com>
2021-10-22 15:44:36 +0100

    Update basics-review-order.sql

--- SQL/SQL-Server/QueryPerformance/basics-review-order.sql
+++ SQL/SQL-Server/QueryPerformance/basics-review-order.sql
@@ -21,3 +21,27 @@ FROM Players
 WHERE Country = 'USA'
     OR Country = 'Canada';
 -- ORDER BY BMI;
+
+-- Debugging queries with comments. Maintains a record of the query history
+-- Your friend's query
+-- First attempt, contains errors and inconsistent formatting
+/*
+select PlayerName, p.Country,sum(ps.TotalPoints) 
+AS TotalPoints  
+FROM PlayerStats ps inner join Players On ps.PlayerName = p.PlayerName
+WHERE p.Country = 'New Zeeland'
+Group 
+by PlayerName, Country
+order by Country;
+*/
+
+-- Your query
+-- Second attempt - errors corrected and formatting fixed
+
+SELECT p.PlayerName, p.Country,
+		SUM(ps.TotalPoints) AS TotalPoints  
+FROM PlayerStats ps 
+INNER JOIN Players p
+	ON ps.PlayerName = p.PlayerName
+WHERE p.Country = 'New Zealand'
+GROUP BY p.PlayerName, p.Country;
