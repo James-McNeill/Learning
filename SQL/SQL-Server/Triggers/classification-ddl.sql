5a5e1f5afeed9bf568e8c2da3a70855ffe1ccaa7
James_McNeill <jmcneill06@outlook.com>
2021-10-22 14:16:37 +0100

    Update classification-ddl.sql

--- SQL/SQL-Server/Triggers/classification-ddl.sql
+++ SQL/SQL-Server/Triggers/classification-ddl.sql
@@ -11,3 +11,13 @@ FOR CREATE_TABLE,
 AS
 	INSERT INTO TablesChangeLog (EventData, ChangedBy)
     VALUES (EVENTDATA(), USER);
+
+-- 2. Rollback changes that shouldn't take place (DROP_TABLE)
+-- Add a trigger to disable the removal of tables
+CREATE TRIGGER PreventTableDeletion
+ON DATABASE
+FOR DROP_TABLE
+AS
+	RAISERROR ('You are not allowed to remove tables from this database.', 16, 1);
+    -- Revert the statement that removes the table
+    ROLLBACK;
