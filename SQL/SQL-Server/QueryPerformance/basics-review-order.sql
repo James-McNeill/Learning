c2a0eae40018bd8125abf8e60fd9452fc274e1cb
James_McNeill <jmcneill06@outlook.com>
2021-10-22 15:41:23 +0100

    Update basics-review-order.sql

--- SQL/SQL-Server/QueryPerformance/basics-review-order.sql
+++ SQL/SQL-Server/QueryPerformance/basics-review-order.sql
@@ -1,1 +1,10 @@
 -- Working with SQL in a structured manner
+
+-- 1. Formatting
+-- Ensure all SQL syntax is in upper case
+SELECT PlayerName, Country,
+ROUND(Weight_kg/SQUARE(Height_cm/100),2) BMI
+FROM Players 
+WHERE Country = 'USA'
+    OR Country = 'Canada'
+ORDER BY BMI;
