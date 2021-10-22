c056a2b3649fa4d3236a563cd3dc54221c72dff6
James_McNeill <jmcneill06@outlook.com>
2021-10-22 15:04:50 +0100

    Update optimise-manage-overview.sql

--- SQL/SQL-Server/Triggers/optimise-manage-overview.sql
+++ SQL/SQL-Server/Triggers/optimise-manage-overview.sql
@@ -29,3 +29,21 @@ ON Orders;
 -- Resume the trigger execution
 ENABLE TRIGGER PreventOrdersUpdate
 ON Orders;
+
+-- 5. Managing existing triggers
+-- Get the disabled triggers
+SELECT name,
+	   object_id,
+	   parent_class_desc
+FROM sys.triggers
+WHERE is_disabled = 1;
+
+-- Check for unchanged server triggers
+SELECT *
+FROM sys.server_triggers
+WHERE create_date = modify_date;
+
+-- Get the database triggers
+SELECT *
+FROM sys.triggers
+WHERE parent_class_desc = 'DATABASE';
