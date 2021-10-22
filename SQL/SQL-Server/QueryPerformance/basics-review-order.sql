68511c9aa000c34c2fa8be7eef3f2fd7074cb786
James_McNeill <jmcneill06@outlook.com>
2021-10-22 15:46:58 +0100

    Update basics-review-order.sql

--- SQL/SQL-Server/QueryPerformance/basics-review-order.sql
+++ SQL/SQL-Server/QueryPerformance/basics-review-order.sql
@@ -45,3 +45,16 @@ INNER JOIN Players p
 	ON ps.PlayerName = p.PlayerName
 WHERE p.Country = 'New Zealand'
 GROUP BY p.PlayerName, p.Country;
+
+-- 2. Using Aliasing. Helps to streamline the code
+SELECT Team, 
+   ROUND(AVG(BMI),2) AS AvgTeamBMI -- Alias the new column
+FROM PlayerStats AS ps -- Alias PlayerStats table
+INNER JOIN
+		(SELECT PlayerName, Country,
+			Weight_kg/SQUARE(Height_cm/100) BMI
+		 FROM Players) AS p -- Alias the sub-query
+             -- Alias the joining columns
+	ON ps.PlayerName = p.PlayerName 
+GROUP BY Team
+HAVING AVG(BMI) >= 25;
