bb8599590402250eb3d909ab3f17d97c7203ed29
James_McNeill <jmcneill06@outlook.com>
2021-10-22 10:46:38 +0100

    Update data-summarize-grouping.r

--- R/DataAnalyst/Tidyverse/data-summarize-grouping.r
+++ R/DataAnalyst/Tidyverse/data-summarize-grouping.r
@@ -32,3 +32,9 @@ gapminder %>%
     group_by(continent) %>%
     summarize(medianLifeExp = median(lifeExp),
             maxGdpPercap = max(gdpPercap))
+
+# Find median life expectancy and maximum GDP per capita in each continent/year combination
+gapminder %>%
+    group_by(continent, year) %>%
+    summarize(medianLifeExp = median(lifeExp),
+            maxGdpPercap = max(gdpPercap))
