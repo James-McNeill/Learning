1f5381f0e17c6783ae2c4452053796e2d6d775ca
James_McNeill <jmcneill06@outlook.com>
2021-10-22 10:40:41 +0100

    Update data-summarize-grouping.r

--- R/DataAnalyst/Tidyverse/data-summarize-grouping.r
+++ R/DataAnalyst/Tidyverse/data-summarize-grouping.r
@@ -12,3 +12,9 @@ gapminder %>%
 gapminder %>%
     filter(year == 1957) %>%
     summarize(medianLifeExp = median(lifeExp))
+
+# Filter for 1957 then summarize the median life expectancy and the maximum GDP per capita
+gapminder %>%
+    filter(year == 1957) %>%
+    summarize(medianLifeExp = median(lifeExp),
+            maxGdpPercap = max(gdpPercap))
