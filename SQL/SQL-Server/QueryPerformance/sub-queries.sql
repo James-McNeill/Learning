814a3c89433b3be5b9690769ae7725b574c9f0ce
James_McNeill <jmcneill06@outlook.com>
2021-10-22 16:40:39 +0100

    Update sub-queries.sql

--- SQL/SQL-Server/QueryPerformance/sub-queries.sql
+++ SQL/SQL-Server/QueryPerformance/sub-queries.sql
@@ -20,3 +20,23 @@ SELECT UNContinentRegion,
 FROM Nations n
 ORDER BY UNContinentRegion DESC, 
          AverageMagnitude DESC;
+
+-- 3. Sub-query Vs INNER JOIN
+SELECT
+	n.CountryName,
+	 (SELECT MAX(c.Pop2017) -- Add 2017 population column
+	 FROM Cities AS c 
+                       -- Outer query country code column
+	 WHERE c.CountryCode = n.Code2) AS BiggestCity
+FROM Nations AS n; -- Outer query table
+
+-- Using an INNER JOIN can help with query performance
+SELECT n.CountryName, 
+       c.BiggestCity 
+FROM Nations AS n
+INNER JOIN -- Join the Nations table and sub-query
+    (SELECT CountryCode, 
+     MAX(Pop2017) AS BiggestCity 
+     FROM Cities
+     GROUP BY CountryCode) AS c
+ON n.Code2 = c.CountryCode; -- Add the joining columns
