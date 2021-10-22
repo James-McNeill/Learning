8bfa096b6e84d8b24574b43e656f9ed2596f916b
James_McNeill <jmcneill06@outlook.com>
2021-10-22 14:36:59 +0100

    Create use-case-after.sql

---
+++ SQL/SQL-Server/Triggers/use-case-after.sql
@@ -0,0 +1,12 @@
+-- Use cases for AFTER DML
+
+-- 1. Create a trigger to keep row history. Keep track of any adjustments to a table due to INSERT / UPDATE statements
+CREATE TRIGGER CopyCustomersToHistory
+ON Customers
+-- Fire the trigger for new and updated rows
+AFTER INSERT, UPDATE
+AS
+	INSERT INTO CustomersHistory (CustomerID, Customer, ContractID, ContractDate, Address, PhoneNo, Email, ChangeDate)
+	SELECT CustomerID, Customer, ContractID, ContractDate, Address, PhoneNo, Email, GETDATE()
+    -- Get info from the special table that keeps new rows
+    FROM inserted;
