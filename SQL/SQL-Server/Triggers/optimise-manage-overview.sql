c9bfe9f2ed6b898ff5c9015c0af3f4da1c278a5a
James_McNeill <jmcneill06@outlook.com>
2021-10-22 15:12:06 +0100

    Update optimise-manage-overview.sql

--- SQL/SQL-Server/Triggers/optimise-manage-overview.sql
+++ SQL/SQL-Server/Triggers/optimise-manage-overview.sql
@@ -47,3 +47,18 @@ WHERE create_date = modify_date;
 SELECT *
 FROM sys.triggers
 WHERE parent_class_desc = 'DATABASE';
+
+-- 6. Keeping track of trigger execution
+-- Modify the trigger to add new functionality
+ALTER TRIGGER PreventOrdersUpdate
+ON Orders
+-- Prevent any row changes
+INSTEAD OF UPDATE
+AS
+	-- Keep history of trigger executions
+	INSERT INTO TriggerAudit (TriggerName, ExecutionDate)
+	SELECT 'PreventOrdersUpdate', 
+           GETDATE();
+
+	RAISERROR ('Updates on "Orders" table are not permitted.
+                Place a new order to add new products.', 16, 1);
