fea5c926ec8028a42051d8d71cdb90fd95dd1407
James_McNeill <jmcneill06@outlook.com>
2021-10-22 17:08:55 +0100

    Update presence-abscence.sql

--- SQL/SQL-Server/QueryPerformance/presence-abscence.sql
+++ SQL/SQL-Server/QueryPerformance/presence-abscence.sql
@@ -40,3 +40,22 @@ WHERE EXISTS -- Add the operator to compare queries
 	  (SELECT 1
 	   FROM Earthquakes AS e
 	   WHERE n.Capital = e.NearestPop); -- Columns being compared
+
+-- 4. NOT IN and NOT EXISTS
+SELECT WorldBankRegion,
+       CountryName
+FROM Nations
+WHERE Code2 NOT IN -- Add the operator to compare queries
+	(SELECT CountryCode -- Country code column
+	 FROM Cities);
+	 
+SELECT WorldBankRegion,
+       CountryName,
+	   Code2,
+       Capital, -- Country capital column
+	   Pop2017
+FROM Nations AS n
+WHERE NOT EXISTS -- Add the operator to compare queries
+	(SELECT 1
+	 FROM Cities AS c
+	 WHERE n.Code2 = c.CountryCode); -- Columns being compared
