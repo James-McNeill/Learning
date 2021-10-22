3e6f76dffabe305c99a2009b1bef9cc0e29b3b76
James_McNeill <jmcneill06@outlook.com>
2021-10-22 16:36:46 +0100

    Create sub-queries.sql

---
+++ SQL/SQL-Server/QueryPerformance/sub-queries.sql
@@ -0,0 +1,11 @@
+-- Working with sub-queries
+
+-- 1. Uncorrelated sub-query
+SELECT UNStatisticalRegion,
+       CountryName 
+FROM Nations
+WHERE Code2 -- Country code for outer query 
+         IN (SELECT Country -- Country code for sub-query
+             FROM Earthquakes
+             WHERE Depth >= 400 ) -- Depth filter
+ORDER BY UNStatisticalRegion;
