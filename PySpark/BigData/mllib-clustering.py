aabe334c21640cf97d6f8af12f70fb2a1a6ce6c5
James_McNeill <jmcneill06@outlook.com>
2021-10-27 16:52:29 +0100

    Update mllib-clustering.py

--- PySpark/BigData/mllib-clustering.py
+++ PySpark/BigData/mllib-clustering.py
@@ -25,3 +25,15 @@ model = KMeans.train(rdd_split_int, k=15, seed=1)
 
 # Get cluster centers
 cluster_centers = model.clusterCenters
+
+# 3. Visualize the model
+# Convert rdd_split_int RDD into Spark DataFrame and then to Pandas DataFrame
+rdd_split_int_df_pandas = spark.createDataFrame(rdd_split_int, schema=["col1", "col2"]).toPandas()
+
+# Convert cluster_centers to a pandas DataFrame
+cluster_centers_pandas = pd.DataFrame(cluster_centers, columns=["col1", "col2"])
+
+# Create an overlaid scatter plot of clusters and centroids
+plt.scatter(rdd_split_int_df_pandas["col1"], rdd_split_int_df_pandas["col2"])
+plt.scatter(cluster_centers_pandas["col1"], cluster_centers_pandas["col2"], color="red", marker="x")
+plt.show()
