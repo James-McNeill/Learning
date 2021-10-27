9cda31c051e1720398a1033d7ae902fe8e3e1b30
James_McNeill <jmcneill06@outlook.com>
2021-10-27 16:31:25 +0100

    Update mllib-collaborative-filtering.py

--- PySpark/BigData/mllib-collaborative-filtering.py
+++ PySpark/BigData/mllib-collaborative-filtering.py
@@ -11,3 +11,15 @@ ratings_final = ratings.map(lambda line: Rating(int(line[0]), int(line[1]), floa
 
 # Split the data into training and test
 training_data, test_data = ratings_final.randomSplit([0.8, 0.2])
+
+# Create the ALS model on the training data
+model = ALS.train(training_data, rank=10, iterations=10)
+
+# Drop the ratings column 
+testdata_no_rating = test_data.map(lambda p: (p[0], p[1]))
+
+# Predict the model  
+predictions = model.predictAll(testdata_no_rating)
+
+# Return the first 2 rows of the RDD
+predictions.take(2)
