e1bab476a28c9ae5a06b29baed6f5aeccb349160
James_McNeill <jmcneill06@outlook.com>
2021-10-27 16:50:48 +0100

    Update mllib-clustering.py

--- PySpark/BigData/mllib-clustering.py
+++ PySpark/BigData/mllib-clustering.py
@@ -12,3 +12,16 @@ rdd_split_int = rdd_split.map(lambda x: [int(x[0]), int(x[1])])
 
 # Count the number of rows in RDD 
 print("There are {} rows in the rdd_split_int dataset".format(rdd_split_int.count()))
+
+# 2. Train the model
+# Train the model with clusters from 13 to 16 and compute WSSSE
+for clst in range(13, 17):
+    model = KMeans.train(rdd_split_int, clst, seed=1)
+    WSSSE = rdd_split_int.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+    print("The cluster {} has Within Set Sum of Squared Error {}".format(clst, WSSSE))
+
+# Train the model again with the best k
+model = KMeans.train(rdd_split_int, k=15, seed=1)
+
+# Get cluster centers
+cluster_centers = model.clusterCenters
