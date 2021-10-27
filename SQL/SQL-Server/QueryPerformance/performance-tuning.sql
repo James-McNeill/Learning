ddb1f70f47ad2bbc500f7d65609c26a7f6c9e256
James_McNeill <jmcneill06@outlook.com>
2021-10-27 10:53:04 +0100

    Update performance-tuning.sql

--- SQL/SQL-Server/QueryPerformance/performance-tuning.sql
+++ SQL/SQL-Server/QueryPerformance/performance-tuning.sql
@@ -52,3 +52,18 @@ WHERE CustomerID IN -- Add filter operator
 		WHERE ShipCity IN
             ('Berlin','Bern','Bruxelles','Helsinki',
 			'Lisboa','Madrid','Paris','London'));
+
+-- Example 2. Optimised the number of reads made against each table
+SELECT c.CustomerID,
+       c.CompanyName,
+       COUNT(o.CustomerID)
+FROM Customers AS c
+INNER JOIN Orders AS o -- Join operator
+    ON c.CustomerID = o.CustomerID
+WHERE o.ShipCity IN -- Shipping destination column
+     ('Berlin','Bern','Bruxelles','Helsinki',
+	 'Lisboa','Madrid','Paris','London')
+GROUP BY c.CustomerID,
+         c.CompanyName;
+	 
+SET STATISTICS IO OFF -- Turn the IO command off
