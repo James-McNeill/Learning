87ad5304801b24ebab92075adaeb2b4425cd3c8f
James_McNeill <jmcneill06@outlook.com>
2021-10-27 16:22:48 +0100

    Create mllib-overview.py

---
+++ PySpark/BigData/mllib-overview.py
@@ -0,0 +1,11 @@
+# PySpark MLlib algorithms
+# Have to use the RDD data structure when working with the MLlib API
+
+# Import the library for ALS
+from pyspark.mllib.recommendation import ALS
+
+# Import the library for Logistic Regression
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+
+# Import the library for Kmeans
+from pyspark.mllib.clustering import KMeans
