578826004cfd1d030dc254856632bc10bb58c60c
James_McNeill <jmcneill06@outlook.com>
2021-10-27 12:49:24 +0100

    Update rdd-basic.py

--- PySpark/BigData/rdd-basic.py
+++ PySpark/BigData/rdd-basic.py
@@ -15,3 +15,19 @@ numbers_all = cubedRDD.collect()
 # Print the numbers from numbers_all
 for numb in numbers_all:
 	print(numb)
+
+# Basic RDD actions:
+# collect(): return all elements of the dataset as an array
+# take(N): returns an array with the first N elements of the dataset
+# first(): prints the first element of the RDD
+# count(): returns the number of elements in the RDD
+
+# Filter the fileRDD to select lines with Spark keyword
+fileRDD_filter = fileRDD.filter(lambda line: 'Spark' in line)
+
+# How many lines are there in fileRDD?
+print("The total number of lines with the keyword Spark is", fileRDD_filter.count())
+
+# Print the first four lines of fileRDD
+for line in fileRDD_filter.take(4): 
+  print(line)
