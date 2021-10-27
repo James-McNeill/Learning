d3b3296c0d0b00946e4084e46604a58a728139f2
James_McNeill <jmcneill06@outlook.com>
2021-10-27 16:06:40 +0100

    Update dataframe-spark-sql.py

--- PySpark/BigData/dataframe-spark-sql.py
+++ PySpark/BigData/dataframe-spark-sql.py
@@ -12,3 +12,13 @@ people_df_names = spark.sql(query)
 
 # Print the top 10 names of the people
 people_df_names.show(10)
+
+# 2. Perform filter operations
+# Filter the people table to select female sex 
+people_female_df = spark.sql('SELECT * FROM people WHERE sex=="female"')
+
+# Filter the people table DataFrame to select male sex
+people_male_df = spark.sql('SELECT * FROM people WHERE sex=="male"')
+
+# Count the number of rows in both DataFrames
+print("There are {} rows in the people_female_df and {} rows in the people_male_df DataFrames".format(people_female_df.count(), people_male_df.count()))
