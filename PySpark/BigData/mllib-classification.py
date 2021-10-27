cd4d9b18ae271c64863b8d642c291e522e61e5c7
James_McNeill <jmcneill06@outlook.com>
2021-10-27 16:42:04 +0100

    Update mllib-classification.py

--- PySpark/BigData/mllib-classification.py
+++ PySpark/BigData/mllib-classification.py
@@ -1,6 +1,5 @@
 # MLlib classification
 
-
 # 1. Load the data
 # Load the datasets into RDDs
 spam_rdd = sc.textFile(file_path_spam)
@@ -13,3 +12,18 @@ non_spam_words = non_spam_rdd.flatMap(lambda email: email.split(' '))
 # Print the first element in the split RDD
 print("The first element in spam_words is", spam_words.first())
 print("The first element in non_spam_words is", non_spam_words.first())
+
+# 2. Feature hashing and LabelPoint
+# Create a HashingTf instance with 200 features - helpful for text classification
+tf = HashingTF(numFeatures=200)
+
+# Map each word to one feature
+spam_features = tf.transform(spam_words)
+non_spam_features = tf.transform(non_spam_words)
+
+# Label the features: 1 for spam, 0 for non-spam
+spam_samples = spam_features.map(lambda features:LabeledPoint(1, features))
+non_spam_samples = non_spam_features.map(lambda features:LabeledPoint(0, features))
+
+# Combine the two datasets
+samples = spam_samples.join(non_spam_samples)
