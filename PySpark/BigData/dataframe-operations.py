557d8c171241b02bc7c364955c81caa987f611e9
James_McNeill <jmcneill06@outlook.com>
2021-10-27 14:46:27 +0100

    Update dataframe-operations.py

--- PySpark/BigData/dataframe-operations.py
+++ PySpark/BigData/dataframe-operations.py
@@ -9,3 +9,16 @@ print("There are {} rows in the people_df DataFrame.".format(people_df.count()))
 
 # Count the number of columns and their names
 print("There are {} columns in the people_df DataFrame and their names are {}".format(len(people_df.columns), people_df.columns))
+
+# 2. Subsetting and cleaning
+# Select name, sex and date of birth columns
+people_df_sub = people_df.select('name', 'sex', 'date of birth')
+
+# Print the first 10 observations from people_df_sub
+people_df_sub.show(10)
+
+# Remove duplicate entries from people_df_sub
+people_df_sub_nodup = people_df_sub.dropDuplicates()
+
+# Count the number of rows
+print("There were {} rows before removing duplicates, and {} rows after removing duplicates".format(people_df_sub.count(), people_df_sub_nodup.count()))
