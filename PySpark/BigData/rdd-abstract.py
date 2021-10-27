33d1866d678ceeed19b1d35fffed5ba099fa719f
James_McNeill <jmcneill06@outlook.com>
2021-10-27 12:36:40 +0100

    Create rdd-abstract.py

---
+++ PySpark/BigData/rdd-abstract.py
@@ -0,0 +1,8 @@
+# Abstraction method in PySpark
+# Resilient Distributed Dataset (RDD), which is the fundamental backbone of the engine
+
+# Create an RDD from a list of words
+RDD = sc.parallelize(["Spark", "is", "a", "framework", "for", "Big Data processing"])
+
+# Print out the type of the created object
+print("The type of RDD is", type(RDD))
