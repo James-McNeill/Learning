183891d0ce25fdd616151a9efdcd7762cfbc2d73
James_McNeill <jmcneill06@outlook.com>
2021-10-27 12:39:15 +0100

    Update rdd-abstract.py

--- PySpark/BigData/rdd-abstract.py
+++ PySpark/BigData/rdd-abstract.py
@@ -1,12 +1,13 @@
 # Abstraction method in PySpark
 # Resilient Distributed Dataset (RDD), which is the fundamental backbone of the engine
 
-# Create an RDD from a list of words
+# 1. Create an RDD from a list of words
 RDD = sc.parallelize(["Spark", "is", "a", "framework", "for", "Big Data processing"])
 
 # Print out the type of the created object
 print("The type of RDD is", type(RDD))
 
+# 2. Create RDD from text file
 # Print the file_path
 print("The file_path is", file_path)
 
@@ -15,3 +16,13 @@ fileRDD = sc.textFile(file_path)
 
 # Check the type of fileRDD
 print("The file type of fileRDD is", type(fileRDD))
+
+# 3. Partitions: adjusting the number of partitions that dataset is distributed across can improve performance
+# Check the number of partitions in fileRDD
+print("Number of partitions in fileRDD is", fileRDD.getNumPartitions())
+
+# Create a fileRDD_part from file_path with 5 partitions
+fileRDD_part = sc.textFile(file_path, minPartitions = 5)
+
+# Check the number of partitions in fileRDD_part
+print("Number of partitions in fileRDD_part is", fileRDD_part.getNumPartitions())
