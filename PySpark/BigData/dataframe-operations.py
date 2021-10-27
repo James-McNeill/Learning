9dee70c939b6bc2da0396ba70054d25af1e33cbf
James_McNeill <jmcneill06@outlook.com>
2021-10-27 14:44:36 +0100

    Create dataframe-operations.py

---
+++ PySpark/BigData/dataframe-operations.py
@@ -0,0 +1,11 @@
+# Performing operations on the DataFrame
+
+# 1. Initial EDA
+# Print the first 10 observations 
+people_df.show(10)
+
+# Count the number of rows 
+print("There are {} rows in the people_df DataFrame.".format(people_df.count()))
+
+# Count the number of columns and their names
+print("There are {} columns in the people_df DataFrame and their names are {}".format(len(people_df.columns), people_df.columns))
