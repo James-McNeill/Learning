b54e21fbe21654a3ddba1861af4b354f7de1d4c0
James_McNeill <jmcneill06@outlook.com>
2021-10-27 16:15:49 +0100

    Update dataframe-visualization.py

--- PySpark/BigData/dataframe-visualization.py
+++ PySpark/BigData/dataframe-visualization.py
@@ -23,3 +23,22 @@ fifa_df.show(10)
 
 # Print the total number of rows
 print("There are {} rows in the fifa_df DataFrame".format(fifa_df.count()))
+
+# Create a temporary view of fifa_df
+fifa_df.createOrReplaceTempView('fifa_df_table')
+
+# Construct the "query"
+query = '''SELECT Age FROM fifa_df_table WHERE Nationality == "Germany"'''
+
+# Apply the SQL "query"
+fifa_df_germany_age = spark.sql(query)
+
+# Generate basic statistics
+fifa_df_germany_age.describe().show()
+
+# Convert fifa_df to fifa_df_germany_age_pandas DataFrame
+fifa_df_germany_age_pandas = fifa_df_germany_age.toPandas()
+
+# Plot the 'Age' density of Germany Players
+fifa_df_germany_age_pandas.plot(kind='density')
+plt.show()
