3d97b4e05eee15760482c85e0d67469956b659f5
James_McNeill <jmcneill06@outlook.com>
2021-10-27 13:54:47 +0100

    Update rdd-advanced.py

--- PySpark/BigData/rdd-advanced.py
+++ PySpark/BigData/rdd-advanced.py
@@ -10,7 +10,8 @@ print("The type of total is", type(total))
 for k, v in total.items(): 
   print("key", k, "has", v, "counts")
 
-# 2. Create a baseRDD from the file path
+# 2. Example. Working with the entire works of Shakespeare
+# Create a baseRDD from the file path
 baseRDD = sc.textFile(file_path)
 
 # Split the lines of baseRDD into words
@@ -18,3 +19,12 @@ splitRDD = baseRDD.flatMap(lambda x: x.split())
 
 # Count the total number of words
 print("Total number of words in splitRDD:", splitRDD.count())
+
+# Convert the words in lower case and remove stop words from the stop_words curated list
+splitRDD_no_stop = splitRDD.filter(lambda x: x.lower() not in stop_words)
+
+# Create a tuple of the word and 1 
+splitRDD_no_stop_words = splitRDD_no_stop.map(lambda w: (w, 1))
+
+# Count of the number of occurences of each word
+resultRDD = splitRDD_no_stop_words.reduceByKey(lambda x, y: x + y)
