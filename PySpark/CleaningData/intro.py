c3a098e277129863400ad4ab695a939a40fae8f8
James_McNeill <jmcneill06@outlook.com>
2021-10-28 10:49:09 +0100

    Update intro.py

--- PySpark/CleaningData/intro.py
+++ PySpark/CleaningData/intro.py
@@ -10,3 +10,16 @@ people_schema = StructType([
   StructField('age', IntegerType(), False),
   StructField('city', StringType(), False)
 ])
+
+# Using lazy processing: data will not be processed until an action method is called
+# Load the CSV file
+aa_dfw_df = spark.read.format('csv').options(Header=True).load('AA_DFW_2018.csv.gz')
+
+# Add the airport column using the F.lower() method
+aa_dfw_df = aa_dfw_df.withColumn('airport', F.lower(aa_dfw_df['Destination Airport']))
+
+# Drop the Destination Airport column
+aa_dfw_df = aa_dfw_df.drop(aa_dfw_df['Destination Airport'])
+
+# Show the DataFrame: this is the action method which will process the prior steps
+aa_dfw_df.show()
