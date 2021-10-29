704d8ab90e00ecad32109671194e0a835f15b7ab
James_McNeill <jmcneill06@outlook.com>
2021-10-29 15:03:16 +0100

    Update Intro.py

--- PySpark/SparkSQL/Intro.py
+++ PySpark/SparkSQL/Intro.py
@@ -13,3 +13,14 @@ spark.sql("DESCRIBE schedule").show()
 # Alternative options
 # spark.sql("SHOW COLUMNS FROM tablename").show()
 # spark.sql("SELECT * FROM tablename LIMIT 0").show()
+
+# B. Window functions
+# Add col running_total that sums diff_min col in each group. diff_min: difference in minutes between train stops during the journey
+query = """
+SELECT train_id, station, time, diff_min,
+SUM(diff_min) OVER (PARTITION BY train_id ORDER BY time) AS running_total
+FROM schedule
+"""
+
+# Run the query and display the result
+spark.sql(query).show()
