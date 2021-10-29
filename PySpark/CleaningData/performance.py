9ad16d9b26ca2c05353ee133a80bffc1e750cac6
James_McNeill <jmcneill06@outlook.com>
2021-10-29 10:57:30 +0100

    Update performance.py

--- PySpark/CleaningData/performance.py
+++ PySpark/CleaningData/performance.py
@@ -13,3 +13,14 @@ print("Counting %d rows took %f seconds" % (departures_df.count(), time.time() -
 # Count the rows again, noting the variance in time of a cached DataFrame
 start_time = time.time()
 print("Counting %d rows again took %f seconds" % (departures_df.count(), time.time() - start_time))
+
+# 2. Remove the DataFrame from cache
+# Determine if departures_df is in the cache
+print("Is departures_df cached?: %s" % departures_df.is_cached)
+print("Removing departures_df from cache")
+
+# Remove departures_df from the cache
+departures_df.unpersist()
+
+# Check the cache status again
+print("Is departures_df cached?: %s" % departures_df.is_cached)
