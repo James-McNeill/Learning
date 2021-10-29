0b12b1d100cf15cd358571449c065b3cacc29cbc
James_McNeill <jmcneill06@outlook.com>
2021-10-29 11:13:14 +0100

    Update performance.py

--- PySpark/CleaningData/performance.py
+++ PySpark/CleaningData/performance.py
@@ -41,6 +41,7 @@ print("Total rows in split DataFrame:\t%d" % split_df.count())
 print("Time to run: %f" % (time.time() - start_time_b))
 
 # C. Cluster configuration
+# 1. Check cluster configuration settings
 # Name of the Spark application instance
 app_name = spark.conf.get('spark.app.name')
 
@@ -54,3 +55,17 @@ num_partitions = spark.conf.get('spark.sql.shuffle.partitions')
 print("Name: %s" % app_name)
 print("Driver TCP port: %s" % driver_tcp_port)
 print("Number of partitions: %s" % num_partitions)
+
+# 2. Adjust cluster configuration settings
+# Store the number of partitions in variable
+before = departures_df.rdd.getNumPartitions()
+
+# Configure Spark to use 500 partitions
+spark.conf.set('spark.sql.shuffle.partitions', 500)
+
+# Recreate the DataFrame using the departures data file
+departures_df = spark.read.csv('departures.txt.gz').distinct()
+
+# Print the number of partitions for each instance
+print("Partition count before change: %d" % before)
+print("Partition count after change: %d" % departures_df.rdd.getNumPartitions())
