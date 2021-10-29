ed99885547b29c46e509dd217dae435f1fd1e77e
James_McNeill <jmcneill06@outlook.com>
2021-10-29 11:08:58 +0100

    Update performance.py

--- PySpark/CleaningData/performance.py
+++ PySpark/CleaningData/performance.py
@@ -39,3 +39,18 @@ print("Time to run: %f" % (time.time() - start_time_a))
 start_time_b = time.time()
 print("Total rows in split DataFrame:\t%d" % split_df.count())
 print("Time to run: %f" % (time.time() - start_time_b))
+
+# C. Cluster configuration
+# Name of the Spark application instance
+app_name = spark.conf.get('spark.app.name')
+
+# Driver TCP port
+driver_tcp_port = spark.conf.get('spark.driver.port')
+
+# Number of join partitions
+num_partitions = spark.conf.get('spark.sql.shuffle.partitions')
+
+# Show the results
+print("Name: %s" % app_name)
+print("Driver TCP port: %s" % driver_tcp_port)
+print("Number of partitions: %s" % num_partitions)
