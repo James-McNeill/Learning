3dd405d80168f27091d70efeb51e93310c992725
James_McNeill <jmcneill06@outlook.com>
2021-10-29 12:22:45 +0100

    Update pipelines.py

--- PySpark/CleaningData/pipelines.py
+++ PySpark/CleaningData/pipelines.py
@@ -1,1 +1,14 @@
 # Creating data Pipelines
+
+# A. Simple pipeline
+# Import the data to a DataFrame
+departures_df = spark.read.csv('2015-departures.csv.gz', header=True)
+
+# Remove any duration of 0
+departures_df = departures_df.filter(departures_df['Actual elapsed time (Minutes)'] > 0)
+
+# Add an ID column
+departures_df = departures_df.withColumn('id', F.monotonically_increasing_id())
+
+# Write the file out to JSON format
+departures_df.write.json('output.json', mode='overwrite')
