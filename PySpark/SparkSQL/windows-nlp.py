f54fd0fdc6d2e45d8e42291bc43c49a167c00337
James_McNeill <jmcneill06@outlook.com>
2021-10-29 16:16:30 +0100

    Update windows-nlp.py

--- PySpark/SparkSQL/windows-nlp.py
+++ PySpark/SparkSQL/windows-nlp.py
@@ -18,3 +18,18 @@ exploded_df.show(10)
 
 # Count the resulting number of rows in exploded_df
 print("\nNumber of rows: ", exploded_df.count())
+
+# B. Moving window analysis
+# 1. Creating context window features
+# Word for each row, previous two and subsequent two words
+query = """
+SELECT
+part,
+LAG(word, 2) OVER(PARTITION BY part ORDER BY id) AS w1,
+LAG(word, 1) OVER(PARTITION BY part ORDER BY id) AS w2,
+word AS w3,
+LEAD(word, 1) OVER(PARTITION BY part ORDER BY id) AS w4,
+LEAD(word, 2) OVER(PARTITION BY part ORDER BY id) AS w5
+FROM text
+"""
+spark.sql(query).where("part = 12").show(10)
