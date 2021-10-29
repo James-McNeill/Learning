6b4ccfec161ae5396097c0d5f5d59467c8fe1acd
James_McNeill <jmcneill06@outlook.com>
2021-10-29 12:45:59 +0100

    Update pipelines.py

--- PySpark/CleaningData/pipelines.py
+++ PySpark/CleaningData/pipelines.py
@@ -55,3 +55,18 @@ split_df = split_df.withColumn('height', split_cols.getItem(3))
 
 # Add split_cols as a column
 split_df = split_df.withColumn('split_cols', split_cols)
+
+# 4. Further data parsing
+# Define UDF that will return a list of string items
+def retriever(cols, colcount):
+  # Return a list of dog data
+  return cols[4:colcount]
+
+# Define the method as a UDF
+udfRetriever = F.udf(retriever, ArrayType(StringType()))
+
+# Create a new column using your UDF
+split_df = split_df.withColumn('dog_list', udfRetriever(split_df['split_cols'], split_df['colcount']))
+
+# Remove the original column, split_cols, and the colcount
+split_df = split_df.drop('_c0').drop('split_cols').drop('colcount')
