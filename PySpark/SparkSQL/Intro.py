674dd9fd0728fb495209029e3b63f5afb416a366
James_McNeill <jmcneill06@outlook.com>
2021-10-29 14:59:12 +0100

    Update Intro.py

--- PySpark/SparkSQL/Intro.py
+++ PySpark/SparkSQL/Intro.py
@@ -7,3 +7,9 @@ df = spark.read.csv("trainsched.txt", header=True)
 # Create temporary table called table1
 df.createOrReplaceTempView('table1')
 
+# 2. Inspect the columns within a table
+# Inspect the columns in the table df
+spark.sql("DESCRIBE schedule").show()
+# Alternative options
+# spark.sql("SHOW COLUMNS FROM tablename").show()
+# spark.sql("SELECT * FROM tablename LIMIT 0").show()
