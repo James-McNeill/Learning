eeb2609e03624636567a5c476cd7cfb9eeb91eae
James_McNeill <jmcneill06@outlook.com>
2021-11-01 14:03:35 +0000

    Update logging.py

--- PySpark/SparkSQL/logging.py
+++ PySpark/SparkSQL/logging.py
@@ -16,3 +16,13 @@ logging.warning("The first row of text_df:\n %s", text_df.first())
 
 # Log selected columns of text_df as error message
 logging.error("Selected columns: %s", text_df.select("id", "word"))
+
+# Working with logging statements
+# Uncomment the 5 statements that do NOT trigger text_df
+logging.debug("text_df columns: %s", text_df.columns)
+logging.info("table1 is cached: %s", spark.catalog.isCached(tableName="table1"))
+# logging.warning("The first row of text_df: %s", text_df.first())
+logging.error("Selected columns: %s", text_df.select("id", "word"))
+logging.info("Tables: %s", spark.sql("show tables").collect())
+logging.debug("First row: %s", spark.sql("SELECT * FROM table1 limit 1"))
+# logging.debug("Count: %s", spark.sql("SELECT COUNT(*) AS count FROM table1").collect())
