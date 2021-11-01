d9477b06ede7af0cebae8ea334130731155229a3
James_McNeill <jmcneill06@outlook.com>
2021-11-01 12:49:30 +0000

    Update caching.py

--- PySpark/SparkSQL/caching.py
+++ PySpark/SparkSQL/caching.py
@@ -33,3 +33,15 @@ run(df1, "df1_1st")
 run(df1, "df1_2nd") 
 run(df2, "df2_1st") 
 run(df2, "df2_2nd", elapsed=True)
+
+# 3. Cache and uncache tables
+# List the tables
+print("Tables:\n", spark.catalog.listTables())
+
+# Cache table1 and Confirm that it is cached
+spark.catalog.cacheTable('table1')
+print("table1 is cached: ", spark.catalog.isCached('table1'))
+
+# Uncache table1 and confirm that it is uncached
+spark.catalog.uncacheTable('table1')
+print("table1 is cached: ", spark.catalog.isCached('table1'))
