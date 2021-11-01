0027343132519699b1007ce62903f733c3f2e878
James_McNeill <jmcneill06@outlook.com>
2021-11-01 15:45:24 +0000

    Update text-class-classify-evaluate.py

--- PySpark/SparkSQL/text-class-classify-evaluate.py
+++ PySpark/SparkSQL/text-class-classify-evaluate.py
@@ -6,3 +6,15 @@ testSummary = df_fitted.evaluate(df_testset)
 
 # Print the AUC metric
 print("\ntest AUC: %.3f" % testSummary.areaUnderROC)
+
+# 2. Predict test data
+# Apply the model to the test data
+predictions = df_fitted.transform(df_testset).select(fields)
+
+# Print incorrect if prediction does not match label
+for x in predictions.take(8):
+    print()
+    if x.label != int(x.prediction):
+        print("INCORRECT ==> ")
+    for y in fields:
+        print(y,":", x[y])
