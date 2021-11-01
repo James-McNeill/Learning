ca6e977dbe2718523c0ed8293345ac338dfc7d18
James_McNeill <jmcneill06@outlook.com>
2021-11-01 14:00:53 +0000

    Create logging.py

---
+++ PySpark/SparkSQL/logging.py
@@ -0,0 +1,18 @@
+# Logging with PySpark
+
+# Import module
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                    format='%(levelname)s - %(message)s')
+
+# Log columns of text_df as debug message
+logging.debug("text_df columns: %s", text_df.columns)
+
+# Log whether table1 is cached as info message
+logging.info("table1 is cached: %s", spark.catalog.isCached(tableName="table1"))
+
+# Log first row of text_df as warning message
+logging.warning("The first row of text_df:\n %s", text_df.first())
+
+# Log selected columns of text_df as error message
+logging.error("Selected columns: %s", text_df.select("id", "word"))
