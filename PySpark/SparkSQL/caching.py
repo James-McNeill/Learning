79ce8a70857cfc73ba4488eea65a9d7e437fb43e
James_McNeill <jmcneill06@outlook.com>
2021-11-01 12:46:32 +0000

    Update caching.py

--- PySpark/SparkSQL/caching.py
+++ PySpark/SparkSQL/caching.py
@@ -4,6 +4,7 @@
 # Need to be careful and not cache all DataFrames as the more computationally intensive operations are then the slower the machine nodes will
 # be able to work.
 
+# 1. Cache DataFrames
 # Unpersists df1 and df2 and initializes a timer
 prep(df1, df2) 
 
@@ -18,3 +19,17 @@ run(df2, "df2_2nd", elapsed=True)
 
 # Prove df1 is cached
 print(df1.is_cached)
+
+# 2. Alternative method to cache the DataFrame
+# Unpersist df1 and df2 and initializes a timer
+prep(df1, df2) 
+
+# Persist df2 using memory and disk storage level. This is the default setting. Whenever this is being used it will perform the exact same operations
+# as the cache() method.
+df2.persist(storageLevel=pyspark.StorageLevel.MEMORY_AND_DISK)
+
+# Run actions both dataframes
+run(df1, "df1_1st") 
+run(df1, "df1_2nd") 
+run(df2, "df2_1st") 
+run(df2, "df2_2nd", elapsed=True)
