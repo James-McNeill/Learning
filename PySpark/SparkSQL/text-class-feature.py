f27443e6d3193d3ec7ba7b3a1891a6625ef16251
James_McNeill <jmcneill06@outlook.com>
2021-11-01 14:44:51 +0000

    Create text-class-feature.py

---
+++ PySpark/SparkSQL/text-class-feature.py
@@ -0,0 +1,12 @@
+# Creating features for the text classifier
+
+# 1. Creating a UDF for vector data
+# Selects the first element of a vector column
+first_udf = udf(lambda x:
+            float(x.indices[0]) 
+            if (x and hasattr(x, "toArray") and x.numNonzeros())
+            else 0.0,
+            FloatType())
+
+# Apply first_udf to the output column
+df.select(first_udf("output").alias("result")).show(5)
