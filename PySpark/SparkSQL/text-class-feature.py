4a92f1c616828503988fb849318b7bf8a7d8e4df
James_McNeill <jmcneill06@outlook.com>
2021-11-01 14:48:21 +0000

    Update text-class-feature.py

--- PySpark/SparkSQL/text-class-feature.py
+++ PySpark/SparkSQL/text-class-feature.py
@@ -10,3 +10,10 @@ first_udf = udf(lambda x:
 
 # Apply first_udf to the output column
 df.select(first_udf("output").alias("result")).show(5)
+
+# 2. Applying a UDF to vector data
+# Add label by applying the get_first_udf to output column
+df_new = df.withColumn('label', get_first_udf('output'))
+
+# Show the first five rows 
+df_new.show(5)
