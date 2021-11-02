bc628e26403cc1287f707dba4bac5e57422e4a1f
James_McNeill <jmcneill06@outlook.com>
2021-11-02 11:49:27 +0000

    Update aggregation.r

--- R/DataAnalyst/Dplyr/aggregation.r
+++ R/DataAnalyst/Dplyr/aggregation.r
@@ -1,2 +1,9 @@
 # Aggregating data
 
+# A. Count() verb
+
+# 1. Count and sorting
+# Use count to find the number of counties in each region
+# sort: sorts the data in descending order
+counties_selected %>%
+  count(region, sort = TRUE)
