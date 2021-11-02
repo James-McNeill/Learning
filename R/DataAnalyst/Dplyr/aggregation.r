3686ff90223eb1bf7de4382280ace94796ac596e
James_McNeill <jmcneill06@outlook.com>
2021-11-02 11:51:14 +0000

    Update aggregation.r

--- R/DataAnalyst/Dplyr/aggregation.r
+++ R/DataAnalyst/Dplyr/aggregation.r
@@ -7,3 +7,8 @@
 # sort: sorts the data in descending order
 counties_selected %>%
   count(region, sort = TRUE)
+
+# 2. Count and weighting parameter
+# Find number of counties per state, weighted by citizens, sorted in descending order
+counties_selected %>%
+  count(state, wt = citizens, sort = TRUE)
