4cdd37be7211b4b498e91d44b7bd073ba7f3ddbc
James_McNeill <jmcneill06@outlook.com>
2021-11-02 11:01:58 +0000

    Update basics.r

--- R/DataAnalyst/Dplyr/basics.r
+++ R/DataAnalyst/Dplyr/basics.r
@@ -2,3 +2,8 @@
 
 # 1. Glimpse at a dataset: provides the details of what the overall dataset looks like
 glimpse(counties)
+
+# 2. Select the columns from a dataset using the pipe operator and the select() verb
+counties %>%
+  # Select the columns
+  select(state, county, population, poverty)
