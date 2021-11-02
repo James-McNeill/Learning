6fae7e01a255aa2771e26ca36b2ff3e503fc9ddc
James_McNeill <jmcneill06@outlook.com>
2021-11-02 15:47:09 +0000

    Update grouped-windows.r

--- R/DataAnalyst/Dplyr/grouped-windows.r
+++ R/DataAnalyst/Dplyr/grouped-windows.r
@@ -11,3 +11,14 @@ babynames %>%
   # Find the year each name is most common
   group_by(name) %>%
   top_n(1, fraction)
+
+# 2. Add total and maximum for each name
+babynames %>%
+  # Add columns name_total and name_max for each name
+  group_by(name) %>%
+  mutate(name_total = sum(number),
+         name_max = max(number)) %>%
+  # Ungroup the table 
+  ungroup() %>%
+  # Add the fraction_max column containing the number by the name maximum 
+  mutate(fraction_max = number / name_max)
