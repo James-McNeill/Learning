38653194bbb8dc542e095fecbd193e2fe3d5529d
James_McNeill <jmcneill06@outlook.com>
2021-11-02 16:05:52 +0000

    Update grouped-windows.r

--- R/DataAnalyst/Dplyr/grouped-windows.r
+++ R/DataAnalyst/Dplyr/grouped-windows.r
@@ -41,3 +41,12 @@ babynames_fraction %>%
   group_by(name) %>%
   # Add a ratio column that contains the ratio of fraction between each year 
   mutate(ratio = fraction / lag(fraction))
+
+# 2. Biggest jumps in a name
+babynames_ratios_filtered %>%
+  # Extract the largest ratio from each name 
+  top_n(1, ratio) %>%
+  # Sort the ratio column in descending order 
+  arrange(desc(ratio)) %>%
+  # Filter for fractions greater than or equal to 0.001
+  filter(fraction >= 0.001)
