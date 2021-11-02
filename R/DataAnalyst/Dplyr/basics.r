601d40b8e954ac444c2008ad3cef0faa2eda503d
James_McNeill <jmcneill06@outlook.com>
2021-11-02 11:31:50 +0000

    Update basics.r

--- R/DataAnalyst/Dplyr/basics.r
+++ R/DataAnalyst/Dplyr/basics.r
@@ -23,3 +23,14 @@ counties_selected <- counties %>%
 counties_selected %>%
   # Filter for counties with a population above 1000000
   filter(state == 'California' & population > 1000000)
+
+# 5. Filtering and arranging
+counties_selected <- counties %>%
+  select(state, county, population, private_work, public_work, self_employed)
+
+# Filter for Texas and more than 10000 people; sort in descending order of private_work
+counties_selected %>%
+  # Filter for Texas and more than 10000 people
+  filter(state == 'Texas' & population > 10000) %>%
+  # Sort in descending order of private_work
+  arrange(desc(private_work))
