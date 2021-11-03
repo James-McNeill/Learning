72629df478a67160579c793341e3b864d630fed4
James_McNeill <jmcneill06@outlook.com>
2021-11-03 11:24:53 +0000

    Update left-right-self.r

--- R/DataAnalyst/Dplyr/JoiningData/left-right-self.r
+++ R/DataAnalyst/Dplyr/JoiningData/left-right-self.r
@@ -1,6 +1,22 @@
 # Working with left, right and self joins
 
 # A. Left join
+# 1. Left join by two different variables
 # Combine the star_destroyer and millennium_falcon tables
 millennium_falcon %>%
     left_join(star_destroyer, by = c('part_num', 'color_id'), suffix = c('_falcon', '_star_destroyer'))
+
+# 2. Left joining two sets by color
+# Aggregate Millennium Falcon for the total quantity in each part
+millennium_falcon_colors <- millennium_falcon %>%
+  group_by(color_id) %>%
+  summarize(total_quantity = sum(quantity))
+
+# Aggregate Star Destroyer for the total quantity in each part
+star_destroyer_colors <- star_destroyer %>%
+  group_by(color_id) %>%
+  summarize(total_quantity = sum(quantity))
+
+# Left join the Millennium Falcon colors to the Star Destroyer colors
+millennium_falcon_colors %>%
+  left_join(star_destroyer_colors, by = 'color_id', suffix = c('_falcon', '_star_destroyer'))
