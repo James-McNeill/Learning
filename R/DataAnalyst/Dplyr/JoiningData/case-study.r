6ac8cd90a6088243b5b03cb81a4705e6ac840a0a
James_McNeill <jmcneill06@outlook.com>
2021-11-03 16:19:40 +0000

    Update case-study.r

--- R/DataAnalyst/Dplyr/JoiningData/case-study.r
+++ R/DataAnalyst/Dplyr/JoiningData/case-study.r
@@ -1,5 +1,6 @@
 # Case Study: Working with Stack Overflow
 
+# A. Initial insights
 # 1. Left joining
 questions %>%
   left_join(question_tags, by = c("id" = "question_id")) %>%
@@ -20,3 +21,11 @@ questions_with_tags %>%
 # Using a join, filter for tags that are never on an R question
 tags %>%
     anti_join(question_tags, by = c("id" = "tag_id"))
+
+# B. Reviewing questions and answers
+# 1. Finding the gap
+questions %>%
+	# Inner join questions and answers with proper suffixes
+	inner_join(answers, by = c("id" = "question_id"), suffix = c("_question", "_answer")) %>%
+	# Subtract creation_date_question from creation_date_answer to create gap
+	mutate(gap = as.integer(creation_date_answer - creation_date_question))
