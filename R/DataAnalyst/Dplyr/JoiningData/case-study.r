44885e06bd1ef8908ecd7c89f87b6386e0b47c95
James_McNeill <jmcneill06@outlook.com>
2021-11-03 15:13:19 +0000

    Create case-study.r

---
+++ R/DataAnalyst/Dplyr/JoiningData/case-study.r
@@ -0,0 +1,7 @@
+# Case Study: Working with Stack Overflow
+
+# 1. Left joining
+questions %>%
+  left_join(question_tags, by = c("id" = "question_id")) %>%
+  left_join(tags, by = c("tag_id" = "id")) %>%
+  replace_na(list(tag_name = "only-r"))
