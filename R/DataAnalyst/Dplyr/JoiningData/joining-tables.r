c6fe853fefef05c714316abbd013b467add12892
James_McNeill <jmcneill06@outlook.com>
2021-11-03 11:04:37 +0000

    Update joining-tables.r

--- R/DataAnalyst/Dplyr/JoiningData/joining-tables.r
+++ R/DataAnalyst/Dplyr/JoiningData/joining-tables.r
@@ -4,3 +4,8 @@
 # Use the suffix argument to replace .x and .y suffixes
 parts %>% 
 	inner_join(part_categories, by = c("part_cat_id" = "id"), suffix = c('_part', '_category'))
+
+# 2. One-to-many join
+# Combine the parts and inventory_parts tables. If the two tables have the same variable name then can include only one for the by parameter
+parts %>%
+    inner_join(inventory_parts, by = 'part_num')
