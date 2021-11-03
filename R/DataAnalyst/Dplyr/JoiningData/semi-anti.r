0544c1269066b4c247cc7a453e204e1ee64ac8b6
James_McNeill <jmcneill06@outlook.com>
2021-11-03 14:22:12 +0000

    Create semi-anti.r

---
+++ R/DataAnalyst/Dplyr/JoiningData/semi-anti.r
@@ -0,0 +1,12 @@
+# Semi and anti joins
+# semi_join(): similar to a left join, keeps all in left and only those that match in right
+# anti_join(): similar to a right join, keeps all in right and only those that match in left
+
+# 1. Something in one set but not the other
+# Filter the batwing set for parts that are also in the batmobile set
+batwing %>%
+    semi_join(batmobile, by = "part_num")
+
+# Filter the batwing set for parts that aren't in the batmobile set
+batwing %>%
+    anti_join(batmobile, by = "part_num")
