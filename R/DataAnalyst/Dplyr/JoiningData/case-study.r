300bd84be01e8f9c46f10455d965c974279016f0
James_McNeill <jmcneill06@outlook.com>
2021-11-03 15:24:38 +0000

    Update case-study.r

--- R/DataAnalyst/Dplyr/JoiningData/case-study.r
+++ R/DataAnalyst/Dplyr/JoiningData/case-study.r
@@ -5,3 +5,13 @@ questions %>%
   left_join(question_tags, by = c("id" = "question_id")) %>%
   left_join(tags, by = c("tag_id" = "id")) %>%
   replace_na(list(tag_name = "only-r"))
+
+# 2. Compare scores across tags
+questions_with_tags %>%
+	# Group by tag_name
+	group_by(tag_name) %>%
+	# Get mean score and num_questions
+	summarize(score = mean(score),
+          	  num_questions = n()) %>%
+	# Sort num_questions in descending order
+	arrange(desc(num_questions))
