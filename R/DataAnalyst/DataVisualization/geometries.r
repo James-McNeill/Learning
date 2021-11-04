a034f2f95eb344831a57c5985b62d491a64ea27e
James_McNeill <jmcneill06@outlook.com>
2021-11-04 14:49:48 +0000

    Update geometries.r

--- R/DataAnalyst/DataVisualization/geometries.r
+++ R/DataAnalyst/DataVisualization/geometries.r
@@ -7,3 +7,6 @@ plt_price_vs_carat_by_clarity <- ggplot(diamonds, aes(carat, price, color = clar
 
 # Add a point layer with tiny points
 plt_price_vs_carat_by_clarity + geom_point(alpha = 0.5, shape = ".")
+
+# Set transparency to 0.5. Remove line outlines by adjusting shape
+plt_price_vs_carat_by_clarity + geom_point(alpha = 0.5, shape = 16)
