d17bc8a5a27ed6bade497a282580fe7d55c4e641
James_McNeill <jmcneill06@outlook.com>
2021-11-04 14:54:07 +0000

    Update geometries.r

--- R/DataAnalyst/DataVisualization/geometries.r
+++ R/DataAnalyst/DataVisualization/geometries.r
@@ -10,3 +10,16 @@ plt_price_vs_carat_by_clarity + geom_point(alpha = 0.5, shape = ".")
 
 # Set transparency to 0.5. Remove line outlines by adjusting shape
 plt_price_vs_carat_by_clarity + geom_point(alpha = 0.5, shape = 16)
+
+# 2. Overplotting 2: aligned values. Occurs when one axis is continuous and the other is categorical
+# Plot base
+plt_mpg_vs_fcyl_by_fam <- ggplot(mtcars, aes(fcyl, mpg, color = fam))
+
+# Default points are shown for comparison
+plt_mpg_vs_fcyl_by_fam + geom_point()
+
+# Alter the point positions by jittering, width 0.3
+plt_mpg_vs_fcyl_by_fam + geom_point(position = position_jitter(0.3))
+
+# Now jitter and dodge the point positions
+plt_mpg_vs_fcyl_by_fam + geom_point(position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.3))
