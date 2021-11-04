a6c1ce25cbb63bdbd287584984d36da055a23195
James_McNeill <jmcneill06@outlook.com>
2021-11-04 11:43:08 +0000

    Update Intro.r

--- R/DataAnalyst/DataVisualization/Intro.r
+++ R/DataAnalyst/DataVisualization/Intro.r
@@ -11,3 +11,12 @@ str(mtcars)
 # Execute the following command
 ggplot(mtcars, aes(cyl, mpg)) +
   geom_point()
+
+# 2. Change data column types for plots. Ensuring that the correct data type is being used
+# Converting the numeric column to a categorical column
+# Load the ggplot2 package
+library(ggplot2)
+
+# Change the command below so that cyl is treated as factor. Using the factor() method helps to convert the data type
+ggplot(mtcars, aes(factor(cyl), mpg)) +
+  geom_point()
