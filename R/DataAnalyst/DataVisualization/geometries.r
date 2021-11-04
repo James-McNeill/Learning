035c7a3751bde3a94575ac73ad7739c2e61cfd85
James_McNeill <jmcneill06@outlook.com>
2021-11-04 14:48:54 +0000

    Create geometries.r

---
+++ R/DataAnalyst/DataVisualization/geometries.r
@@ -0,0 +1,9 @@
+# Working with the different geometries
+
+# A. Scatter plots
+# 1. Overplotting 1: large datasets
+# Plot price vs. carat, colored by clarity
+plt_price_vs_carat_by_clarity <- ggplot(diamonds, aes(carat, price, color = clarity))
+
+# Add a point layer with tiny points
+plt_price_vs_carat_by_clarity + geom_point(alpha = 0.5, shape = ".")
