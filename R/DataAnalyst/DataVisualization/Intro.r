3384f53c069e0549f1fb9b4e04b2c4acd22d7d23
James_McNeill <jmcneill06@outlook.com>
2021-11-04 11:40:36 +0000

    Create Intro.r

---
+++ R/DataAnalyst/DataVisualization/Intro.r
@@ -0,0 +1,13 @@
+# Introduction to Data Visualization using ggplot2
+
+# A. Initial plots
+# 1. First plot
+# Load the ggplot2 package
+library(ggplot2)
+
+# Explore the mtcars data frame with str()
+str(mtcars)
+
+# Execute the following command
+ggplot(mtcars, aes(cyl, mpg)) +
+  geom_point()
