96d87b52ff5b5041553224e8e24c5c52ab4ee878
James_McNeill <jmcneill06@outlook.com>
2021-11-04 15:07:13 +0000

    Update geometries.r

--- R/DataAnalyst/DataVisualization/geometries.r
+++ R/DataAnalyst/DataVisualization/geometries.r
@@ -23,3 +23,16 @@ plt_mpg_vs_fcyl_by_fam + geom_point(position = position_jitter(0.3))
 
 # Now jitter and dodge the point positions
 plt_mpg_vs_fcyl_by_fam + geom_point(position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.3))
+
+# 3. Overplotting 3: Low-precision data. Measurements for data points were taken at low resolution level (e.g. mm)
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  # Swap for jitter layer with width 0.1
+  geom_jitter(alpha = 0.5, width = 0.1)
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  # Set the position to jitter
+  geom_point(position = "jitter", alpha = 0.5)
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  # Use a jitter position function with width 0.1
+  geom_point(alpha = 0.5, position = position_jitter(width = 0.1))
