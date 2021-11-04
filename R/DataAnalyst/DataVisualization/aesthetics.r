fb7518dc19b8c9ed1684ab54857a14a5878b12ad
James_McNeill <jmcneill06@outlook.com>
2021-11-04 12:17:09 +0000

    Update aesthetics.r

--- R/DataAnalyst/DataVisualization/aesthetics.r
+++ R/DataAnalyst/DataVisualization/aesthetics.r
@@ -5,3 +5,11 @@
 ggplot(mtcars, aes(wt, mpg, color = fcyl)) +
   # Set the shape and size of the points
   geom_point(shape = 1, size = 4)
+
+# 2. Color Vs Fill attributes
+# shape: different shape values can be displayed. Value 21 allows a circle to be colored with a fill and outline
+# fill: color the inside
+# color: color the outline
+# Map color to fam
+ggplot(mtcars, aes(wt, mpg, fill = fcyl, color = fam)) +
+  geom_point(shape = 21, size = 4, alpha = 0.6)
