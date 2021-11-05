665f02d959e4510be7eed3dcaa6e473a14fa9733
James_McNeill <jmcneill06@outlook.com>
2021-11-05 12:43:37 +0000

    Update intro.py

--- Python/DL/TensorFlow/intro.py
+++ Python/DL/TensorFlow/intro.py
@@ -13,3 +13,18 @@ print('\n The datatype is:', credit_constant.dtype)
 
 # Print constant shape
 print('\n The shape is:', credit_constant.shape)
+
+# 2. Variables.
+# Unlike a constant a variables value can be updated. This will be useful when working with parameter values that change over time
+# Note that the Variable sub-module has been imported from TensorFlow already
+# Define the 1-dimensional variable A1
+A1 = Variable([1, 2, 3, 4])
+
+# Print the variable A1
+print('\n A1: ', A1)
+
+# Convert A1 to a numpy array and assign it to B1
+B1 = A1.numpy()
+
+# Print B1
+print('\n B1: ', B1)
