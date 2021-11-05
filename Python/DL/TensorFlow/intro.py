394970626ff905807d5fb11d0186934545a51f58
James_McNeill <jmcneill06@outlook.com>
2021-11-05 12:52:40 +0000

    Update intro.py

--- Python/DL/TensorFlow/intro.py
+++ Python/DL/TensorFlow/intro.py
@@ -47,3 +47,16 @@ C23 = multiply(A23, B23)
 # Print the tensors C1 and C23
 print('\n C1: {}'.format(C1.numpy()))
 print('\n C23: {}'.format(C23.numpy()))
+
+# 2. Making predictions with matrix multiplication
+# Define features, params, and bill as constants
+features = constant([[2, 24], [2, 26], [2, 57], [1, 37]])
+params = constant([[1000], [150]])
+bill = constant([[3913], [2682], [8617], [64400]])
+
+# Compute billpred using features and params
+billpred = matmul(features, params)
+
+# Compute and print the error
+error = bill - billpred
+print(error.numpy())
