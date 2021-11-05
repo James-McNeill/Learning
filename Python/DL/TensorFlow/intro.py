6853604291ba7bfd2ddb604580c5f7e12f6acb0e
James_McNeill <jmcneill06@outlook.com>
2021-11-05 12:49:05 +0000

    Update intro.py

--- Python/DL/TensorFlow/intro.py
+++ Python/DL/TensorFlow/intro.py
@@ -28,3 +28,22 @@ B1 = A1.numpy()
 
 # Print B1
 print('\n B1: ', B1)
+
+# B. Basic operations
+# 1. Performing element-wise multiplication
+# Note that the sub-modules have been imported: multiply(), constant() and ones_like()
+# Define tensors A1 and A23 as constants
+A1 = constant([1, 2, 3, 4])
+A23 = constant([[1, 2, 3], [1, 6, 4]])
+
+# Define B1 and B23 to have the correct shape
+B1 = ones_like(4,)
+B23 = ones_like(2,3)
+
+# Perform element-wise multiplication
+C1 = multiply(A1, B1)
+C23 = multiply(A23, B23)
+
+# Print the tensors C1 and C23
+print('\n C1: {}'.format(C1.numpy()))
+print('\n C23: {}'.format(C23.numpy()))
