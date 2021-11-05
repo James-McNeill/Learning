47b4909717239455520e56eb0018811b1959a6c1
James_McNeill <jmcneill06@outlook.com>
2021-11-05 16:58:47 +0000

    Update linear-models.py

--- Python/DL/TensorFlow/linear-models.py
+++ Python/DL/TensorFlow/linear-models.py
@@ -83,3 +83,18 @@ def loss_function(intercept, slope, features = size_log, targets = price_log):
 # Compute the loss for different slope and intercept values
 print(loss_function(0.1, 0.1).numpy())
 print(loss_function(0.1, 0.5).numpy())
+
+# 2. Train a linear model
+# Initialize an Adam optimizer
+opt = keras.optimizers.Adam(0.5)
+
+for j in range(100):
+	# Apply minimize, pass the loss function, and supply the variables
+	opt.minimize(lambda: loss_function(intercept, slope), var_list=[intercept, slope])
+
+	# Print every 10th value of the loss
+	if j % 10 == 0:
+		print(loss_function(intercept, slope).numpy())
+
+# Plot data and regression line
+plot_results(intercept, slope)
