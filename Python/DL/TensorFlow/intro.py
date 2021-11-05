b30bbb6edc16b1cbde052215c190a1dd4e0631a0
James_McNeill <jmcneill06@outlook.com>
2021-11-05 14:14:08 +0000

    Update intro.py

--- Python/DL/TensorFlow/intro.py
+++ Python/DL/TensorFlow/intro.py
@@ -68,3 +68,20 @@ gray_vector = reshape(gray_tensor, (784, 1))
 
 # Reshape the color image tensor into a vector
 color_vector = reshape(color_tensor, (2352, 1))
+
+# 2. Optimizing with gradients
+def compute_gradient(x0):
+  	# Define x as a variable with an initial value of x0
+	x = Variable(x0)
+  # Compute the slope using the GradientTape() method
+	with GradientTape() as tape:
+		tape.watch(x)
+        # Define y using the multiply operation
+		y = multiply(x, x)
+    # Return the gradient of y with respect to x
+	return tape.gradient(y, x).numpy()
+
+# Compute and print gradients at x = -1, 1, and 0
+print(compute_gradient(-1.0))
+print(compute_gradient(1.0))
+print(compute_gradient(0.0))
