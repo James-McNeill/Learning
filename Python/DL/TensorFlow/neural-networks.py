7d896b0a6a3e4cdecb88c93ca46cad73688d6a52
James_McNeill <jmcneill06@outlook.com>
2021-11-05 18:02:10 +0000

    Update neural-networks.py

--- Python/DL/TensorFlow/neural-networks.py
+++ Python/DL/TensorFlow/neural-networks.py
@@ -49,3 +49,21 @@ predictions = keras.layers.Dense(1, activation="sigmoid")(dense2)
 print('\n shape of dense1: ', dense1.shape)
 print('\n shape of dense2: ', dense2.shape)
 print('\n shape of predictions: ', predictions.shape)
+
+# B. Activation functions
+# 1. Binary classification
+# Construct input layer from features
+inputs = constant(bill_amounts, float32)
+
+# Define first dense layer
+dense1 = keras.layers.Dense(3, activation='relu')(inputs)
+
+# Define second dense layer
+dense2 = keras.layers.Dense(2, activation='relu')(dense1)
+
+# Define output layer
+outputs = keras.layers.Dense(1, activation="sigmoid")(dense2)
+
+# Print error for first five examples
+error = default[:5] - outputs.numpy()[:5]
+print(error)
