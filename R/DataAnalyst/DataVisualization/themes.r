83bc7a0cce49c10081fd4298b97daa4b30005362
James_McNeill <jmcneill06@outlook.com>
2021-11-05 11:00:21 +0000

    Create themes.r

---
+++ R/DataAnalyst/DataVisualization/themes.r
@@ -0,0 +1,9 @@
+# Working with Themes from scratch
+
+# 1. Themes from scratch
+# View the default plot
+plt_prop_unemployed_over_time
+
+# Remove legend entirely
+plt_prop_unemployed_over_time +
+  theme(legend.position = "none")
