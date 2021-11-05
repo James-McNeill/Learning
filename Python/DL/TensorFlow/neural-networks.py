abf56d6a82ca1059abd1cf8a13bf4b99b7d18321
James_McNeill <jmcneill06@outlook.com>
2021-11-05 17:28:00 +0000

    Update neural-networks.py

--- Python/DL/TensorFlow/neural-networks.py
+++ Python/DL/TensorFlow/neural-networks.py
@@ -19,3 +19,16 @@ product2 = matmul(dense1, weights2)
 prediction = keras.activations.sigmoid(product2 + bias2)
 print('\n prediction: {}'.format(prediction.numpy()[0,0]))
 print('\n actual: 1')
+
+# 2. The low-level approach with multiple examples
+# Compute the product of borrower_features and weights1
+products1 = matmul(borrower_features, weights1)
+
+# Apply a sigmoid activation function to products1 + bias1
+dense1 = keras.activations.sigmoid(products1 + bias1)
+
+# Print the shapes of borrower_features, weights1, bias1, and dense1
+print('\n shape of borrower_features: ', borrower_features.shape)
+print('\n shape of weights1: ', weights1.shape)
+print('\n shape of bias1: ', bias1.shape)
+print('\n shape of dense1: ', dense1.shape)
