d248878647dae85257665b610ad3431cf42d8aba
James_McNeill <jmcneill06@outlook.com>
2021-11-05 14:59:50 +0000

    Update linear-models.py

--- Python/DL/TensorFlow/linear-models.py
+++ Python/DL/TensorFlow/linear-models.py
@@ -65,3 +65,21 @@ def loss_function(scalar, features = features, targets = targets):
 
 # Evaluate the loss function and print the loss
 print(loss_function(scalar).numpy())
+
+# C. Linear regression model
+# 1. Set up a linear regression model
+# Define a linear regression model
+def linear_regression(intercept, slope, features = size_log):
+	return intercept + slope * features
+
+# Set loss_function() to take the variables as arguments
+def loss_function(intercept, slope, features = size_log, targets = price_log):
+	# Set the predicted values
+	predictions = linear_regression(intercept, slope, features)
+    
+    # Return the mean squared error loss
+	return keras.losses.mse(targets, predictions)
+
+# Compute the loss for different slope and intercept values
+print(loss_function(0.1, 0.1).numpy())
+print(loss_function(0.1, 0.5).numpy())
