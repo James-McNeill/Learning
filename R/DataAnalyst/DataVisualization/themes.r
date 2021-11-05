c4c56b24fbc406480d29b9baa855ebe11f91f364
James_McNeill <jmcneill06@outlook.com>
2021-11-05 11:29:55 +0000

    Update themes.r

--- R/DataAnalyst/DataVisualization/themes.r
+++ R/DataAnalyst/DataVisualization/themes.r
@@ -89,3 +89,22 @@ plt_prop_unemployed_over_time +
 # Use the Wall Street Journal theme
 plt_prop_unemployed_over_time +
   theme_wsj()
+
+# 3. Setting themes
+# Save the theme as theme_recession
+theme_recession <- theme(
+  rect = element_rect(fill = "grey92"),
+  legend.key = element_rect(color = NA),
+  axis.ticks = element_blank(),
+  panel.grid = element_blank(),
+  panel.grid.major.y = element_line(color = "white", size = 0.5, linetype = "dotted"),
+  axis.text = element_text(color = "grey25"),
+  plot.title = element_text(face = "italic", size = 16),
+  legend.position = c(0.6, 0.1)
+)
+
+# Combine the Tufte theme with theme_recession
+theme_tufte_recession <- theme_tufte() + theme_recession
+
+# Add the Tufte recession theme to the plot
+plt_prop_unemployed_over_time + theme_tufte_recession
