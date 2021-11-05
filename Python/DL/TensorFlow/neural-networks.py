8f3c0fd1cf7c21ef0aefc29ecb007d987c687e2e
James_McNeill <jmcneill06@outlook.com>
2021-11-05 18:09:46 +0000

    Update neural-networks.py

--- Python/DL/TensorFlow/neural-networks.py
+++ Python/DL/TensorFlow/neural-networks.py
@@ -67,3 +67,19 @@ outputs = keras.layers.Dense(1, activation="sigmoid")(dense2)
 # Print error for first five examples
 error = default[:5] - outputs.numpy()[:5]
 print(error)
+
+# 2. Multiclass classification
+# Construct input layer from borrower features
+inputs = constant(borrower_features, float32)
+
+# Define first dense layer
+dense1 = keras.layers.Dense(10, activation='sigmoid')(inputs)
+
+# Define second dense layer
+dense2 = keras.layers.Dense(8, activation='relu')(dense1)
+
+# Define output layer
+outputs = keras.layers.Dense(6, activation='softmax')(dense2)
+
+# Print first five predictions
+print(outputs.numpy()[:5])
