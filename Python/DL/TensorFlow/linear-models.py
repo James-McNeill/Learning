9a48b9e1419632e43ab2f9d7c25a9efa14d8d906
James_McNeill <jmcneill06@outlook.com>
2021-11-05 14:22:54 +0000

    Create linear-models.py

---
+++ Python/DL/TensorFlow/linear-models.py
@@ -0,0 +1,16 @@
+# Linear models within TensorFlow
+# Working to predict house price's
+
+# A. Input data
+# 1. Load data
+# Import pandas under the alias pd
+import pandas as pd
+
+# Assign the path to a string variable named data_path
+data_path = 'kc_house_data.csv'
+
+# Load the dataset as a dataframe named housing
+housing = pd.read_csv(data_path)
+
+# Print the price column of housing
+print(housing['price'])
