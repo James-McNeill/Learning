0c399b372ee7ce3e253135398b23009e0ae22527
James_McNeill <jmcneill06@outlook.com>
2021-11-06 10:39:51 +0000

    Update neural-networks.py

--- Python/DL/TensorFlow/neural-networks.py
+++ Python/DL/TensorFlow/neural-networks.py
@@ -124,3 +124,16 @@ for j in range(100):
 
 # Print x_1 and x_2 as numpy arrays
 print(x_1.numpy(), x_2.numpy())
+
+# 3. Training a network
+# Define the layer 1 weights
+w1 = Variable(random.normal([23, 7]))
+
+# Initialize the layer 1 bias
+b1 = Variable(ones([7]))
+
+# Define the layer 2 weights
+w2 = Variable(random.normal([7, 1]))
+
+# Define the layer 2 bias
+b2 = Variable(0.0)
