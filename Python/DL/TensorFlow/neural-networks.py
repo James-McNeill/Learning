acc25692421cabe14ccc2f3643fbcc81d9b02dee
James_McNeill <jmcneill06@outlook.com>
2021-11-06 10:44:07 +0000

    Update neural-networks.py

--- Python/DL/TensorFlow/neural-networks.py
+++ Python/DL/TensorFlow/neural-networks.py
@@ -137,3 +137,19 @@ w2 = Variable(random.normal([7, 1]))
 
 # Define the layer 2 bias
 b2 = Variable(0.0)
+
+# 4. Defining the model and loss function
+# Define the model
+def model(w1, b1, w2, b2, features = borrower_features):
+	# Apply relu activation functions to layer 1
+	layer1 = keras.activations.relu(matmul(features, w1) + b1)
+    # Apply dropout rate of 0.25. Works to reduce the chances of overfitting. By randomly dropping 25% of the layer then the model will not overfit the training
+    # dataset as much and should work much better with the test set
+	dropout = keras.layers.Dropout(0.25)(layer1)
+	return keras.activations.sigmoid(matmul(dropout, w2) + b2)
+
+# Define the loss function
+def loss_function(w1, b1, w2, b2, features = borrower_features, targets = default):
+	predictions = model(w1, b1, w2, b2)
+	# Pass targets and predictions to the cross entropy loss
+	return keras.losses.binary_crossentropy(targets, predictions)
