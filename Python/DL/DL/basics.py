b38b56baedf48844fee72adedeae63ec72bf651d
James_McNeill <jmcneill06@outlook.com>
2021-11-08 12:49:51 +0000

    Update basics.py

--- Python/DL/DL/basics.py
+++ Python/DL/DL/basics.py
@@ -21,3 +21,34 @@ output = (hidden_layer_outputs * weights['output']).sum()
 
 # Print output
 print(output)
+
+# B. Activation functions
+# Used to introduce non-linear interactions that occur. Aims to move away from simple linear interactions
+# These functions are applied at each node to convert the inputs into an output value for that node.
+
+# 1. The Rectified Linear Activation Function. Aims to predict only positive values by taking the maximum of input and zero. Don't want to
+# predict negative transaction values
+def relu(input):
+    '''Define your relu activation function here'''
+    # Calculate the value for the output of the relu function: output
+    output = max(input, 0)
+    
+    # Return the value just calculated
+    return(output)
+
+# Calculate node 0 value: node_0_output
+node_0_input = (input_data * weights['node_0']).sum()
+node_0_output = relu(node_0_input)
+
+# Calculate node 1 value: node_1_output
+node_1_input = (input_data * weights['node_1']).sum()
+node_1_output = relu(node_1_input)
+
+# Put node values into array: hidden_layer_outputs
+hidden_layer_outputs = np.array([node_0_output, node_1_output])
+
+# Calculate model output (do not apply relu)
+model_output = (hidden_layer_outputs * weights['output']).sum()
+
+# Print model output
+print(model_output)
