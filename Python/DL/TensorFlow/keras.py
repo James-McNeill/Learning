ff83b1b2a970c19c563f54e88f10f10f34fa7a92
James_McNeill <jmcneill06@outlook.com>
2021-11-08 10:49:26 +0000

    Update keras.py

--- Python/DL/TensorFlow/keras.py
+++ Python/DL/TensorFlow/keras.py
@@ -33,3 +33,22 @@ model.compile('adam', loss='categorical_crossentropy')
 
 # Print a model summary
 print(model.summary())
+
+# 3. Defining a multiple input model
+# The two models are defined using the functional API architecture. The aim here was to train two different models together.
+# The differences between the two models are due to the activation methods, one uses sigmoid and the other is relu
+
+# For model 1, pass the input layer to layer 1 and layer 1 to layer 2
+m1_layer1 = keras.layers.Dense(12, activation='sigmoid')(m1_inputs)
+m1_layer2 = keras.layers.Dense(4, activation='softmax')(m1_layer1)
+
+# For model 2, pass the input layer to layer 1 and layer 1 to layer 2
+m2_layer1 = keras.layers.Dense(12, activation='relu')(m2_inputs)
+m2_layer2 = keras.layers.Dense(4, activation='softmax')(m2_layer1)
+
+# Merge model outputs and define a functional model
+merged = keras.layers.add([m1_layer2, m2_layer2])
+model = keras.Model(inputs=[m1_inputs, m2_inputs], outputs=merged)
+
+# Print a model summary. Shows how the model architecture works. Also outputs a connected to column to ensure users understand where the layers work together
+print(model.summary())
