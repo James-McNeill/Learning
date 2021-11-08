ce0b17a731ccbd93d0e032e12cb473ba6587c1a8
James_McNeill <jmcneill06@outlook.com>
2021-11-08 10:43:54 +0000

    Update keras.py

--- Python/DL/TensorFlow/keras.py
+++ Python/DL/TensorFlow/keras.py
@@ -16,3 +16,20 @@ model.add(keras.layers.Dense(4, activation='softmax'))
 
 # Print the model architecture. This method allows us to see the model details prior to compiling
 print(model.summary())
+
+# 2. Compiling a sequential model
+# Building a model with fewer layers but more nodes in place. The dropout method is to reduce the chances for overfitting
+# Define the first dense layer
+model.add(keras.layers.Dense(16, activation='sigmoid', input_shape=(784,)))
+
+# Apply dropout to the first layer's output
+model.add(keras.layers.Dropout(0.25))
+
+# Define the output layer
+model.add(keras.layers.Dense(4, activation='softmax'))
+
+# Compile the model
+model.compile('adam', loss='categorical_crossentropy')
+
+# Print a model summary
+print(model.summary())
