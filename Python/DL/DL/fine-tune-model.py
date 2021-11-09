b1bdf591778d6b476527498a5544a8bb2b63ee34
James_McNeill <jmcneill06@outlook.com>
2021-11-09 16:32:34 +0000

    Update fine-tune-model.py

--- Python/DL/DL/fine-tune-model.py
+++ Python/DL/DL/fine-tune-model.py
@@ -68,3 +68,32 @@ early_stopping_monitor = EarlyStopping(patience=2)
 # epochs: default value is 10. So increasing the amount will potentially increase the number of model builds
 # callbacks: have to be provided as a list. A number of options can be assigned
 model.fit(predictors, target, epochs=30, validation_split=0.3, callbacks=[early_stopping_monitor])
+
+# 3. Experiment with wider networks
+# Define early_stopping_monitor
+early_stopping_monitor = EarlyStopping(patience=2)
+
+# Create the new model: model_2
+model_2 = Sequential()
+
+# Add the first and second layers
+model_2.add(Dense(100, activation='relu', input_shape=input_shape))
+model_2.add(Dense(100, activation='relu'))
+
+# Add the output layer
+model_2.add(Dense(2, activation='softmax'))
+
+# Compile model_2
+model_2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit model_1
+model_1_training = model_1.fit(predictors, target, epochs=15, validation_split=0.2, callbacks=[early_stopping_monitor], verbose=False)
+
+# Fit model_2
+model_2_training = model_2.fit(predictors, target, epochs=15, validation_split=0.2, callbacks=[early_stopping_monitor], verbose=False)
+
+# Create the plot. Comparison of a shallow (model_1) and a deeper (model_2) networks
+plt.plot(model_1_training.history['val_loss'], 'r', model_2_training.history['val_loss'], 'b')
+plt.xlabel('Epochs')
+plt.ylabel('Validation score')
+plt.show()
