eac91f2ec80c1b0ce868609151a4f2e1fa814ad6
James_McNeill <jmcneill06@outlook.com>
2021-11-09 16:13:44 +0000

    Create fine-tune-model.py

---
+++ Python/DL/DL/fine-tune-model.py
@@ -0,0 +1,27 @@
+# Fine tuning the keras model
+
+# A. Model Optimization
+# 1. Changing optimization parameters
+# Import the SGD optimizer
+from keras.optimizers import SGD
+
+# Create list of learning rates: lr_to_test. As the learning rate moves the model performance gains change as well.
+# A very small learning rate shows some gains. An average learning rate show the optimal gains. A large learning rate
+# shows no improvements for this classification exercise
+lr_to_test = [0.000001, 0.01, 1]
+
+# Loop over learning rates
+for lr in lr_to_test:
+    print('\n\nTesting model with learning rate: %f\n'%lr )
+    
+    # Build new model to test, unaffected by previous models. Function that creates a new model each time to fairly compare the learning rate parameter changes
+    model = get_new_model()
+    
+    # Create SGD optimizer with specified learning rate: my_optimizer
+    my_optimizer = SGD(lr=lr)
+    
+    # Compile the model
+    model.compile(optimizer=my_optimizer, loss='categorical_crossentropy')
+    
+    # Fit the model
+    model.fit(predictors, target)
