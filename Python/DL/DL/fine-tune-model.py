8e56ebd2bf81d3e6a918e86ef11d0b47d524c881
James_McNeill <jmcneill06@outlook.com>
2021-11-09 16:19:30 +0000

    Update fine-tune-model.py

--- Python/DL/DL/fine-tune-model.py
+++ Python/DL/DL/fine-tune-model.py
@@ -25,3 +25,21 @@ for lr in lr_to_test:
     
     # Fit the model
     model.fit(predictors, target)
+
+# B. Model Validation
+# 1. Evaluating model accuracy on validation dataset
+# Save the number of columns in predictors: n_cols
+n_cols = predictors.shape[1]
+input_shape = (n_cols,)
+
+# Specify the model
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape = input_shape))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+hist = model.fit(predictors, target, validation_split=0.3)
