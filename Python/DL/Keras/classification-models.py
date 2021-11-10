3e18f86223f7887e2e49e9545c0d8ead54f5b6c3
James_McNeill <jmcneill06@outlook.com>
2021-11-10 14:18:32 +0000

    Update classification-models.py

--- Python/DL/Keras/classification-models.py
+++ Python/DL/Keras/classification-models.py
@@ -18,3 +18,20 @@ print('Dataset stats: \n', banknotes.describe())
 
 # Count the number of observations per class
 print('Observations per class: \n', banknotes['class'].value_counts())
+
+# 2. Create binary classification model
+# Import the sequential model and dense layer
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Create a sequential model
+model = Sequential()
+
+# Add a dense layer 
+model.add(Dense(1, input_shape=(4,), activation='sigmoid'))
+
+# Compile your model
+model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# Display a summary of your model
+model.summary()
