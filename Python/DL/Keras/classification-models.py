c29b7403185d1f3c6301dfaab363e80e19ff31e8
James_McNeill <jmcneill06@outlook.com>
2021-11-10 15:17:36 +0000

    Update classification-models.py

--- Python/DL/Keras/classification-models.py
+++ Python/DL/Keras/classification-models.py
@@ -197,3 +197,22 @@ monitor_val_acc = EarlyStopping(monitor='val_acc',
 model.fit(X_train, y_train, 
            epochs=1000, validation_data=(X_test, y_test),
            callbacks= [monitor_val_acc])
+
+# 3. Combination of callbacks
+# ModelCheckpoint: allows the model to store the best only that has been produced so far. This is great as in the future we can start from this
+# point as well for future iterations of the model development
+
+# Import the EarlyStopping and ModelCheckpoint callbacks
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+# Early stop on validation accuracy
+monitor_val_acc = EarlyStopping(monitor = 'val_acc', patience = 3)
+
+# Save the best model as best_banknote_model.hdf5
+modelCheckpoint = ModelCheckpoint('best_banknote_model.hdf5', save_best_only = True)
+
+# Fit your model for a stupid amount of epochs
+h_callback = model.fit(X_train, y_train,
+                    epochs = 1000000000000,
+                    callbacks = [monitor_val_acc, modelCheckpoint],
+                    validation_data = (X_test, y_test))
