54375a6dcd910407a1ff04ab3cff0086bbe0a29b
James_McNeill <jmcneill06@outlook.com>
2021-11-10 14:15:51 +0000

    Update classification-models.py

--- Python/DL/Keras/classification-models.py
+++ Python/DL/Keras/classification-models.py
@@ -1,3 +1,20 @@
 # Developing neural network models to perform classification analysis
 
 # A. Binary classification
+# Example classification problem relates to banknote authentication (real / fake)
+
+# 1. Initial exploration of the dataset
+# Import seaborn
+import seaborn as sns
+
+# Use pairplot and set the hue to be our class column
+sns.pairplot(banknotes, hue='class') 
+
+# Show the plot
+plt.show()
+
+# Describe the data
+print('Dataset stats: \n', banknotes.describe())
+
+# Count the number of observations per class
+print('Observations per class: \n', banknotes['class'].value_counts())
