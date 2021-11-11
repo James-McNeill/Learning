12706978b4abdf1639e9a178dc379814dd920adb
James_McNeill <jmcneill06@outlook.com>
2021-11-11 09:58:59 +0000

    Update model-performance.py

--- Python/DL/Keras/model-performance.py
+++ Python/DL/Keras/model-performance.py
@@ -1,4 +1,20 @@
 # Model performance review
 
 # A. Learning curves
-# 1. 
+# Reviewing the MNIST digit data images showing numbers from 0 to 9
+
+# 1. Compile the model
+# Instantiate a Sequential model
+model = Sequential()
+
+# Input and hidden layer with input_shape, 16 neurons, and relu 
+model.add(Dense(16, input_shape = (64,), activation = 'relu'))
+
+# Output layer with 10 neurons (one per digit) and softmax
+model.add(Dense(10, activation='softmax'))
+
+# Compile your model
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+# Test if your model is well assembled by predicting before training
+print(model.predict(X_train))
