7fd9e0b3a1a6a82882a6faf8f8eb447e41d8d520
James_McNeill <jmcneill06@outlook.com>
2021-11-11 16:34:59 +0000

    Update advanced-architecture.py

--- Python/DL/Keras/advanced-architecture.py
+++ Python/DL/Keras/advanced-architecture.py
@@ -108,3 +108,20 @@ axs[0].matshow(activations[0,:,:,14], cmap = 'viridis')
 # Do the same but for the 18th filter now
 axs[1].matshow(activations[0,:,:,17], cmap = 'viridis')
 plt.show()
+
+# 3. Preparing the input image
+# Import image and preprocess_input
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input
+
+# Load the image with the right target size for your model
+img = image.load_img(img_path, target_size=(224, 224))
+
+# Turn it into an array
+img_array = image.img_to_array(img)
+
+# Expand the dimensions of the image, this is so that it fits the expected model input format
+img_expanded = np.expand_dims(img_array, axis = 0)
+
+# Pre-process the img in the same way original images were
+img_ready = preprocess_input(img_expanded)
