db73a701ae4ed5365dcc51b1218daec480cc0785
James_McNeill <jmcneill06@outlook.com>
2021-11-11 16:30:55 +0000

    Update advanced-architecture.py

--- Python/DL/Keras/advanced-architecture.py
+++ Python/DL/Keras/advanced-architecture.py
@@ -91,3 +91,20 @@ model.add(Flatten())
 
 # Add as many outputs as classes with softmax activation
 model.add(Dense(10, activation = 'softmax'))
+
+# 2. Looking at convolutional layers
+# Obtain a reference to the outputs of the first layer
+first_layer_output = model.layers[0].output
+
+# Build a model using the model's input and the first layer output
+first_layer_model = Model(inputs = model.layers[0].input, outputs = first_layer_output)
+
+# Use this model to predict on X_test
+activations = first_layer_model.predict(X_test)
+
+# Plot the activations of first digit of X_test for the 15th filter
+axs[0].matshow(activations[0,:,:,14], cmap = 'viridis')
+
+# Do the same but for the 18th filter now
+axs[1].matshow(activations[0,:,:,17], cmap = 'viridis')
+plt.show()
