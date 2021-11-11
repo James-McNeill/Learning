c81f305abb5749ffea30cb41c02c923d968ef015
James_McNeill <jmcneill06@outlook.com>
2021-11-11 10:35:43 +0000

    Update model-performance.py

--- Python/DL/Keras/model-performance.py
+++ Python/DL/Keras/model-performance.py
@@ -18,3 +18,40 @@ model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = [
 
 # Test if your model is well assembled by predicting before training
 print(model.predict(X_train))
+
+# 2. Check for model overfitting. Review the train and test loss curves
+def plot_loss(loss,val_loss):
+  plt.figure()
+  plt.plot(loss)
+  plt.plot(val_loss)
+  plt.title('Model loss')
+  plt.ylabel('Loss')
+  plt.xlabel('Epoch')
+  plt.legend(['Train', 'Test'], loc='upper right')
+  plt.show()
+
+# Train your model for 60 epochs, using X_test and y_test as validation data
+h_callback = model.fit(X_train, y_train, epochs = 60, validation_data = (X_test, y_test), verbose=0)
+
+# Extract from the h_callback object loss and val_loss to plot the learning curve
+plot_loss(h_callback.history['loss'], h_callback.history['val_loss'])
+
+# 3. Is more data required? Understanding if more training samples are required to improve model performance helps to train the model better
+# Initial weights variable is extracted from the earlier model fit
+initial_weights = model.get_weights()
+
+# training_sizes: list of four training size values increasing over time
+for size in training_sizes:
+  	# Get a fraction of training data (we only care about the training data)
+    X_train_frac, y_train_frac = X_train[:size], y_train[:size]
+
+    # Reset the model to the initial weights and train it on the new training data fraction
+    model.set_weights(initial_weights)
+    model.fit(X_train_frac, y_train_frac, epochs = 50, callbacks = [early_stop])
+
+    # Evaluate and store both: the training data fraction and the complete test set results
+    train_accs.append(model.evaluate(X_train_frac, y_train_frac)[1])
+    test_accs.append(model.evaluate(X_test, y_test)[1])
+    
+# Plot train vs test accuracies
+plot_results(train_accs, test_accs)
