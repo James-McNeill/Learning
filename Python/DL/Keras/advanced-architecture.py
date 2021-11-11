ad984eea13dfc41c2b994e36e87fdcce7b77fccb
James_McNeill <jmcneill06@outlook.com>
2021-11-11 16:06:19 +0000

    Update advanced-architecture.py

--- Python/DL/Keras/advanced-architecture.py
+++ Python/DL/Keras/advanced-architecture.py
@@ -18,3 +18,24 @@ inp_to_out = K.function([inp], [out])
 
 # Print the results of passing X_test through the 1st layer
 print(inp_to_out([X_test]))
+
+# 2. Neural separation
+for i in range(0, 21):
+  	# Train model for 1 epoch
+    h = model.fit(X_train, y_train, batch_size = 16, epochs = 1, verbose = 0)
+    if i%4==0: 
+      # Get the output of the first layer
+      layer_output = inp_to_out([X_test])[0]
+      
+      # Evaluate model accuracy for this epoch
+      test_accuracy = model.evaluate(X_test, y_test)[1] 
+      
+      # Plot 1st vs 2nd neuron output
+      plot()
+
+# Plot function used to return the visual outputs
+def plot():
+  fig, ax = plt.subplots()
+  plt.scatter(layer_output[:, 0], layer_output[:, 1],c = y_test,edgecolors='none')
+  plt.title('Epoch: {}, Test Accuracy: {:3.1f} %'.format(i+1, test_accuracy * 100.0))
+  plt.show()
