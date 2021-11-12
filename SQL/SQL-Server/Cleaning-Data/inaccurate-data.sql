ed0721fe9fabef5718b99ec972d6321dc1b6fbeb
James_McNeill <jmcneill06@outlook.com>
2021-11-12 14:22:36 +0000

    Update inaccurate-data.sql

--- SQL/SQL-Server/Cleaning-Data/inaccurate-data.sql
+++ SQL/SQL-Server/Cleaning-Data/inaccurate-data.sql
@@ -9,3 +9,12 @@ WHERE num_ratings NOT BETWEEN 0 AND 5000
 SELECT * FROM series
 -- Detect the out of range values
 WHERE num_ratings < 0 OR num_ratings > 5000
+
+-- 2. Excluding out of range values
+SELECT * FROM series
+-- Exclude the out of range values
+WHERE num_ratings BETWEEN 0 AND 5000
+
+SELECT * FROM series
+-- Exclude the out of range values
+WHERE num_ratings >= 0 AND num_ratings <= 5000
