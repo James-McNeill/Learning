8cb25845fa7a4d4569f6fea4751d2ddb3d903710
James_McNeill <jmcneill06@outlook.com>
2021-11-12 14:20:42 +0000

    Update inaccurate-data.sql

--- SQL/SQL-Server/Cleaning-Data/inaccurate-data.sql
+++ SQL/SQL-Server/Cleaning-Data/inaccurate-data.sql
@@ -2,3 +2,10 @@
 
 -- A. Out of range and inaccurate
 -- 1. Out of range
+SELECT * FROM series
+-- Detect the out of range values
+WHERE num_ratings NOT BETWEEN 0 AND 5000
+
+SELECT * FROM series
+-- Detect the out of range values
+WHERE num_ratings < 0 OR num_ratings > 5000
