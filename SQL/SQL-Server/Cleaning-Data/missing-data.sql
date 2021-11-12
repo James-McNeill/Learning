2f9528bae2286266790c930ad66f7dfd28178f0d
James_McNeill <jmcneill06@outlook.com>
2021-11-12 12:20:21 +0000

    Update missing-data.sql

--- SQL/SQL-Server/Cleaning-Data/missing-data.sql
+++ SQL/SQL-Server/Cleaning-Data/missing-data.sql
@@ -25,3 +25,13 @@ SELECT *
 FROM airports
 -- Return only the rows where airport_city is missing
 WHERE airport_city = ''
+
+-- 3. Fill missing values with isnull()
+SELECT
+  airport_code,
+  airport_name,
+  -- Replace missing values for airport_city with 'Unknown'
+  ISNULL(airport_city, 'Unknown') AS airport_city,
+  -- Replace missing values for airport_state with 'Unknown'
+  ISNULL(airport_state, 'Unknown') AS airport_state
+FROM airports
