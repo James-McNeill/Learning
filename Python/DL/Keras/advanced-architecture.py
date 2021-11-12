88faf2c938b4fe258193eba4671b07565bbd037b
James_McNeill <jmcneill06@outlook.com>
2021-11-12 10:30:23 +0000

    Update advanced-architecture.py

--- Python/DL/Keras/advanced-architecture.py
+++ Python/DL/Keras/advanced-architecture.py
@@ -135,3 +135,21 @@ preds = model.predict(img_ready)
 
 # Decode the first 3 predictions
 print('Predicted:', decode_predictions(preds, top=3)[0])
+
+# C. LSTMs
+# 1. Text prediction with LSTMs
+# Split text into an array of words 
+words = text.split()
+
+# Make sentences of 4 words each, moving one word at a time
+sentences = []
+for i in range(4, len(words)):
+  sentences.append(' '.join(words[i-4:i]))
+
+# Instantiate a Tokenizer, then fit it on the sentences
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sentences)
+
+# Turn sentences into a sequence of numbers
+sequences = tokenizer.texts_to_sequences(sentences)
+print("Sentences: \n {} \n Sequences: \n {}".format(sentences[:5],sequences[:5]))
