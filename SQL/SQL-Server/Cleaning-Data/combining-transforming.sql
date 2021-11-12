572ad3e37ca183bb6506bc92ea1a0837eaec0941
James_McNeill <jmcneill06@outlook.com>
2021-11-12 15:02:49 +0000

    Update combining-transforming.sql

--- SQL/SQL-Server/Cleaning-Data/combining-transforming.sql
+++ SQL/SQL-Server/Cleaning-Data/combining-transforming.sql
@@ -58,3 +58,31 @@ SELECT
     -- Extract the name of the state
     RIGHT(city_state, CHARINDEX(' ,', REVERSE(city_state)) - 1) AS state
 FROM clients_split
+
+-- C. Pivot or unpivot
+-- 1. Turning rows into columns
+SELECT
+	year_of_sale,
+    -- Select the pivoted columns
+	notebooks, 
+	pencils, 
+	crayons
+FROM
+   (SELECT 
+		SUBSTRING(product_name_units, 1, charindex('-', product_name_units)-1) product_name, 
+		CAST(SUBSTRING(product_name_units, charindex('-', product_name_units)+1, len(product_name_units)) AS INT) units,	
+		year_of_sale
+	FROM paper_shop_monthly_sales) sales
+-- Sum the units for column that contains the values that will be column headers
+PIVOT (SUM(units) FOR product_name IN (notebooks, pencils, crayons))
+-- Give the alias name
+AS paper_shop_pivot
+
+-- 2. Turning columns into rows. Note that the unique data that created the original pivot table can not be reproduced by using unpivot
+SELECT * FROM pivot_sales
+-- Use the operator to convert columns into rows
+UNPIVOT
+	-- The resulting column that will contain the turned columns into rows
+	(units FOR product_name IN (notebooks, pencils, crayons))
+-- Give the alias name
+AS unpivot_sales
