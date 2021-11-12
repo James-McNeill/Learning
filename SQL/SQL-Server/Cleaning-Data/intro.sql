dfed274741d10eac4508fcf85ac92063b7a1303e
James_McNeill <jmcneill06@outlook.com>
2021-11-12 10:56:27 +0000

    Update intro.sql

--- SQL/SQL-Server/Cleaning-Data/intro.sql
+++ SQL/SQL-Server/Cleaning-Data/intro.sql
@@ -1,6 +1,7 @@
 -- Working with SQL Server to clean datasets
 
 -- A. Unifying formats
+-- 1. Using the replicate method
 SELECT 
 	-- Concat the strings
 	CONCAT(
@@ -12,6 +13,21 @@ SELECT
 		', ', 
 		airport_code)
 	AS registration_code
+FROM flight_statistics
+-- Filter registers with more than 100 delays
+WHERE delayed > 100
+
+-- 2. Using the format method
+SELECT 
+    -- Concat the strings
+	CONCAT(
+		carrier_code, 
+		' - ', 
+        -- Format the code. Input value to method has to be numeric or date/time 
+		FORMAT(CAST(registration_code AS INT), '0000000'),
+		', ', 
+		airport_code
+	) AS registration_code
 FROM flight_statistics
 -- Filter registers with more than 100 delays
 WHERE delayed > 100
