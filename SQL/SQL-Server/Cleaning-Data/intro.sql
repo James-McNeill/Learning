9725c5a94eb063b1e0102a4b69c1c96631da5a36
James_McNeill <jmcneill06@outlook.com>
2021-11-12 10:53:16 +0000

    Create intro.sql

---
+++ SQL/SQL-Server/Cleaning-Data/intro.sql
@@ -0,0 +1,17 @@
+-- Working with SQL Server to clean datasets
+
+-- A. Unifying formats
+SELECT 
+	-- Concat the strings
+	CONCAT(
+		carrier_code, 
+		' - ', 
+      	-- Replicate zeros
+		REPLICATE('0', 9 - LEN(registration_code)), 
+		registration_code, 
+		', ', 
+		airport_code)
+	AS registration_code
+FROM flight_statistics
+-- Filter registers with more than 100 delays
+WHERE delayed > 100
