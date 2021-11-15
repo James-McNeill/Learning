b8d36766cc1ff301798459901c89b757f3b34c2f
James_McNeill <jmcneill06@outlook.com>
2021-11-15 15:09:03 +0000

    Update statistics.r

--- R/DataScientist/IntermediateDataVis/statistics.r
+++ R/DataScientist/IntermediateDataVis/statistics.r
@@ -30,3 +30,27 @@ ggplot(mtcars, aes(x = wt, y = mpg, color = fcyl)) +
   geom_point() +
   stat_smooth(method = "lm", se = FALSE) +
   stat_smooth(aes(group = 1), method = "lm", se = FALSE) # shows a dummy curve that has used all of the data points for one curve
+
+# 3. Modifying stat_smooth
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  # Add 3 smooth LOESS stats, varying span & color. Providing span parameter relates to the number of data points that are using. Shorter
+  # span will include more of the noise across the time series and could result in some overfitting
+  stat_smooth(se = FALSE, span = 0.9, color = "red") +
+  stat_smooth(se = FALSE, span = 0.6, color = "green") +
+  stat_smooth(se = FALSE, span = 0.3, color = "blue")
+
+# Amend the plot to color by fcyl
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  # Add a smooth LOESS stat, no ribbon
+  stat_smooth(se = FALSE) +
+  # Add a smooth lin. reg. stat, no ribbon
+  stat_smooth(method = "lm", se = FALSE)
+
+# Amend the plot
+ggplot(mtcars, aes(x = wt, y = mpg, color = fcyl)) +
+  geom_point() +
+  # Map color to dummy variable "All"
+  stat_smooth(se = FALSE, aes(color = "All")) +
+  stat_smooth(method = "lm", se = FALSE)
