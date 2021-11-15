1d9b3f9ea48b190f9ef0ffeb2dec9f05e76864bf
James_McNeill <jmcneill06@outlook.com>
2021-11-15 14:50:20 +0000

    Update statistics.r

--- R/DataScientist/IntermediateDataVis/statistics.r
+++ R/DataScientist/IntermediateDataVis/statistics.r
@@ -2,3 +2,18 @@
 
 # A. stats with geoms
 # 1. Smoothing
+# Amend the plot to add a smooth layer
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth()
+
+# Amend the plot. Use lin. reg. smoothing; turn off std err ribbon
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# Amend the plot. Swap geom_smooth() for stat_smooth(). Shows that the linear model (lm) can be created with either method
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE)
+
