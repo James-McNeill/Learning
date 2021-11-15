e931521ac2dca486e048a6ed8936b0aae110adcd
James_McNeill <jmcneill06@outlook.com>
2021-11-15 14:55:00 +0000

    Update statistics.r

--- R/DataScientist/IntermediateDataVis/statistics.r
+++ R/DataScientist/IntermediateDataVis/statistics.r
@@ -17,3 +17,16 @@ ggplot(mtcars, aes(x = wt, y = mpg)) +
   geom_point() +
   stat_smooth(method = "lm", se = FALSE)
 
+# 2. Grouping variables
+# Using mtcars, plot mpg vs. wt, colored by fcyl
+ggplot(mtcars, aes(x = wt, y = mpg, color = fcyl)) +
+  # Add a point layer
+  geom_point() +
+  # Add a smooth lin reg stat, no ribbon
+  stat_smooth(method = "lm", se = FALSE)
+
+# Amend the plot to add another smooth layer with dummy grouping
+ggplot(mtcars, aes(x = wt, y = mpg, color = fcyl)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE) +
+  stat_smooth(aes(group = 1), method = "lm", se = FALSE) # shows a dummy curve that has used all of the data points for one curve
