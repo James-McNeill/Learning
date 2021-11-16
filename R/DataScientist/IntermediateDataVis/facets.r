c232740ca9394821d64133dd2af0235c429b04a6
James_McNeill <jmcneill06@outlook.com>
2021-11-16 12:53:43 +0000

    Update facets.r

--- R/DataScientist/IntermediateDataVis/facets.r
+++ R/DataScientist/IntermediateDataVis/facets.r
@@ -2,3 +2,18 @@
 
 # A. Facets layer
 # 1. Basics
+ggplot(mtcars, aes(wt, mpg)) + 
+  geom_point() +
+  # Facet rows by am
+  facet_grid(rows = vars(am))
+
+ggplot(mtcars, aes(wt, mpg)) + 
+  geom_point() +
+  # Facet columns by cyl
+  facet_grid(cols = vars(cyl))
+
+ggplot(mtcars, aes(wt, mpg)) + 
+  geom_point() +
+  # Facet rows by am and columns by cyl
+  facet_grid(rows = vars(am), cols = vars(cyl))
+
