917926b5616933cabcdbe3be8ad3acb784dd2c11
James_McNeill <jmcneill06@outlook.com>
2021-11-16 12:44:45 +0000

    Update coordinates.r

--- R/DataScientist/IntermediateDataVis/coordinates.r
+++ R/DataScientist/IntermediateDataVis/coordinates.r
@@ -117,3 +117,19 @@ ggplot(mtcars, aes(car, wt)) +
   geom_point() +
   labs(x = "car", y = "weight") +
   coord_flip()
+
+# D. Polar coordinates
+# 1. Pie charts
+# Run the code, view the plot, then update it
+ggplot(mtcars, aes(x = 1, fill = fcyl)) +
+  geom_bar() +
+  # Add a polar coordinate system
+  coord_polar(theta = "y")
+
+ggplot(mtcars, aes(x = 1, fill = fcyl)) +
+  # Reduce the bar width to 0.1
+  geom_bar(width = 0.1) +
+  coord_polar(theta = "y") +
+  # Add a continuous x scale from 0.5 to 1.5
+  scale_x_continuous(limits = c(0.5, 1.5))
+
