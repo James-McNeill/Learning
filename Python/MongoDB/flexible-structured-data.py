63e661eab67455e74a991c338c04c5344f316541
James_McNeill <jmcneill06@outlook.com>
2021-11-17 15:05:45 +0000

    Update flexible-structured-data.py

--- Python/MongoDB/flexible-structured-data.py
+++ Python/MongoDB/flexible-structured-data.py
@@ -31,3 +31,23 @@ print(db_names)
 # Save a list of names of the collections managed by the "nobel" database
 nobel_coll_names = client.nobel.list_collection_names()
 print(nobel_coll_names)
+
+# 3. List fields of a document
+# Connect to the "nobel" database
+db = client.nobel
+
+# Retrieve sample prize and laureate documents
+prize = db.prizes.find_one()
+laureate = db.laureates.find_one()
+
+# Print the sample prize and laureate documents
+print(prize)
+print(laureate)
+print(type(laureate))
+
+# Get the fields present in each type of document
+prize_fields = list(prize.keys())
+laureate_fields = list(laureate.keys())
+
+print(prize_fields)
+print(laureate_fields)
