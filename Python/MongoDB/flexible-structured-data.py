9615b86685d61bff27b9f8818dbf9a23a3537719
James_McNeill <jmcneill06@outlook.com>
2021-11-17 14:54:00 +0000

    Create flexible-structured-data.py

---
+++ Python/MongoDB/flexible-structured-data.py
@@ -0,0 +1,5 @@
+# Introduction to using MongoDB
+
+# A MongoDB database can consist of several collections. Collections, in turn, consist of documents, which store the data.
+# Recall that you can access databases by name as attributes of the client, like client.my_database (a connected client is 
+# already provided to you as client). Similarly, collections can be accessed by name as attributes of databases (my_database.my_collection).
