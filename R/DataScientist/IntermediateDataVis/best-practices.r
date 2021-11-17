0607b0084252dcf6a7a51f821d09014c0385935c
James_McNeill <jmcneill06@outlook.com>
2021-11-17 10:25:12 +0000

    Update best-practices.r

--- R/DataScientist/IntermediateDataVis/best-practices.r
+++ R/DataScientist/IntermediateDataVis/best-practices.r
@@ -8,3 +8,13 @@ ggplot(mtcars, aes(x = fcyl, y = wt)) +
   stat_summary(fun.y = mean, geom = "bar", fill = "skyblue") +
   # Add an errorbar summary stat std deviation limits
   stat_summary(fun.data = mean_sdl, fun.args = list(mult = 1), geom = "errorbar", width = 0.1)
+
+# 2. Position dodging
+# Define a dodge position object with width 0.9
+posn_d <- position_dodge(width = 0.9)
+
+# For each summary stat, update the position to posn_d
+ggplot(mtcars, aes(x = fcyl, y = wt, color = fam, fill = fam)) +
+  stat_summary(fun.y = mean, geom = "bar", position = posn_d, alpha = 0.5) +
+  stat_summary(fun.data = mean_sdl, fun.args = list(mult = 1), width = 0.1, position = posn_d, geom = "errorbar")
+
