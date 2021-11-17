ac827993457e69e3d13dd514e5d8965bbbf43841
James_McNeill <jmcneill06@outlook.com>
2021-11-17 11:02:51 +0000

    Update best-practices.r

--- R/DataScientist/IntermediateDataVis/best-practices.r
+++ R/DataScientist/IntermediateDataVis/best-practices.r
@@ -80,3 +80,47 @@ ggplot(barley, aes(x = year, y = yield, color = site, group = site, fill = site)
   stat_summary(fun.y = mean, geom = "line") +
   # Add a ribbon summary stat with 10% opacity, no color
   stat_summary(fun.data = mean_sdl, fun.args = list(mult = 1), geom = "ribbon", alpha = 0.1, color = NA)
+
+# C. Typical problems with plots. Not enough details are included when creating the plots
+# Initial plot
+growth_by_dose <- ggplot(TG, aes(dose, len, color = supp)) +
+  stat_summary(fun.data = mean_sdl,
+               fun.args = list(mult = 1),
+               position = position_dodge(0.1)) +
+  theme_classic()
+
+# View plot
+growth_by_dose
+
+# Change type
+TG$dose <- as.numeric(as.character(TG$dose))
+
+# Plot
+growth_by_dose <- ggplot(TG, aes(dose, len, color = supp)) +
+  stat_summary(fun.data = mean_sdl,
+               fun.args = list(mult = 1),
+               position = position_dodge(0.2)) +
+  theme_classic()
+
+# View plot
+growth_by_dose
+
+# Change type
+TG$dose <- as.numeric(as.character(TG$dose))
+
+# Plot
+growth_by_dose <- ggplot(TG, aes(dose, len, color = supp)) +
+  stat_summary(fun.data = mean_sdl,
+               fun.args = list(mult = 1),
+               position = position_dodge(0.2)) +
+  stat_summary(fun.y = mean,
+               geom = "line",
+               position = position_dodge(0.1)) +
+  theme_classic() +
+  # Adjust labels and colors:
+  labs(x = "Dose (mg/day)", y = "Odontoblasts length (mean, standard deviation)", color = "Supplement") +
+  scale_color_brewer(palette = "Set1", labels = c("Orange juice", "Ascorbic acid")) +
+  scale_y_continuous(limits = c(0,35), breaks = seq(0, 35, 5), expand = c(0,0))
+
+# View plot
+growth_by_dose
