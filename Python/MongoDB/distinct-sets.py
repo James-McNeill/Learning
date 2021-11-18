a5e492c961b9308e7659adf3cd7abaf829aeb6f2
James_McNeill <jmcneill06@outlook.com>
2021-11-18 11:01:39 +0000

    Update distinct-sets.py

--- Python/MongoDB/distinct-sets.py
+++ Python/MongoDB/distinct-sets.py
@@ -36,3 +36,23 @@ db.laureates.count_documents({
         "category": "physics",
         "share": {"$ne": "1"},
         "year": {"$lt": "1945"}}}})
+
+# 2. Sharing in other categories
+# Save a filter for laureates with unshared prizes
+unshared = {
+    "prizes": {"$elemMatch": {
+        "category": {"$nin": ["physics", "chemistry", "medicine"]},
+        "share": "1",
+        "year": {"$gte": "1945"},
+    }}}
+
+# Save a filter for laureates with shared prizes
+shared = {
+    "prizes": {"$elemMatch": {
+        "category": {"$nin": ["physics", "chemistry", "medicine"]},
+        "share": {"$ne": "1"},
+        "year": {"$gte": "1945"},
+    }}}
+
+ratio = db.laureates.count_documents(unshared) / db.laureates.count_documents(shared)
+print(ratio)
