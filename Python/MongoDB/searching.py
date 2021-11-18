b3ebc9f1d294ed1e98032c2ed4c0d87973420a29
James_McNeill <jmcneill06@outlook.com>
2021-11-18 12:36:53 +0000

    Update searching.py

--- Python/MongoDB/searching.py
+++ Python/MongoDB/searching.py
@@ -1,3 +1,9 @@
 # Methods to help improve searching
 
 # A. Projection
+# 1. Minimum projection to get data required
+# "_id":0, this clause means that the _id variable will be excluded from the results. Whereas the value of one ensures that columns are included
+db.laureates.find_one(
+  {"prizes": {"$elemMatch": {"category": "physics", "year": "1903"}}}, 
+  projection={"firstname":1, "surname":1, "prizes.share":1, "_id":0}
+)
