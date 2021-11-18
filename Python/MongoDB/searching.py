204df668715287043f7fbf69855db41c86192b9f
James_McNeill <jmcneill06@outlook.com>
2021-11-18 14:35:47 +0000

    Update searching.py

--- Python/MongoDB/searching.py
+++ Python/MongoDB/searching.py
@@ -110,3 +110,21 @@ for category in sorted(db.prizes.distinct("category")):
     report += "{category}: {year}\n".format(**doc)
 
 print(report)
+
+# 2. Born and affiliated
+from collections import Counter
+
+# Ensure an index on country of birth
+db.laureates.create_index([("bornCountry", 1)])
+
+# Collect a count of laureates for each country of birth
+n_born_and_affiliated = {
+    country: db.laureates.count_documents({
+        "bornCountry": country,
+        "prizes.affiliations.country": country
+    })
+    for country in db.laureates.distinct("bornCountry")
+}
+
+five_most_common = Counter(n_born_and_affiliated).most_common(5)
+print(five_most_common)
