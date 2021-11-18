986b9b66cc4a029d2dfa96a9c2c16fc0514a284a
James_McNeill <jmcneill06@outlook.com>
2021-11-18 10:46:18 +0000

    Update distinct-sets.py

--- Python/MongoDB/distinct-sets.py
+++ Python/MongoDB/distinct-sets.py
@@ -10,3 +10,10 @@ print(countries)
 # The number of distinct countries of laureate affiliation for prizes
 count = len(db.laureates.distinct("prizes.affiliations.country"))
 print(count)
+
+# B. Distinct values given filters
+# 1. Born here, went there
+# In which countries have USA-born laureates had affiliations for their prizes? The string works as a pipe operation for the distinct method. So
+# the filter will work in the second parameter to create the list of filtered values that are then used to perform the distinct operation on the
+# first parameter. In this case the unique list of countries will be returned were the prizes were affiliated to
+db.laureates.distinct("prizes.affiliations.country", {"bornCountry":"USA"})
