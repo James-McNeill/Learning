ea9080db0e09c2590991dd4012c7791998b665c9
James_McNeill <jmcneill06@outlook.com>
2021-11-18 15:11:12 +0000

    Update aggregation-pipelines.py

--- Python/MongoDB/aggregation-pipelines.py
+++ Python/MongoDB/aggregation-pipelines.py
@@ -19,4 +19,21 @@ pipeline = [
 for doc in db.laureates.aggregate(pipeline):
     print("{bornCountry}: {prizes}".format(**doc))
 
-# 2. 
+# 2. Passing the aggregation baton to Python
+from collections import OrderedDict
+from itertools import groupby
+from operator import itemgetter
+
+original_categories = set(db.prizes.distinct("category", {"year": "1901"}))
+
+# Save an pipeline to collect original-category prizes
+pipeline = [
+    {"$match": {"category": {"$in": list(original_categories)}}},
+    {"$project": {"year": 1, "category": 1}},
+    {"$sort": OrderedDict([("year", -1)])}
+]
+cursor = db.prizes.aggregate(pipeline)
+for key, group in groupby(cursor, key=itemgetter("year")):
+    missing = original_categories - {doc["category"] for doc in group}
+    if missing:
+        print("{year}: {missing}".format(year=key, missing=", ".join(sorted(missing))))
