11a29d87030108b514f8140de8261bac0103b1c9
James_McNeill <jmcneill06@outlook.com>
2021-11-18 12:27:50 +0000

    Update distinct-sets.py

--- Python/MongoDB/distinct-sets.py
+++ Python/MongoDB/distinct-sets.py
@@ -95,3 +95,10 @@ print(set(db.laureates.distinct("bornCountry", criteria)))
 criteria = {"bornCountry": Regex("now" + " Germany\\)" + "$")}
 print(set(db.laureates.distinct("bornCountry", criteria)))
 
+# 3. The prized transistor
+# Save a filter for laureates with prize motivation values containing "transistor" as a substring
+criteria = {"prizes.motivation": Regex("transistor")}
+
+# Save the field names corresponding to a laureate's first name and last name
+first, last = "firstname", "surname"
+print([(laureate[first], laureate[last]) for laureate in db.laureates.find(criteria)])
