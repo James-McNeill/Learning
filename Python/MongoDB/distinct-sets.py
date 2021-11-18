a63bf6c56314c269ac30218ae2d48df439196dd1
James_McNeill <jmcneill06@outlook.com>
2021-11-18 10:36:42 +0000

    Update distinct-sets.py

--- Python/MongoDB/distinct-sets.py
+++ Python/MongoDB/distinct-sets.py
@@ -1,2 +1,7 @@
 # Working with distinct() and sets
 
+# A. Distinct values
+# 1. Comparing sets
+# Countries recorded as countries of death but not as countries of birth. Distinct method will show the unique values in the document
+countries = set(db.laureates.distinct("diedCountry")) - set(db.laureates.distinct("bornCountry"))
+print(countries)
