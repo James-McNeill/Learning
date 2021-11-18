55b08dc65ae2f6af2fed88d9e5d5bee6fe1a1510
James_McNeill <jmcneill06@outlook.com>
2021-11-18 15:05:45 +0000

    Update aggregation-pipelines.py

--- Python/MongoDB/aggregation-pipelines.py
+++ Python/MongoDB/aggregation-pipelines.py
@@ -1,3 +1,22 @@
 # Aggregation pipeline construction
 
 # A. Intro to aggregation
+# 1. Aggregating a few individuals' country data
+# Cursor equivalent
+cursor = (db.laureates.find(
+    {"gender": {"$ne": "org"}},
+    ["bornCountry", "prizes.affiliations.country"]
+).limit(3))
+
+# Aggregation pipeline
+# Translate cursor to aggregation pipeline
+pipeline = [
+    {"$match": {"gender": {"$ne": "org"}}},
+    {"$project": {"bornCountry": 1, "prizes.affiliations.country": 1}},
+    {"$limit": 3}
+]
+
+for doc in db.laureates.aggregate(pipeline):
+    print("{bornCountry}: {prizes}".format(**doc))
+
+# 2. 
