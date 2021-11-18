113ae57fd51b104a1762d1feabdb49c47019f3f8
James_McNeill <jmcneill06@outlook.com>
2021-11-18 14:45:06 +0000

    Update searching.py

--- Python/MongoDB/searching.py
+++ Python/MongoDB/searching.py
@@ -136,3 +136,16 @@ list(db.prizes.find({"category": "economics"},
      .sort("year")
      .limit(3)
      .limit(5))
+
+# 2. The first five prizes with a quarter share
+from pprint import pprint
+
+# Fetch prizes with quarter-share laureate(s)
+filter_ = {"laureates.share": "4"}
+
+# Save the list of field names
+projection = ["category", "year", "laureates.motivation"]
+
+# Save a cursor to yield the first five prizes
+cursor = db.prizes.find(filter_, projection).sort("year").limit(5)
+pprint(list(cursor))
