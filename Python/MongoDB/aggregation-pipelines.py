f258d3ce0b7668d7a8113ee9d990e7002ab8036c
James_McNeill <jmcneill06@outlook.com>
2021-11-23 12:29:13 +0000

    Update aggregation-pipelines.py

--- Python/MongoDB/aggregation-pipelines.py
+++ Python/MongoDB/aggregation-pipelines.py
@@ -133,3 +133,20 @@ pipeline = [
 ]
 
 print(list(db.laureates.aggregate(pipeline)))
+
+# 2. Filter out unaffiliated people. People that are not associated to the country
+pipeline = [
+    {"$match": {"gender": {"$ne": "org"}}},
+    {"$project": {"bornCountry": 1, "prizes.affiliations.country": 1}},
+    {"$unwind": "$prizes"},
+    {"$addFields": {"bornCountryInAffiliations": {"$in": ["$bornCountry", "$prizes.affiliations.country"]}}},
+    {"$match": {"bornCountryInAffiliations": False}},
+    {"$count": "awardedElsewhere"},
+]
+
+# Construct the additional filter stage
+added_stage = {"$match": {"prizes.affiliations.country": {"$in": db.laureates.distinct("prizes.affiliations.country")}}}
+
+# Insert this stage into the pipeline
+pipeline.insert(3, added_stage)
+print(list(db.laureates.aggregate(pipeline)))
