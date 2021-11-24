2a56ba0647072c5a41ac261b6a7b8b54ea3e6858
James_McNeill <jmcneill06@outlook.com>
2021-11-24 15:29:33 +0000

    Update excel-data.r

--- R/DataAnalyst/ImportingData/excel-data.r
+++ R/DataAnalyst/ImportingData/excel-data.r
@@ -1,4 +1,10 @@
 # Importing excel data
 
 # A. readxl() intro
-# 1. 
+# 1. List the sheet names of an excel file
+# Load the readxl package
+library("readxl")
+
+# Print the names of all worksheets
+excel_sheets("urbanpop.xlsx")
+
