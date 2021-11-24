0cfcf44befc24c2cb2c44f8db275b617d8f988ce
James_McNeill <jmcneill06@outlook.com>
2021-11-24 17:00:32 +0000

    Update xlconnect.r

--- R/DataAnalyst/ImportingData/xlconnect.r
+++ R/DataAnalyst/ImportingData/xlconnect.r
@@ -12,3 +12,16 @@ my_book <- loadWorkbook("urbanpop.xlsx")
 
 # Print out the class of my_book
 class(my_book)
+
+# 2. List and read Excel sheets
+# XLConnect is already available
+
+# Build connection to urbanpop.xlsx
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# List the sheets in my_book
+getSheets(my_book)
+
+# Import the second sheet in my_book
+my_book_1 <- readWorksheet(my_book, sheet = 2)
+my_book_1
