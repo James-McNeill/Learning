123b2eedc1aea829eb4425dac9d719eb65b6505b
James_McNeill <jmcneill06@outlook.com>
2021-11-24 13:32:47 +0000

    Update intro.r

--- R/DataAnalyst/ImportingData/intro.r
+++ R/DataAnalyst/ImportingData/intro.r
@@ -42,3 +42,17 @@ hotdogs <- read.table(path,
 
 # Call head() on hotdogs
 head(hotdogs)
+
+# 3. arguments
+# Finish the read.delim() call
+hotdogs <- read.delim("hotdogs.txt", header = FALSE, col.names = c("type", "calories", "sodium"))
+
+# Select the hot dog with the least calories: lily
+lily <- hotdogs[which.min(hotdogs$calories), ]
+
+# Select the observation with the most sodium: tom
+tom <- hotdogs[which.max(hotdogs$sodium), ]
+
+# Print lily and tom
+lily
+tom
