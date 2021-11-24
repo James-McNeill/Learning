68b446e43dcf98c2a33a54868bef2680abf9df62
James_McNeill <jmcneill06@outlook.com>
2021-11-24 14:48:03 +0000

    Update readr-data.table.r

--- R/DataAnalyst/ImportingData/readr-data.table.r
+++ R/DataAnalyst/ImportingData/readr-data.table.r
@@ -9,3 +9,16 @@ library("readr")
 
 # Import potatoes.csv with read_csv(): potatoes. read_csv() is a method from the readr package
 potatoes <- read_csv("potatoes.csv")
+
+# 2. read_tsv(). tsv() is short for tab separated values
+# readr is already loaded
+
+# Column names. Have to add these as the input file did not contain a header row
+properties <- c("area", "temp", "size", "storage", "method",
+                "texture", "flavor", "moistness")
+
+# Import potatoes.txt: potatoes
+potatoes <- read_tsv("potatoes.txt", col_names = properties)
+
+# Call head() on potatoes
+head(potatoes)
