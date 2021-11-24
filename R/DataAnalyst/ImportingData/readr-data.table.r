74ba77c66251d55fcb731670613cd9f127c04e26
James_McNeill <jmcneill06@outlook.com>
2021-11-24 14:46:03 +0000

    Update readr-data.table.r

--- R/DataAnalyst/ImportingData/readr-data.table.r
+++ R/DataAnalyst/ImportingData/readr-data.table.r
@@ -1,1 +1,11 @@
 # Working with readr and data.table
+
+# A. read_csv() and read_tsv()
+# read_csv() is a wrapper function around read_delim() that handles all the default parameter arguments
+
+# 1. read_csv()
+# Load the readr package
+library("readr")
+
+# Import potatoes.csv with read_csv(): potatoes. read_csv() is a method from the readr package
+potatoes <- read_csv("potatoes.csv")
