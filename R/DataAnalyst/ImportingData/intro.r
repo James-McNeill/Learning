f393271a83854079fa6b3fe2741816cb4933c9a3
James_McNeill <jmcneill06@outlook.com>
2021-11-24 13:27:29 +0000

    Update intro.r

--- R/DataAnalyst/ImportingData/intro.r
+++ R/DataAnalyst/ImportingData/intro.r
@@ -18,3 +18,12 @@ pools <- read.csv("swimming_pools.csv", stringsAsFactors = FALSE)
 
 # Check the structure of pools
 str(pools)
+
+# B. read.delim() and read.table()
+# 1. read.delim()
+# header: by default set to TRUE, to show that the variable names are in the first row. Adjust if not the case
+# Import hotdogs.txt: hotdogs
+hotdogs <- read.delim("hotdogs.txt", header = FALSE)
+
+# Summarize hotdogs. Display summary stats
+summary(hotdogs)
