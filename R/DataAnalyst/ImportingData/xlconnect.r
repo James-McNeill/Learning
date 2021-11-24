0705ca2709e58aeadd5e902a1c7bd05f38dcba3d
James_McNeill <jmcneill06@outlook.com>
2021-11-24 16:57:43 +0000

    Update xlconnect.r

--- R/DataAnalyst/ImportingData/xlconnect.r
+++ R/DataAnalyst/ImportingData/xlconnect.r
@@ -1,3 +1,14 @@
 # Reproducible Excel work with XLConnect
 
-# A. 
+# A. Reading sheets
+# 1. Connect to a workbook
+# urbanpop.xlsx is available in your working directory
+
+# Load the XLConnect package
+library("XLConnect")
+
+# Build connection to urbanpop.xlsx: my_book
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# Print out the class of my_book
+class(my_book)
