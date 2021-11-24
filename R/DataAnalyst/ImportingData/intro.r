f337652db9f6a82950d700b48bee8444eedf2c7c
James_McNeill <jmcneill06@outlook.com>
2021-11-24 13:35:11 +0000

    Update intro.r

--- R/DataAnalyst/ImportingData/intro.r
+++ R/DataAnalyst/ImportingData/intro.r
@@ -56,3 +56,19 @@ tom <- hotdogs[which.max(hotdogs$sodium), ]
 # Print lily and tom
 lily
 tom
+
+# 4. Column classes
+# Previous call to import hotdogs.txt
+hotdogs <- read.delim("hotdogs.txt", header = FALSE, col.names = c("type", "calories", "sodium"))
+
+# Display structure of hotdogs
+str(hotdogs)
+
+# Edit the colClasses argument to import the data correctly: hotdogs2. By using the argument "NULL" this means that the column will not be included
+hotdogs2 <- read.delim("hotdogs.txt", header = FALSE, 
+                       col.names = c("type", "calories", "sodium"),
+                       colClasses = c("factor", "NULL", "numeric"))
+
+
+# Display structure of hotdogs2
+str(hotdogs2)
