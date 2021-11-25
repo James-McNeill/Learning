2d294b532838b999dac159285f53f36adb61efd6
James_McNeill <jmcneill06@outlook.com>
2021-11-25 10:49:03 +0000

    Create from-database.r

---
+++ R/DataAnalyst/ImportingData/Intermediate/from-database.r
@@ -0,0 +1,14 @@
+# Importing data from database
+
+# A. Connect to database
+# 1. Establish a connection
+# Load the DBI package
+library("DBI")
+
+# Edit dbConnect() call
+con <- dbConnect(RMySQL::MySQL(), 
+                 dbname = "tweater", 
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
