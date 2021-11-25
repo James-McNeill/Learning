426a42348f94727dbf9b05a3160719f2972d885b
James_McNeill <jmcneill06@outlook.com>
2021-11-25 12:43:40 +0000

    Create k-fold.py

---
+++ Python/ML-ModelValidation/k-fold.py
@@ -0,0 +1,34 @@
+# Cross Validation using KFold
+
+# DataFrames already imported are X and y
+
+# Create the KFold splits
+from sklearn.model_selection import KFold
+
+# Use KFold
+kf = KFold(n_splits=5, shuffle=True, random_state=1111)
+
+# Create splits
+splits = kf.split(X)
+
+# Print the number of indices
+for train_index, val_index in splits:
+    print("Number of training indices: %s" % len(train_index))
+    print("Number of validation indices: %s" % len(val_index))
+
+# 1. Applying the method to a RFR model
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+rfc = RandomForestRegressor(n_estimators=25, random_state=1111)
+
+# Access the training and validation indices of splits
+for train_index, val_index in splits:
+    # Setup the training and validation data
+    X_train, y_train = X[train_index], y[train_index]
+    X_val, y_val = X[val_index], y[val_index]
+    # Fit the random forest model
+    rfc.fit(X_train, y_train)
+    # Make predictions, and print the accuracy
+    predictions = rfc.predict(X_val)
+    print("Split accuracy: " + str(mean_squared_error(y_val, predictions)))
