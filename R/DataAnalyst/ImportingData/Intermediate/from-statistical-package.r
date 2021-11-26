1fe591f415e019c2588b0714c3b5770b29428758
James_McNeill <jmcneill06@outlook.com>
2021-11-26 11:10:00 +0000

    Update from-statistical-package.r

--- R/DataAnalyst/ImportingData/Intermediate/from-statistical-package.r
+++ R/DataAnalyst/ImportingData/Intermediate/from-statistical-package.r
@@ -1,3 +1,16 @@
 # Working with data that is taken from statistical packages
 # Examples are shown for SAS, STATA and SPSS
 
+# A. haven
+# This is one of the packages that can be used to import data
+# A very fast package for working with external data types
+# 1. Import SAS data
+# Load the haven package
+library(haven)
+
+# Import sales.sas7bdat: sales
+sales <- read_sas("sales.sas7bdat")
+
+# Display the structure of sales
+str(sales)
+
