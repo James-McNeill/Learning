ee13bbc72fc0993cb14f250c697efd50a02e616d
James_McNeill <jmcneill06@outlook.com>
2021-11-30 15:22:23 +0000

    Update tame-data.r

--- R/DataAnalyst/Tidyverse/CleanTidy/tame-data.r
+++ R/DataAnalyst/Tidyverse/CleanTidy/tame-data.r
@@ -1,3 +1,17 @@
 # Tame the data
 
-# A. 
+# A. Cast Column types
+# 1. Cast a column to a date
+# Find format to parse uk_airdate 
+parse_date("17 August 2010", format = "%d %B %Y")
+
+# Edit to cast uk_airdate
+desserts <- read_csv("desserts.csv", 
+                     col_types = cols(
+                       uk_airdate = col_date(format = "%d %B %Y")
+                     )
+                    )
+
+# Arrange by descending uk_airdate
+desserts %>% 
+	arrange(desc(uk_airdate))
