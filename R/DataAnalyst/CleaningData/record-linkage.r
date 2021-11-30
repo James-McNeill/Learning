2c4e25fccdb145c69a8c3286bddfc1c0948ec9cf
James_McNeill <jmcneill06@outlook.com>
2021-11-30 13:12:24 +0000

    Update record-linkage.r

--- R/DataAnalyst/CleaningData/record-linkage.r
+++ R/DataAnalyst/CleaningData/record-linkage.r
@@ -33,3 +33,16 @@ pair_blocking(zagat, fodors)
 
 # Generate pairs with same city. Ensures that some form of filtering is applied, otherwise all possible combinations are created
 pair_blocking(zagat, fodors, blocking_var = "city")
+
+# 2. Comparing pairs
+# Generate pairs
+pair_blocking(zagat, fodors, blocking_var = "city") %>%
+  # Compare pairs by name using lcs()
+  compare_pairs(by = "name",
+      default_comparator = lcs())
+
+# Generate pairs
+pair_blocking(zagat, fodors, blocking_var = "city") %>%
+  # Compare pairs by name, phone, addr
+  compare_pairs(by = c("name", "phone", "addr"),
+      default_comparator = jaro_winkler())
