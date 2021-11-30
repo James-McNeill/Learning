f21387f8deb4d33c2c0731c3e0315d6418d2e588
James_McNeill <jmcneill06@outlook.com>
2021-11-30 13:28:42 +0000

    Update explore.r

--- R/DataAnalyst/Tidyverse/CleanTidy/explore.r
+++ R/DataAnalyst/Tidyverse/CleanTidy/explore.r
@@ -10,3 +10,19 @@ bakeoff <- read_csv("bakeoff.csv", skip = 1)
 
 # Print bakeoff
 bakeoff
+
+# 2. Assign missing values
+# Load dplyr
+library(dplyr)
+
+# Filter rows where showstopper is UNKNOWN 
+bakeoff %>% 
+    filter(showstopper == "UNKNOWN")
+
+# Edit to add list of missing values
+bakeoff <- read_csv("bakeoff.csv", skip = 1,
+                    na = c("", "NA", "UNKNOWN"))
+
+# Filter rows where showstopper is NA 
+bakeoff %>%
+    filter(is.na(showstopper))
