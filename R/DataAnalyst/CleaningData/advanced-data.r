700a2fc465007523c1a35238c6dbac8c702d1e7d
James_McNeill <jmcneill06@outlook.com>
2021-11-30 11:14:41 +0000

    Update advanced-data.r

--- R/DataAnalyst/CleaningData/advanced-data.r
+++ R/DataAnalyst/CleaningData/advanced-data.r
@@ -12,3 +12,19 @@ formats <- c("%Y-%m-%d", "%B %d, %Y")
 # Convert dates to the same format
 accounts %>%
   mutate(date_opened_clean = parse_date_time(date_opened, orders = formats))
+
+# 2. Currency uniformity
+# dplyr and ggplot2 are loaded and the accounts and account_offices data frames are available
+# Scatter plot of opening date and total amount
+accounts %>%
+  ggplot(aes(x = date_opened, y = total)) +
+  geom_point()
+
+# Left join accounts to account_offices by id
+accounts %>%
+  left_join(account_offices, by = "id") %>%
+  # Convert totals from the Tokyo office to USD
+  mutate(total_usd = ifelse(office == "Tokyo", total / 104, total)) %>%
+  # Scatter plot of opening date vs total_usd
+  ggplot(aes(x = date_opened, y = total_usd)) +
+    geom_point()
