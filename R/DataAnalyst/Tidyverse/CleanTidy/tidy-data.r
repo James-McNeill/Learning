ae2d87cb787760fec62d4e8b34ae232d0e14712d
James_McNeill <jmcneill06@outlook.com>
2021-12-01 12:29:49 +0000

    Update tidy-data.r

--- R/DataAnalyst/Tidyverse/CleanTidy/tidy-data.r
+++ R/DataAnalyst/Tidyverse/CleanTidy/tidy-data.r
@@ -1,3 +1,22 @@
 # Tidy data using R pipes
 
-# A. 
+# A. gather()
+# Used to convert tame (wide) data structure to tidy (long) data structure
+
+# 1. Gather & plot
+tidy_ratings <- ratings %>%
+    # Gather and convert episode to factor.
+    # factor_key: maintains the original order of columns
+    # na.rm: removes the missing values from value
+	gather(key = "episode", value = "viewers_7day", -series, 
+           factor_key = TRUE, na.rm = TRUE) %>%
+	# Sort in ascending order by series and episode
+    arrange(series, episode) %>% 
+	# Create new variable using row_number()
+    mutate(episode_count = row_number())
+
+# Plot viewers by episode and series
+ggplot(tidy_ratings, aes(x = episode_count, 
+                y = viewers_7day, 
+                fill = series)) +
+    geom_col()
