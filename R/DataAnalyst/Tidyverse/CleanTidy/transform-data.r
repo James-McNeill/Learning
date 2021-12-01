9f60d83f6228239e5ecb52e3e71531b471371a50
James_McNeill <jmcneill06@outlook.com>
2021-12-01 15:55:05 +0000

    Update transform-data.r

--- R/DataAnalyst/Tidyverse/CleanTidy/transform-data.r
+++ R/DataAnalyst/Tidyverse/CleanTidy/transform-data.r
@@ -1,3 +1,17 @@
 # Transform data
 
-# A.
+# A. case_when()
+# 1. Combine two variables
+# Create skill variable with 3 levels
+bakers_skill <- bakers %>% 
+  mutate(skill = case_when(
+    star_baker > technical_winner ~ "super_star",
+    star_baker < technical_winner ~ "high_tech",
+    TRUE ~ "well_rounded"
+  ))
+  
+# Filter zeroes to examine skill variable
+bakers_skill %>% 
+  filter(star_baker == 0 & technical_winner == 0) %>% 
+  count(skill)
+
