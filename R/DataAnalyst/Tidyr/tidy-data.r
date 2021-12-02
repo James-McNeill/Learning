06f01d1ce9b4b64b5801815c9f6f23aa5204e301
James_McNeill <jmcneill06@outlook.com>
2021-12-02 11:18:36 +0000

    Update tidy-data.r

--- R/DataAnalyst/Tidyr/tidy-data.r
+++ R/DataAnalyst/Tidyr/tidy-data.r
@@ -19,3 +19,19 @@ tvshow_df %>%
   rename(actor = cast) %>% 
   count(actor, sort = TRUE) %>% 
   head()
+
+# 3. Separating into columns and rows
+drink_df %>% 
+  # Separate the ingredients over rows
+  separate_rows(ingredients, sep = "; ") %>% 
+  # Separate ingredients into three columns
+  separate(
+    ingredients, 
+    into = c("ingredient", "quantity", "unit"), 
+    sep = " ", 
+    convert = TRUE
+  ) %>% 
+  # Group by ingredient and unit
+  group_by(ingredient, unit) %>% 
+  # Calculate the total quantity of each ingredient
+  summarize(quantity = sum(quantity))
