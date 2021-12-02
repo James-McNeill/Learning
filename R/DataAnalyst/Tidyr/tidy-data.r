cd69cf6b211f567265271cba168b25473c9718c8
James_McNeill <jmcneill06@outlook.com>
2021-12-02 11:04:40 +0000

    Update tidy-data.r

--- R/DataAnalyst/Tidyr/tidy-data.r
+++ R/DataAnalyst/Tidyr/tidy-data.r
@@ -6,3 +6,8 @@ netflix_df %>%
   # Split the duration column into value and unit columns. Using convert parameter creates the appropriate column data type conversion
   separate(duration, into = c("value", "unit"), sep = " ", convert = TRUE)
 
+# B. Columns with multiple values
+# 1. International phone numbers
+phone_nr_df %>%
+  # Unite the country_code and national_number columns
+  unite("international_number", country_code, national_number, sep = " ")
