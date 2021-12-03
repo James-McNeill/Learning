f026cb0aec7560ca8885e9ef4e1eefa1a8f8aed7
James_McNeill <jmcneill06@outlook.com>
2021-12-03 15:51:18 +0000

    Update rectangling-data.r

--- R/DataAnalyst/Tidyr/rectangling-data.r
+++ R/DataAnalyst/Tidyr/rectangling-data.r
@@ -15,3 +15,15 @@ tibble(movie = movie_planets_list) %>%
   unnest_wider(movie) %>% 
   # Unnest the planets column
   unnest_wider(planets)
+
+# B. From nested values to observations
+# 1. Rectangling Star Wars planets
+# unnest_wider(): common length of lists
+# unnest_longer(): varying length of lists
+
+# Create a tibble from movie_planets_list
+tibble(movie = movie_planets_list) %>% 
+  # Unnest the movie column in the correct direction
+  unnest_wider(movie) %>% 
+  # Unnest the planets column in the correct direction
+  unnest_longer(planets)
