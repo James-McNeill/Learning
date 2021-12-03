211558bccc546c9299d43a234ed004eae59b76cd
James_McNeill <jmcneill06@outlook.com>
2021-12-03 12:18:42 +0000

    Update wide-to-long-data.r

--- R/DataAnalyst/Tidyr/wide-to-long-data.r
+++ R/DataAnalyst/Tidyr/wide-to-long-data.r
@@ -1,4 +1,17 @@
 # Conversion from wide to long and back again
 
 # A. From wide to long
-# 1. 
+# 1. Pivot_longer()
+nuke_df %>% 
+  # Pivot the data to a longer format
+  pivot_longer(
+    -year, 
+    # Overwrite the names of the two new columns
+    names_to = "country", 
+    values_to = "n_bombs"
+  ) %>% 
+  # Replace NA values for n_bombs with 0L
+  replace_na(list(n_bombs = 0L)) %>% 
+  # Plot the number of bombs per country over time
+  ggplot(aes(x = year, y = n_bombs, color = country)) +
+  geom_line()
