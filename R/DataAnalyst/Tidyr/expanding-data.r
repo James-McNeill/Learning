6ec06ae6a4731ec9e5f9a326f55d718fef58784a
James_McNeill <jmcneill06@outlook.com>
2021-12-03 14:20:18 +0000

    Update expanding-data.r

--- R/DataAnalyst/Tidyr/expanding-data.r
+++ R/DataAnalyst/Tidyr/expanding-data.r
@@ -14,3 +14,19 @@ codon_df <- expand_grid(
 codon_df %>% 
   # Unite these three columns into a "codon" column
   unite("codon", letter1:letter3, sep = " ")
+
+# 2. When did humans replace dogs in space
+# Create a tibble with all combinations of years and species
+full_df <- expand_grid(
+  year = 1951:1970, 
+  species = c("Human", "Dog")
+)
+
+space_df %>% 
+  # Join with full_df so that missing values are introduced
+  right_join(full_df, by = c("year", "species")) %>% 
+  # Overwrite NA values for n_in_space with 0L
+  replace_na(list(n_in_space = 0L)) %>% 
+  # Create a line plot with n_in_space over year, color by species
+  ggplot(aes(x = year, y = n_in_space, color = species)) +
+  geom_line()
