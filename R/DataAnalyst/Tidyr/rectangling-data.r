5d4c370d0745c6e6deffdd63b5e1777beaf7114b
James_McNeill <jmcneill06@outlook.com>
2021-12-03 15:49:04 +0000

    Update rectangling-data.r

--- R/DataAnalyst/Tidyr/rectangling-data.r
+++ R/DataAnalyst/Tidyr/rectangling-data.r
@@ -1,3 +1,17 @@
 # Rectangling data
 
-# A. 
+# A. Intro to non-rectangular data
+# 1. Rectangling Star Wars movies
+# Data is taken from Star Wars API and is in JSON format
+
+# Create a movie column from the movie_list
+tibble(movie = movie_list) %>% 
+  # Unnest the movie column
+  unnest_wider(movie)
+
+# Create a tibble with a movie column
+tibble(movie = movie_planets_list) %>% 
+  # Unnest the movie column
+  unnest_wider(movie) %>% 
+  # Unnest the planets column
+  unnest_wider(planets)
