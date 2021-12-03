7810e962a9cde5a7cc70a62628809ac4d1a5ae17
James_McNeill <jmcneill06@outlook.com>
2021-12-03 14:15:14 +0000

    Update expanding-data.r

--- R/DataAnalyst/Tidyr/expanding-data.r
+++ R/DataAnalyst/Tidyr/expanding-data.r
@@ -1,4 +1,16 @@
 # Expanding datasets
 
 # A. Creating unique combinations of vectors
-# 1. 
+# 1. Letters of the genetic code
+letters <- c("A", "C", "G", "U")
+
+# Create a tibble with all possible 3 way combinations
+codon_df <- expand_grid(
+  letter1 = letters,
+  letter2 = letters,
+  letter3 = letters
+)
+
+codon_df %>% 
+  # Unite these three columns into a "codon" column
+  unite("codon", letter1:letter3, sep = " ")
