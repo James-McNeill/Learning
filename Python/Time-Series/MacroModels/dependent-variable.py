b01ab8449c57de048f1a00835714219b24a5001e
James_McNeill <jmcneill06@outlook.com>
2021-12-08 16:39:46 +0000

    Update dependent-variable.py

--- Python/Time-Series/MacroModels/dependent-variable.py
+++ Python/Time-Series/MacroModels/dependent-variable.py
@@ -1,4 +1,35 @@
 # Performing a review of the dependent variable
 # Dependent variable stationarity testing
 
-# 
+# Create connection to AWS Athena
+# Import utilities, PyAthena and other modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import sys
+import os
+import plotly.express as px
+
+# Print current working directory location
+print(os.getcwd()+'/')
+# Connect to the repo package folder to use in this Notebook
+sys.path.append("/home/ec2-user/SageMaker/s3-user-bucket/.../")
+# Import the sub-packages
+from stmd import utils as stmd
+
+# Formatting for the float variables
+pd.options.display.float_format = '{:,.5f}'.format
+
+# Create the pandas cursor object
+pandas_cursor = stmd.create_cursor()
+
+# Seaborn style defaults
+sns.set(rc={'figure.figsize':(30,12)})
+sns.set_style("darkgrid")
+
+# Import the dataset that was created in the ODR-curve.py
+df = pd.read_csv('odr.csv', index_col='month', parse_dates=True)
+df.head()
+
