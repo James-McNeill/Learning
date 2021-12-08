76a5cea9f92f5cdbff1b78d9afb02d53218ccaec
James_McNeill <jmcneill06@outlook.com>
2021-12-08 15:28:09 +0000

    Update fine-tuning-efficient-base-r.r

--- R/Programming/Writing-efficient-r-code/fine-tuning-efficient-base-r.r
+++ R/Programming/Writing-efficient-r-code/fine-tuning-efficient-base-r.r
@@ -15,3 +15,15 @@ growing <- function(n) {
 # Use <- with system.time() to store the result as res_grow
 system.time(res_grow <- growing(n = 30000))
 
+# 2. Timings - pre-allocation
+# Fast code
+pre_allocate <- function(n) {
+    x <- numeric(n) # Pre-allocate
+    for(i in 1:n) 
+        x[i] <- rnorm(1)
+    x
+}
+
+# Use <- with system.time() to store the result as res_allocate
+n <- 30000
+system.time(res_allocate <- pre_allocate(n))
