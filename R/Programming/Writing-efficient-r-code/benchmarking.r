4c60a0f07897047b22caf2a1510ed00fc66798c0
James_McNeill <jmcneill06@outlook.com>
2021-12-08 14:51:01 +0000

    Create benchmarking.r

---
+++ R/Programming/Writing-efficient-r-code/benchmarking.r
@@ -0,0 +1,12 @@
+# Benchmarking code
+# Before optimising code we have to ensure that there is an initial benchmark to work with
+
+# A. R version
+# Print the R version details using version
+version
+
+# Assign the variable major to the major component
+major <- version$major
+
+# Assign the variable minor to the minor component
+minor <- version$minor
