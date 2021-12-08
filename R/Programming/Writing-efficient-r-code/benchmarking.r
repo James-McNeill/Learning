6a926b7d74b383fa02823a7a0c1a1294e7cf60c0
James_McNeill <jmcneill06@outlook.com>
2021-12-08 15:13:35 +0000

    Update benchmarking.r

--- R/Programming/Writing-efficient-r-code/benchmarking.r
+++ R/Programming/Writing-efficient-r-code/benchmarking.r
@@ -30,3 +30,17 @@ compare <- microbenchmark(read.csv("movies.csv"),
 
 # Print compare
 compare
+
+# C. How good is your machine?
+# 1. DataCamp hardware
+# Load the benchmarkme package
+library("benchmarkme")
+
+# Assign the variable ram to the amount of RAM on this machine
+ram <- get_ram()
+ram
+
+# Assign the variable cpu to the cpu specs
+cpu <- get_cpu()
+cpu
+
