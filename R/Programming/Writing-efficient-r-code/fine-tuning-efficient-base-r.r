9b49a1a163607baea237d0b4be1a530b25e1ae80
James_McNeill <jmcneill06@outlook.com>
2021-12-08 15:19:40 +0000

    Create fine-tuning-efficient-base-r.r

---
+++ R/Programming/Writing-efficient-r-code/fine-tuning-efficient-base-r.r
@@ -0,0 +1,5 @@
+# R is flexible because you can often solve a single problem in many different ways. 
+# Some ways can be several orders of magnitude faster than the others.
+
+# A. Memory allocation
+# 1. Timings - growing a vector
