b8464b05042125ac2c0452c3a516b23ec0b78950
James_McNeill <jmcneill06@outlook.com>
2021-12-10 16:06:56 +0000

    Update parallel-programming.r

--- R/Programming/Writing-efficient-r-code/parallel-programming.r
+++ R/Programming/Writing-efficient-r-code/parallel-programming.r
@@ -1,3 +1,14 @@
 # Parallel programming
 
-# 
+# A. CPUs
+# By default R uses 1 core when processing
+# 1. Check for number of cores
+# Load the parallel package
+library(parallel)
+
+# Store the number of cores in the object no_of_cores
+no_of_cores <- detectCores()
+
+# Print no_of_cores
+no_of_cores
+
