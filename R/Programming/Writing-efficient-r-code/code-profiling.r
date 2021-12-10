cf6cbc45b31eb6c55b4bee29e35b7dbae4d21ec6
James_McNeill <jmcneill06@outlook.com>
2021-12-10 15:28:33 +0000

    Update code-profiling.r

--- R/Programming/Writing-efficient-r-code/code-profiling.r
+++ R/Programming/Writing-efficient-r-code/code-profiling.r
@@ -1,3 +1,27 @@
 # Code profiling
 # Profiling helps you locate the bottlenecks in your code
 
+# A. Code profiling
+# 1. Profvis in action
+# Load the data set
+data(movies, package = "ggplot2movies") 
+
+# Load the profvis package
+library(profvis)
+
+# Profile the following code with the profvis function
+profvis ({
+  # Load and select data
+  comedies <- movies[movies$Comedy == 1, ]
+
+  # Plot data of interest
+  plot(comedies$year, comedies$rating)
+
+  # Loess regression line
+  model <- loess(rating ~ year, data = comedies)
+  j <- order(comedies$year)
+  
+  # Add fitted line to the plot
+  lines(comedies$year[j], model$fitted[j], col = "red")
+})
+
