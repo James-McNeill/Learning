dfd3d09988144c67dd83ac040facaca17ab9f871
James_McNeill <jmcneill06@outlook.com>
2021-12-10 16:43:15 +0000

    Update parallel-programming.r

--- R/Programming/Writing-efficient-r-code/parallel-programming.r
+++ R/Programming/Writing-efficient-r-code/parallel-programming.r
@@ -12,7 +12,7 @@ no_of_cores <- detectCores()
 # Print no_of_cores
 no_of_cores
 
-# 2. Moving to parApply
+# B. Moving to parApply
 # Determine the number of available cores
 detectCores()
 
@@ -21,6 +21,33 @@ cl <- makeCluster(2)
 
 # Parallelize this code. dd: relates to the DataFrame
 parApply(cl, dd, 2, median)
+
+# Stop the cluster
+stopCluster(cl)
+
+# C. Using parSapply
+# play() function game
+play <- function() {
+  total <- no_of_rolls <- 0
+  while(total < 10) {
+    total <- total + sample(1:6, 1)
+
+    # If even. Reset to 0
+    if(total %% 2 == 0) total <- 0 
+    no_of_rolls <- no_of_rolls + 1
+  }
+  no_of_rolls
+}
+
+library("parallel")
+# Create a cluster via makeCluster (2 cores)
+cl <- makeCluster(2)
+
+# Export the play() function to the cluster. Makes it easier to work with the function in a cluster
+clusterExport(cl, "play")
+
+# Re-write sapply as parSapply
+res <- parSapply(cl, 1:100, function(i) play())
 
 # Stop the cluster
 stopCluster(cl)
