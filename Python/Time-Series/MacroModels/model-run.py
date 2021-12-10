0efdf84fd5a99b9ebb9165e97d2a19538592e631
James_McNeill <jmcneill06@outlook.com>
2021-12-10 11:50:40 +0000

    Update model-run.py

--- Python/Time-Series/MacroModels/model-run.py
+++ Python/Time-Series/MacroModels/model-run.py
@@ -108,3 +108,136 @@ dep = df_dep
 indep = df_ind2[['var1','var2']]
 indep
 
+# Merge the dataframes together by index
+mod_df = pd.merge(dep
+                 ,indep
+                 ,how='left'
+                 ,left_index=True
+                 ,right_index=True
+                 )
+
+mod_df = mod_df.loc[(mod_df['var1'].notnull()) & (mod_df.index <= '2019-12-01') & (mod_df.index >= '2009-06-01')]
+# # Scale up the dependent and independent variables
+mod_df['var2'] = mod_df['var2'] * 100
+mod_df
+
+# E. Linear Regression model
+# Independent variables
+indep_vars = ['var1', 'var2']
+
+x = mod_df[indep_vars]
+y = mod_df[['ODR_X']]
+
+# Understand initial relationships
+mod_df.plot(x='ODR_X', y='var1', style='o')
+plt.title('Default rate by var1')
+plt.xlabel('Def_Rate')
+plt.ylabel('var1')
+plt.show();
+
+# Lets review the dispertion of the default rate
+plt.figure(figsize=(10,5))
+plt.tight_layout()
+sns.displot(mod_df['ODR_X']);
+
+# Run the linear regression
+model = LinearRegression(fit_intercept=False) # Turn off the intercept
+model.fit(x,y) # training the algorithm
+
+mod_df['predicted'] = model.predict(x)
+
+y_pred = mod_df[['predicted']]
+mod_df.head()
+
+# Retrieve the results
+# The intercept
+print(model.intercept_)
+# The variable coefficients
+print(model.coef_)
+
+# Display the prediction vs actual
+mod_df[['ODR_X', 'predicted']].plot(alpha=0.5);
+
+# Display summary statistics
+print('Mean Absolute Error:', metrics.mean_absolute_error(y, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y, y_pred)))
+
+# Run the analysis using statsmodels. Produces more meaningful results
+sm_model = sm.OLS(y, x).fit()
+sm_model.summary()
+
+#Draw a plot to compare the true relationship to OLS predictions. 
+# Confidence intervals around the predictions are built using the wls_prediction_std command.
+prstd, iv_l, iv_u = wls_prediction_std(sm_model)
+
+fig, ax = plt.subplots(figsize=(8,6))
+
+ax.plot(y, 'o', label="data")
+ax.plot(sm_model.fittedvalues, 'r--.', label="OLS")
+ax.plot(iv_u, 'r--', label="uCI", color='green')
+ax.plot(iv_l, 'r--', label="lCI", color='purple')
+ax.legend(loc='best');
+
+# Get the influence of each data point in the regression
+infl = sm_model.get_influence()
+sum0 = infl.summary_frame().filter(regex="dfb")
+sum1 = infl.summary_frame()
+sum1.head()
+
+# Review variable influence
+plt.plot(sum0['var1'], label="var1")
+plt.plot(sum0['var2'], label="var2")
+plt.legend()
+plt.show();
+
+# Reviewing the student_resid
+fig = px.bar(sum1, x=sum1.index, y="student_resid")
+fig.show()
+
+# Reviewing the variance inflation factors
+vif = [variance_inflation_factor(x.values, i) for i in range(x.shape[1])]
+vif
+
+# Normality and auto correlation for residuals
+resids = sm_model.resid
+resids
+
+#Normality of residuals
+
+# Jarque-Bera test:
+name = ['Jarque-Bera', 'Chi^2 two-tail prob.', 'Skew', 'Kurtosis']
+test = sms.jarque_bera(resids)
+lzip(name,test)
+
+# Omni test:
+name = ['Chi^2', 'Two-tail probability']
+test = sms.omni_normtest(resids)
+lzip(name, test)
+
+# Influence tests
+test_class = OLSInfluence(sm_model)
+test_betas = test_class.dfbetas
+# test_betas
+
+# Information on the leverage can be reviewed
+fig, ax = plt.subplots(figsize=(8,6))
+fig = plot_leverage_resid2(sm_model, ax=ax)
+
+# Multicollinearity
+np.linalg.cond(sm_model.model.exog)
+
+# Heteroskedasticity tests
+name = ['Lagrange multiplier statistic', 'p-value',
+        'f-value', 'f p-value']
+test = sms.het_breuschpagan(sm_model.resid, sm_model.model.exog)
+lzip(name, test)
+
+name = ['F statistic', 'p-value']
+test = sms.het_goldfeldquandt(sm_model.resid, sm_model.model.exog)
+lzip(name, test)
+
+# Linearity
+name = ['t value', 'p value']
+test = sms.linear_harvey_collier(sm_model)
+lzip(name, test)
