28fee8eaac673f62e6949d2bac500837be85fc34
James_McNeill <jmcneill06@outlook.com>
2021-12-10 15:57:51 +0000

    Update code-profiling.r

--- R/Programming/Writing-efficient-r-code/code-profiling.r
+++ R/Programming/Writing-efficient-r-code/code-profiling.r
@@ -68,3 +68,30 @@ microbenchmark(
     app_sol = app(rolls),
     r_sum_sol = r_sum(rolls)
 )
+
+# 4. Use && instead of &
+# The additional ampersand aims to review each logical statement as it goes. This means that if the first logical statement
+# returns FALSE then the final result will always be FALSE. Whereas the single ampersand will aim to review each logical 
+# statement even if the first value returns FALSE
+# Example data
+is_double
+
+# Define the previous solution
+move <- function(is_double) {
+    if (is_double[1] & is_double[2] & is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
+
+# Define the improved solution
+improved_move <- function(is_double) {
+    if (is_double[1] && is_double[2] && is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
+
+# microbenchmark both solutions
+# Very occassionally the improved solution is actually a little slower
+# This is just random chance
+microbenchmark(move(is_double), improved_move(is_double), times = 1e5)
+
