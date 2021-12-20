a2e40ab32558f161f2cec93aea4b79f88c977087
James_McNeill <jmcneill06@outlook.com>
2021-12-20 15:06:22 +0000

    Update arguments.r

--- R/Programming/Writing-functions/arguments.r
+++ R/Programming/Writing-functions/arguments.r
@@ -1,3 +1,19 @@
 # Function arguments
 
-# A. 
+# A. Default arguments
+# 1. Numeric defaults
+# Set the default for n to 5. Remember to only set for numeric detail arguments and not data arguments
+cut_by_quantile <- function(x, n = 5, na.rm, labels, interval_type) {
+  probs <- seq(0, 1, length.out = n + 1)
+  qtiles <- quantile(x, probs, na.rm = na.rm, names = FALSE)
+  right <- switch(interval_type, "(lo, hi]" = TRUE, "[lo, hi)" = FALSE)
+  cut(x, qtiles, labels = labels, right = right, include.lowest = TRUE)
+}
+
+# Remove the n argument from the call
+cut_by_quantile(
+  n_visits, 
+  na.rm = FALSE, 
+  labels = c("very low", "low", "medium", "high", "very high"),
+  interval_type = "(lo, hi]"
+)
