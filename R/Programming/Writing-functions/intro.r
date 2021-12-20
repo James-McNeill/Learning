a876c48bee79b1aca0deed1eae4268cb0864851f
James_McNeill <jmcneill06@outlook.com>
2021-12-20 14:39:52 +0000

    Update intro.r

--- R/Programming/Writing-functions/intro.r
+++ R/Programming/Writing-functions/intro.r
@@ -1,4 +1,18 @@
 # Writing functions in R
 
 # A. Why you should use functions?
-# 1. 
+# 1. Calling functions
+# Look at the gold medals data
+gold_medals
+
+# Note the arguments to median()
+args(median)
+
+# Rewrite this function call, following best practices. For best practice, only require to outline less often used arguments
+median(gold_medals, na.rm = TRUE)
+
+# Note the arguments to rank()
+args(rank)
+
+# Rewrite this function call, following best practices. na.last = "keep", means keep the rank of NA values as NA
+rank(-gold_medals, na.last = "keep", ties.method = "min")
