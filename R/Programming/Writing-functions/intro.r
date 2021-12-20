4d85d9fa6c868cd6c594c60b14131acb74290607
James_McNeill <jmcneill06@outlook.com>
2021-12-20 15:01:47 +0000

    Update intro.r

--- R/Programming/Writing-functions/intro.r
+++ R/Programming/Writing-functions/intro.r
@@ -61,3 +61,30 @@ toss_coin <- function(n_flips, p_head) {
 
 # Generate 10 coin tosses
 toss_coin(10, 0.8)
+
+# C. Renaming functions
+# In order to allow for better readability, be careful with function naming convention
+# 1. Renaming GLM
+# Run a generalized linear regression 
+glm(
+  # Model no. of visits vs. gender, income, travel
+  n_visits ~ gender + income + travel, 
+  # Use the snake_river_visits dataset
+  data = snake_river_visits, 
+  # Make it a Poisson regression
+  family = poisson
+)
+
+# Create the function to better apply the regression method
+run_poisson_regression <- function(data, formula) {
+  glm(formula, data, family = poisson)
+}
+
+# Re-run the Poisson regression, using your function. The pipe operation allows for the data to be passed through without explicitly calling it
+model <- snake_river_visits %>%
+  run_poisson_regression(n_visits ~ gender + income + travel)
+
+# Run this to see the predictions
+snake_river_explanatory %>%
+  mutate(predicted_n_visits = predict(model, ., type = "response"))%>%
+  arrange(desc(predicted_n_visits))
