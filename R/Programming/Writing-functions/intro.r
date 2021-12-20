f44941c84fd389d37d50fddac91ef5ef3c8c2522
James_McNeill <jmcneill06@outlook.com>
2021-12-20 14:46:39 +0000

    Update intro.r

--- R/Programming/Writing-functions/intro.r
+++ R/Programming/Writing-functions/intro.r
@@ -16,3 +16,25 @@ args(rank)
 
 # Rewrite this function call, following best practices. na.last = "keep", means keep the rank of NA values as NA
 rank(-gold_medals, na.last = "keep", ties.method = "min")
+
+# B. Converting scripts into functions
+# 1. First function: tossing a coin
+coin_sides <- c("head", "tail")
+
+# Sample from coin_sides once
+sample(coin_sides, 1)
+
+# Write a template for your function, toss_coin()
+toss_coin <- function() {
+  # (Leave the contents of the body for later)
+# Add punctuation to finish the body
+}
+
+# Your functions, from previous steps
+toss_coin <- function() {
+  coin_sides <- c("head", "tail")
+  sample(coin_sides, 1)
+}
+
+# Call your function
+toss_coin()
