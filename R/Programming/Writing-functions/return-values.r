7012c5383687e8df03c774a637b9c040dcb62fcd
James_McNeill <jmcneill06@outlook.com>
2021-12-23 12:54:44 +0000

    Update return-values.r

--- R/Programming/Writing-functions/return-values.r
+++ R/Programming/Writing-functions/return-values.r
@@ -1,3 +1,22 @@
 # Returning values from a function
 
-# A. 
+# A. Returning values from functions
+# 1. Returning early
+# Should a result occur earlier in the function then a result can be returned early without having to run the entire function
+is_leap_year <- function(year) {
+  # If year is div. by 400 return TRUE
+  if(year %% 400 == 0) {
+    return(TRUE)
+  }
+  # If year is div. by 100 return FALSE
+  if(year %% 100 == 0) {
+    return(FALSE)
+  }  
+  # If year is div. by 4 return TRUE
+  if(year %% 4 == 0) {
+    return(TRUE)
+  }
+  
+  # Otherwise return FALSE
+  return(FALSE)
+}
