cb9a2e81afad8950434930c8a024ed9e5c81160c
James_McNeill <jmcneill06@outlook.com>
2021-12-23 13:55:15 +0000

    Update case-study-grain-yields.r

--- R/Programming/Writing-functions/case-study-grain-yields.r
+++ R/Programming/Writing-functions/case-study-grain-yields.r
@@ -1,3 +1,31 @@
 # Case Study - grain yields
 
-# A. 
+# A. Grain yields and unit conversion
+# 1. Converting areas to metric 1
+# Write a function to convert acres to sq. yards
+acres_to_sq_yards <- function(acres) {
+  acres * 4840
+}
+
+# Write a function to convert yards to meters
+yards_to_meters <- function(yards) {
+    yards * 36 * 0.0254
+}
+
+# Write a function to convert sq. meters to hectares
+sq_meters_to_hectares <- function(sq_meters) {
+    sq_meters / 10000
+}
+
+# 2. Converting areas to metric 2
+# Write a function to convert sq. yards to sq. meters
+sq_yards_to_sq_meters <- function(sq_yards) {
+  sq_yards %>%
+    # Take the square root
+    sqrt() %>%
+    # Convert yards to meters
+    yards_to_meters() %>%
+    # Square it
+    raise_to_power(2)
+}
+
