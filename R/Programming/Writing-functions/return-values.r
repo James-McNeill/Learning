1e279ad6e791363697e7e01f5770d40d3339f69b
James_McNeill <jmcneill06@outlook.com>
2021-12-23 13:38:30 +0000

    Update return-values.r

--- R/Programming/Writing-functions/return-values.r
+++ R/Programming/Writing-functions/return-values.r
@@ -105,3 +105,14 @@ parent <- parent.env(rsa_env)
 
 # Print its name
 environmentName(parent)
+
+# 2. Do variables exist
+# Compare the contents of the global environment and rsa_env
+ls.str(globalenv())
+ls.str(rsa_env)
+
+# Does population exist in rsa_env?
+exists("population", envir = rsa_env)
+
+# Does population exist in rsa_env, ignoring inheritance?
+exists("population", envir = rsa_env, inherit = FALSE)
