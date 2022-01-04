903905ab2354e81cc5368947bc9002a1319c0de7
James_McNeill <jmcneill06@outlook.com>
2022-01-04 15:01:17 +0000

    Update case-study-grain-yields.r

--- R/Programming/Writing-functions/case-study-grain-yields.r
+++ R/Programming/Writing-functions/case-study-grain-yields.r
@@ -141,3 +141,12 @@ fortify_with_census_region <- function(data) {
 # Try it on the wheat dataset
 fortify_with_census_region(wheat)
 
+# 3. Plotting yields over time by region
+# Wrap this code into a function
+plot_yield_vs_year_by_region <- function(data) {
+  plot_yield_vs_year(data) +
+    facet_wrap(vars(census_region))
+}
+
+# Try it on the wheat dataset
+plot_yield_vs_year_by_region(wheat)
