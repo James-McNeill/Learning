d8d5707de33dcb49eb049e642ac161a9e9014d70
James_McNeill <jmcneill06@outlook.com>
2022-01-04 14:57:21 +0000

    Update case-study-grain-yields.r

--- R/Programming/Writing-functions/case-study-grain-yields.r
+++ R/Programming/Writing-functions/case-study-grain-yields.r
@@ -126,3 +126,18 @@ plot_yield_vs_year <- function(data) {
 
 # Test it on the wheat dataset
 plot_yield_vs_year(wheat)
+
+# 2. A nation divided
+# Inner join the corn dataset to usa_census_regions by state
+corn %>%
+  inner_join(usa_census_regions, by = "state")
+
+# Wrap this code into a function
+fortify_with_census_region <- function(data) {
+  data %>%
+    inner_join(usa_census_regions, by = "state")
+}
+
+# Try it on the wheat dataset
+fortify_with_census_region(wheat)
+
