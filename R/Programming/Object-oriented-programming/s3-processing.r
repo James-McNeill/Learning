5d29058a2d689d07c3ba6e1be7a4efddc6caef04
James_McNeill <jmcneill06@outlook.com>
2022-01-05 11:40:37 +0000

    Update s3-processing.r

--- R/Programming/Object-oriented-programming/s3-processing.r
+++ R/Programming/Object-oriented-programming/s3-processing.r
@@ -15,3 +15,19 @@ get_n_elements <- function(x, ...)
   UseMethod("get_n_elements")
 }
 
+# 2. Creating an S3 method
+# By itself the generic doesn't do anything. It is the method that helps to perform a task
+# View get_n_elements
+get_n_elements
+
+# Create a data.frame method for get_n_elements
+get_n_elements.data.frame <- function(x, ...)
+{
+    nrow(x) * ncol(x)
+}
+
+# Call the method on the sleep dataset
+n_elements_sleep <- get_n_elements(sleep)
+
+# View the result
+n_elements_sleep
