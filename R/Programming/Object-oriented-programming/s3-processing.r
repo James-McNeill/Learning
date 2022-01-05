a47ce1ef3c008fae91f188b0b0f4e67a793d8d4d
James_McNeill <jmcneill06@outlook.com>
2022-01-05 11:34:32 +0000

    Update s3-processing.r

--- R/Programming/Object-oriented-programming/s3-processing.r
+++ R/Programming/Object-oriented-programming/s3-processing.r
@@ -1,3 +1,17 @@
 # Working with S3
 # S3 is a very simple object-oriented system that lets you define different behavior for functions, depending upon their input argument
 
+# A. Generics and methods or function overload
+# 1. Creating a Generic function
+# The ellipsis is included in case arguments need to be passed from one method to another
+# Overall structure of an S3 generic
+an_s3_generic <- function(x, maybe = "some", other = "arguments", ...) {
+  UseMethod("an_s3_generic")
+}
+
+# Create get_n_elements
+get_n_elements <- function(x, ...)
+{
+  UseMethod("get_n_elements")
+}
+
