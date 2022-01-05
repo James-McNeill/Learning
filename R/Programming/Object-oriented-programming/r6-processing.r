3c2a05866bb50b77665bf03f8cb30617cd389e12
James_McNeill <jmcneill06@outlook.com>
2022-01-05 12:13:10 +0000

    Create r6-processing.r

---
+++ R/Programming/Object-oriented-programming/r6-processing.r
@@ -0,0 +1,3 @@
+# R6 processes
+# Learn how to define R6 classes, and to create R6 objects. You'll also learn about the structure of R6 classes, 
+# and how to separate the user interface from the implementation details
