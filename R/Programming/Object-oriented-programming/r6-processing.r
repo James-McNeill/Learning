05d3f135f654d2108321389ea8bb78c7e95438b5
James_McNeill <jmcneill06@outlook.com>
2022-01-05 12:33:40 +0000

    Update r6-processing.r

--- R/Programming/Object-oriented-programming/r6-processing.r
+++ R/Programming/Object-oriented-programming/r6-processing.r
@@ -30,3 +30,27 @@ microwave_oven_factory
 
 # Make a new microwave oven. The new method doesn't need to be defined when it is being created
 microwave_oven <- microwave_oven_factory$new()
+
+# B. Hiding complexity with Encapsulation
+# The third argument to R6Class() is called public and holds the user-facing functionality for the object. This argument should be a list, with names for each of its elements.
+# The public element of an R6 class contains the functionality available to the user. Usually it will only contain functions
+# 1. Learning to cook
+# Add a cook method to the factory definition
+microwave_oven_factory <- R6Class(
+  "MicrowaveOven",
+  private = list(
+    power_rating_watts = 800
+  ),
+  public = list(
+    cook = function(time_seconds) {
+      Sys.sleep(time_seconds)
+      print("Your food is cooked!")
+    }
+  )
+)
+
+# Create microwave oven object
+a_microwave_oven <- microwave_oven_factory$new()
+
+# Call cook method for 1 second
+a_microwave_oven$cook(1)
