4837f6b8ff4c49af8608400268c30eb0122e2e34
James_McNeill <jmcneill06@outlook.com>
2022-01-07 15:51:03 +0000

    Update advanced-r6.r

--- R/Programming/Object-oriented-programming/advanced-r6.r
+++ R/Programming/Object-oriented-programming/advanced-r6.r
@@ -1,3 +1,12 @@
 # Advanced R6
 
 # A. Environments, Reference Behavior, & Shared Fields
+# 1. Working with Environments
+# Define a new environment
+env <- new.env()
+  
+# Add an element named perfect
+  env$perfect <- list(6, 28, 496) 
+
+# Add an element named bases
+  env[["bases"]] <- list("A", "C", "G", "T")
