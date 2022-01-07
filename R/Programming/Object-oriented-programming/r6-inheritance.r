8ce1f5955372deeef4a27abe9afa8784fc2bb027
James_McNeill <jmcneill06@outlook.com>
2022-01-07 14:40:55 +0000

    Update r6-inheritance.r

--- R/Programming/Object-oriented-programming/r6-inheritance.r
+++ R/Programming/Object-oriented-programming/r6-inheritance.r
@@ -1,4 +1,14 @@
 # R6 inheritance
 
 # A. Functionality with inheritance
-# 1. 
+# 1. Specifying a Fancy Microwave Oven
+# Inheritance is used to propagate – that is, copy – functionality from one class to another
+
+# Explore the microwave oven class
+microwave_oven_factory
+
+# Define a fancy microwave class inheriting from microwave oven
+fancy_microwave_oven_factory <- R6Class(
+    "FancyMicrowaveOven",
+    inherit = microwave_oven_factory
+)
