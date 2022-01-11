50129c65c29441d4e835b28d55e7d8f34aad9029
James_McNeill <jmcneill06@outlook.com>
2022-01-11 16:39:24 +0000

    Update keras-api.py

--- Python/DL/Keras/Advanced/keras-api.py
+++ Python/DL/Keras/Advanced/keras-api.py
@@ -9,3 +9,16 @@ from keras.layers import Input
 # Create an input layer of shape 1. As there is only one feature (column) within the input data
 input_tensor = Input(shape=(1,))
 
+# 2. Dense layers
+# Load layers
+from keras.layers import Input, Dense
+
+# Input layer
+input_tensor = Input(shape=(1,))
+
+# Dense layer
+output_layer = Dense(1)
+
+# Connect the dense layer to the input_tensor
+output_tensor = output_layer(input_tensor)
+
