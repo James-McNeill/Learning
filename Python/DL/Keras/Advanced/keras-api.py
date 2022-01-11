3c4fe1a64240cdf5614db66082bd68a2f16d2afb
James_McNeill <jmcneill06@outlook.com>
2022-01-11 16:56:15 +0000

    Update keras-api.py

--- Python/DL/Keras/Advanced/keras-api.py
+++ Python/DL/Keras/Advanced/keras-api.py
@@ -75,3 +75,13 @@ model.fit(games_tourney_train["seed_diff"], games_tourney_train["score_diff"],
           batch_size=128,
           validation_split=0.1,
           verbose=True)
+
+# 2. Evaluate the model on the test set
+# Load the X variable from the test data
+X_test = games_tourney_test["seed_diff"]
+
+# Load the y variable from the test data
+y_test = games_tourney_test["score_diff"]
+
+# Evaluate the model on the test data
+print(model.evaluate(X_test, y_test, verbose=False))
