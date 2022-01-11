a5d86ca0c352b2ad86606534166a622964217458
James_McNeill <jmcneill06@outlook.com>
2022-01-11 12:41:17 +0000

    Create mapping_locations.py

---
+++ Python/GeoMapping/mapping_locations.py
@@ -0,0 +1,10 @@
+# Location Analysis
+Working with the module Folium
+Sample dataset: ie_towns_sample.csv
+
+Dataset location: https://www.irelandtownslist.com/
+
+The dataset provides a sample of the towns within Ireland beginning with the letter A. With a sample of locations the latitude and longitude can be used to highlight the methods available within the Folium module.
+
+Additional analysis
+Aim to bring in the planning applications data: https://data.gov.ie/dataset/irishplanningapplications
