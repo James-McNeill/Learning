2d9171a0e4b89998aa13c7234e51cad06f0a700a
James_McNeill <jmcneill06@outlook.com>
2022-01-11 12:42:40 +0000

    Update mapping_locations.py

--- Python/GeoMapping/mapping_locations.py
+++ Python/GeoMapping/mapping_locations.py
@@ -1,10 +1,24 @@
 # Location Analysis
-Working with the module Folium
-Sample dataset: ie_towns_sample.csv
+# Working with the module Folium
+# Sample dataset: ie_towns_sample.csv
 
-Dataset location: https://www.irelandtownslist.com/
+# Dataset location: https://www.irelandtownslist.com/
 
-The dataset provides a sample of the towns within Ireland beginning with the letter A. With a sample of locations the latitude and longitude can be used to highlight the methods available within the Folium module.
+# The dataset provides a sample of the towns within Ireland beginning with the letter A. 
+# With a sample of locations the latitude and longitude can be used to highlight the methods available within the Folium module.
 
-Additional analysis
-Aim to bring in the planning applications data: https://data.gov.ie/dataset/irishplanningapplications
+# Additional analysis
+# Aim to bring in the planning applications data: https://data.gov.ie/dataset/irishplanningapplications
+
+# Import modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot
+import seaborn as sns
+import time
+import sys
+
+# check and Install pyathena package.
+# check_and_install_python_mod("geopandas")
+# check_and_install_python_mod("folium")
+
