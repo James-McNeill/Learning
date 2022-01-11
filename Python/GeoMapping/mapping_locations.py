2bd098c769cb384825c6e5fae8297c570ba22285
James_McNeill <jmcneill06@outlook.com>
2022-01-11 12:48:36 +0000

    Update mapping_locations.py

--- Python/GeoMapping/mapping_locations.py
+++ Python/GeoMapping/mapping_locations.py
@@ -1,7 +1,7 @@
 # Location Analysis
 # Working with the module Folium
-# Sample dataset: ie_towns_sample.csv
 
+# Sample dataset: ie_towns_sample.csv
 # Dataset location: https://www.irelandtownslist.com/
 
 # The dataset provides a sample of the towns within Ireland beginning with the letter A. 
@@ -9,6 +9,8 @@
 
 # Additional analysis
 # Aim to bring in the planning applications data: https://data.gov.ie/dataset/irishplanningapplications
+
+# In order to see the map visualization, have to run the analysis using Jupyter Notebook
 
 # Import modules
 import pandas as pd
@@ -26,3 +28,36 @@ import sys
 import geopandas as gpd
 import folium
 
+# Bring in the Irish towns dataset
+df = pd.read_csv('ie_towns_sample.csv')
+df.head()
+
+# Review the columns within the DataFrame
+df.columns
+
+# Clustering the data
+from folium.plugins import MarkerCluster
+
+# Starting datapoint locations
+Lat = 53.349921
+Long = -6.260265
+
+# Created a zipped list of the location data points
+locations = list(zip(df.latitude, df.longitude))
+
+# Create the intial map template
+map1 = folium.Map(location=[Lat,Long], zoom_start=10)
+
+# Add the data clusters to the map that was created
+markerCluster = MarkerCluster(data=locations).add_to(map1)
+
+# Provide data point details for each location
+for p in range(0, len(locations)):
+    #folium.Marker(locations[p], popup=df['name'][p]).add_to(markerCluster)
+    folium.Marker(locations[p] 
+                  ,popup=df['name'][p]+',\n'+df['postal_town'][p]
+                  +',\n'+df['county'][p]+',\n'+df['nuts3_region'][p]
+                 ).add_to(markerCluster)
+
+# Display the map
+map1
