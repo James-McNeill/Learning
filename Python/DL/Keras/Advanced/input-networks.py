571b153395ad713b0515225a6a5859787a8dbcd5
James_McNeill <jmcneill06@outlook.com>
2022-01-12 16:54:28 +0000

    Update input-networks.py

--- Python/DL/Keras/Advanced/input-networks.py
+++ Python/DL/Keras/Advanced/input-networks.py
@@ -54,3 +54,12 @@ team_1_strength = team_strength_model(team_in_1)
 
 # Lookup team 2 in the team strength model
 team_2_strength = team_strength_model(team_in_2)
+
+# C. Merge layers
+# 1. Output layer using shared layer
+# Import the Subtract layer from keras
+from keras.layers import Subtract
+
+# Create a subtract layer using the inputs from the previous exercise. Subtracts the team strength rating to determine a winner
+score_diff = Subtract()([team_1_strength, team_2_strength])
+
