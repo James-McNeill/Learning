ed6cba7e266dd0ca41a1022777f30cbe43555b16
James_McNeill <jmcneill06@outlook.com>
2022-01-12 17:03:04 +0000

    Update input-networks.py

--- Python/DL/Keras/Advanced/input-networks.py
+++ Python/DL/Keras/Advanced/input-networks.py
@@ -93,3 +93,13 @@ model.fit([input_1, input_2],
           validation_split=0.10,
           verbose=True)
 
+# 2. Evaluate the model on the tournament test data
+# Interesting to see how well the model fits when some teams don't play each other during the regular season. Will see if the model overfits
+# Get team_1 from the tournament data
+input_1 = games_tourney["team_1"]
+
+# Get team_2 from the tournament data
+input_2 = games_tourney["team_2"]
+
+# Evaluate the model using these inputs
+print(model.evaluate([input_1, input_2], games_tourney["score_diff"], verbose=False))
