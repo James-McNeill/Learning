33523deb3ede19cce4c10d900df83d2d34eb6f56
James_McNeill <jmcneill06@outlook.com>
2022-01-12 16:39:07 +0000

    Update input-networks.py

--- Python/DL/Keras/Advanced/input-networks.py
+++ Python/DL/Keras/Advanced/input-networks.py
@@ -1,3 +1,17 @@
 # Input networks
 
-# A. 
+# A. Category embeddings
+# 1. Define team lookup
+# The embedding layer is a lot like a dictionary, but your model learns the values for each key
+# Imports
+from keras.layers import Embedding
+from numpy import unique
+
+# Count the unique number of teams
+n_teams = unique(games_season["team_1"]).shape[0]
+
+# Create an embedding layer
+team_lookup = Embedding(input_dim=n_teams,
+                        output_dim=1, # creating 1 output value by team
+                        input_length=1, # each team is represented by a unique id in the dataset for each game that took place
+                        name='Team-Strength')
