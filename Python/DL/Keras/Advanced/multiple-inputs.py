df1668a60f60bfe4cf4db8e43926a5f04b88ac7d
James_McNeill <jmcneill06@outlook.com>
2022-01-13 12:19:15 +0000

    Update multiple-inputs.py

--- Python/DL/Keras/Advanced/multiple-inputs.py
+++ Python/DL/Keras/Advanced/multiple-inputs.py
@@ -72,3 +72,9 @@ model = Model(input_tensor, output_tensor)
 # Compile the model
 model.compile(optimizer="adam", loss="mean_absolute_error")
 
+# 3. Fit the model
+# Fit the model
+model.fit(games_tourney_train[['home', 'seed_diff', 'pred']],
+          games_tourney_train['score_diff'],
+          epochs=1,
+          verbose=True)
