6ece9104cf99ed42615d89c5f9e866c24e5eaa90
James_McNeill <jmcneill06@outlook.com>
2022-01-13 16:04:11 +0000

    Update multiple-outputs.py

--- Python/DL/Keras/Advanced/multiple-outputs.py
+++ Python/DL/Keras/Advanced/multiple-outputs.py
@@ -35,3 +35,20 @@ print(games_tourney_train.mean())
 # Evaluate the model on the tournament test data
 print(model.evaluate(games_tourney_test[['seed_diff', 'pred']], 
         games_tourney_test[['score_1', 'score_2']], verbose=False))
+
+# B. Single model for classification and regression
+# 1. Classification and regression in one model
+# In this model, turn off the bias, or intercept for each layer. Note this kind of model is only possible with a Neural Network
+# Create an input layer with 2 columns
+input_tensor = Input((2,))
+
+# Create the first output
+output_tensor_1 = Dense(1, activation='linear', use_bias=False)(input_tensor)
+
+# Create the second output (use the first output as input here)
+output_tensor_2 = Dense(1, activation='sigmoid', use_bias=False)(output_tensor_1)
+
+# Create a model with 2 outputs
+model = Model(input_tensor, [output_tensor_1, output_tensor_2])
+
+# 2. 
