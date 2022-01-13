e5151b6457238c48bd24b18684361a9ab74c3995
James_McNeill <jmcneill06@outlook.com>
2022-01-13 10:55:48 +0000

    Create multiple-inputs.py

---
+++ Python/DL/Keras/Advanced/multiple-inputs.py
@@ -0,0 +1,18 @@
+# Building a model with multiple inputs
+
+# A. Three input models
+# 1. Make an input layer for home Vs away
+# Create an Input for each team
+team_in_1 = Input(shape=(1,), name='Team-1-In')
+team_in_2 = Input(shape=(1,), name='Team-2-In')
+
+# Create an input for home vs away
+home_in = Input(shape=(1,), name='Home-In')
+
+# Lookup the team inputs in the team strength model (taken from previous chapter)
+team_1_strength = team_strength_model(team_in_1)
+team_2_strength = team_strength_model(team_in_2)
+
+# Combine the team strengths with the home input using a Concatenate layer, then add a Dense layer
+out = Concatenate()([team_1_strength, team_2_strength, home_in])
+out = Dense(1)(out)
