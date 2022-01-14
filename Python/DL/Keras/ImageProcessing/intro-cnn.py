b97b9224c66d6114d07a469d98554221cdd41393
James_McNeill <jmcneill06@outlook.com>
2022-01-14 12:02:18 +0000

    Update intro-cnn.py

--- Python/DL/Keras/ImageProcessing/intro-cnn.py
+++ Python/DL/Keras/ImageProcessing/intro-cnn.py
@@ -28,3 +28,22 @@ data[:10, :10, 2] = 0
 # Visualize the result
 plt.imshow(data)
 plt.show()
+
+# B. Classifying images
+# Providing the algorithm with labels allows it to learn which features to train for
+# 1. Using one-hot encoding to represent images
+# The number of image categories
+n_categories = 3
+
+# The unique values of categories in the data
+categories = np.array(["shirt", "dress", "shoe"])
+
+# Initialize ohe_labels as all zeros. Creates the 2D array to map each row and column
+ohe_labels = np.zeros((len(labels), n_categories))
+
+# Loop over the labels
+for ii in range(len(labels)):
+    # Find the location of this label in the categories variable
+    jj = np.where(categories == labels[ii])
+    # Set the corresponding zero to one
+    ohe_labels[ii, jj] = 1
