0445897e007474042dbe242eecc47a49ba76559b
James_McNeill <jmcneill06@outlook.com>
2022-01-14 11:56:06 +0000

    Update intro-cnn.py

--- Python/DL/Keras/ImageProcessing/intro-cnn.py
+++ Python/DL/Keras/ImageProcessing/intro-cnn.py
@@ -13,3 +13,18 @@ data = plt.imread('bricks.png')
 plt.imshow(data)
 plt.show()
 
+# 2. Changing images
+# The last dimension of the data allows the user to interact with the Red Green Blue (RGB) dynamics of the image.
+# If all three dimensions where set to 1 then the image would display a yellow colour
+# Set the red channel in this part of the image to 1
+data[:10, :10, 0] = 1
+
+# Set the green channel in this part of the image to 0
+data[:10, :10, 1] = 0
+
+# Set the blue channel in this part of the image to 0
+data[:10, :10, 2] = 0
+
+# Visualize the result
+plt.imshow(data)
+plt.show()
