54143c6a672ddf46f8b8815db6f6db49684d462d
James_McNeill <jmcneill06@outlook.com>
2022-01-14 16:18:51 +0000

    Create deeper-cnn.py

---
+++ Python/DL/Keras/ImageProcessing/deeper-cnn.py
@@ -0,0 +1,9 @@
+# Building deeper CNN
+# The term deep learning comes from this idea of building multiple layers to learn
+
+# For an image multiple elements in the image need to be reviewed. Therefore, each deep learning layer is aiming to understand a different
+# element of the image. Similar to drawing an image, different sections require a different process to complete and are processed in a multi-layered
+# order. This is now the aim of building a deeper network to help follow different aspects with each layer
+
+# A. Going deeper
+# 1. Creating a deep learning network
