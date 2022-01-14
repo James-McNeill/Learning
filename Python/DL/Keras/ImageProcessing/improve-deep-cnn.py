ebf5927867b21da91a0dcd5d2715bfeff5dd2815
James_McNeill <jmcneill06@outlook.com>
2022-01-14 16:53:19 +0000

    Update improve-deep-cnn.py

--- Python/DL/Keras/ImageProcessing/improve-deep-cnn.py
+++ Python/DL/Keras/ImageProcessing/improve-deep-cnn.py
@@ -26,3 +26,14 @@ model.load_weights('weights.hdf5')
 
 # Predict from the first three images in the test data
 model.predict(test_data[:3])
+
+# 3. Storing the optimal parameters
+from keras.callbacks import ModelCheckpoint
+# This checkpoint object will store the model parameters in the file "weights.hdf5"
+checkpoint = ModelCheckpoint('weights.hdf5', monitor='val_loss',
+                             save_best_only=True) # the model with the lowest val_loss will be stored
+# Store in a list to be used during training
+callbacks_list = [checkpoint]
+# Fit the model on a training set, using the checkpoint as a callback
+model.fit(train_data, train_labels, validation_split=0.2,           
+          epochs=3, callbacks=callbacks_list)
