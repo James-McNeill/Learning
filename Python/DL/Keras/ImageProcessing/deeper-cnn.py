0c3f56fbb5bd4af613fb5c048af6589fd333efa7
James_McNeill <jmcneill06@outlook.com>
2022-01-14 16:22:31 +0000

    Update deeper-cnn.py

--- Python/DL/Keras/ImageProcessing/deeper-cnn.py
+++ Python/DL/Keras/ImageProcessing/deeper-cnn.py
@@ -7,3 +7,19 @@
 
 # A. Going deeper
 # 1. Creating a deep learning network
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+
+model = Sequential()
+
+# Add a convolutional layer (15 units)
+model.add(Conv2D(15, kernel_size=2, activation='relu',
+    input_shape=(img_rows, img_cols, 1)
+))
+
+# Add another convolutional layer (5 units). As the second convolution layer takes input from the previous layer it does not require an input shape parameter
+model.add(Conv2D(5, kernel_size=2, activation='relu'))
+
+# Flatten and feed to output layer
+model.add(Flatten())
+model.add(Dense(3, activation='softmax'))
