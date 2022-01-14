c5fb8a678d231c602578856504ff168fa5bf48df
James_McNeill <jmcneill06@outlook.com>
2022-01-14 17:02:11 +0000

    Update improve-deep-cnn.py

--- Python/DL/Keras/ImageProcessing/improve-deep-cnn.py
+++ Python/DL/Keras/ImageProcessing/improve-deep-cnn.py
@@ -37,3 +37,20 @@ callbacks_list = [checkpoint]
 # Fit the model on a training set, using the checkpoint as a callback
 model.fit(train_data, train_labels, validation_split=0.2,           
           epochs=3, callbacks=callbacks_list)
+
+# B. Regularization
+# 1. Adding a dropout to the CNN
+# Dropout is a form of regularization that removes a different random subset of the units in a layer in each round of training
+# Add a convolutional layer
+model.add(Conv2D(15, kernel_size=2, activation='relu', 
+                 input_shape=(img_rows, img_cols, 1)))
+
+# Add a dropout layer
+model.add(Dropout(0.20))
+
+# Add another convolutional layer
+model.add(Conv2D(5, kernel_size=2, activation='relu'))
+
+# Flatten and feed to output layer
+model.add(Flatten())
+model.add(Dense(3, activation='softmax'))
