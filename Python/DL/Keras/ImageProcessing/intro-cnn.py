fc0b8094b32f4b3ecfe1a0f15beec3c0d9b08d52
James_McNeill <jmcneill06@outlook.com>
2022-01-14 11:47:28 +0000

    Create intro-cnn.py

---
+++ Python/DL/Keras/ImageProcessing/intro-cnn.py
@@ -0,0 +1,15 @@
+# Image Processing With Neural Networks
+
+# A. Intro to CNN
+# Convolutional neural networks use the data that is represented in images to learn
+# 1. Images as data: visualizations
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Load the image
+data = plt.imread('bricks.png')
+
+# Display the image
+plt.imshow(data)
+plt.show()
+
