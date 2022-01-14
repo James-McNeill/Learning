8bbf820090e670b89034dc8f68ca04d39d07ca9f
James_McNeill <jmcneill06@outlook.com>
2022-01-14 16:03:43 +0000

    Update convolutions.py

--- Python/DL/Keras/ImageProcessing/convolutions.py
+++ Python/DL/Keras/ImageProcessing/convolutions.py
@@ -48,3 +48,23 @@ kernel = np.array([[-1, -1, -1],
 kernel = np.array([[1, 1, 1], 
                    [1, -1, 1],
                    [1, 1, 1]])
+
+# B. Implementing image convolutions in Keras
+# 1. Convolutional network for image classification
+# Import the necessary components from Keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+
+# Initialize the model object
+model = Sequential()
+
+# Add a convolutional layer
+model.add(Conv2D(10, kernel_size=3, activation='relu', 
+               input_shape=(img_rows, img_cols, 1)))
+
+# Flatten the output of the convolutional layer
+model.add(Flatten())
+# Add an output layer for the 3 categories
+model.add(Dense(3, activation='softmax'))
+
+# 2. 
