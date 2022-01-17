c0381c74d93ef5758b850acae2297fb2b44aff74
James_McNeill <jmcneill06@outlook.com>
2022-01-17 16:05:18 +0000

    Create inspect-function.py

---
+++ Python/ML-Hyperparameter-Tuning/inspect-function.py
@@ -0,0 +1,26 @@
+# For a function that has been created we are able to display the definition by using inspect
+
+# Use the function to display the output code
+print(inspect.getsource(sample_and_visualize_hyperparameters))
+
+# The function that was created
+def sample_and_visualize_hyperparameters(n_samples):
+
+  # If asking for all combinations, just return the entire list.
+  if n_samples == len(combinations_list):
+    combinations_random_chosen = combinations_list
+  else:
+    combinations_random_chosen = []
+    random_combinations_index = np.random.choice(range(0, len(combinations_list)), n_samples, replace=False)
+    combinations_random_chosen = [combinations_list[x] for x in random_combinations_index]
+    
+  # Pull out the X and Y to plot
+  rand_y, rand_x = [x[0] for x in combinations_random_chosen], [x[1] for x in combinations_random_chosen]
+
+  # Plot 
+  plt.clf() 
+  plt.scatter(rand_y, rand_x, c=['blue']*len(combinations_random_chosen))
+  plt.gca().set(xlabel='learn_rate', ylabel='min_samples_leaf', title='Random Search Hyperparameters')
+  plt.gca().set_xlim(x_lims)
+  plt.gca().set_ylim(y_lims)
+  plt.show()
