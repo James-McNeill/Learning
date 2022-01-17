a71bb959759d17a17cb3bff9ed5547331d5073da
James_McNeill <jmcneill06@outlook.com>
2022-01-17 11:52:51 +0000

    Update grid-search.py

--- Python/ML-Hyperparameter-Tuning/grid-search.py
+++ Python/ML-Hyperparameter-Tuning/grid-search.py
@@ -1,4 +1,15 @@
 # Grid Search
 
 # A. Introducing Grid Search
-# 1. 
+# 1. Build grid search functions
+# Create the function
+def gbm_grid_search(learning_rate, max_depth):
+
+	# Create the model
+    model = GradientBoostingClassifier(learning_rate=learning_rate, max_depth=max_depth)
+    
+    # Use the model to make predictions
+    predictions = model.fit(X_train, y_train).predict(X_test)
+    
+    # Return the hyperparameters and score
+    return([learning_rate, max_depth, accuracy_score(y_test, predictions)])
