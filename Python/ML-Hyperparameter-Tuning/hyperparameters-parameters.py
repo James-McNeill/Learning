b8e5e54d1f730582c8f86c0d284e7fb5b5eac9bf
James_McNeill <jmcneill06@outlook.com>
2022-01-17 10:19:18 +0000

    Update hyperparameters-parameters.py

--- Python/ML-Hyperparameter-Tuning/hyperparameters-parameters.py
+++ Python/ML-Hyperparameter-Tuning/hyperparameters-parameters.py
@@ -1,4 +1,18 @@
 # Hyperparameters and parameters
 
 # A. Introduction
-# 1.
+# 1. Extracting a logistic regression parameter
+# Create a list of original variable names from the training DataFrame
+original_variables = X_train.columns
+
+# Extract the coefficients of the logistic regression estimator
+model_coefficients = log_reg_clf.coef_[0]
+
+# Create a dataframe of the variables and coefficients & print it out
+coefficient_df = pd.DataFrame({"Variable" : original_variables, "Coefficient": model_coefficients})
+print(coefficient_df)
+
+# Print out the top 3 positive variables
+top_three_df = coefficient_df.sort_values(by=["Coefficient"], axis=0, ascending=False)[0:3]
+print(top_three_df)
+
