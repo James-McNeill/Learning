2578cce5ce02bcfbea915346c62c42b4ab54454a
James_McNeill <jmcneill06@outlook.com>
2022-01-17 11:56:36 +0000

    Update grid-search.py

--- Python/ML-Hyperparameter-Tuning/grid-search.py
+++ Python/ML-Hyperparameter-Tuning/grid-search.py
@@ -13,3 +13,28 @@ def gbm_grid_search(learning_rate, max_depth):
     
     # Return the hyperparameters and score
     return([learning_rate, max_depth, accuracy_score(y_test, predictions)])
+
+# 2. Iteratively tune multiple hyperparameters
+# Create the relevant lists
+results_list = []
+learn_rate_list = [0.01, 0.1, 0.5]
+max_depth_list = [2, 4, 6]
+
+# Create the for loop
+for learn_rate in learn_rate_list:
+    for max_depth in max_depth_list:
+        results_list.append(gbm_grid_search(learn_rate,max_depth))
+
+# Print the results
+print(results_list)   
+
+# Extend the function input
+def gbm_grid_search_extended(learn_rate, max_depth, subsample):
+
+	# Extend the model creation section
+    model = GradientBoostingClassifier(learning_rate=learn_rate, max_depth=max_depth, subsample=subsample)
+    
+    predictions = model.fit(X_train, y_train).predict(X_test)
+    
+    # Extend the return part
+    return([learn_rate, max_depth, subsample, accuracy_score(y_test, predictions)]) 
