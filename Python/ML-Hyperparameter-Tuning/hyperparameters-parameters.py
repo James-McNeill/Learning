948106477e009fd34b65f1778bb40ebfc9dda619
James_McNeill <jmcneill06@outlook.com>
2022-01-17 10:35:22 +0000

    Update hyperparameters-parameters.py

--- Python/ML-Hyperparameter-Tuning/hyperparameters-parameters.py
+++ Python/ML-Hyperparameter-Tuning/hyperparameters-parameters.py
@@ -54,3 +54,19 @@ rf_new_predictions = rf_clf_new.fit(X_train, y_train).predict(X_test)
 print("Confusion Matrix: \n\n", confusion_matrix(y_test, rf_new_predictions))
 print("Accuracy Score: \n\n", accuracy_score(y_test, rf_new_predictions))
 
+# 2. Hyperparameters of KNN classifier
+# Build a knn estimator for each value of n_neighbours
+knn_5 = KNeighborsClassifier(n_neighbors=5)
+knn_10 = KNeighborsClassifier(n_neighbors=10)
+knn_20 = KNeighborsClassifier(n_neighbors=20)
+
+# Fit each to the training data & produce predictions
+knn_5_predictions = knn_5.fit(X_train, y_train).predict(X_test)
+knn_10_predictions = knn_10.fit(X_train, y_train).predict(X_test)
+knn_20_predictions = knn_20.fit(X_train, y_train).predict(X_test)
+
+# Get an accuracy score for each of the models
+knn_5_accuracy = accuracy_score(y_test, knn_5_predictions)
+knn_10_accuracy = accuracy_score(y_test, knn_10_predictions)
+knn_20_accuracy = accuracy_score(y_test, knn_20_predictions)
+print("The accuracy of 5, 10, 20 neighbours was {}, {}, {}".format(knn_5_accuracy, knn_10_accuracy, knn_20_accuracy))
