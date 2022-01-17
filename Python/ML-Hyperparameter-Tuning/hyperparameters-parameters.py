9990c43d91dd9040a7a5332acf9b1dade61258ac
James_McNeill <jmcneill06@outlook.com>
2022-01-17 10:32:34 +0000

    Update hyperparameters-parameters.py

--- Python/ML-Hyperparameter-Tuning/hyperparameters-parameters.py
+++ Python/ML-Hyperparameter-Tuning/hyperparameters-parameters.py
@@ -1,6 +1,7 @@
 # Hyperparameters and parameters
 
-# A. Introduction
+# A. Introduction to parameters
+# The parameters that the ML model learns when the algorithm is processing
 # 1. Extracting a logistic regression parameter
 # Create a list of original variable names from the training DataFrame
 original_variables = X_train.columns
@@ -31,3 +32,25 @@ split_value = chosen_tree.tree_.threshold[0]
 
 # Print out the feature and level
 print("This node split on feature {}, at a value of {}".format(split_column_name, split_value))
+
+# B. Introduction to hyperparameters
+# The parameters that can be adjusted prior to the ML model running. Make sure to understand which default parameters make most sense to adjust
+# 1. Exploring Random Forest Hyperparameters
+# Print out the old estimator, notice which hyperparameter is badly set
+print(rf_clf_old)
+
+# Get confusion matrix & accuracy for the old rf_model
+print("Confusion Matrix: \n\n {} \n Accuracy Score: \n\n {}".format(
+  confusion_matrix(y_test, rf_old_predictions),
+  accuracy_score(y_test, rf_old_predictions))) 
+
+# Create a new random forest classifier with better hyperparamaters
+rf_clf_new = RandomForestClassifier(n_estimators=500)
+
+# Fit this to the data and obtain predictions
+rf_new_predictions = rf_clf_new.fit(X_train, y_train).predict(X_test)
+
+# Assess the new model (using new predictions!)
+print("Confusion Matrix: \n\n", confusion_matrix(y_test, rf_new_predictions))
+print("Accuracy Score: \n\n", accuracy_score(y_test, rf_new_predictions))
+
