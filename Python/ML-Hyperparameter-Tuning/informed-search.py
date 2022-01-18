90a00849d4b26a7276a21f1872fe08a704d7be52
James_McNeill <jmcneill06@outlook.com>
2022-01-18 10:42:31 +0000

    Update informed-search.py

--- Python/ML-Hyperparameter-Tuning/informed-search.py
+++ Python/ML-Hyperparameter-Tuning/informed-search.py
@@ -3,3 +3,27 @@
 
 # A. Introduction to informed search
 # 1. Visualizing Coarse to Fine
+
+import inspect
+print(inspect.getsource(visualize_hyperparameter))
+
+def visualize_hyperparameter(name):
+  plt.clf()
+  plt.scatter(results_df[name],results_df['accuracy'], c=['blue']*500)
+  plt.gca().set(xlabel='{}'.format(name), ylabel='accuracy', title='Accuracy for different {}s'.format(name))
+  plt.gca().set_ylim([0,100])
+  plt.show()
+
+# Confirm the size of the combinations_list
+print(len(combinations_list))
+
+# Sort the results_df by accuracy and print the top 10 rows
+print(results_df.sort_values(by='accuracy', ascending=False).head(10))
+
+# Confirm which hyperparameters were used in this search
+print(results_df.columns)
+
+# Call visualize_hyperparameter() with each hyperparameter in turn
+visualize_hyperparameter('max_depth')
+visualize_hyperparameter('min_samples_leaf')
+visualize_hyperparameter('learn_rate')
