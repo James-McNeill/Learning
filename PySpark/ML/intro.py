c3c1f1c1121fd0b178a35eeb43a283fc81017c00
James_McNeill <jmcneill06@outlook.com>
2022-01-18 12:19:29 +0000

    Update intro.py

--- PySpark/ML/intro.py
+++ PySpark/ML/intro.py
@@ -40,4 +40,18 @@ flights.show(5)
 # Check column data types
 print(flights.dtypes)
 
-# 2. 
+# 2. Loading SMS spam data
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+# Specify column names and types
+schema = StructType([
+    StructField("id", IntegerType()),
+    StructField("text", StringType()),
+    StructField("label", IntegerType())
+])
+
+# Load data from a delimited file
+sms = spark.read.csv('sms.csv', sep=';', header=False, schema=schema)
+
+# Print schema of DataFrame
+sms.printSchema()
