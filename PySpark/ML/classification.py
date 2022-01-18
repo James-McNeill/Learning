2744a0313f37f501969f2553a788d661420014bc
James_McNeill <jmcneill06@outlook.com>
2022-01-18 12:59:03 +0000

    Update classification.py

--- PySpark/ML/classification.py
+++ PySpark/ML/classification.py
@@ -79,3 +79,29 @@ flights_train, flights_test = flights.randomSplit([0.8, 0.2], seed=17)
 # Check that training set has around 80% of records
 training_ratio = flights_train.count() / flights.count()
 print(training_ratio)
+
+# 2. Build a decision tree
+# Import the Decision Tree Classifier class
+from pyspark.ml.classification import DecisionTreeClassifier
+
+# Create a classifier object and fit to the training data
+tree = DecisionTreeClassifier()
+tree_model = tree.fit(flights_train)
+
+# Create predictions for the testing data and take a look at the predictions
+prediction = tree_model.transform(flights_test)
+prediction.select('label', 'prediction', 'probability').show(5, False)
+
+# 3. Evaluate the Decision Tree
+# Create a confusion matrix
+prediction.groupBy('label', 'prediction').count().show()
+
+# Calculate the elements of the confusion matrix
+TN = prediction.filter('prediction = 0 AND label = prediction').count()
+TP = prediction.filter('prediction = 1 AND label = prediction').count()
+FN = prediction.filter('prediction = 0 AND label = 1').count()
+FP = prediction.filter('prediction = 1 AND label = 0').count()
+
+# Accuracy measures the proportion of correct predictions
+accuracy = (TN + TP) / (TN + TP + FN + FP)
+print(accuracy)
