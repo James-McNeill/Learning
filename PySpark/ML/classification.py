20512c1338db364a2be25ccd6c323e0a481e6a0f
James_McNeill <jmcneill06@outlook.com>
2022-01-18 12:36:02 +0000

    Update classification.py

--- PySpark/ML/classification.py
+++ PySpark/ML/classification.py
@@ -17,3 +17,15 @@ flights_none_missing = flights_valid_delay.dropna()
 print(flights_none_missing.count())
 
 # 2. Column manipulation
+# Import the required function
+from pyspark.sql.functions import round
+
+# Convert 'mile' to 'km' and drop 'mile' column (1 mile is equivalent to 1.60934 km)
+flights_km = flights.withColumn('km', round(flights.mile * 1.60934, 0)) \
+                    .drop('mile')
+
+# Create 'label' column indicating whether flight delayed (1) or not (0)
+flights_km = flights_km.withColumn('label', (flights.delay >= 15).cast('integer'))
+
+# Check first five records
+flights_km.show(5)
