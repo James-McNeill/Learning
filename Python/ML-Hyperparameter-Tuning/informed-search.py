09d5d4e96857c0df635c64b1bcbebee26f288738
James_McNeill <jmcneill06@outlook.com>
2022-01-18 10:46:52 +0000

    Update informed-search.py

--- Python/ML-Hyperparameter-Tuning/informed-search.py
+++ Python/ML-Hyperparameter-Tuning/informed-search.py
@@ -27,3 +27,26 @@ print(results_df.columns)
 visualize_hyperparameter('max_depth')
 visualize_hyperparameter('min_samples_leaf')
 visualize_hyperparameter('learn_rate')
+
+# 2. Coarse to fine iterations
+def visualize_first():
+  for name in results_df.columns[0:2]:
+    plt.clf()
+    plt.scatter(results_df[name],results_df['accuracy'], c=['blue']*500)
+    plt.gca().set(xlabel='{}'.format(name), ylabel='accuracy', title='Accuracy for different {}s'.format(name))
+    plt.gca().set_ylim([0,100])
+    x_line = 20   # shows the cut-off value for the max depth
+    if name == "learn_rate":
+      	x_line = 1
+    plt.axvline(x=x_line, color="red", linewidth=4)
+    plt.show() 
+
+# Use the provided function to visualize the first results
+# visualize_first()
+
+# Create some combinations lists & combine:
+max_depth_list = list(range(1,21))
+learn_rate_list = np.linspace(0.001,1,50)
+
+# Call the function to visualize the second results. For this function the results_df was updated with the new list's above to see the difference in model accuracy
+visualize_second()
