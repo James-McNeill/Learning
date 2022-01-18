fe46bb4b92f5a07b78fea2b6cf10dfdbc691f253
James_McNeill <jmcneill06@outlook.com>
2022-01-18 12:31:13 +0000

    Update classification.py

--- PySpark/ML/classification.py
+++ PySpark/ML/classification.py
@@ -2,4 +2,18 @@
 # Building two types of classification model: Decision Trees and Logistic Regression
 
 # A. Data Preparation
-# 1. 
+# 1. Removing columns and rows
+# Remove the 'flight' column
+flights_drop_column = flights.drop('flight')
+
+# Number of records with missing 'delay' values
+flights_drop_column.filter('delay IS NULL').count()
+
+# Remove records with missing 'delay' values
+flights_valid_delay = flights_drop_column.filter('delay IS NOT NULL')
+
+# Remove records with missing values in any column and get the number of remaining rows
+flights_none_missing = flights_valid_delay.dropna()
+print(flights_none_missing.count())
+
+# 2. Column manipulation
