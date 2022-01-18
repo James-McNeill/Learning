0a2d9937a92e461c868ded5563016bc082f58bfc
James_McNeill <jmcneill06@outlook.com>
2022-01-18 10:32:48 +0000

    Update random-search.py

--- Python/ML-Hyperparameter-Tuning/random-search.py
+++ Python/ML-Hyperparameter-Tuning/random-search.py
@@ -82,3 +82,32 @@ random_rf_class.fit(X_train, y_train)
 # Print the values used for both hyperparameters
 print(random_rf_class.cv_results_['param_max_depth'])
 print(random_rf_class.cv_results_['param_max_features'])
+
+# C. Comparing grid and random search
+# 1. Grid and Random Search Side by Side
+def visualize_search(grid_combinations_chosen, random_combinations_chosen):
+  grid_y, grid_x = [x[0] for x in grid_combinations_chosen], [x[1] for x in grid_combinations_chosen]
+  rand_y, rand_x = [x[0] for x in random_combinations_chosen], [x[1] for x in random_combinations_chosen]
+
+  # Plot all together
+  plt.scatter(grid_y + rand_y, grid_x + rand_x, c=['red']*300 + ['blue']*300)
+  plt.gca().set(xlabel='learn_rate', ylabel='min_samples_leaf', title='Grid and Random Search Hyperparameters')
+  plt.gca().set_xlim(x_lims)
+  plt.gca().set_ylim(y_lims)
+  plt.show()
+
+# Sample grid coordinates
+grid_combinations_chosen = combinations_list[0:300]
+
+# Create a list of sample indexes
+sample_indexes = list(range(0,len(combinations_list)))
+
+# Randomly sample 300 indexes
+random_indexes = np.random.choice(sample_indexes, 300, replace=False)
+
+# Use indexes to create random sample
+random_combinations_chosen = [combinations_list[index] for index in random_indexes]
+
+# Call the function to produce the visualization
+# You can really see how a grid search will cover a small area completely whilst random search will cover a much larger area but not completely.
+visualize_search(grid_combinations_chosen, random_combinations_chosen)
