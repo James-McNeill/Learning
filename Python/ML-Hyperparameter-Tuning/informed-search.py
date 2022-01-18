9585821dae9f342ac330d665e5ef0817578bcf90
James_McNeill <jmcneill06@outlook.com>
2022-01-18 10:36:32 +0000

    Create informed-search.py

---
+++ Python/ML-Hyperparameter-Tuning/informed-search.py
@@ -0,0 +1,5 @@
+# Informed search
+# This includes a methodology known as Coarse To Fine as well as Bayesian & Genetic hyperparameter tuning algorithms
+
+# A. Introduction to informed search
+# 1. Visualizing Coarse to Fine
