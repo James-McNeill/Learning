7e9d5d27162c5e791af4eab636bd5ca14df26f23
James_McNeill <jmcneill06@outlook.com>
2022-01-18 11:37:01 +0000

    Update informed-search.py

--- Python/ML-Hyperparameter-Tuning/informed-search.py
+++ Python/ML-Hyperparameter-Tuning/informed-search.py
@@ -80,3 +80,22 @@ def objective(params):
 # Run the algorithm
 best = fmin(fn=objective,space=space, max_evals=20, rstate=np.random.RandomState(42), algo=tpe.suggest)
 print(best)
+
+# C. Genetic Algorithms
+# 1. Genetic Hyperparameter Tuning with TPOT
+# Assign the values outlined to the inputs
+number_generations = 3
+population_size = 4
+offspring_size = 3
+scoring_function = 'accuracy'
+
+# Create the tpot classifier
+tpot_clf = TPOTClassifier(generations=number_generations, population_size=population_size,
+                          offspring_size=offspring_size, scoring=scoring_function,
+                          verbosity=2, random_state=2, cv=2)
+
+# Fit the classifier to the training data
+tpot_clf.fit(X_train, y_train)
+
+# Score on the test set
+print(tpot_clf.score(X_test, y_test))
