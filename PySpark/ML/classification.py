5fb1a4b0a53f1aaee50bc6ae994e048c404b83f8
James_McNeill <jmcneill06@outlook.com>
2022-01-19 12:17:22 +0000

    Update classification.py

--- PySpark/ML/classification.py
+++ PySpark/ML/classification.py
@@ -118,3 +118,20 @@ logistic = LogisticRegression().fit(flights_train)
 prediction = logistic.transform(flights_test)
 prediction.groupBy('label', 'prediction').count().show()
 
+# 2. Evaluate the Log Reg model
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator, BinaryClassificationEvaluator
+
+# Calculate precision and recall
+precision = TP / (TP + FP)
+recall = TP / (TP + FN)
+print('precision = {:.2f}\nrecall    = {:.2f}'.format(precision, recall))
+
+# Find weighted precision
+multi_evaluator = MulticlassClassificationEvaluator()
+weighted_precision = multi_evaluator.evaluate(prediction, {multi_evaluator.metricName: "weightedPrecision"})
+
+# Find AUC
+binary_evaluator = BinaryClassificationEvaluator()
+auc = binary_evaluator.evaluate(prediction, {binary_evaluator.metricName:"areaUnderROC"})
+
+# D. 
