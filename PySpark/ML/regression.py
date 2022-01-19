7156f80fd6c92ca1582dcdaba6bb4fa0283ddb39
James_McNeill <jmcneill06@outlook.com>
2022-01-19 12:50:54 +0000

    Update regression.py

--- PySpark/ML/regression.py
+++ PySpark/ML/regression.py
@@ -31,4 +31,17 @@ SparseVector(8, [0, 5], [1, 7])
 # Result
 SparseVector(8, {0: 1.0, 5: 7.0})
 
-# B.
+# B. Regression
+# 1. Flight duration model: Just distance
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# Create a regression object and train on training data
+regression = LinearRegression(labelCol='duration').fit(flights_train)
+
+# Create predictions for the testing data and take a look at the predictions
+predictions = regression.transform(flights_test)
+predictions.select('duration', 'prediction').show(5, False)
+
+# Calculate the RMSE
+RegressionEvaluator(labelCol='duration').evaluate(predictions)
