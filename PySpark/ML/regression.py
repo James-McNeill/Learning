904be09e8bf285fc271b1c7a449e3e023e426f02
James_McNeill <jmcneill06@outlook.com>
2022-01-20 13:09:12 +0000

    Update regression.py

--- PySpark/ML/regression.py
+++ PySpark/ML/regression.py
@@ -128,3 +128,23 @@ print(avg_night_ogg)
 avg_night_jfk = regression.intercept + regression.coefficients[3] + regression.coefficients[8]
 print(avg_night_jfk)
 
+# D. Regularization
+# 1. Flight duration model: More features
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# Fit linear regression model to training data
+regression = LinearRegression(labelCol='duration').fit(flights_train)
+
+# Make predictions on testing data
+predictions = regression.transform(flights_test)
+
+# Calculate the RMSE on testing data
+rmse = RegressionEvaluator(labelCol='duration').evaluate(predictions)
+print("The test RMSE is", rmse)
+
+# Look at the model coefficients
+coeffs = regression.coefficients
+print(coeffs)
+
+# 2. 
