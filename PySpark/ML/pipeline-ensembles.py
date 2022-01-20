1e656ddbe74bf5fde7c72cb8f7aec3f4b039e2ab
James_McNeill <jmcneill06@outlook.com>
2022-01-20 16:12:35 +0000

    Update pipeline-ensembles.py

--- PySpark/ML/pipeline-ensembles.py
+++ PySpark/ML/pipeline-ensembles.py
@@ -127,3 +127,20 @@ params = params.build()
 
 # D. Ensemble
 # 1. Delayed flights with Gradient-Boosted Trees
+# Import the classes required
+from pyspark.ml.classification import DecisionTreeClassifier, GBTClassifier
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+# Create model objects and train on training data
+tree = DecisionTreeClassifier().fit(flights_train)
+gbt = GBTClassifier().fit(flights_train)
+
+# Compare AUC on testing data
+evaluator = BinaryClassificationEvaluator()
+evaluator.evaluate(tree.transform(flights_test))
+evaluator.evaluate(gbt.transform(flights_test))
+
+# Find the number of trees and the relative importance of features
+print(gbt.getNumTrees)
+print(gbt.featureImportances)
+
