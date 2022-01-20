5fdc4a5b99cf9173a6636782cdf4e31df12ba542
James_McNeill <jmcneill06@outlook.com>
2022-01-20 15:08:14 +0000

    Update pipeline-ensembles.py

--- PySpark/ML/pipeline-ensembles.py
+++ PySpark/ML/pipeline-ensembles.py
@@ -3,4 +3,18 @@
 # test your models and select good model parameters. Finally you'll dabble in two types of ensemble model.
 
 # A. Pipeline
-# 1. 
+# 1. Flight duration model: Pipeline stages
+# Convert categorical strings to index values
+indexer = StringIndexer(inputCol='org', outputCol='org_idx')
+
+# One-hot encode index values
+onehot = OneHotEncoderEstimator(
+    inputCols=['org_idx', 'dow'],
+    outputCols=['org_dummy', 'dow_dummy']
+)
+
+# Assemble predictors into a single column
+assembler = VectorAssembler(inputCols=['km', 'org_dummy', 'dow_dummy'], outputCol=['features'])
+
+# A linear regression object
+regression = LinearRegression(labelCol='duration')
