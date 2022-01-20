88ed656b7f6444cfa0f80ecc71ec840a578ed65f
James_McNeill <jmcneill06@outlook.com>
2022-01-20 13:17:18 +0000

    Update pipeline-ensembles.py

--- PySpark/ML/pipeline-ensembles.py
+++ PySpark/ML/pipeline-ensembles.py
@@ -1,4 +1,6 @@
 # Pipelines and ensembles
+# How to use pipelines to make your code clearer and easier to maintain. Then you'll use cross-validation to better 
+# test your models and select good model parameters. Finally you'll dabble in two types of ensemble model.
 
 # A. Pipeline
 # 1. 
