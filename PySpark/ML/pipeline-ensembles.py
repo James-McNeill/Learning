633b2c86018f7f782767eebe20727787495a5d74
James_McNeill <jmcneill06@outlook.com>
2022-01-20 16:17:21 +0000

    Update pipeline-ensembles.py

--- PySpark/ML/pipeline-ensembles.py
+++ PySpark/ML/pipeline-ensembles.py
@@ -144,3 +144,20 @@ evaluator.evaluate(gbt.transform(flights_test))
 print(gbt.getNumTrees)
 print(gbt.featureImportances)
 
+# 2. Delayed flights with a Random Forest
+# Create a random forest classifier
+forest = RandomForestClassifier()
+
+# Create a parameter grid
+params = ParamGridBuilder() \
+            .addGrid(forest.featureSubsetStrategy, ['all', 'onethird', 'sqrt', 'log2']) \
+            .addGrid(forest.maxDepth, [2, 5, 10]) \
+            .build()
+
+# Create a binary classification evaluator
+evaluator = BinaryClassificationEvaluator()
+
+# Create a cross-validator
+cv = CrossValidator(estimator=forest, estimatorParamMaps=params, evaluator=evaluator, numFolds=5)
+
+# 3. 
