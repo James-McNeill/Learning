833591531f8db7c13a25b59ea2162fbc92604b85
James_McNeill <jmcneill06@outlook.com>
2022-01-21 16:37:37 +0000

    Update modeling.py

--- Python/Kaggle/Comp-Example/modeling.py
+++ Python/Kaggle/Comp-Example/modeling.py
@@ -46,3 +46,61 @@ test['fare_amount'] = rf.predict(test[features])
 
 # Write predictions
 test[['id','fare_amount']].to_csv('rf_sub.csv', index=False)
+
+# B. Hyperparameter tuning
+# 1. Grid search
+def get_cv_score(train, params):
+    # Create KFold object
+    kf = KFold(n_splits=3, shuffle=True, random_state=123)
+
+    rmse_scores = []
+    
+    # Loop through each split
+    for train_index, test_index in kf.split(train):
+        cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    
+        # Train a Gradient Boosting model
+        gb = GradientBoostingRegressor(random_state=123, **params).fit(cv_train[features], cv_train.fare_amount)
+    
+        # Make predictions on the test data
+        pred = gb.predict(cv_test[features])
+    
+        fold_score = np.sqrt(mean_squared_error(cv_test['fare_amount'], pred))
+        rmse_scores.append(fold_score)
+    
+    return np.round(np.mean(rmse_scores) + np.std(rmse_scores), 5)
+
+# Possible max depth values
+max_depth_grid = [3, 6, 9, 12, 15]
+results = {}
+
+# For each value in the grid
+for max_depth_candidate in max_depth_grid:
+    # Specify parameters for the model
+    params = {'max_depth': max_depth_candidate}
+
+    # Calculate validation score for a particular hyperparameter
+    validation_score = get_cv_score(train, params)
+
+    # Save the results for each max depth value
+    results[max_depth_candidate] = validation_score   
+print(results)
+
+# 2. 2D grid search
+import itertools
+
+# Hyperparameter grids
+max_depth_grid = [3, 5, 7]
+subsample_grid = [0.8, 0.9, 1]
+results = {}
+
+# For each couple in the grid
+for max_depth_candidate, subsample_candidate in itertools.product(max_depth_grid, subsample_grid):
+    params = {'max_depth': max_depth_candidate,
+              'subsample': subsample_candidate}
+    validation_score = get_cv_score(train, params)
+    # Save the results for each couple
+    results[(max_depth_candidate, subsample_candidate)] = validation_score   
+print(results)
+
+# C. 
