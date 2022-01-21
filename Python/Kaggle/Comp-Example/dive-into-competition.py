460fc16b74063e32a9954c532a0f38d31177dd1d
James_McNeill <jmcneill06@outlook.com>
2022-01-21 15:40:55 +0000

    Update dive-into-competition.py

--- Python/Kaggle/Comp-Example/dive-into-competition.py
+++ Python/Kaggle/Comp-Example/dive-into-competition.py
@@ -103,4 +103,22 @@ for train_index, test_index in kf.split(train):
     print('Medium interest listings in CV train: {}\n'.format(sum(cv_train.interest_level == 'medium')))
     fold += 1
 
-# 2. 
+# 2. Stratified K-fold
+# Allows for splitting the data and retaining the target variable balance across splits. Could be key in classification models
+# Import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
+
+# Create a StratifiedKFold object
+str_kf = StratifiedKFold(n_splits=3, shuffle=True, random_state=123)
+
+# Loop through each split
+fold = 0
+for train_index, test_index in str_kf.split(train, train['interest_level']):
+    # Obtain training and testing folds
+    cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    print('Fold: {}'.format(fold))
+    print('CV train shape: {}'.format(cv_train.shape))
+    print('Medium interest listings in CV train: {}\n'.format(sum(cv_train.interest_level == 'medium')))
+    fold += 1
+
+# C. 
