f2f8110cd344ed40c814dde0c21c9bb37ea6d90a
James_McNeill <jmcneill06@outlook.com>
2022-01-21 16:29:48 +0000

    Create modeling.py

---
+++ Python/Kaggle/Comp-Example/modeling.py
@@ -0,0 +1,48 @@
+# Modeling
+
+# A. Baseline model
+# 1. Replicate validation score
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+# Calculate the mean fare_amount on the validation_train data
+naive_prediction = np.mean(validation_train['fare_amount'])
+
+# Assign naive prediction to all the holdout observations
+validation_test['pred'] = naive_prediction
+
+# Measure the local RMSE
+rmse = sqrt(mean_squared_error(validation_test['fare_amount'], validation_test['pred']))
+print('Validation RMSE for Baseline I model: {:.3f}'.format(rmse))
+
+# 2. Baseline based on the date
+# Get pickup hour from the pickup_datetime column
+train['hour'] = train['pickup_datetime'].dt.hour
+test['hour'] = test['pickup_datetime'].dt.hour
+
+# Calculate average fare_amount grouped by pickup hour 
+hour_groups = train.groupby('hour')['fare_amount'].mean()
+
+# Make predictions on the test set
+test['fare_amount'] = test.hour.map(hour_groups)
+
+# Write predictions
+test[['id','fare_amount']].to_csv('hour_mean_sub.csv', index=False)
+
+# 3. Baseline based on the gradient boosting
+from sklearn.ensemble import RandomForestRegressor
+
+# Select only numeric features
+features = ['pickup_longitude', 'pickup_latitude', 'dropoff_longitude',
+            'dropoff_latitude', 'passenger_count', 'hour']
+
+# Train a Random Forest model
+rf = RandomForestRegressor()
+rf.fit(train[features], train.fare_amount)
+
+# Make predictions on the test data
+test['fare_amount'] = rf.predict(test[features])
+
+# Write predictions
+test[['id','fare_amount']].to_csv('rf_sub.csv', index=False)
