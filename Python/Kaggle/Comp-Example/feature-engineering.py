11ecbd2e774abf91570533000cb14b95cc1bf918
James_McNeill <jmcneill06@outlook.com>
2022-01-21 15:54:08 +0000

    Create feature-engineering.py

---
+++ Python/Kaggle/Comp-Example/feature-engineering.py
@@ -0,0 +1,43 @@
+# Feature Engineering
+
+# A. Feature Engineering
+# 1. Arithmetical features
+def get_kfold_rmse(train):
+    mse_scores = []
+
+    for train_index, test_index in kf.split(train):
+        train = train.fillna(0)
+        feats = [x for x in train.columns if x not in ['Id', 'SalePrice', 'RoofStyle', 'CentralAir']]
+        
+        fold_train, fold_test = train.loc[train_index], train.loc[test_index]
+
+        # Fit the data and make predictions
+        # Create a Random Forest object
+        rf = RandomForestRegressor(n_estimators=10, min_samples_split=10, random_state=123)
+
+        # Train a model
+        rf.fit(X=fold_train[feats], y=fold_train['SalePrice'])
+
+        # Get predictions for the test set
+        pred = rf.predict(fold_test[feats])
+    
+        fold_score = mean_squared_error(fold_test['SalePrice'], pred)
+        mse_scores.append(np.sqrt(fold_score))
+        
+    return round(np.mean(mse_scores) + np.std(mse_scores), 2)
+
+# Look at the initial RMSE
+print('RMSE before feature engineering:', get_kfold_rmse(train))
+
+# Find the total area of the house
+train['TotalArea'] = train['TotalBsmtSF'] + train['FirstFlrSF'] + train['SecondFlrSF']
+print('RMSE with total area:', get_kfold_rmse(train))
+
+# Find the area of the garden
+train['GardenArea'] = train['LotArea'] - train['FirstFlrSF']
+print('RMSE with garden area:', get_kfold_rmse(train))
+
+# Find total number of bathrooms
+train['TotalBath'] = train['FullBath'] + train['HalfBath']
+print('RMSE with number of bathrooms:', get_kfold_rmse(train))
+
