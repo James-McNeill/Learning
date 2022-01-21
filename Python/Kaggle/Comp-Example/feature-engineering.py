c63b3c648d997a3d0deaed7999b772c64f9071e5
James_McNeill <jmcneill06@outlook.com>
2022-01-21 16:00:56 +0000

    Update feature-engineering.py

--- Python/Kaggle/Comp-Example/feature-engineering.py
+++ Python/Kaggle/Comp-Example/feature-engineering.py
@@ -41,3 +41,54 @@ print('RMSE with garden area:', get_kfold_rmse(train))
 train['TotalBath'] = train['FullBath'] + train['HalfBath']
 print('RMSE with number of bathrooms:', get_kfold_rmse(train))
 
+# 2. Date features
+# Concatenate train and test together
+taxi = pd.concat([train, test])
+
+# Convert pickup date to datetime object
+taxi['pickup_datetime'] = pd.to_datetime(taxi['pickup_datetime'])
+
+# Create a day of week feature
+taxi['dayofweek'] = taxi['pickup_datetime'].dt.dayofweek
+
+# Create an hour feature
+taxi['hour'] = taxi['pickup_datetime'].dt.hour
+
+# Split back into train and test
+new_train = taxi[taxi['id'].isin(train['id'])]
+new_test = taxi[taxi['id'].isin(test['id'])]
+
+# B. Categorical Features
+# 1. Label encoding
+# Concatenate train and test together
+houses = pd.concat([train, test])
+
+# Label encoder
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+
+# Create new features
+houses['RoofStyle_enc'] = le.fit_transform(houses['RoofStyle'])
+houses['CentralAir_enc'] = le.fit_transform(houses['CentralAir'])
+
+# Look at new features
+print(houses[['RoofStyle', 'RoofStyle_enc', 'CentralAir', 'CentralAir_enc']].head())
+
+# 2. One hot encoding
+# Concatenate train and test together
+houses = pd.concat([train, test])
+
+# Label encode binary 'CentralAir' feature
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+houses['CentralAir_enc'] = le.fit_transform(houses['CentralAir'])
+
+# Create One-Hot encoded features
+ohe = pd.get_dummies(houses['RoofStyle'], prefix='RoofStyle')
+
+# Concatenate OHE features to houses
+houses = pd.concat([houses, ohe], axis=1)
+
+# Look at OHE features
+print(houses[[col for col in houses.columns if 'RoofStyle' in col]].head(3))
+
