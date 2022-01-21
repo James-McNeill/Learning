c028c3fd812d7528df914077d88a0b5aa294d3f8
James_McNeill <jmcneill06@outlook.com>
2022-01-21 14:48:24 +0000

    Create competition-process.py

---
+++ Python/Kaggle/Comp-Example/competition-process.py
@@ -0,0 +1,16 @@
+# Kaggle competition process
+
+# A. Prepare first submission
+# 1. Train simple model
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+# Read the train data
+train = pd.read_csv('train.csv')
+
+# Create a Random Forest object
+rf = RandomForestRegressor()
+
+# Train a model
+rf.fit(X=train[['store', 'item']], y=train['sales'])
+
