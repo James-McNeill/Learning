5767395c5338f288cf874cf2ebc46c238d61172e
James_McNeill <jmcneill06@outlook.com>
2022-01-21 15:05:46 +0000

    Create dive-into-competition.py

---
+++ Python/Kaggle/Comp-Example/dive-into-competition.py
@@ -0,0 +1,10 @@
+# Competition review
+
+# Reviewing the four stages of developing a solution workflow
+# 1. Understand the problem
+# 2. EDA
+# 3. Local validation
+# 4. Modeling
+
+# A. Understand the problem
+# 1. Define a competition metric
