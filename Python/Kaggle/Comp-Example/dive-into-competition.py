17f86d8394ba67c4d7baa87ffd14121ed9a24678
James_McNeill <jmcneill06@outlook.com>
2022-01-21 15:37:58 +0000

    Update dive-into-competition.py

--- Python/Kaggle/Comp-Example/dive-into-competition.py
+++ Python/Kaggle/Comp-Example/dive-into-competition.py
@@ -84,3 +84,23 @@ plt.ylabel('Median fare amount')
 plt.title('Fare amount based on day time')
 plt.xticks(range(24))
 plt.show()
+
+# C. Local validation
+# 1. K-fold cross-validation
+# Import KFold
+from sklearn.model_selection import KFold
+
+# Create a KFold object
+kf = KFold(n_splits=3, shuffle=True, random_state=123)
+
+# Loop through each split
+fold = 0
+for train_index, test_index in kf.split(train):
+    # Obtain training and testing folds
+    cv_train, cv_test = train.iloc[train_index], train.iloc[test_index]
+    print('Fold: {}'.format(fold))
+    print('CV train shape: {}'.format(cv_train.shape))
+    print('Medium interest listings in CV train: {}\n'.format(sum(cv_train.interest_level == 'medium')))
+    fold += 1
+
+# 2. 
