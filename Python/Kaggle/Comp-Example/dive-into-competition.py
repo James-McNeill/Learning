ad718d4949f4da1862ec9a74633e69f662762c84
James_McNeill <jmcneill06@outlook.com>
2022-01-21 15:07:58 +0000

    Update dive-into-competition.py

--- Python/Kaggle/Comp-Example/dive-into-competition.py
+++ Python/Kaggle/Comp-Example/dive-into-competition.py
@@ -8,3 +8,34 @@
 
 # A. Understand the problem
 # 1. Define a competition metric
+import numpy as np
+
+# Import MSE from sklearn
+from sklearn.metrics import mean_squared_error
+
+# Define your own MSE function
+def own_mse(y_true, y_pred):
+  	# Raise differences to the power of 2
+    squares = np.power(y_true - y_pred, 2)
+    # Find mean over all observations
+    err = np.mean(squares)
+    return err
+
+print('Sklearn MSE: {:.5f}. '.format(mean_squared_error(y_regression_true, y_regression_pred)))
+print('Your MSE: {:.5f}. '.format(own_mse(y_regression_true, y_regression_pred)))
+
+import numpy as np
+
+# Import log_loss from sklearn
+from sklearn.metrics import log_loss
+
+# Define your own LogLoss function
+def own_logloss(y_true, prob_pred):
+  	# Find loss for each observation
+    terms = y_true * np.log(prob_pred) + (1 - y_true) * np.log(1 - prob_pred)
+    # Find mean over all observations
+    err = np.mean(terms) 
+    return -err
+
+print('Sklearn LogLoss: {:.5f}'.format(log_loss(y_classification_true, y_classification_pred)))
+print('Your LogLoss: {:.5f}'.format(own_logloss(y_classification_true, y_classification_pred)))
