c2af4f93891cf85eaf123c1940a8c1043505f216
James_McNeill <jmcneill06@outlook.com>
2022-01-21 14:51:57 +0000

    Update competition-process.py

--- Python/Kaggle/Comp-Example/competition-process.py
+++ Python/Kaggle/Comp-Example/competition-process.py
@@ -14,3 +14,17 @@ rf = RandomForestRegressor()
 # Train a model
 rf.fit(X=train[['store', 'item']], y=train['sales'])
 
+# 2. Prepare a submission
+# Read test and sample submission data
+test = pd.read_csv('test.csv')
+sample_submission = pd.read_csv('sample_submission.csv')
+
+# Show the head() of the sample_submission
+print(sample_submission.head())
+
+# Get predictions for the test set
+test['sales'] = rf.predict(test[['store', 'item']])
+
+# Write test predictions using the sample_submission format
+test[['id', 'sales']].to_csv('kaggle_submission.csv', index=False)
+
