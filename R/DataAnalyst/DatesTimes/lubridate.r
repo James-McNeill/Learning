5e5513d19c18fee07a9f5edffae6196583bbe03d
James_McNeill <jmcneill06@outlook.com>
2022-01-24 14:43:21 +0000

    Update lubridate.r

--- R/DataAnalyst/DatesTimes/lubridate.r
+++ R/DataAnalyst/DatesTimes/lubridate.r
@@ -1,4 +1,33 @@
 # Parsing and Manipulating Dates and Times with lubridate
 
 # A. Parsing dates with lubridate
-# 1. 
+# 1. Selecting the right parsing function
+# As lubridate has a similar method name "date" which is also included in the base package, the lubridate will take precedence.
+# If we need to use the "date" method from the base package then we need to select base::date to specify.
+library(lubridate)
+
+# Parse x 
+x <- "2010 September 20th" # 2010-09-20
+ymd(x)
+
+# Parse y 
+y <- "02.01.2010"  # 2010-01-02
+dmy(y)
+
+# Parse z 
+z <- "Sep, 12th 2010 14:00"  # 2010-09-12T14:00
+mdy_hm(z)
+
+# 2. Specifying an order with 'parse_date_time()'
+# Specify an order string to parse x
+x <- "Monday June 1st 2010 at 4pm"
+parse_date_time(x, orders = "AmdyIp")
+
+# Specify order to include both "mdy" and "dmy"
+two_orders <- c("October 7, 2001", "October 13, 2002", "April 13, 2003", 
+  "17 April 2005", "23 April 2017")
+parse_date_time(two_orders, orders = c("mdy", "dmy"))
+
+# Specify order to include "dOmY", "OmY" and "Y"
+short_dates <- c("11 December 1282", "May 1372", "1253")
+parse_date_time(short_dates, orders = c("dOmY", "OmY", "Y"))
