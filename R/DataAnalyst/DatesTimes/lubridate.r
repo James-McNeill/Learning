9528600a983a919efdec90f63610f71c9f2b3b8e
James_McNeill <jmcneill06@outlook.com>
2022-01-24 15:01:05 +0000

    Update lubridate.r

--- R/DataAnalyst/DatesTimes/lubridate.r
+++ R/DataAnalyst/DatesTimes/lubridate.r
@@ -31,3 +31,57 @@ parse_date_time(two_orders, orders = c("mdy", "dmy"))
 # Specify order to include "dOmY", "OmY" and "Y"
 short_dates <- c("11 December 1282", "May 1372", "1253")
 parse_date_time(short_dates, orders = c("dOmY", "OmY", "Y"))
+
+# B. Weather in Auckland
+# 1. Import daily weather data
+library(lubridate)
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+# Import CSV with read_csv()
+akl_daily_raw <- read_csv("akl_weather_daily.csv")
+
+# Print akl_daily_raw
+print(akl_daily_raw)
+
+# Parse date 
+akl_daily <- akl_daily_raw %>%
+  mutate(date = as.Date(date))
+
+# Print akl_daily
+print(akl_daily)
+
+# Plot to check work
+ggplot(akl_daily, aes(x = date, y = max_temp)) +
+  geom_line() 
+
+# 2. Import hourly weather data
+library(lubridate)
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+# Import "akl_weather_hourly_2016.csv"
+akl_hourly_raw <- read_csv("akl_weather_hourly_2016.csv")
+
+# Print akl_hourly_raw
+print(akl_hourly_raw)
+
+# Use make_date() to combine year, month and mday 
+akl_hourly  <- akl_hourly_raw  %>% 
+  mutate(date = make_date(year = year, month = month, day = mday))
+
+# Parse datetime_string 
+akl_hourly <- akl_hourly  %>% 
+  mutate(
+    datetime_string = paste(date, time, sep = "T"),
+    datetime = parse_datetime(datetime_string)
+  )
+
+# Print date, time and datetime columns of akl_hourly
+akl_hourly %>% select(date, time, datetime)
+
+# Plot to check work
+ggplot(akl_hourly, aes(x = datetime, y = temperature)) +
+  geom_line()
