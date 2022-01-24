b9b827dc0ea51b494ae9d2098bdbfacb73c40173
James_McNeill <jmcneill06@outlook.com>
2022-01-24 15:18:53 +0000

    Update lubridate.r

--- R/DataAnalyst/DatesTimes/lubridate.r
+++ R/DataAnalyst/DatesTimes/lubridate.r
@@ -85,3 +85,90 @@ akl_hourly %>% select(date, time, datetime)
 # Plot to check work
 ggplot(akl_hourly, aes(x = datetime, y = temperature)) +
   geom_line()
+
+# C. Extracting parts of a datetime
+# 1. What can you extract?
+# Examine the head() of release_time
+head(release_time)
+
+# Examine the head() of the months of release_time
+head(month(release_time))
+
+# Extract the month of releases 
+month(release_time) %>% table()
+
+# Extract the year of releases
+year(release_time) %>% table()
+
+# How often is the hour before 12 (noon)?
+mean(hour(release_time) < 12)
+
+# How often is the release in am?
+mean(am(release_time))
+
+# 2. Adding useful labels
+# head(month(release_time, label = TRUE, abbr = FALSE))
+# label: displays the label value for the variable
+# abbr: set to TRUE, provides the abbreviation of the variable label value
+library(ggplot2)
+
+# Use wday() to tabulate release by day of the week
+wday(releases$datetime) %>% table()
+
+# Add label = TRUE to make table more readable
+wday(releases$datetime, label = TRUE) %>% table()
+
+# Create column wday to hold labelled week days
+releases$wday <- wday(releases$datetime, label = TRUE)
+
+# Plot barchart of weekday by type of release
+ggplot(releases, aes(x = wday)) +
+  geom_bar() +
+  facet_wrap(~ type, ncol = 1, scale = "free_y")
+
+# 3. Extracting for plotting
+library(ggplot2)
+library(dplyr)
+library(ggridges)
+
+# Add columns for year, yday and month
+akl_daily <- akl_daily %>%
+  mutate(
+    year = year(date),
+    yday = yday(date),
+    month = month(date, label = TRUE))
+
+# Plot max_temp by yday for all years
+ggplot(akl_daily, aes(x = yday, y = max_temp)) +
+  geom_line(aes(group = year), alpha = 0.5)
+
+# Examine distribution of max_temp by month
+ggplot(akl_daily, aes(x = max_temp, y = month, height = ..density..)) +
+  geom_density_ridges(stat = "density")
+
+# 4. Extracting for filtering and summarizing
+# Create new columns hour, month and rainy
+akl_hourly <- akl_hourly %>%
+  mutate(
+    hour = hour(datetime),
+    month = month(datetime, label = TRUE),
+    rainy = weather == "Precipitation"
+  )
+
+# Filter for hours between 8am and 10pm (inclusive)
+akl_day <- akl_hourly %>% 
+  filter(hour >= 8, hour <= 22)
+
+# Summarise for each date if there is any rain
+rainy_days <- akl_day %>% 
+  group_by(month, date) %>%
+  summarise(
+    any_rain = any(rainy)
+  )
+
+# Summarise for each month, the number of days with rain
+rainy_days %>% 
+  summarise(
+    days_rainy = sum(any_rain)
+  )
+
