29df84ef79b05503a898842cd90f13d437ba96a8
James_McNeill <jmcneill06@outlook.com>
2022-01-25 17:01:05 +0000

    Update arithmetic.r

--- R/DataAnalyst/DatesTimes/arithmetic.r
+++ R/DataAnalyst/DatesTimes/arithmetic.r
@@ -27,3 +27,43 @@ difftime(mar_13, mar_12, units = "secs")
 # Difference between mar_12 and mar_11 in seconds
 difftime(mar_12, mar_11, units = "secs")
 
+# B. Time spans
+# 1. Adding or substracting a time span to a datetime
+# periods: relevant to one step change for the metric (days, months). Movement pays no attention to changes in time e.g. daylight saving impacts
+# duration: relevant to the time movement for the metric
+# Add a period of one week to mon_2pm
+mon_2pm <- dmy_hm("27 Aug 2018 14:00")
+mon_2pm + weeks(1)
+
+# Add a duration of 81 hours to tue_9am
+tue_9am <- dmy_hm("28 Aug 2018 9:00")
+tue_9am + dhours(81)
+
+# Subtract a period of five years from today()
+today() - years(5)
+
+# Subtract a duration of five years from today()
+today() - dyears(5)
+
+# 2. Arithmetic with timespans
+# Time of North American Eclipse 2017
+eclipse_2017 <- ymd_hms("2017-08-21 18:26:40")
+
+# Duration of 29 days, 12 hours, 44 mins and 3 secs
+synodic <- ddays(29) + dhours(12) + dminutes(44) + dseconds(3)
+
+# 223 synodic months
+saros <- synodic * 223
+
+# Add saros to eclipse_2017
+saros + eclipse_2017
+
+# 3. Generating sequences of datetimes
+# Add a period of 8 hours to today
+today_8am <- today() + hours(8)
+
+# Sequence of two weeks from 1 to 26
+every_two_weeks <- 1:26 * weeks(2)
+
+# Create datetime for every two weeks for a year
+today_8am + every_two_weeks
