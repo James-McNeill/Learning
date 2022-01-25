7288869ac3962bd9225f202d106a17829c9ef630
James_McNeill <jmcneill06@outlook.com>
2022-01-25 16:51:02 +0000

    Update arithmetic.r

--- R/DataAnalyst/DatesTimes/arithmetic.r
+++ R/DataAnalyst/DatesTimes/arithmetic.r
@@ -12,3 +12,18 @@ difftime(today(), date_landing, units = "days")
 # How many seconds since the first man on the moon?
 difftime(now(), moment_step, units = "secs")
 
+# 2. How many seconds are in a day?
+# Three dates
+mar_11 <- ymd_hms("2017-03-11 12:00:00", 
+  tz = "America/Los_Angeles")
+mar_12 <- ymd_hms("2017-03-12 12:00:00", 
+  tz = "America/Los_Angeles")
+mar_13 <- ymd_hms("2017-03-13 12:00:00", 
+  tz = "America/Los_Angeles")
+
+# Difference between mar_13 and mar_12 in seconds
+difftime(mar_13, mar_12, units = "secs")
+
+# Difference between mar_12 and mar_11 in seconds
+difftime(mar_12, mar_11, units = "secs")
+
