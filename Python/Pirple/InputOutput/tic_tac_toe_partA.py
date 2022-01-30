9c7fbdd897eba0d19b2a28912f4bf6244cc717f3
James_McNeill <jmcneill06@outlook.com>
2022-01-30 10:16:23 +0000

    Update tic_tac_toe_partA.py

--- Python/Pirple/InputOutput/tic_tac_toe_partA.py
+++ Python/Pirple/InputOutput/tic_tac_toe_partA.py
@@ -1,3 +1,16 @@
 # Creating a tic tac toe game
 
-# 
+# A. Create the initial function to draw field
+def drawField():
+  for row in range(5):
+    if row % 2 == 0:
+      for column in range(5):
+        if column % 2 == 0:
+          if column != 4:
+            print(" ", end="")
+          else:
+            print(" ")
+        else:
+          print("|", end="")
+    else:
+      print("-----")
