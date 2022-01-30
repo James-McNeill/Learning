81c1408038d2c643b6cfc02871695d9c8aea8b7c
James_McNeill <jmcneill06@outlook.com>
2022-01-30 10:30:04 +0000

    Update tic_tac_toe_partA.py

--- Python/Pirple/InputOutput/tic_tac_toe_partA.py
+++ Python/Pirple/InputOutput/tic_tac_toe_partA.py
@@ -1,6 +1,7 @@
 # Creating a tic tac toe game
 
 # A. Create the initial function to draw field
+# Create the tic tac toe board
 def drawField():
   for row in range(5):
     if row % 2 == 0:
@@ -14,3 +15,24 @@ def drawField():
           print("|", end="")
     else:
       print("-----")
+
+# Draw the empty board
+drawField()
+
+# Creating the user moves
+Player = 1
+currentField = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+print(currentField)
+while(True):
+    print("Players turn: ", Player)
+    MoveRow = int(input("Please enter the row\n"))
+    MoveColumn = int(input("Please enter the column\n"))
+    if Player == 1:
+        # player 1 move
+        currentField[MoveColumn][MoveRow] = "X"
+        Player = 2 # switches player
+    else:
+        # player 2
+        currentField[MoveColumn][MoveRow] = "O"
+        Player = 1 # switches player
+    print(currentField)
