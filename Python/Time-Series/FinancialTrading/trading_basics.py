a4c783bea8e6e15a800b943a75b4228da78e3387
James_McNeill <jmcneill06@outlook.com>
2022-02-03 17:12:32 +0000

    Update trading_basics.py

--- Python/Time-Series/FinancialTrading/trading_basics.py
+++ Python/Time-Series/FinancialTrading/trading_basics.py
@@ -16,3 +16,20 @@ plt.plot(bitcoin_data['Low'], color='red')
 plt.title('Daily high low prices')
 plt.show()
 
+# 2. Plot a candlestick chart
+import plotly.graph_objects as go
+
+# Define the candlestick data
+candlestick = go.Candlestick(
+    x=bitcoin_data.index,
+    open=bitcoin_data['Open'],
+    high=bitcoin_data['High'],
+    low=bitcoin_data['Low'],
+    close=bitcoin_data['Close'])
+
+# Create a candlestick figure   
+fig = go.Figure(data=[candlestick])
+fig.update_layout(title='Bitcoin prices')                        
+
+# Show the plot
+fig.show()
