3e20e4d92701abf61d96743791feab0972877f06
James_McNeill <jmcneill06@outlook.com>
2022-02-03 17:23:03 +0000

    Update trading_basics.py

--- Python/Time-Series/FinancialTrading/trading_basics.py
+++ Python/Time-Series/FinancialTrading/trading_basics.py
@@ -33,3 +33,43 @@ fig.update_layout(title='Bitcoin prices')
 
 # Show the plot
 fig.show()
+
+# B. Getting familiar with your trading data
+# 1. Resample the data
+# Switch the sample from a day trader (hourly, time periods) to a swing trader (daily) and then a position trader (weekly), and manipulate the data to suit your needs
+# Resample the data to daily by calculating the mean values
+eurusd_daily = eurusd_4h.resample('d').mean()
+
+# Print the top 5 rows
+print(eurusd_daily.head())
+
+# Resample the data to weekly by calculating the mean values
+eurusd_weekly = eurusd_4h.resample('w').mean()
+
+# Print the top 5 rows
+print(eurusd_weekly.head())
+
+# 2. Plot a return histogram
+# Calculate daily returns
+tsla_data['daily_return'] = tsla_data['Close'].pct_change() * 100
+
+# Plot the histogram
+tsla_data['daily_return'].hist(bins=100, color='red')
+plt.ylabel('Frequency')
+plt.xlabel('Daily return')
+plt.title('Daily return histogram')
+plt.show()
+
+# 3. Calculate and plot Simple Moving Averages (SMAs)
+# Calculate SMA
+aapl_data['sma_50'] = aapl_data['Close'].rolling(50).mean()
+
+# Plot the SMA
+plt.plot(aapl_data['sma_50'], color='green', label='SMA_50')
+# Plot the close price
+plt.plot(aapl_data['Close'], color='red', label='Close')
+
+# Customize and show the plot
+plt.title('Simple moving averages')
+plt.legend()
+plt.show()
