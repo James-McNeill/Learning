489febf734fa8152563e6bfe631658defe09f091
James_McNeill <jmcneill06@outlook.com>
2022-02-04 15:52:46 +0000

    Create check_file_exists.py

---
+++ Python/00_Repo/check_file_exists.py
@@ -0,0 +1,4 @@
+# Print the current working directory
+print("Current working directory: {}".format(os.getcwd()))
+# Print if the file exists
+print("Checking if file exists: {}".format(os.path.isfile('./winedata.csv')))
