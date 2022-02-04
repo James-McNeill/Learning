075584a36d16a82e90589bbd215583396691a948
James_McNeill <jmcneill06@outlook.com>
2022-02-04 10:44:59 +0000

    Update technical_indicators.py

--- Python/Time-Series/FinancialTrading/technical_indicators.py
+++ Python/Time-Series/FinancialTrading/technical_indicators.py
@@ -40,3 +40,39 @@ plt.plot(stock_data['Close'], label='Close')
 plt.legend()
 plt.title('SMA vs EMA')
 plt.show()
+
+# B. Strength indicator: ADX
+# ADX: Average directional moving index
+# Range: 0 - 100
+# <= 25: no trend, > 25: trending market, > 50: strong trending market
+# 1. Calculate the ADX
+# More sensitivity to shorted time periods
+# Calculate the ADX with the default time period
+stock_data['ADX_14'] = talib.ADX(stock_data['High'],
+                            stock_data['Low'], 
+                            stock_data['Close'])
+
+# Calculate the ADX with the time period set to 21
+stock_data['ADX_21'] = talib.ADX(stock_data['High'],
+                            stock_data['Low'], 
+                            stock_data['Close'],
+                            timeperiod=21)
+
+# Print the last five rows
+print(stock_data.tail())
+
+# 2. Visualize the ADX
+# Calculate ADX
+stock_data['ADX'] = talib.ADX(stock_data['High'], stock_data['Low'], stock_data['Close'])
+
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(2)
+
+# Plot ADX with the price
+ax1.set_ylabel('Price')
+ax1.plot(stock_data['Close'])
+ax2.set_ylabel('ADX')
+ax2.plot(stock_data['ADX'], color='red')
+
+ax1.set_title('Price and ADX')
+plt.show()
