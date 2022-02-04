ea5c2697e19db4ccca7aa4053db58e7f2e7f3944
James_McNeill <jmcneill06@outlook.com>
2022-02-04 11:13:16 +0000

    Create trading_strategies.py

---
+++ Python/Time-Series/FinancialTrading/trading_strategies.py
@@ -0,0 +1,8 @@
+# Trading strategies
+# You’re now ready to construct signals and use them to build trading strategies. 
+# You’ll get to know the two main styles of trading strategies: trend following and mean reversion. 
+# Working with real-life stock data, you’ll gain hands-on experience in implementing and backtesting 
+# these strategies and become more familiar with the concepts of strategy optimization and benchmarking.
+
+# A. Trading signals
+# 1. Build an SMA-based signal strategy
