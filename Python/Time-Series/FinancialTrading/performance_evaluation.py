89ffff24c7304e0e8642cacd7216115f7aecc8e8
James_McNeill <jmcneill06@outlook.com>
2022-02-04 12:55:42 +0000

    Create performance_evaluation.py

---
+++ Python/Time-Series/FinancialTrading/performance_evaluation.py
@@ -0,0 +1,6 @@
+# Performance Evaluation
+# How is your trading strategy performing? Now it’s time to take a look at the detailed statistics of the strategy backtest result. 
+# You’ll gain knowledge of useful performance metrics, such as returns, drawdowns, Calmar ratio, Sharpe ratio, and Sortino ratio. 
+# You’ll then tie it all together by learning how to obtain these ratios from the backtest results and evaluate the strategy performance on a risk-adjusted basis
+
+# A. Strategy return analysis
