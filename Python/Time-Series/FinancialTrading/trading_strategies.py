e115abe1d7dca2ff8d5d88488665ad60bca8ef1b
James_McNeill <jmcneill06@outlook.com>
2022-02-04 11:49:23 +0000

    Update trading_strategies.py

--- Python/Time-Series/FinancialTrading/trading_strategies.py
+++ Python/Time-Series/FinancialTrading/trading_strategies.py
@@ -6,3 +6,37 @@
 
 # A. Trading signals
 # 1. Build an SMA-based signal strategy
+# Calculate the SMA
+sma = price_data.rolling(20).mean()
+
+# Define the strategy
+bt_strategy = bt.Strategy('AboveSMA', 
+                          [bt.algos.SelectWhere(price_data > sma),
+                           bt.algos.WeighEqually(),
+                           bt.algos.Rebalance()])
+
+# Create the backtest and run it
+bt_backtest = bt.Backtest(bt_strategy, price_data)
+bt_result = bt.run(bt_backtest)
+# Plot the backtest result
+bt_result.plot(title='Backtest result')
+plt.show()
+
+# 2. Build an EMA-based signal strategy
+# price_data: contains the closing price data points time series
+# Calculate the EMA
+ema['Close'] = talib.EMA(price_data['Close'], timeperiod=20)
+
+# Define the strategy
+bt_strategy = bt.Strategy('AboveEMA',
+                          [bt.algos.SelectWhere(price_data > ema),
+                           bt.algos.WeighEqually(),
+                           bt.algos.Rebalance()])
+
+# Create the backtest and run it
+bt_backtest = bt.Backtest(bt_strategy, price_data)
+bt_result = bt.run(bt_backtest)
+# Plot the backtest result
+bt_result.plot(title='Backtest result')
+plt.show()
+
