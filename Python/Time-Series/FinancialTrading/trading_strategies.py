c45b2ee4c16ebd35bdfe6fdd83bf83f4ea31dbb2
James_McNeill <jmcneill06@outlook.com>
2022-02-04 12:09:11 +0000

    Update trading_strategies.py

--- Python/Time-Series/FinancialTrading/trading_strategies.py
+++ Python/Time-Series/FinancialTrading/trading_strategies.py
@@ -71,3 +71,30 @@ bt_result = bt.run(bt_backtest)
 bt_result.plot(title='Backtest result')
 plt.show()
 
+# C. Mean reversion strategy
+# 1. Construct an RSI based signal
+# Construct the signal
+signal[stock_rsi > 70] = -1
+signal[stock_rsi < 30] = 1
+signal[(stock_rsi <= 70) & (stock_rsi >= 30)] = 0
+
+# Merge the data
+combined_df = bt.merge(signal, price_data)
+combined_df.columns = ['signal', 'Price']
+combined_df.plot(secondary_y=['signal'])
+plt.show()
+
+# 2. Build and backtest a mean reversion strategy
+# Define the strategy
+bt_strategy = bt.Strategy('RSI_MeanReversion', 
+                          [bt.algos.WeighTarget(signal),
+                           bt.algos.Rebalance()])
+
+# Create the backtest and run it
+bt_backtest = bt.Backtest(bt_strategy, price_data)
+bt_result = bt.run(bt_backtest)
+# Plot the backtest result
+bt_result.plot(title='Backtest result')
+plt.show()
+
+# D. Strategy optimization and benchmarking
