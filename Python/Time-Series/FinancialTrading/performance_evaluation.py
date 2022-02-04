221a6713e3e636a3552df776bb642bfe176f5d0b
James_McNeill <jmcneill06@outlook.com>
2022-02-04 15:22:18 +0000

    Update performance_evaluation.py

--- Python/Time-Series/FinancialTrading/performance_evaluation.py
+++ Python/Time-Series/FinancialTrading/performance_evaluation.py
@@ -66,3 +66,21 @@ print('Calmar Ratio calculated: %.2f'% calmar_calc)
 calmar = resInfo.loc['calmar']
 print('Calmar Ratio: %.2f'% calmar)
 
+# C. Sharpe and sortino ratios
+# 1. Evaluate strategy performance Sharpe Ratio
+# Get annual return and volatility
+yearly_return = resInfo.loc['yearly_mean']
+print('Annual return: %.2f'% yearly_return)
+yearly_vol = resInfo.loc['yearly_vol']
+print('Annual volatility: %.2f'% yearly_vol)
+
+# Calculate the Sharpe ratio manually
+sharpe_ratio = yearly_return / yearly_vol
+print('Sharpe ratio calculated: %.2f'% sharpe_ratio)
+
+# Print the Sharpe ratio
+print('Sharpe ratio %.2f'% resInfo.loc['yearly_sharpe'])
+
+# 2. Sortino Ratio
+yearly_sortino = resInfo.loc['yearly_sortino']
+monthly_sortino = resInfo.loc['monthly_sortino']
