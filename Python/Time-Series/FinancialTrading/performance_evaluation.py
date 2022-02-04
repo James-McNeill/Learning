253d21ab45f8c99aad399411076f8e41561926a4
James_McNeill <jmcneill06@outlook.com>
2022-02-04 14:31:04 +0000

    Update performance_evaluation.py

--- Python/Time-Series/FinancialTrading/performance_evaluation.py
+++ Python/Time-Series/FinancialTrading/performance_evaluation.py
@@ -34,3 +34,35 @@ plt.show()
 lookback_returns = bt_results.display_lookback_returns()
 print(lookback_returns)
 
+# B. Drawdown
+# Reviews the difference between peak and trough values
+# 1. Review performance with drawdowns
+# Obtain all backtest stats
+resInfo = bt_result.stats
+
+# Get the average drawdown
+avg_drawdown = resInfo.loc['avg_drawdown']
+print('Average drawdown: %.2f'% avg_drawdown)
+
+# Get the average drawdown days
+avg_drawdown_days = resInfo.loc['avg_drawdown_days']
+print('Average drawdown days: %.0f'% avg_drawdown_days)
+
+# 2. Calculate and review the Calmar ratio
+# Calmar ratio performs a risk adjusted review. A value >= 3 is viewed as good
+# Get the CAGR
+cagr = resInfo.loc['cagr']
+print('Compound annual growth rate: %.4f'% cagr)
+
+# Get the max drawdown
+max_drawdown = resInfo.loc['max_drawdown']
+print('Maximum drawdown: %.2f'% max_drawdown)
+
+# Calculate Calmar ratio manually
+calmar_calc = cagr / max_drawdown * (-1)
+print('Calmar Ratio calculated: %.2f'% calmar_calc)
+
+# Get the Calmar ratio
+calmar = resInfo.loc['calmar']
+print('Calmar Ratio: %.2f'% calmar)
+
