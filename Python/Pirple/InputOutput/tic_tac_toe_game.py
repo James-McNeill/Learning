0af835bc1f6673512c54a4837310d9d36b01dc6c
James_McNeill <jmcneill06@outlook.com>
2022-02-05 10:35:58 +0000

    Update tic_tac_toe_game.py

--- Python/Pirple/InputOutput/tic_tac_toe_game.py
+++ Python/Pirple/InputOutput/tic_tac_toe_game.py
@@ -2,37 +2,38 @@
 
 # A. Create the initial function to draw field
 # Create the tic tac toe board
-def drawField():
+def drawField(field: list):
   for row in range(5):
     if row % 2 == 0:
+      practicalRow = int(row / 2)
       for column in range(5):
         if column % 2 == 0:
+          practicalColumn = int(column / 2)
           if column != 4:
-            print(" ", end="")
+            print(field[practicalColumn][practicalRow], end="")
           else:
-            print(" ")
+            print(field[practicalColumn][practicalRow])
         else:
           print("|", end="")
     else:
       print("-----")
 
-# Draw the empty board
-drawField()
-
 # Creating the user moves
 Player = 1
 currentField = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-print(currentField)
+drawField(currentField)
 while(True):
     print("Players turn: ", Player)
     MoveRow = int(input("Please enter the row\n"))
     MoveColumn = int(input("Please enter the column\n"))
     if Player == 1:
         # player 1 move
-        currentField[MoveColumn][MoveRow] = "X"
-        Player = 2 # switches player
+        if currentField[MoveColumn][MoveRow] == " ":
+          currentField[MoveColumn][MoveRow] = "X"
+          Player = 2 # switches player
     else:
         # player 2
-        currentField[MoveColumn][MoveRow] = "O"
-        Player = 1 # switches player
-    print(currentField)
+        if currentField[MoveColumn][MoveRow] == " ":
+          currentField[MoveColumn][MoveRow] = "O"
+          Player = 1 # switches player
+    drawField(currentField)
