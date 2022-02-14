8831166c8f6767ac649529d0ab2a5e1e452baff7
James_McNeill <jmcneill06@outlook.com>
2022-02-14 00:18:40 +0000

    Update basics.r

--- R/Programming/StringR/basics.r
+++ R/Programming/StringR/basics.r
@@ -1,4 +1,42 @@
 # Basics with StringR
 
 # A. Intro
-# 1. 
+# 1. Quotes
+# Define line1
+line1 <- "The table was a large one, but the three were all crowded together at one corner of it:"
+
+# Define line2
+line2 <- '"No room! No room!" they cried out when they saw Alice coming.'
+
+# Define line3
+line3 <- "\"There's plenty of room!\" said Alice indignantly, and she sat down in a large arm-chair at one end of the table."
+
+# 2. Working with lines
+# Putting lines in a vector
+lines <- c(line1, line2, line3)
+
+# Print lines
+print(lines)
+
+# Use writeLines() on lines
+writeLines(lines)
+
+# Write lines with a space separator
+writeLines(lines, sep=" ")
+
+# Use writeLines() on the string "hello\n\U1F30D"
+writeLines("hello\n\U1F30D")
+
+# 3. Escape sentences
+# Should display: To have a \ you need \\
+writeLines("To have a \\ you need \\\\")
+
+# Should display: 
+# This is a really 
+# really really 
+# long string
+writeLines("This is a really\n really really\n long string")
+
+# Use writeLines() with 
+# "\u0928\u092e\u0938\u094d\u0924\u0947 \u0926\u0941\u0928\u093f\u092f\u093e"
+writeLines("\u0928\u092e\u0938\u094d\u0924\u0947 \u0926\u0941\u0928\u093f\u092f\u093e")
