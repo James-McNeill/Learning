02c5e92778faccaef2aea04bfaba06ce37ca10cb
James_McNeill <jmcneill06@outlook.com>
2022-02-25 15:32:00 +0000

    Update intro.r

--- R/Programming/StringR/intro.r
+++ R/Programming/StringR/intro.r
@@ -91,3 +91,17 @@ boy_names[contains_zz]
 
 # Which rows in boy_df have names that contain "zz"?
 boy_df[contains_zz,]
+
+# 2. Subsetting strings based on match
+# Find boy_names that contain "zz"
+str_subset(boy_names, pattern = "zz")
+
+# Find girl_names that contain "zz"
+str_subset(girl_names, pattern = "zz")
+
+# Find girl_names that contain "U"
+starts_U <- str_subset(girl_names, pattern = "U")
+starts_U
+
+# Find girl_names that contain "U" and "z"
+str_subset(starts_U, pattern = "z")
