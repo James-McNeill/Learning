cdf44b52280d3f2f02d01d036dfa9d97b3a5ae69
James_McNeill <jmcneill06@outlook.com>
2022-03-08 15:31:33 +0000

    Update using-color.py

--- Python/DataVisualizations/ImprovingDataVis/using-color.py
+++ Python/DataVisualizations/ImprovingDataVis/using-color.py
@@ -66,3 +66,18 @@ sns.heatmap(nov_2015_CO,
 plt.yticks(rotation = 0)
 plt.show()
 
+# 3. Adjusting your palette according to context
+# Dark plot background
+plt.style.use("dark_background")
+
+# Modify palette for dark background
+color_palette = sns.diverging_palette(250, 0,
+                                      center = 'dark',
+                                      as_cmap = True)
+
+# Pass palette to plot and set center
+sns.heatmap(oct_2015_o3,
+            cmap = color_palette,
+            center = 0)
+plt.yticks(rotation = 0)
+plt.show()
