18f05da2d84b935614a9c0927cc03ac83676bd79
James_McNeill <jmcneill06@outlook.com>
2022-03-08 16:30:02 +0000

    Update showing-uncertainty.py

--- Python/DataVisualizations/ImprovingDataVis/showing-uncertainty.py
+++ Python/DataVisualizations/ImprovingDataVis/showing-uncertainty.py
@@ -20,3 +20,48 @@ g.map(plt.scatter, 'seen', 'y', color = 'orangered').set_ylabels('').set_xlabels
 
 plt.show()
 
+# 2. Annotating confidence intervals
+# Set start and ends according to intervals 
+# Make intervals thicker
+plt.hlines(y = 'year', xmin = 'lower', xmax = 'upper', 
+           linewidth = 5, color = 'steelblue', alpha = 0.7,
+           data = diffs_by_year)
+# Point estimates
+plt.plot('mean', 'year', 'k|', data = diffs_by_year)
+
+# Add a 'null' reference line at 0 and color orangered
+plt.axvline(x = 0, color = 'orangered', linestyle = '--')
+
+# Set descriptive axis labels and title
+plt.xlabel('95% CI')
+plt.title('Avg SO2 differences between Cincinnati and Indianapolis')
+plt.show()
+
+# B. Confidence Bands
+# 1. Making a confidence band
+# Draw 99% inverval bands for average NO2
+vandenberg_NO2['lower'] = vandenberg_NO2['mean'] - 2.58*vandenberg_NO2['std_err']
+vandenberg_NO2['upper'] = vandenberg_NO2['mean'] + 2.58*vandenberg_NO2['std_err']
+
+# Plot mean estimate as a white semi-transparent line
+plt.plot('day', 'mean', data = vandenberg_NO2,
+         color = 'white', alpha = 0.4)
+
+# Fill between the upper and lower confidence band values
+plt.fill_between(x = 'day', 
+                 y1 = 'lower', y2 = 'upper', 
+                 data = vandenberg_NO2)
+
+plt.show()
+
+# 2. Separating a lot of bands
+# Setup a grid of plots with columns divided by location
+g = sns.FacetGrid(eastern_SO2, col = 'city', col_wrap = 2)
+
+# Map interval plots to each cities data with corol colored ribbons
+g.map(plt.fill_between, 'day', 'lower', 'upper', color = 'coral')
+
+# Map overlaid mean plots with white line
+g.map(plt.plot, 'day', 'mean', color = 'white')
+
+plt.show()
