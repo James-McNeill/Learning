316f0da3135732df4c5d1fcd3f1aea3b1880927c
James_McNeill <jmcneill06@outlook.com>
2022-03-08 16:13:37 +0000

    Create showing-uncertainty.py

---
+++ Python/DataVisualizations/ImprovingDataVis/showing-uncertainty.py
@@ -0,0 +1,7 @@
+# Showing uncertainty
+# Uncertainty occurs everywhere in data science, but it's frequently left out of visualizations where it should be included. 
+# Here, we review what a confidence interval is and how to visualize them for both single estimates and continuous functions. 
+# Additionally, we discuss the bootstrap resampling technique for assessing uncertainty and how to visualize it properly
+
+# A. Point estimate intervals
+# 1. Basic confidence intervals
