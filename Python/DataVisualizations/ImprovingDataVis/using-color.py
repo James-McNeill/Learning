86b4810ef2fad21bb3517141212f774df6e4a716
James_McNeill <jmcneill06@outlook.com>
2022-03-08 15:22:16 +0000

    Update using-color.py

--- Python/DataVisualizations/ImprovingDataVis/using-color.py
+++ Python/DataVisualizations/ImprovingDataVis/using-color.py
@@ -35,3 +35,21 @@ sns.barplot(y = 'city', x = 'CO',
               # Replace border with bar colors
             color = 'cadetblue')
 plt.show()
+
+# B. Continuous color palettes
+# 1. Making a custom continouous palette
+# Filter the data
+cinci_2014 = pollution.query("city  ==  'Cincinnati' & year  ==  2014")
+
+# Define a custom continuous color palette
+color_palette = sns.light_palette('orangered',
+                         as_cmap = True)
+
+# Plot mapping the color of the points with custom palette
+sns.scatterplot(x = 'CO',
+                y = 'NO2',
+                hue = 'O3', 
+                data = cinci_2014,
+                palette = color_palette)
+plt.show()
+
