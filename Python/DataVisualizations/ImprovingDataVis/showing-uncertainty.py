1dbfa1bb58e2935b43eea367d8ef787893bdda52
James_McNeill <jmcneill06@outlook.com>
2022-03-08 16:55:24 +0000

    Update showing-uncertainty.py

--- Python/DataVisualizations/ImprovingDataVis/showing-uncertainty.py
+++ Python/DataVisualizations/ImprovingDataVis/showing-uncertainty.py
@@ -140,3 +140,29 @@ plt.plot('est', 'pollutant', 'wo', data = rocket_model, label = 'Point Estimate'
 plt.legend(loc = 'center left', bbox_to_anchor = (1, 0.5))
 plt.show()
 
+# D. Visualizing the bootstrap
+# 1. The bootstrap histogram
+# create function to bootstrap
+# Perform bootstrapped mean on a vector
+def bootstrap(data, n_boots):
+           return [np.mean(np.random.choice(data,len(data))) for _ in range(n_boots)]
+
+# Alternative method using lambda (single line function)
+bootstrap = lambda data, n_boots: np.array([np.mean(np.random.choice(data,len(data))) for _ in range(n_boots)])
+
+# Filtered vector of data
+cinci_may_NO2 = pollution.query("city  ==  'Cincinnati' & month  ==  5").NO2
+
+# Generate bootstrap samples
+boot_means = bootstrap(cinci_may_NO2, 1000)
+
+# Get lower and upper 95% interval bounds
+lower, upper = np.percentile(boot_means, [2.5, 97.5])
+
+# Plot shaded area for interval
+plt.axvspan(lower, upper, color = 'gray', alpha = 0.2)
+
+# Draw histogram of bootstrap samples
+sns.distplot(boot_means, bins = 100, kde = False)
+
+plt.show()
