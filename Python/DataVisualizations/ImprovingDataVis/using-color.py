c963d2ba0ac065f9c47db7b832d1aa350f031802
James_McNeill <jmcneill06@outlook.com>
2022-03-08 15:26:50 +0000

    Update using-color.py

--- Python/DataVisualizations/ImprovingDataVis/using-color.py
+++ Python/DataVisualizations/ImprovingDataVis/using-color.py
@@ -53,3 +53,16 @@ sns.scatterplot(x = 'CO',
                 palette = color_palette)
 plt.show()
 
+# 2. Customizing a diverging palette heatmap
+# Define a custom palette
+color_palette = sns.diverging_palette(250, 0, as_cmap = True)
+
+# Pass palette to plot and set axis ranges
+sns.heatmap(nov_2015_CO,
+            cmap = color_palette,
+            center = 0,
+            vmin = -4,
+            vmax = 4)
+plt.yticks(rotation = 0)
+plt.show()
+
