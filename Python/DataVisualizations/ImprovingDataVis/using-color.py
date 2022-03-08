f5bc8ea61f907b4287c91a2ef9f5a23449f35c91
James_McNeill <jmcneill06@outlook.com>
2022-03-08 15:03:55 +0000

    Update using-color.py

--- Python/DataVisualizations/ImprovingDataVis/using-color.py
+++ Python/DataVisualizations/ImprovingDataVis/using-color.py
@@ -1,4 +1,19 @@
 # Using color in your visualizations
 
 # A. Color in your visualizations
-# 1. 
+# 1. Getting rid of unnecessary color
+# Hard to read scatter of CO and NO2 w/ color mapped to city
+# sns.scatterplot('CO', 'NO2',
+#                 alpha = 0.2,
+#                 hue = 'city',
+#                 data = pollution)
+
+# Setup a facet grid to separate the cities apart
+g = sns.FacetGrid(data = pollution,
+                  col = 'city',
+                  col_wrap = 3)
+
+# Map sns.scatterplot to create separate city scatter plots
+g.map(sns.scatterplot, 'CO', 'NO2', alpha = 0.2)
+plt.show()
+
