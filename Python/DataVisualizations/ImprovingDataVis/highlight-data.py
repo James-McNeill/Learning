abb632a6a4843b60985fbbedfff203d7a7f35268
James_McNeill <jmcneill06@outlook.com>
2022-03-08 14:50:09 +0000

    Update highlight-data.py

--- Python/DataVisualizations/ImprovingDataVis/highlight-data.py
+++ Python/DataVisualizations/ImprovingDataVis/highlight-data.py
@@ -78,3 +78,15 @@ sns.swarmplot(y = "city",
 plt.title('March Ozone levels by city')
 plt.show()
 
+# C. Annotations
+# 1. A basic text annotation
+# Draw basic scatter plot of pollution data for August
+sns.scatterplot(x = 'CO', y = 'SO2', data = pollution[pollution.month  ==  8])
+
+# Label highest SO2 value with text annotation
+plt.text(0.57, 41,
+         "Cincinnati had highest observed\nSO2 value on Aug 11, 2013", 
+         # Set the font to large
+         fontdict = {'ha': 'left', 'size': 'large'})
+plt.show()
+
