c74b8381a7c74ba8b0b2445ce0ffb3556c593e25
James_McNeill <jmcneill06@outlook.com>
2022-03-09 16:41:40 +0000

    Create build-two-layers.py

---
+++ Python/DataVisualizations/GeoSpatial-Data/build-two-layers.py
@@ -0,0 +1,13 @@
+# Building 2-layer maps : combining polygons and scatterplots
+
+# A. Introduction
+# 1. Styling a scatterplot
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Scatterplot 1 - father heights vs. son heights with darkred square markers
+plt.scatter(father_son.fheight, father_son.sheight, c = 'darkred', marker = 's')
+
+# Show your plot
+plt.show()
+
