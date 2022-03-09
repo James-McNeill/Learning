1733c68adfe522ec37855ab942c247b616f84a79
James_McNeill <jmcneill06@outlook.com>
2022-03-09 16:07:32 +0000

    Update data-science-workflow.py

--- Python/DataVisualizations/ImprovingDataVis/data-science-workflow.py
+++ Python/DataVisualizations/ImprovingDataVis/data-science-workflow.py
@@ -83,4 +83,23 @@ for _,row in goods_by_state_small.iterrows():
 plt.show()
 
 # C. Making your visualizations efficient
-# 1. 
+# 1. Stacking to find trends
+# Setup two stacked plots
+_, (ax1, ax2) = plt.subplots(2, 1)
+
+# Draw location scatter plot on first plot
+sns.scatterplot("lon", "lat", 'months_open', 
+                palette = sns.light_palette("orangered",n_colors = 12), 
+                legend = False, data = markets,
+                ax = ax1);
+
+# Plot a regression plot on second plot
+sns.regplot('lon', 'months_open',
+            scatter_kws = {'alpha': 0.2, 'color': 'gray', 'marker': '|'},
+            lowess = True,
+            marker = '|', data = markets, 
+            ax = ax2)
+
+plt.show()
+
+# 2. 
