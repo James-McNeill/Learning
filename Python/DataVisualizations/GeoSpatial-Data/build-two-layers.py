400ddc2adb6f96f7a7a0d506f7242706e544a66b
James_McNeill <jmcneill06@outlook.com>
2022-03-09 16:46:20 +0000

    Update build-two-layers.py

--- Python/DataVisualizations/GeoSpatial-Data/build-two-layers.py
+++ Python/DataVisualizations/GeoSpatial-Data/build-two-layers.py
@@ -11,3 +11,33 @@ plt.scatter(father_son.fheight, father_son.sheight, c = 'darkred', marker = 's')
 # Show your plot
 plt.show()
 
+# Scatterplot 2 - yellow markers with darkblue borders
+plt.scatter(father_son.fheight, father_son.sheight, c = 'yellow', edgecolor = 'darkblue')
+
+# Show the plot
+plt.show()
+
+# Scatterplot 3
+plt.scatter(father_son.fheight, father_son.sheight,  c = 'yellow', edgecolor = 'darkblue')
+plt.grid()
+plt.xlabel('father height (inches)')
+plt.ylabel('son height (inches)')
+plt.title('Son Height as a Function of Father Height')
+
+# Show your plot
+plt.show()
+
+# 2. Extracting longitude and latitude
+# print the first few rows of df 
+print(df.head())
+
+# extract latitude to a new column: lat
+df['lat'] = [loc[0] for loc in df.Location]
+
+# extract longitude to a new column: lng
+df['lng'] = [loc[1] for loc in df.Location]
+
+# print the first few rows of df again
+print(df.head())
+
+# 3. 
