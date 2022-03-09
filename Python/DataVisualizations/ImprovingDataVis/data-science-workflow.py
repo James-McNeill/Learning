d80502f05cb8e135a5b02903b7af64ec9f22ef39
James_McNeill <jmcneill06@outlook.com>
2022-03-09 12:38:21 +0000

    Update data-science-workflow.py

--- Python/DataVisualizations/ImprovingDataVis/data-science-workflow.py
+++ Python/DataVisualizations/ImprovingDataVis/data-science-workflow.py
@@ -1,4 +1,12 @@
 # Visualization in the data science workflow
 
 # A. First explorations
-# 1. 
+# 1. Looking at the data
+# Print first three rows of data and transpose
+first_rows = markets.head(3).transpose()
+print(first_rows)
+
+# Get descriptions of every column
+col_descriptions = markets.describe(include = 'all',
+                                percentiles = [0.5]).transpose()
+print(col_descriptions)
