9ac75d99afa0c826b655c3c98e6364ec4335e9e9
James_McNeill <jmcneill06@outlook.com>
2022-03-10 15:05:51 +0000

    Update geodataframes.py

--- Python/DataVisualizations/GeoSpatial-Data/geodataframes.py
+++ Python/DataVisualizations/GeoSpatial-Data/geodataframes.py
@@ -57,3 +57,55 @@ art['geometry'] = art.apply(lambda x: Point(float(x.lng), float(x.lat)), axis=1)
 # Create a GeoDataFrame from art and verify the type
 art_geo = gpd.GeoDataFrame(art, crs = neighborhoods.crs, geometry = art.geometry)
 print(type(art_geo))
+
+# C. Spatial joins
+# 1. Spatial joins practice
+# Spatially join art_geo and neighborhoods 
+art_intersect_neighborhoods = gpd.sjoin(art_geo, neighborhoods, op = 'intersects')
+
+# Print the shape property of art_intersect_neighborhoods
+print(art_intersect_neighborhoods.shape)
+
+# Create art_within_neighborhoods by spatially joining art_geo and neighborhoods
+art_within_neighborhoods = gpd.sjoin(art_geo, neighborhoods, op = 'within')
+
+# Print the shape property of art_within_neighborhoods
+print(art_within_neighborhoods.shape)
+
+# Spatially join art_geo and neighborhoods and using the contains op
+art_containing_neighborhoods = gpd.sjoin(art_geo, neighborhoods, op = 'contains')
+
+# Print the shape property of art_containing_neighborhoods
+print(art_containing_neighborhoods.shape)
+
+# 2. Finding the neighborhood with the most public art
+# import packages
+import geopandas as gpd
+import pandas as pd
+
+# Spatially join neighborhoods with art_geo
+neighborhood_art = gpd.sjoin(art_geo, neighborhoods, op = "within")
+
+# Print the first few rows
+print(neighborhood_art.head())
+
+# 3. Aggregating points within polygons
+# Get name and title from neighborhood_art and group by name
+neighborhood_art_grouped = neighborhood_art[['name', 'title']].groupby('name')
+
+# Aggregate the grouped data and count the artworks within each polygon
+print(neighborhood_art_grouped.agg('count').sort_values(by = 'title', ascending = False)) 
+
+# 4. Plotting the Urban Residents neighborhood and art
+# Create urban_art from neighborhood_art where the neighborhood name is Urban Residents
+urban_art = neighborhood_art.loc[neighborhood_art.name == 'Urban Residents']
+
+# Get just the Urban Residents neighborhood polygon and save it as urban_polygon
+urban_polygon = neighborhoods.loc[neighborhoods.name == "Urban Residents"]
+
+# Plot the urban_polygon as ax 
+ax = urban_polygon.plot(color = 'lightgreen')
+
+# Add a plot of the urban_art and show it
+urban_art.plot(ax = ax, column = 'type', legend = True);
+plt.show()
