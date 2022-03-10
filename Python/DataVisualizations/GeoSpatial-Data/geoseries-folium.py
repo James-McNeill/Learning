6ac6f0811b230d6c12a6dd58435463782b5baccf
James_McNeill <jmcneill06@outlook.com>
2022-03-10 15:11:44 +0000

    Update geoseries-folium.py

--- Python/DataVisualizations/GeoSpatial-Data/geoseries-folium.py
+++ Python/DataVisualizations/GeoSpatial-Data/geoseries-folium.py
@@ -1,4 +1,14 @@
 # GeoSeries and Folium
 
 # A. GeoSeries attributes and methods I
-# 1. 
+# 1. Find the area of the Urban Residents neighborhood
+# Print the head of the urban polygon 
+print(urban_polygon.head())
+
+# Create a copy of the urban_polygon using EPSG:3857 and print the head
+urban_poly_3857 = urban_polygon.to_crs(epsg = 3857)
+print(urban_poly_3857.head())
+
+# Print the area of urban_poly_3857 in kilometers squared
+area = urban_poly_3857.geometry.area / 10**6
+print('The area of the Urban Residents neighborhood is ', area[0], ' km squared')
