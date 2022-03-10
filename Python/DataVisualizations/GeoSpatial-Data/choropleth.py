b2023638ce12f17ec977dc1dd5daa419ea74c2b3
James_McNeill <jmcneill06@outlook.com>
2022-03-10 17:03:42 +0000

    Update choropleth.py

--- Python/DataVisualizations/GeoSpatial-Data/choropleth.py
+++ Python/DataVisualizations/GeoSpatial-Data/choropleth.py
@@ -46,3 +46,71 @@ districts_and_permits['permit_density'] = districts_and_permits.apply(lambda row
 # Print the head of districts_and_permits
 print(districts_and_permits.head())
 
+# B. Choropleths with Geopandas
+# 1. Geopandas Choropleths
+# Import packages
+import matplotlib.pyplot as plt
+import pandas as pd
+import geopandas as gpd
+
+# Simple plot of building permit_density
+districts_and_permits.plot(column = 'permit_density', legend = True);
+plt.show();
+
+# Polished choropleth of building permit_density
+districts_and_permits.plot(column = 'permit_density', cmap = "BuGn", edgecolor = 'black', legend = True)
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.xticks(rotation = 'vertical')
+plt.title('2017 Building Project Density by Council District')
+plt.show();
+
+# 2. Area in km squared, geometry in decimal degrees
+# Change council_districts crs to epsg 3857
+council_districts = council_districts.to_crs(epsg = 3857)
+print(council_districts.crs)
+print(council_districts.head())
+
+# Create area in square km
+sqm_to_sqkm = 10**6
+council_districts['area'] = council_districts.geometry.area / sqm_to_sqkm
+
+# Change council_districts crs back to epsg 4326
+council_districts = council_districts.to_crs(epsg = 4326)
+print(council_districts.crs)
+print(council_districts.head())
+
+# 3. Spatially joining and getting counts
+# Create permits_geo
+permits_geo = gpd.GeoDataFrame(permits, crs = council_districts.crs, geometry = permits.geometry)
+
+# Spatially join permits_geo and council_districts
+permits_by_district = gpd.sjoin(permits_geo, council_districts, op = 'within')
+print(permits_by_district.head(2))
+
+# Count permits in each district
+permit_counts = permits_by_district.groupby('district').size()
+
+# Convert permit_counts to a df with 2 columns: district and bldg_permits
+counts_df = permit_counts.to_frame()
+counts_df = counts_df.reset_index()
+counts_df.columns = ['district', 'bldg_permits']
+print(counts_df.head(2))
+
+# 4. Building a polished Geopandas choropleth
+# Merge permits_by_district and counts_df
+districts_and_permits = pd.merge(permits_by_district, counts_df, on = 'district')
+
+# Create permit_density column
+districts_and_permits['permit_density'] = districts_and_permits.apply(lambda row: row.bldg_permits / row.area, axis = 1)
+print(districts_and_permits.head(2))
+
+# Create choropleth plot
+districts_and_permits.plot(column = 'permit_density', cmap = 'OrRd', edgecolor = 'black', legend = True)
+
+# Add axis labels and title
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.title('2017 Building Project Density by Council District')
+plt.show()
+
