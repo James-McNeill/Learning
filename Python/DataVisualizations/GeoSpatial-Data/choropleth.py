a95fb7fea731e509bb400e195eb3aa3e1bcef674
James_McNeill <jmcneill06@outlook.com>
2022-03-10 16:35:25 +0000

    Update choropleth.py

--- Python/DataVisualizations/GeoSpatial-Data/choropleth.py
+++ Python/DataVisualizations/GeoSpatial-Data/choropleth.py
@@ -2,3 +2,20 @@
 
 # A. What is a choropleth
 # 1. Finding counts from a spatial join
+from shapely.geometry import Point
+
+# Create a shapely Point from lat and lng
+permits['geometry'] = permits.apply(lambda x: Point((x.lng, x.lat)), axis = 1)
+
+# Build a GeoDataFrame: permits_geo
+permits_geo = gpd.GeoDataFrame(permits, crs = council_districts.crs, geometry = permits.geometry)
+
+# Spatial join of permits_geo and council_districts
+permits_by_district = gpd.sjoin(permits_geo, council_districts, op = 'within')
+print(permits_by_district.head(2))
+
+# Create permit_counts
+permit_counts = permits_by_district.groupby(['district']).size()
+print(permit_counts)
+
+# 2. Council district areas and permit counts
