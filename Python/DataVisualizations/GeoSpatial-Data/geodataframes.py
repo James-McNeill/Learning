a5394b2e4a31731f19aeeb126975867c07890356
James_McNeill <jmcneill06@outlook.com>
2022-03-10 14:15:21 +0000

    Update geodataframes.py

--- Python/DataVisualizations/GeoSpatial-Data/geodataframes.py
+++ Python/DataVisualizations/GeoSpatial-Data/geodataframes.py
@@ -31,3 +31,29 @@ plt.show()
 
 # B. Projections and coordinate reference systems
 # 1. Changing coordinate reference systems
+# Print the first row of school districts GeoDataFrame and the crs
+print(school_districts.head(1))
+print(school_districts.crs)
+
+# Convert the crs to epsg:3857
+school_districts.geometry = school_districts.geometry.to_crs(epsg = 3857)
+                        
+# Print the first row of school districts GeoDataFrame and the crs again
+print(school_districts.head(1))
+print(school_districts.crs)
+
+# 2. Construct a GeoDataFrame from a DataFrame
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+
+# Print the first few rows of the art DataFrame
+print(art.head())
+
+# Create a geometry column from lng & lat
+art['geometry'] = art.apply(lambda x: Point(float(x.lng), float(x.lat)), axis=1)
+
+# Create a GeoDataFrame from art and verify the type
+art_geo = gpd.GeoDataFrame(art, crs = neighborhoods.crs, geometry = art.geometry)
+print(type(art_geo))
