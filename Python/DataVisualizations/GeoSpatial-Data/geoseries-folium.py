6f3f4ebcee117b131edbadba5c75e0d861c50a36
James_McNeill <jmcneill06@outlook.com>
2022-03-10 15:20:58 +0000

    Update geoseries-folium.py

--- Python/DataVisualizations/GeoSpatial-Data/geoseries-folium.py
+++ Python/DataVisualizations/GeoSpatial-Data/geoseries-folium.py
@@ -12,3 +12,52 @@ print(urban_poly_3857.head())
 # Print the area of urban_poly_3857 in kilometers squared
 area = urban_poly_3857.geometry.area / 10**6
 print('The area of the Urban Residents neighborhood is ', area[0], ' km squared')
+
+# B. GeoSeries attributes and methods II
+# 1. The center of the Urban Residents neighborhood
+# Create downtown_center from urban_poly_3857
+downtown_center = urban_poly_3857.geometry.centroid
+
+# Print the type of downtown_center 
+print(type(downtown_center))
+
+# Plot the urban_poly_3857 as ax and add the center point
+ax = urban_poly_3857.plot(color = 'lightgreen')
+downtown_center.plot(ax = ax, color = 'black')
+plt.xticks(rotation = 45)
+
+# Show the plot
+plt.show()
+
+# 2. Prepare to calculate distances
+# Import packages
+from shapely.geometry import Point
+import geopandas as gpd
+import pandas as pd
+
+# Create art_dist_meters using art and the geometry from art
+art_dist_meters = gpd.GeoDataFrame(art, geometry = art.geometry, crs = {'init': 'epsg:4326'})
+print(art_dist_meters.head(2))
+
+# Set the crs of art_dist_meters to use EPSG:3857
+art_dist_meters.geometry = art_dist_meters.geometry.to_crs(epsg = 3857)
+print(art_dist_meters.head(2))
+
+# Add a column to art_meters, center
+art_dist_meters['center'] = center_point
+
+# 3. Art distances from neighborhood center
+# Import package for pretty printing
+import pprint
+
+# Build a dictionary of titles and distances for Urban Residents art
+art_distances = {}
+for row in art_dist_meters.iterrows():
+    vals = row[1]
+    key = vals['title']
+    ctr = vals['center']
+    art_distances[key] = vals['geometry'].distance(ctr)
+
+# Pretty print the art_distances
+pprint.pprint(art_distances)
+
