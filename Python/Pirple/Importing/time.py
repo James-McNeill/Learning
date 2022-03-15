b01b7ae504ce0e44ed6da5fbe88a2925354737f9
James_McNeill <jmcneill06@outlook.com>
2022-03-15 16:44:17 +0000

    Update time.py

--- Python/Pirple/Importing/time.py
+++ Python/Pirple/Importing/time.py
@@ -1,1 +1,17 @@
 # Reviewing the time library
+import time
+
+currentTime = time.process_time()
+print("Hello")
+pastTime = time.process_time()
+print(pastTime - currentTime)
+
+# The process will sleep for three seconds
+print("1")
+time.sleep(3)
+print("2")
+
+# Create values within the range. Sleep for 1 second each time
+for i in range(1, 11):
+    print(i)
+    time.sleep(1)
