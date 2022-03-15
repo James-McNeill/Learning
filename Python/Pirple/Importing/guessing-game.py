be91f565f54708880f735df8274eb0091b67abb7
James_McNeill <jmcneill06@outlook.com>
2022-03-15 16:57:21 +0000

    Update guessing-game.py

--- Python/Pirple/Importing/guessing-game.py
+++ Python/Pirple/Importing/guessing-game.py
@@ -1,1 +1,17 @@
+# Import a package
+from random import randint
+
+randVal = randint(0,100)
+
+while(True):
+    guess = int(input("Please enter your guess: "))
+    if guess == randVal:
+        break
+    elif guess < randVal:
+        print("Your guess was too low")
+    else:
+        print("Your guess was too high")
+
+print("You guessed correctly with:", guess)
+
 
