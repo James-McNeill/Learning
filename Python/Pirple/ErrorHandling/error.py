6344d3e6b82e6f0367cea239a2c6cd8dea379c86
James_McNeill <jmcneill06@outlook.com>
2022-03-15 17:19:55 +0000

    Update error.py

--- Python/Pirple/ErrorHandling/error.py
+++ Python/Pirple/ErrorHandling/error.py
@@ -1,1 +1,52 @@
+# Try block for error handling
+try:
+    print(int("Hello"))
+except:
+    print("Entered exception")
+    
+print("Past exception")
 
+keyWord = "123"
+try:
+    print(int(keyWord))
+except:
+    print("Entered exception")
+    
+print("Past exception")
+
+keyWord = "Hello"
+try:
+    print(int(keyWord))
+except:
+    print("Entered exception")
+    
+print("Past exception")
+
+keyWord = "Hello"
+try:
+    print(int(keyWord))
+except:
+    pass # keyword means that nothing is done
+    print("Entered exception")
+    
+print("Past exception")
+
+keyWord = "Hello"
+try:
+    print(int(keyWord))
+except Exception as e: # helps to identify exceptions
+    print(str(e))
+    
+print("Past exception")
+
+keyWord = "Hello"
+try:
+    print(int(keyWord))
+except ValueError: # check for special errors
+    print("got a ValueError")
+except:
+    print("Other type of exception")
+finally: # review all the checks and finally we do this
+    print("finally")
+    
+print("Past exception")
