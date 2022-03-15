98dca4d532f2a2f3e7b575610fff34303faa995b
James_McNeill <jmcneill06@outlook.com>
2022-03-15 16:50:58 +0000

    Update math.py

--- Python/Pirple/Importing/math.py
+++ Python/Pirple/Importing/math.py
@@ -1,1 +1,26 @@
+# Reviewing the math library
+import math
 
+Val = 3
+
+sqrtVal = math.sqrt(Val)
+print(sqrtVal ** 2)
+
+expVal = math.exp(Val)
+print(expVal)
+
+factVal = math.factorial(Val)
+print(factVal)
+
+sinVal = math.sin(Val)
+print(sinVal)
+
+Val1 = 3.14
+
+# rounds down to nearest integer
+floorVal = math.floor(Val1)
+print(floorVal)
+
+# rounds up to nearest integer
+celingVal = math.ceil(Val1)
+print(celingVal)
