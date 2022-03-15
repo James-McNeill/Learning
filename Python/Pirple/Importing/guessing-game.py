87ed063f75d1a712208c6202eeab1e2e2703620b
James_McNeill <jmcneill06@outlook.com>
2022-03-15 17:07:16 +0000

    Update guessing-game.py

--- Python/Pirple/Importing/guessing-game.py
+++ Python/Pirple/Importing/guessing-game.py
@@ -1,17 +1,41 @@
+# Guessing game part A
+
 # Import a package
-from random import randint
+# from random import randint
 
-randVal = randint(0,100)
+# randVal = randint(0,100)
 
+# while(True):
+#     guess = int(input("Please enter your guess: "))
+#     if guess == randVal:
+#         break
+#     elif guess < randVal:
+#         print("Your guess was too low")
+#     else:
+#         print("Your guess was too high")
+
+# print("You guessed correctly with:", guess)
+
+# Guessing game part B
+from random import random
+from time import process_time
+
+randVal = random() # range between 0 and < 1
+upper = 1.0
+lower = 0.0
+
+guess = 0.5 # initial guess
+
+startTime = process_time()
 while(True):
-    guess = int(input("Please enter your guess: "))
     if guess == randVal:
         break
     elif guess < randVal:
-        print("Your guess was too low")
+        lower = guess
     else:
-        print("Your guess was too high")
+        upper = guess
+    guess = (upper + lower) / 2
 
-print("You guessed correctly with:", guess)
-
-
+endTime = process_time()
+print(guess)
+print("It took us:", endTime - startTime, "seconds")
