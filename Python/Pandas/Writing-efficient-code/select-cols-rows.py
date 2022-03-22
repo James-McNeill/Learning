82ee60c3de6f8c02ca7cad2117f7064261332ab7
James_McNeill <jmcneill06@outlook.com>
2022-03-22 12:29:10 +0000

    Create select-cols-rows.py

---
+++ Python/Pandas/Writing-efficient-code/select-cols-rows.py
@@ -0,0 +1,5 @@
+# Selecting columns and rows efficiently
+# This chapter will give you an overview of why efficient code matters and selecting specific and random rows and columns efficiently.
+
+# A. The need for efficient coding I
+# 1. Measuring time
