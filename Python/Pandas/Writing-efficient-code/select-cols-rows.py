95868944900716f7657907952294252dde5a6d1b
James_McNeill <jmcneill06@outlook.com>
2022-03-22 12:51:12 +0000

    Update select-cols-rows.py

--- Python/Pandas/Writing-efficient-code/select-cols-rows.py
+++ Python/Pandas/Writing-efficient-code/select-cols-rows.py
@@ -99,3 +99,32 @@ names_end_time = time.time()
 # Print the time it took
 print("Time using selection by name : {} sec".format(names_end_time - names_start_time))
 
+# C. Select random rows
+# 1. Random row selection
+# The built in dataframe method was more efficient
+# Extract number of rows in dataset
+N=poker_hands.shape[0]
+
+# Select and time the selection of the 75% of the dataset's rows
+rand_start_time = time.time()
+poker_hands.iloc[np.random.randint(low=0, high=N, size=int(0.75 * N))]
+print("Time using Numpy: {} sec".format(time.time() - rand_start_time))
+
+# Select and time the selection of the 75% of the dataset's rows using sample()
+samp_start_time = time.time()
+poker_hands.sample(int(0.75 * N), axis=0, replace = True)
+print("Time using .sample: {} sec".format(time.time() - samp_start_time))
+
+# 2. Random column selection
+# Extract number of columns in dataset
+D=poker_hands.shape[1]
+
+# Select and time the selection of 4 of the dataset's columns using NumPy
+np_start_time = time.time()
+poker_hands.iloc[:,np.random.randint(low=0, high=D, size=4)]
+print("Time using NymPy's random.randint(): {} sec".format(time.time() - np_start_time))
+
+# Select and time the selection of 4 of the dataset's columns using pandas
+pd_start_time = time.time()
+poker_hands.sample(4, axis=1)
+print("Time using panda's .sample(): {} sec".format(time.time() - pd_start_time))
