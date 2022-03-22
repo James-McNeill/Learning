9371b80bee107c3e1ff5141840a5665546990fff
James_McNeill <jmcneill06@outlook.com>
2022-03-22 12:29:38 +0000

    Update select-cols-rows.py

--- Python/Pandas/Writing-efficient-code/select-cols-rows.py
+++ Python/Pandas/Writing-efficient-code/select-cols-rows.py
@@ -3,3 +3,16 @@
 
 # A. The need for efficient coding I
 # 1. Measuring time
+
+
+
+# Calculate the result of the problem using formula() and print the time required
+N = 1000000
+fm_start_time = time.time()
+first_method = formula(N)
+print("Time using formula: {} sec".format(time.time() - fm_start_time))
+
+# Calculate the result of the problem using brute_force() and print the time required
+sm_start_time = time.time()
+second_method = brute_force(N)
+print("Time using the brute force: {} sec".format(time.time() - sm_start_time))
