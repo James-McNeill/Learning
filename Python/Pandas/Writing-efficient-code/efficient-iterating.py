07516e576cf0f390e205ecf34ffb975cd58f41e0
James_McNeill <jmcneill06@outlook.com>
2022-03-22 17:10:53 +0000

    Update efficient-iterating.py

--- Python/Pandas/Writing-efficient-code/efficient-iterating.py
+++ Python/Pandas/Writing-efficient-code/efficient-iterating.py
@@ -20,4 +20,23 @@ for index, values in data_generator:
       	# Sum the ranks of all the cards
         hand_sum = sum([values[1], values[3], values[5], values[7], values[9]])
         
-# 3. 
+# B. Looping using the .apply() function
+# 1. .apply() function in every cell
+# Define the lambda transformation
+get_square = lambda x: x**2
+
+# Apply the transformation
+data_sum = poker_hands.apply(get_square)
+print(data_sum.head())
+
+# 2. .apply() for rows iteration
+# Define the lambda transformation
+get_variance = lambda x: np.var(x)
+
+# Apply the transformation
+data_tr = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].apply(get_variance, axis=1)
+print(data_tr.head())
+
+# Apply the transformation - implement on each rank
+data_tr = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].apply(get_variance, axis=0)
+print(data_tr.head())
