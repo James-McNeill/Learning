41aabff04788b22212eb4683a887970de395236c
James_McNeill <jmcneill06@outlook.com>
2022-03-22 16:32:19 +0000

    Update replacing-values.py

--- Python/Pandas/Writing-efficient-code/replacing-values.py
+++ Python/Pandas/Writing-efficient-code/replacing-values.py
@@ -1,4 +1,48 @@
 # Replacing values in a DataFrame
 
 # A. Replace scalar values using .replace()
-# 1. 
+# 1. Replacing scalar values I
+# Replace Class 1 to -2 
+poker_hands['Class'].replace(1, -2, inplace=True)
+# Replace Class 2 to -3
+poker_hands['Class'].replace(2, -3, inplace=True)
+
+print(poker_hands[['Class', 'Explanation']])
+
+# 2. Replace scalar values II
+# a. First method using .loc[]
+start_time = time.time()
+
+# Replace all the entries that has 'FEMALE' as a gender with 'GIRL'
+names['Gender'].loc[names.Gender == 'FEMALE'] = 'GIRL'
+
+print("Time using .loc[]: {} sec".format(time.time() - start_time))
+
+# b. Second method using .replace(). This pandas method is much more efficient
+start_time = time.time()
+
+# Replace all the entries that has 'FEMALE' as a gender with 'GIRL'
+names['Gender'].replace('FEMALE', 'GIRL', inplace=True)
+
+print("Time using .replace(): {} sec".format(time.time() - start_time))
+
+# B. Replace values using lists
+# 1. Replace multiple values I
+start_time = time.time()
+
+# Replace all non-Hispanic ethnicities with 'NON HISPANIC'
+names['Ethnicity'].loc[(names["Ethnicity"] == 'BLACK NON HISP') | 
+                      (names["Ethnicity"] == 'BLACK NON HISPANIC') | 
+                      (names["Ethnicity"] == 'WHITE NON HISP') | 
+                      (names["Ethnicity"] == 'WHITE NON HISPANIC')] = 'NON HISPANIC'
+
+print("Time using .loc[]: sec".format(time.time() - start_time))
+
+# b. The pandas method is quicker
+start_time = time.time()
+
+# Replace all non-Hispanic ethnicities with 'NON HISPANIC'
+names['Ethnicity'].replace(['BLACK NON HISP', 'BLACK NON HISPANIC', 'WHITE NON HISP' , 'WHITE NON HISPANIC'], 'NON HISPANIC', inplace=True)
+
+print("Time using .replace(): {} sec".format(time.time() - start_time))
+
