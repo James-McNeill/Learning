ebbdc05c16fe588c4c548a7a701efa965cdc0a6d
James_McNeill <jmcneill06@outlook.com>
2022-03-22 16:39:05 +0000

    Update replacing-values.py

--- Python/Pandas/Writing-efficient-code/replacing-values.py
+++ Python/Pandas/Writing-efficient-code/replacing-values.py
@@ -54,3 +54,15 @@ names['Ethnicity'].replace(['ASIAN AND PACI','BLACK NON HISP', 'WHITE NON HISP']
 
 print("Time using .replace(): {} sec".format(time.time() - start_time))
 
+# C. Replace values using dictionaries
+# 1. Replace single values I
+# Replace Royal flush or Straight flush to Flush
+poker_hands.replace({'Royal flush':'Flush', 'Straight flush':'Flush'}, inplace=True)
+print(poker_hands['Explanation'].head())
+
+# 2. Replace single values II
+# Replace the number rank by a string
+names['Rank'].replace({1:'FIRST', 2:'SECOND', 3:'THIRD'}, inplace=True)
+print(names.head())
+
+# 3. Replace multiple values III
