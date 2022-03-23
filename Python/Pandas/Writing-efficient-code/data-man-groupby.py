98bb0e6e454c97f86842f43afc33d16a5e1a9650
James_McNeill <jmcneill06@outlook.com>
2022-03-23 12:00:49 +0000

    Create data-man-groupby.py

---
+++ Python/Pandas/Writing-efficient-code/data-man-groupby.py
@@ -0,0 +1,13 @@
+# Data manipulation using .groupby()
+
+# A. Data transformation using .groupby().transform
+# 1. The min-max normalization using .transform()
+# Define the min-max transformation
+min_max_tr = lambda x: (x - x.min()) / (x.max() - x.min())
+
+# Group the data according to the time
+restaurant_grouped = restaurant_data.groupby('time')
+
+# Apply the transformation
+restaurant_min_max_group = restaurant_grouped.transform(min_max_tr)
+print(restaurant_min_max_group.head())
