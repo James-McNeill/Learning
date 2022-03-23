de7847aaf4f7e95e5410c1737bf5a8750d832b8e
James_McNeill <jmcneill06@outlook.com>
2022-03-23 11:07:07 +0000

    Update efficient-iterating.py

--- Python/Pandas/Writing-efficient-code/efficient-iterating.py
+++ Python/Pandas/Writing-efficient-code/efficient-iterating.py
@@ -40,3 +40,17 @@ print(data_tr.head())
 # Apply the transformation - implement on each rank
 data_tr = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].apply(get_variance, axis=0)
 print(data_tr.head())
+
+# C. Vectorization over pandas series
+# 1. Pandas vectorization in action
+# Calculate the mean rank in each hand
+row_start_time = time.time()
+mean_r = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].mean(axis=1)
+print("Time using pandas vectorization for rows: {} sec".format(time.time() - row_start_time))
+print(mean_r.head())
+
+# Calculate the mean rank of each of the 5 card in all hands
+col_start_time = time.time()
+mean_c = poker_hands[['R1', 'R2', 'R3', 'R4', 'R5']].mean(axis=0)
+print("Time using pandas vectorization for columns: {} sec".format(time.time() - col_start_time))
+print(mean_c.head())
