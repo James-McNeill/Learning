c8685fede69d6e5497126da6a4b33399685b6848
James_McNeill <jmcneill06@outlook.com>
2022-03-26 10:11:54 +0000

    Update intro.py

--- Python/DataVisualizations/Bokeh/intro.py
+++ Python/DataVisualizations/Bokeh/intro.py
@@ -42,3 +42,32 @@ fig.vbar(x=positions["position"], top=positions["three_point_perc"])
 # Produce the html file and display the plot
 output_file(filename="3p_fg_by_position.html")
 show(fig)
+
+# B. Configuration tools
+# 1. Setting tools
+# Create a list of tools. Will only be included within the interactive pane 
+tools = ["poly_select", "wheel_zoom", "reset", "save"]
+
+# Create figure and set tools
+fig = figure(x_axis_label="Field Goal (%)", y_axis_label="Points per Game", tools=tools)
+
+# Add circle glyphs
+fig.circle(x=nba["field_goal_perc"], y=nba["points"])
+
+# Generate HTML file and display plot
+output_file(filename="points_vs_field_goal_perc.html")
+show(fig)
+
+# 2. Adding LassoSelectTool
+tools = ['poly_select', 'wheel_zoom', 'reset', 'save']
+
+# Import LassoSelectTool
+from bokeh.models import LassoSelectTool
+
+fig = figure(x_axis_label="Field Goal (%)", y_axis_label="Points per Game", tools=tools)
+fig.circle(x=nba["field_goal_perc"], y=nba["points"])
+
+# Update the figure to include LassoSelectTool
+fig.add_tools(LassoSelectTool())
+output_file(filename="updated_plot_with_lasso_select.html")
+show(fig)
