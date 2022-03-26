ad75894c1b306022b2a30495f04eacdbe7481e5b
James_McNeill <jmcneill06@outlook.com>
2022-03-26 10:30:04 +0000

    Update cust-vis.py

--- Python/DataVisualizations/Bokeh/cust-vis.py
+++ Python/DataVisualizations/Bokeh/cust-vis.py
@@ -18,3 +18,23 @@ fig.circle(x=south["year_built"], y=south["land_area"], color="red", legend_labe
 
 output_file(filename="north_vs_south.html")
 show(fig)
+
+# 2. Customizing glyphs
+# Three data subsets have been created
+houses = melb.loc[melb["type"] == "h"]
+units = melb.loc[melb["type"] == "u"]
+townhouses = melb.loc[melb["type"] == "t"]
+
+# Create figure
+fig = figure(x_axis_label="Year Built", y_axis_label="Distance from CBD (km)")
+
+# Add circle glyphs for houses
+fig.circle(x=houses["year_built"], y=houses["distance"], legend_label="House", color="purple")
+
+# Add square glyphs for units
+fig.square(x=units["year_built"], y=units["distance"], legend_label="Unit", color="red")
+
+# Add triangle glyphs for townhouses
+fig.triangle(x=townhouses["year_built"], y=townhouses["distance"], legend_label="Townhouse", color="green")
+output_file(filename="year_built_vs_distance_by_property_type.html")
+show(fig)
