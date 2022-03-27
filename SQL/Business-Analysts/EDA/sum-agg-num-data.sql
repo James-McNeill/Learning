4ef016d1493345fb0abcd0a1dbb107a7b6851078
James_McNeill <jmcneill06@outlook.com>
2022-03-27 11:42:11 +0100

    Update sum-agg-num-data.sql

--- SQL/Business-Analysts/EDA/sum-agg-num-data.sql
+++ SQL/Business-Analysts/EDA/sum-agg-num-data.sql
@@ -22,3 +22,31 @@ SELECT unanswered_count/question_count::numeric AS computed_pct,
  -- Select rows where question_count is not 0
  WHERE question_count > 0
  LIMIT 10;
+
+-- 3. Summarize numeric columns
+-- Select sector and summary measures of fortune500 profits
+SELECT sector,
+       min(profits),
+       avg(profits),
+       max(profits),
+       stddev(profits)
+  FROM fortune500
+ -- What to group by?
+ GROUP BY sector
+ -- Order by the average profits
+ ORDER BY avg;
+ 
+-- 4. Summarize group statistics
+-- Compute standard deviation of maximum values
+SELECT stddev(maxval),
+	   -- min
+       min(maxval),
+       -- max
+       max(maxval),
+       -- avg
+       avg(maxval)
+  -- Subquery to compute max of question_count by tag
+  FROM (SELECT max(question_count) AS maxval
+          FROM stackoverflow
+         -- Compute max by...
+         GROUP BY tag) AS max_results; -- alias for subquery
