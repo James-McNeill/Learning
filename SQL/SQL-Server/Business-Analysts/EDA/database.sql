74bf7739e0096eba15cd2f7d70d8a5a31b96ae7c
James_McNeill <jmcneill06@outlook.com>
2022-03-27 10:43:22 +0100

    Create database.sql

---
+++ SQL/SQL-Server/Business-Analysts/EDA/database.sql
@@ -0,0 +1,6 @@
+# What's in the database
+# Start exploring a database by identifying the tables and the foreign keys that link them. 
+# Look for missing values, count the number of observations, and join tables to understand how they're related. 
+# Learn about coalescing and casting data along the way.
+
+# A. What's in the database
