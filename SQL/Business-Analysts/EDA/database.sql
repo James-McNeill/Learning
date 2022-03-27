dc91ffd6718bd9e6397d7a04fcec0792822ebee1
James_McNeill <jmcneill06@outlook.com>
2022-03-27 11:14:19 +0100

    Update database.sql

--- SQL/Business-Analysts/EDA/database.sql
+++ SQL/Business-Analysts/EDA/database.sql
@@ -19,3 +19,60 @@ SELECT company.name
   FROM company
        INNER JOIN fortune500
        ON company.ticker=fortune500.ticker;
+
+-- B. The keys to the database
+-- 1. Foreign keys
+-- Recall that foreign keys reference another row in the database via a unique ID. Values in a foreign key column are restricted to values in the referenced column OR NULL.
+
+-- 2. Read an entity relationship diagram
+-- Count the number of tags with each type
+SELECT type, count(tag) AS count
+  FROM tag_type
+ -- To get the count for each type, what do you need to do?
+ GROUP BY type
+ -- Order the results with the most common
+ -- tag types listed first
+ ORDER BY count(tag) DESC;
+ 
+ -- Select the 3 columns desired
+SELECT company.name, tag_type.tag, tag_type.type
+  FROM company
+  	   -- Join to the tag_company table
+       INNER JOIN tag_company 
+       ON company.id = tag_company.company_id
+       -- Join to the tag_type table
+       INNER JOIN tag_type
+       ON tag_company.tag = tag_type.tag
+  -- Filter to most common type
+  WHERE type='cloud';
+  
+-- 3. Coalesce
+-- Use coalesce
+SELECT coalesce(industry, sector, 'Unknown') AS industry2,
+       -- Don't forget to count!
+       count(*) 
+  FROM fortune500 
+-- Group by what? (What are you counting by?)
+ GROUP BY industry2
+-- Order results to see most common first
+ ORDER BY count(*) DESC
+-- Limit results to get just the one value you want
+ LIMIT 1;
+ 
+--  4. Coalesce with a self-join
+SELECT company_original.name, fortune500.title, fortune500.rank
+  -- Start with original company information
+  FROM company AS company_original
+       -- Join to another copy of company with parent
+       -- company information
+	   LEFT JOIN company AS company_parent
+       ON company_original.parent_id = company_parent.id
+       -- Join to fortune500, only keep rows that match
+       INNER JOIN fortune500 
+       -- Use parent ticker if there is one, 
+       -- otherwise original ticker
+       ON coalesce(company_parent.ticker, 
+                   company_original.ticker) = 
+             fortune500.ticker
+ -- For clarity, order by rank
+ ORDER BY rank; 
