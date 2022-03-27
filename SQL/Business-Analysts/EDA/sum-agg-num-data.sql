081a08ff523bd22cbf5f2854b85ee9162d273365
James_McNeill <jmcneill06@outlook.com>
2022-03-27 11:55:12 +0100

    Update sum-agg-num-data.sql

--- SQL/Business-Analysts/EDA/sum-agg-num-data.sql
+++ SQL/Business-Analysts/EDA/sum-agg-num-data.sql
@@ -50,3 +50,42 @@ SELECT stddev(maxval),
           FROM stackoverflow
          -- Compute max by...
          GROUP BY tag) AS max_results; -- alias for subquery
+-- B. Explorting distributions
+-- 1. Truncate
+-- Truncate employees. Removes the values from the overall number e.g. 13456, trunc(value, -3) result: 13000
+SELECT trunc(employees, -4) AS employee_bin,
+       -- Count number of companies with each truncated value
+       count(*)
+  FROM fortune500
+ -- Limit to which companies?
+ WHERE employees < 100000
+ -- Use alias to group
+ GROUP BY employee_bin
+ -- Use alias to order
+ ORDER BY employee_bin;
+ 
+--  2. Generate a series
+-- generate_series(from, to, step)
+-- Bins created in Step 2
+WITH bins AS (
+      SELECT generate_series(2200, 3050, 50) AS lower,
+             generate_series(2250, 3100, 50) AS upper),
+     -- Subset stackoverflow to just tag dropbox (Step 1)
+     dropbox AS (
+      SELECT question_count 
+        FROM stackoverflow
+       WHERE tag='dropbox') 
+-- Select columns for result
+-- What column are you counting to summarize?
+SELECT lower, upper, count(question_count) 
+  FROM bins  -- Created above
+       -- Join to dropbox (created above), 
+       -- keeping all rows from the bins table in the join
+       LEFT JOIN dropbox
+       -- Compare question_count to lower and upper
+         ON question_count >= lower 
+        AND question_count < upper
+ -- Group by lower and upper to count values in each bin
+ GROUP BY lower, upper
+ -- Order by lower to put bins in order
+ ORDER BY lower;
