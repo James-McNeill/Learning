dcb0ab96b09c3d37bfdf4313f0d99d49306d40e1
James_McNeill <jmcneill06@outlook.com>
2022-03-27 10:56:18 +0100

    Update database.sql

--- SQL/Business-Analysts/EDA/database.sql
+++ SQL/Business-Analysts/EDA/database.sql
@@ -1,8 +1,21 @@
-# What's in the database
-# Start exploring a database by identifying the tables and the foreign keys that link them. 
-# Look for missing values, count the number of observations, and join tables to understand how they're related. 
-# Learn about coalescing and casting data along the way.
+-- What's in the database
+-- Start exploring a database by identifying the tables and the foreign keys that link them. 
+-- Look for missing values, count the number of observations, and join tables to understand how they're related. 
+-- Learn about coalescing and casting data along the way.
 
-# Making use of PostgreSQL
+-- Making use of PostgreSQL
 
-# A. What's in the database
+-- A. What's in the database
+-- 1. Count missing values
+-- Select the count of ticker, 
+-- subtract from the total number of rows, 
+-- and alias as missing
+SELECT count(*) - count(ticker) AS missing
+  FROM fortune500;
+  
+-- 2. Join tables
+SELECT company.name
+-- Table(s) to select from
+  FROM company
+       INNER JOIN fortune500
+       ON company.ticker=fortune500.ticker;
