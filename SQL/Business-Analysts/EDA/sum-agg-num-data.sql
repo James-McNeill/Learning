bc30627ba7ce7b96a768bf129747d61472042321
James_McNeill <jmcneill06@outlook.com>
2022-03-27 11:30:44 +0100

    Create sum-agg-num-data.sql

---
+++ SQL/Business-Analysts/EDA/sum-agg-num-data.sql
@@ -0,0 +1,4 @@
+-- Summarizing and aggregating numeric data
+-- You'll build on functions like min and max to summarize numeric data in new ways. 
+-- Add average, variance, correlation, and percentile functions to your toolkit, and learn how to truncate and round numeric values too. 
+-- Build complex queries and save your results by creating temporary tables.
