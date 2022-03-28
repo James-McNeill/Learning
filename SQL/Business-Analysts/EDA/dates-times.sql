86f6716a4c008069ea182e62974d7873ac583c14
James_McNeill <jmcneill06@outlook.com>
2022-03-28 16:45:26 +0100

    Update dates-times.sql

--- SQL/Business-Analysts/EDA/dates-times.sql
+++ SQL/Business-Analysts/EDA/dates-times.sql
@@ -102,3 +102,17 @@ SELECT date_trunc('month', day) AS month,
  ORDER BY month;
 
 -- C. Aggregating with date/time series
+-- 1. Find missing dates
+SELECT day
+-- 1) Subquery to generate all dates
+-- from min to max date_created
+  FROM (SELECT generate_series(min(date_created),
+                               max(date_created),
+                               '1 day'::interval)::date AS day
+          -- What table is date_created in?
+          FROM evanston311) AS all_dates
+-- 4) Select dates (day from above) that are NOT IN the subquery
+ WHERE day NOT IN
+       -- 2) Subquery to select all date_created values as dates
+       (SELECT date_created::date
+          FROM evanston311);
