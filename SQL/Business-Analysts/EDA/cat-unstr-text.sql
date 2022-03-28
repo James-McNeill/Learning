2e3591d22b13e3b211a4648369418d1c1d8bc071
James_McNeill <jmcneill06@outlook.com>
2022-03-28 14:39:24 +0100

    Create cat-unstr-text.sql

---
+++ SQL/Business-Analysts/EDA/cat-unstr-text.sql
@@ -0,0 +1,5 @@
+-- Exploring categorical data and unstructured text
+
+-- Text, or character, data can get messy, but you'll learn how to deal with inconsistencies in case, spacing, and delimiters. 
+-- Learn how to use a temporary table to recode messy categorical data to standardized values you can count and aggregate.
+
