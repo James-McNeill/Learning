0b7784a2963fcbe09912215f6d13f351a1e141af
James_McNeill <jmcneill06@outlook.com>
2022-03-28 15:55:21 +0100

    Create dates-times.sql

---
+++ SQL/Business-Analysts/EDA/dates-times.sql
@@ -0,0 +1,3 @@
+-- 
+
+-- What time is it? In this chapter, you'll learn how to find out. You'll aggregate date/time data by hour, day, month, or year and practice both constructing time series and finding gaps in them.
