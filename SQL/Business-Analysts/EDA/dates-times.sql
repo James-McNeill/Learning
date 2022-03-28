7ebf0c90c9ae12755272218b5c98fdb0682a0e84
James_McNeill <jmcneill06@outlook.com>
2022-03-28 16:37:43 +0100

    Update dates-times.sql

--- SQL/Business-Analysts/EDA/dates-times.sql
+++ SQL/Business-Analysts/EDA/dates-times.sql
@@ -22,3 +22,83 @@ SELECT count(*)
  WHERE date_created >= '2017-03-13'
    AND date_created < '2017-03-13'::date + 1;
 
+-- 2. Date arithmetic
+-- Subtract the min date_created from the max
+SELECT max(date_created) - min(date_created)
+  FROM evanston311;
+
+-- How old is the most recent request?
+SELECT now() - max(date_created)
+  FROM evanston311;
+
+-- Add 100 days to the current timestamp
+SELECT now() + '100 days'::interval;
+
+-- Select the current timestamp, 
+-- and the current timestamp + 5 minutes
+SELECT now(),
+    now() + '5 minutes'::interval;
+
+-- 3. Completion time by category
+-- Select the category and the average completion time by category
+SELECT category, 
+       avg(date_completed - date_created) AS completion_time
+  FROM evanston311
+ GROUP BY category
+-- Order the results
+ ORDER BY completion_time DESC;
+
+-- B. Date/time components and aggregation
+-- 1. Date parts
+-- Extract the month from date_created and count requests
+SELECT date_part('month', date_created) AS month, 
+       count(*)
+  FROM evanston311
+ -- Limit the date range
+ WHERE date_created >= '2016-01-01'
+   AND date_created < '2018-01-01'
+ -- Group by what to get monthly counts?
+ GROUP BY month;
+
+-- Get the hour and count requests
+SELECT date_part('hour', date_created) AS hour,
+       count(*)
+  FROM evanston311
+ GROUP BY hour
+ -- Order results to select most common
+ ORDER BY count DESC
+ LIMIT 1;
+
+-- Count requests completed by hour
+SELECT date_part('hour', date_completed) AS hour,
+       count(*)
+  FROM evanston311
+ GROUP BY hour
+ ORDER BY hour DESC;
+
+-- 2. Variation by day of week
+-- Select name of the day of the week the request was created 
+SELECT to_char(date_created, 'day') AS day, 
+       -- Select avg time between request creation and completion
+       avg(date_completed - date_created) AS duration
+  FROM evanston311 
+ -- Group by the name of the day of the week and 
+ -- integer value of day of week the request was created
+ GROUP BY day, EXTRACT(DOW FROM date_created)
+ -- Order by integer value of the day of the week 
+ -- the request was created
+ ORDER BY EXTRACT(DOW FROM date_created);
+
+-- 3. Date truncation
+-- Aggregate daily counts by month
+SELECT date_trunc('month', day) AS month,
+       avg(count)
+  -- Subquery to compute daily counts
+  FROM (SELECT date_trunc('day', date_created) AS day,
+               count(*) AS count
+          FROM evanston311
+         GROUP BY day) AS daily_count
+ GROUP BY month
+ ORDER BY month;
+
+-- C. Aggregating with date/time series
