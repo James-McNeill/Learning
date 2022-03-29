9ff2f4c206f7094c56239761f30a2dac620b9c4c
James_McNeill <jmcneill06@outlook.com>
2022-03-29 12:06:58 +0100

    Update simple-sql.sql

--- SQL/Business-Analysts/Data-Driven-Decision-Making/simple-sql.sql
+++ SQL/Business-Analysts/Data-Driven-Decision-Making/simple-sql.sql
@@ -26,3 +26,32 @@ FROM renting
 GROUP BY customer_id
 HAVING count(*) > 7 -- Select only customers with more than 7 movie rentals
 ORDER BY avg(rating); -- Order by the average rating in ascending order
+
+-- B. Joining movie ratings with customer data
+-- 1. Join renting and customers
+SELECT avg(r.rating) -- Average ratings of customers from Belgium
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+WHERE c.country='Belgium';
+
+-- 2. Aggregating revenue, rentals and active customers
+SELECT 
+	SUM(m.renting_price), 
+	COUNT(*), 
+	COUNT(DISTINCT r.customer_id)
+FROM renting AS r
+LEFT JOIN movies AS m
+ON r.movie_id = m.movie_id
+-- Only look at movie rentals in 2018
+WHERE date_renting BETWEEN '2018-01-01' AND '2018-12-31';
+
+-- 3. Movies and actors
+SELECT distinct(a.name), -- Create a list of movie titles and actor names
+       m.title
+FROM actsin ai
+LEFT JOIN movies AS m
+ON m.movie_id = ai.movie_id
+LEFT JOIN actors AS a
+ON a.actor_id = ai.actor_id;
+
