dc2873430302573eda04036d06c945fc6b347135
James_McNeill <jmcneill06@outlook.com>
2022-03-29 09:47:25 +0100

    Update intro.sql

--- SQL/Business-Analysts/Data-Driven-Decision-Making/intro.sql
+++ SQL/Business-Analysts/Data-Driven-Decision-Making/intro.sql
@@ -1,4 +1,17 @@
 -- Introduction to business intelligence for a online movie rental database
 
 -- A. Introduction to data driven decision making
--- 1.
+-- 1. Exploring the table renting
+SELECT *  -- Select all
+FROM renting;        -- From table renting
+
+SELECT movie_id,  -- Select all columns needed to compute the average rating per movie
+       rating
+FROM renting;
+
+-- B. Filtering and ordering
+-- 1. Working with dates
+SELECT *
+FROM renting
+WHERE date_renting BETWEEN '2018-04-01' AND '2018-08-31'
+ORDER BY date_renting DESC; -- Order by recency in decreasing order
