743d0cfd897099b8321a73651f8f900a0f1b7616
James_McNeill <jmcneill06@outlook.com>
2022-03-29 15:52:45 +0100

    Update advanced-sql.sql

--- SQL/Business-Analysts/Data-Driven-Decision-Making/advanced-sql.sql
+++ SQL/Business-Analysts/Data-Driven-Decision-Making/advanced-sql.sql
@@ -1,4 +1,31 @@
 -- Data Driven Decision Making with advanced SQL queries
 
 -- A. Nested query
--- 1. 
+-- 1. Often rented movies
+SELECT *
+FROM movies
+WHERE movie_id IN  -- Select movie IDs from the inner query
+	(SELECT movie_id
+	FROM renting
+	GROUP BY movie_id
+	HAVING COUNT(*) > 5)
+
+-- 2. Frequent customers
+SELECT *
+FROM customers
+WHERE customer_id IN            -- Select all customers with more than 10 movie rentals
+	(SELECT customer_id
+	FROM renting
+	GROUP BY customer_id
+	HAVING count(*) > 10);
+
+-- 3. Movies with rating above average
+SELECT title -- Report the movie titles of all movies with average rating higher than the total average
+FROM movies
+WHERE movie_id IN 
+	(SELECT movie_id
+	 FROM renting
+     GROUP BY movie_id
+     HAVING AVG(rating) > 
+		(SELECT AVG(rating)
+		 FROM renting));
