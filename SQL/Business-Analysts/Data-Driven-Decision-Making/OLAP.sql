82fe95f3f30e0164f92dd51036647521f3fc45b4
James_McNeill <jmcneill06@outlook.com>
2022-03-29 17:08:20 +0100

    Update OLAP.sql

--- SQL/Business-Analysts/Data-Driven-Decision-Making/OLAP.sql
+++ SQL/Business-Analysts/Data-Driven-Decision-Making/OLAP.sql
@@ -57,4 +57,60 @@ GROUP BY ROLLUP (c.country, m.genre)
 ORDER BY c.country, m.genre;
 
 -- C. GROUPING SETS
--- 1. 
+-- 1. Exploring nationality and gender of actors. Similar to the CUBE.
+SELECT 
+	nationality, -- Select nationality of the actors
+    gender, -- Select gender of the actors
+    count(*) -- Count the number of actors
+FROM actors
+GROUP BY GROUPING SETS ((nationality), (gender), ()); -- Use the correct GROUPING SETS operation. (): corresponds to overall total. Note this will be the first row
+
+-- 2. Exploring rating by country and gender
+SELECT 
+	c.country, 
+    c.gender,
+	AVG(r.rating)
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+-- Report all info from a Pivot table for country and gender
+GROUP BY GROUPING SETS ((country, gender), (country), (gender), ());
+
+-- D. All together
+-- 1. Customer preference for genres
+SELECT genre,
+	   AVG(rating) AS avg_rating,
+	   COUNT(rating) AS n_rating,
+       COUNT(*) AS n_rentals,     
+	   COUNT(DISTINCT m.movie_id) AS n_movies 
+FROM renting AS r
+LEFT JOIN movies AS m
+ON m.movie_id = r.movie_id
+WHERE r.movie_id IN ( 
+	SELECT movie_id
+	FROM renting
+	GROUP BY movie_id
+	HAVING COUNT(rating) >= 3 )
+AND r.date_renting >= '2018-01-01'
+GROUP BY genre
+ORDER BY avg(rating) DESC; -- Order the table by decreasing average rating
+
+-- 2. Customer preference for actors
+SELECT a.nationality,
+       a.gender,
+	   AVG(r.rating) AS avg_rating,
+	   COUNT(r.rating) AS n_rating,
+	   COUNT(*) AS n_rentals,
+	   COUNT(DISTINCT a.actor_id) AS n_actors
+FROM renting AS r
+LEFT JOIN actsin AS ai
+ON ai.movie_id = r.movie_id
+LEFT JOIN actors AS a
+ON ai.actor_id = a.actor_id
+WHERE r.movie_id IN ( 
+	SELECT movie_id
+	FROM renting
+	GROUP BY movie_id
+	HAVING COUNT(rating) >= 4)
+AND r.date_renting >= '2018-04-01'
+GROUP BY CUBE (a.nationality, a.gender); -- Provide results for all aggregation levels represented in a pivot table
