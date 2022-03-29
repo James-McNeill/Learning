638aae005e9d00992ce540460233a08fd04e3993
James_McNeill <jmcneill06@outlook.com>
2022-03-29 09:50:40 +0100

    Update intro.sql

--- SQL/Business-Analysts/Data-Driven-Decision-Making/intro.sql
+++ SQL/Business-Analysts/Data-Driven-Decision-Making/intro.sql
@@ -15,3 +15,22 @@ SELECT *
 FROM renting
 WHERE date_renting BETWEEN '2018-04-01' AND '2018-08-31'
 ORDER BY date_renting DESC; -- Order by recency in decreasing order
+
+-- 2. Selecting movies
+SELECT *
+FROM movies
+WHERE genre != 'Drama' ; -- All genres except drama
+
+SELECT *
+FROM movies
+WHERE title IN ('Showtime', 'Love Actually', 'The Fighter'); -- Select all movies with the given titles
+
+SELECT *
+FROM movies
+ORDER BY renting_price ; -- Order the movies by increasing renting price
+
+-- 3. Select from renting
+SELECT *
+FROM renting
+WHERE date_renting BETWEEN '2018-01-01' AND '2018-12-31' -- Renting in 2018
+AND rating IS NOT NULL; -- Rating exists
