11260940b8e885f92e5f2fe058e404f9c550c24f
James_McNeill <jmcneill06@outlook.com>
2022-03-29 12:13:58 +0100

    Update simple-sql.sql

--- SQL/Business-Analysts/Data-Driven-Decision-Making/simple-sql.sql
+++ SQL/Business-Analysts/Data-Driven-Decision-Making/simple-sql.sql
@@ -55,3 +55,26 @@ ON m.movie_id = ai.movie_id
 LEFT JOIN actors AS a
 ON a.actor_id = ai.actor_id;
 
+-- C. Money spent per customer with sub-queries
+-- 1. Income from movies
+SELECT rm.title -- Report the income from movie rentals for each movie 
+       ,sum(rm.renting_price) AS income_movie
+FROM
+       (SELECT m.title,  
+               m.renting_price
+       FROM renting AS r
+       LEFT JOIN movies AS m
+       ON r.movie_id=m.movie_id) AS rm
+GROUP BY rm.title
+ORDER BY income_movie DESC; -- Order the result by decreasing income
+
+-- 2. Age of actors from the USA
+SELECT a.gender, -- Report for male and female actors from the USA 
+       min(year_of_birth), -- The year of birth of the oldest actor
+       max(year_of_birth) -- The year of birth of the youngest actor
+FROM
+   (SELECT * -- Use a subsequen SELECT to get all information about actors from the USA
+   FROM actors
+   WHERE nationality = 'USA') as a -- Give the table the name a
+GROUP BY a.gender;
+
