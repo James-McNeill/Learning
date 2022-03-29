0c7e968919d3728a6e02294d2da820619ef8da79
James_McNeill <jmcneill06@outlook.com>
2022-03-29 16:17:33 +0100

    Create OLAP.sql

---
+++ SQL/Business-Analysts/Data-Driven-Decision-Making/OLAP.sql
@@ -0,0 +1,4 @@
+-- Data Driven Decision Making with OLAP SQL queries
+-- The OLAP extensions in SQL are introduced and applied to aggregated data on multiple levels. These extensions are the CUBE, ROLLUP and GROUPING SETS operators.
+
+-- A. OLAP: CUBE operator
