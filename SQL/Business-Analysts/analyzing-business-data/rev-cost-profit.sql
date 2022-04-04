1cb6cac179dcab0cdf4f3abb0e0a1207fd9dd1a1
James_McNeill <jmcneill06@outlook.com>
2022-04-04 14:03:25 +0100

    Create rev-cost-profit.sql

---
+++ SQL/Business-Analysts/analyzing-business-data/rev-cost-profit.sql
@@ -0,0 +1,6 @@
+-- Revenue Cost Profit
+-- Profit is one of the first things people use to assess a company's success. 
+-- In this chapter, you'll learn how to calculate revenue and cost, and then combine the two calculations using Common Table Expressions to calculate profit.
+
+-- A. Revenue
+-- 1.
