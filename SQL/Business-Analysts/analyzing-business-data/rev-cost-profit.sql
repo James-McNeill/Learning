f9468a42edf349acaff0695316782859be5df427
James_McNeill <jmcneill06@outlook.com>
2022-04-04 16:04:24 +0100

    Update rev-cost-profit.sql

--- SQL/Business-Analysts/analyzing-business-data/rev-cost-profit.sql
+++ SQL/Business-Analysts/analyzing-business-data/rev-cost-profit.sql
@@ -21,3 +21,20 @@ SELECT DATE_TRUNC('week', order_date) :: DATE AS delivr_week,
 WHERE order_date BETWEEN '2018-06-01' AND '2018-06-30'
 GROUP BY delivr_week
 ORDER BY delivr_week ASC;
+
+-- B. Using CTE's
+-- The power of the temporary table allows the user to query the data better
+-- Declare a CTE named monthly_cost
+WITH monthly_cost AS (
+  SELECT
+    DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
+    SUM(meal_cost * stocked_quantity) AS cost
+  FROM meals
+  JOIN stock ON meals.meal_id = stock.meal_id
+  GROUP BY delivr_month)
+
+SELECT
+  -- Calculate the average monthly cost before September
+  AVG(cost)
+FROM monthly_cost
+WHERE delivr_month < '2018-09-01';
