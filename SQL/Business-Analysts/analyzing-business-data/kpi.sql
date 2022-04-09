bb26e9443b760e4513542a3372dcd5d21ea9dac8
James_McNeill <jmcneill06@outlook.com>
2022-04-09 17:16:50 +0100

    Update kpi.sql

--- SQL/Business-Analysts/analyzing-business-data/kpi.sql
+++ SQL/Business-Analysts/analyzing-business-data/kpi.sql
@@ -99,3 +99,25 @@ SELECT
   2) AS growth
 FROM orders_with_lag
 ORDER BY delivr_month ASC;
+
+-- RETENTION RATE
+WITH user_monthly_activity AS (
+  SELECT DISTINCT
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    user_id
+  FROM orders)
+
+SELECT
+  -- Calculate the MoM retention rates
+  previous.delivr_month,
+  ROUND(
+    COUNT(DISTINCT current.user_id) :: NUMERIC /
+    GREATEST(COUNT(DISTINCT previous.user_id), 1),
+  2) AS retention_rate
+FROM user_monthly_activity AS previous
+LEFT JOIN user_monthly_activity AS current
+-- Fill in the user and month join conditions
+ON previous.user_id = current.user_id
+AND previous.delivr_month = (current.delivr_month - INTERVAL '1 MONTH')
+GROUP BY previous.delivr_month
+ORDER BY previous.delivr_month ASC;
