0394ac34af7d5818819347d3168989341260df64
James_McNeill <jmcneill06@outlook.com>
2022-04-15 12:33:27 +0100

    Update percentiles.sql

--- SQL/Business-Analysts/analyzing-business-data/percentiles.sql
+++ SQL/Business-Analysts/analyzing-business-data/percentiles.sql
@@ -1,5 +1,6 @@
 -- Working with percentiles
 
+-- Extracting the range of values at key distribution points; 25th, 50th and 75th percentiles
 WITH user_revenues AS (
   -- Select the user IDs and their revenues
   SELECT
@@ -23,3 +24,35 @@ SELECT
   -- Calculate the average
   ROUND(AVG(revenue) :: NUMERIC, 2) AS avg_revenue
 FROM user_revenues;
+
+-- Interquartile range
+WITH user_revenues AS (
+  SELECT
+    -- Select user_id and calculate revenue by user 
+    user_id,
+    SUM(m.meal_price * o.order_quantity) AS revenue
+  FROM meals AS m
+  JOIN orders AS o ON m.meal_id = o.meal_id
+  GROUP BY user_id),
+
+  quartiles AS (
+  SELECT
+    -- Calculate the first and third revenue quartiles
+    ROUND(
+      PERCENTILE_CONT(0.25) WITHIN GROUP
+      (ORDER BY revenue ASC) :: NUMERIC,
+    2) AS revenue_p25,
+    ROUND(
+      PERCENTILE_CONT(0.75) WITHIN GROUP
+      (ORDER BY revenue ASC) :: NUMERIC,
+    2) AS revenue_p75
+  FROM user_revenues)
+
+SELECT
+  -- Count the number of users in the IQR
+  COUNT(DISTINCT user_id) AS users
+FROM user_revenues
+CROSS JOIN quartiles
+-- Only keep users with revenues in the IQR range
+WHERE revenue :: NUMERIC >= revenue_p25
+  AND revenue :: NUMERIC <= revenue_p75;
