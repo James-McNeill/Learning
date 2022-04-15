fef1eb985a8580ed291085c3a104e2fd6a28a3ed
James_McNeill <jmcneill06@outlook.com>
2022-04-15 12:46:25 +0100

    Update useful-functions.sql

--- SQL/Business-Analysts/analyzing-business-data/useful-functions.sql
+++ SQL/Business-Analysts/analyzing-business-data/useful-functions.sql
@@ -29,3 +29,28 @@ FROM user_count_orders
 ORDER BY count_orders_rank ASC
 -- Limit the user IDs selected to 3
 LIMIT 3;
+
+-- Pivoting
+-- Some functions do not exist within the default environment. Therefore the extensions have to be imported. Similar to libraries for Python
+-- Import tablefunc
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+-- The two $ signs are included at the beginning and end to ensure that the function processes the commands as string
+SELECT * FROM CROSSTAB($$
+  SELECT
+    user_id,
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    SUM(meal_price * order_quantity) :: FLOAT AS revenue
+  FROM meals
+  JOIN orders ON meals.meal_id = orders.meal_id
+ WHERE user_id IN (0, 1, 2, 3, 4)
+   AND order_date < '2018-09-01'
+ GROUP BY user_id, delivr_month
+ ORDER BY user_id, delivr_month;
+$$)
+-- Select user ID and the months from June to August 2018
+AS ct (user_id INT,
+       "2018-06-01" FLOAT,
+       "2018-07-01" FLOAT,
+       "2018-08-01" FLOAT)
+ORDER BY user_id ASC;
