e59937739f45f998a1b82c0dc7bcf00587644637
James_McNeill <jmcneill06@outlook.com>
2022-04-15 13:05:48 +0100

    Create exploring.sql

---
+++ SQL/Business-Analysts/reporting/exploring.sql
@@ -0,0 +1,12 @@
+-- Exploring the case study
+
+-- Build base report
+-- Query the sport and distinct number of athletes
+SELECT 
+	sport, 
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+GROUP BY sport
+-- Only include the 3 sports with the most athletes
+ORDER BY athletes DESC
+LIMIT 3;
