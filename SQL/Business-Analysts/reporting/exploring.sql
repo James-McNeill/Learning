8b5c4ceeb61d2c9ea35063ca254136095e2319b0
James_McNeill <jmcneill06@outlook.com>
2022-04-15 13:07:31 +0100

    Update exploring.sql

--- SQL/Business-Analysts/reporting/exploring.sql
+++ SQL/Business-Analysts/reporting/exploring.sql
@@ -10,3 +10,11 @@ GROUP BY sport
 -- Only include the 3 sports with the most athletes
 ORDER BY athletes DESC
 LIMIT 3;
+
+-- Query sport, events, and athletes from summer_games
+SELECT 
+	sport, 
+    COUNT(DISTINCT event) AS events, 
+    COUNT(DISTINCT athlete_id) AS athletes
+FROM summer_games
+GROUP BY sport;
