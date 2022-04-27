f278959e938eec6b14078507f8fc29d0b6b7ca82
James_McNeill <jmcneill06@outlook.com>
2022-04-27 16:24:23 +0100

    Create categorical-data.r

---
+++ R/DataScientist/ExploratoryDataAnalysis/categorical-data.r
@@ -0,0 +1,14 @@
+# Working with categorical data in R
+
+# A) Explore
+# Print the comics data
+comics
+
+# Check levels of align
+levels(comics$align)
+
+# Check the levels of gender
+levels(comics$gender)
+
+# Create a 2-way contingency table
+table(comics$align, comics$gender)
