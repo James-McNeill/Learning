c3fcc456cb81b86569014ef26717e32eabd067cb
James_McNeill <jmcneill06@outlook.com>
2022-04-27 16:53:57 +0100

    Update categorical-data.r

--- R/DataScientist/ExploratoryDataAnalysis/categorical-data.r
+++ R/DataScientist/ExploratoryDataAnalysis/categorical-data.r
@@ -41,3 +41,40 @@ ggplot(comics, aes(x = gender, fill = align)) +
   geom_bar(position = "dodge") +
   theme(axis.text.x = element_text(angle = 90))
 
+# B) Counts Vs Proportions
+tab <- table(comics$align, comics$gender)
+options(scipen = 999, digits = 3) # Print fewer digits
+prop.table(tab)     # Joint proportions
+prop.table(tab, 2)  # Conditional on columns
+
+# Plot of gender by align
+ggplot(comics, aes(x = align, fill = gender)) +
+  geom_bar()
+  
+# Plot proportion of gender, conditional on align
+ggplot(comics, aes(x = align, fill = gender)) + 
+  geom_bar(position = "fill") +
+  ylab("proportion")
+
+# Marginal bar chart
+# Change the order of the levels in align
+comics$align <- factor(comics$align, 
+                       levels = c("Bad", "Neutral", "Good"))
+
+# Create plot of align
+ggplot(comics, aes(x = align)) + 
+  geom_bar()
+
+# Plot of alignment broken down by gender
+ggplot(comics, aes(x = align)) + 
+  geom_bar() +
+  facet_wrap(~ gender)
+
+# Put levels of flavor in descending order
+lev <- c("apple", "key lime", "boston creme", "blueberry", "cherry", "pumpkin", "strawberry")
+pies$flavor <- factor(pies$flavor, levels = lev)
+
+# Create bar chart of flavor
+ggplot(pies, aes(x = flavor)) + 
+  geom_bar(fill = "chartreuse") + 
+  theme(axis.text.x = element_text(angle = 90))
