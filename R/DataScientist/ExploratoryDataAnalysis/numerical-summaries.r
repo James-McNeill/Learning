85d5f988bfe13e9506009f95c79989a9f1ab8f41
James_McNeill <jmcneill06@outlook.com>
2022-04-28 14:45:39 +0100

    Update numerical-summaries.r

--- R/DataScientist/ExploratoryDataAnalysis/numerical-summaries.r
+++ R/DataScientist/ExploratoryDataAnalysis/numerical-summaries.r
@@ -1,3 +1,41 @@
 # Numerical summaries
 # Useful statistics for describing distributions of data
 
+# Measures of center
+# Create dataset of 2007 data
+gap2007 <- filter(gapminder, gapminder$year == 2007)
+
+# Compute groupwise mean and median lifeExp
+gap2007 %>%
+  group_by(continent) %>%
+  summarize(mean(lifeExp),
+            median(lifeExp))
+
+# Generate box plots of lifeExp for each continent
+gap2007 %>%
+  ggplot(aes(x = continent, y = lifeExp)) +
+  geom_boxplot()
+
+# Measures of variability
+# Compute groupwise measures of spread
+gap2007 %>%
+  group_by(continent) %>%
+  summarize(sd(lifeExp),
+            IQR(lifeExp),
+            n())
+
+# Generate overlaid density plots
+gap2007 %>%
+  ggplot(aes(x = lifeExp, fill = continent)) +
+  geom_density(alpha = 0.3)
+
+# Compute stats for lifeExp in Americas
+gap2007 %>%
+  filter(continent == "Americas") %>%
+  summarize(mean(lifeExp),
+            sd(lifeExp))
+
+# Compute stats for population. These stats for central tendency and spread are robust to the impact of outliers
+gap2007 %>%
+  summarize(median(pop),
+            IQR(pop))
