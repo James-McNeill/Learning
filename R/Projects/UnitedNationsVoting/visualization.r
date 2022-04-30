a713229dba353f07967f31831214850cfe904df5
James_McNeill <jmcneill06@outlook.com>
2022-04-30 16:32:39 +0100

    Update visualization.r

--- R/Projects/UnitedNationsVoting/visualization.r
+++ R/Projects/UnitedNationsVoting/visualization.r
@@ -1,2 +1,19 @@
 # Data visualizations with ggplot2
 
+# Define by_year
+by_year <- votes_processed %>%
+  group_by(year) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Create line plot
+ggplot(by_year, aes(year, percent_yes)) +
+  geom_line()
+
+# Change to scatter plot and add smoothing curve
+ggplot(by_year, aes(year, percent_yes)) +
+  geom_point() +
+  geom_smooth()
