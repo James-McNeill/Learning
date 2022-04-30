98e0cfc5139e6b1d1c24f9d3c01426422a91a080
James_McNeill <jmcneill06@outlook.com>
2022-04-30 23:37:03 +0100

    Update joining-tidying.r

--- R/Projects/UnitedNationsVoting/joining-tidying.r
+++ R/Projects/UnitedNationsVoting/joining-tidying.r
@@ -25,3 +25,84 @@ US_co_by_year <- votes_joined %>%
 # Graph the % of "yes" votes over time
 ggplot(US_co_by_year, aes(year, percent_yes)) +
   geom_line()
+
+# Load the tidyr package
+library(tidyr)
+
+# Gather the six me/nu/di/hr/co/ec columns
+votes_joined %>%
+    gather(topic, has_topic, me:ec) # is used to combine multiple variables into one
+
+# Perform gather again, then filter
+votes_gathered <- votes_joined %>%
+    gather(topic, has_topic, me:ec) %>%
+    filter(has_topic == 1)
+
+# Replace the two-letter codes in topic: votes_tidied
+votes_tidied <- votes_gathered %>%
+  mutate(topic = recode(topic,
+                        me = "Palestinian conflict",
+                        nu = "Nuclear weapons and nuclear material",
+                        di = "Arms control and disarmament",
+                        hr = "Human rights",
+                        co = "Colonialism",
+                        ec = "Economic development"))
+
+# Print votes_tidied
+votes_tidied
+
+# Summarize the percentage "yes" per country-year-topic
+by_country_year_topic <- votes_tidied %>%
+    group_by(country, year, topic) %>%
+    summarize(total = n(), percent_yes = mean(vote == 1)) %>%
+    ungroup() # shows the unique rows for each summary variable
+
+# Print by_country_year_topic
+by_country_year_topic
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Filter by_country_year_topic for just the US
+US_by_country_year_topic <- by_country_year_topic %>% filter(country == "United States")
+
+# Plot % yes over time for the US, faceting by topic
+ggplot(US_by_country_year_topic, aes(year, percent_yes)) +
+    geom_line() +
+    facet_wrap(~ topic)
+
+# Building tidy models using the country and topic to nest the data
+# Load purrr, tidyr, and broom
+library(purrr)
+library(tidyr)
+library(broom)
+
+# Print by_country_year_topic
+by_country_year_topic
+
+# Fit model on the by_country_year_topic dataset
+country_topic_coefficients <- by_country_year_topic %>%
+    nest(-country, -topic) %>%
+    mutate(model = map(data, ~ lm(percent_yes ~ year, data = .)),
+            tidied = map(model, tidy)) %>%
+    unnest(tidied)
+
+# Print country_topic_coefficients
+country_topic_coefficients
+
+# Interpreting tidy models
+# Create country_topic_filtered
+country_topic_filtered <- country_topic_coefficients %>%
+    filter(term == "year") %>%
+    mutate(p.adjusted = p.adjust(p.value)) %>%
+    filter(p.adjusted < .05)
+
+# Checking models visually
+# Create vanuatu_by_country_year_topic
+vanuatu_by_country_year_topic <- by_country_year_topic %>%
+  filter(country == "Vanuatu")
+
+# Plot of percentage "yes" over time, faceted by topic
+ggplot(vanuatu_by_country_year_topic, aes(year, percent_yes)) +
+  geom_line() +
+  facet_wrap(~ topic)
