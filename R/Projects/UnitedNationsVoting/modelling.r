8e3beeddb8ebed913622a4fe22aaadcc68b8fa31
James_McNeill <jmcneill06@outlook.com>
2022-04-30 16:50:56 +0100

    Update modelling.r

--- R/Projects/UnitedNationsVoting/modelling.r
+++ R/Projects/UnitedNationsVoting/modelling.r
@@ -1,2 +1,38 @@
 # Modelling the dataset
 
+# Percentage of yes votes from the US by year: US_by_year
+US_by_year <- by_year_country %>%
+  filter(country == "United States")
+
+# Print the US_by_year data
+US_by_year
+
+# Perform a linear regression of percent_yes by year: US_fit
+US_fit <- lm(percent_yes ~ year, data = US_by_year)
+
+# Perform summary() on the US_fit object
+summary(US_fit)
+
+# Load the broom package. Used to produce tidy data frame objects from the model data
+library(broom)
+
+# Call the tidy() function on the US_fit object
+tidy(US_fit)
+
+# Linear regression of percent_yes by year for US
+US_by_year <- by_year_country %>%
+  filter(country == "United States")
+US_fit <- lm(percent_yes ~ year, US_by_year)
+
+# Fit model for the United Kingdom
+UK_by_year <- by_year_country %>%
+  filter(country == "United Kingdom")
+UK_fit <- lm(percent_yes ~ year, UK_by_year)
+
+# Create US_tidied and UK_tidied
+US_tidied <- tidy(US_fit)
+UK_tidied <- tidy(UK_fit)
+
+# Combine the two tidied models
+dplyr::bind_rows(US_tidied, UK_tidied)
+
