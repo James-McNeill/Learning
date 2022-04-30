e838179180c7f11b1f3a1ad41e7a823cb7cd0454
James_McNeill <jmcneill06@outlook.com>
2022-04-30 17:23:34 +0100

    Update joining-tidying.r

--- R/Projects/UnitedNationsVoting/joining-tidying.r
+++ R/Projects/UnitedNationsVoting/joining-tidying.r
@@ -1,2 +1,27 @@
 # Joining and tidying datasets
 
+# Load dplyr package
+library(dplyr)
+
+# Print the votes_processed dataset
+votes_processed
+
+# Print the descriptions dataset
+descriptions
+
+# Join them together based on the "rcid" and "session" columns
+votes_joined <- votes_processed %>%
+    inner_join(descriptions, by = c("rcid","session"))
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Filter, then summarize by year: US_co_by_year
+US_co_by_year <- votes_joined %>%
+  filter(country == "United States", co == 1) %>%
+  group_by(year) %>%
+  summarize(percent_yes = mean(vote == 1))
+
+# Graph the % of "yes" votes over time
+ggplot(US_co_by_year, aes(year, percent_yes)) +
+  geom_line()
