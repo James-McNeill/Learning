bd1011255afdaa40a720b5b2cf5143f32ecfbbce
James_McNeill <jmcneill06@outlook.com>
2022-04-30 12:53:18 +0100

    Update case-study.r

--- R/DataScientist/ExploratoryDataAnalysis/case-study.r
+++ R/DataScientist/ExploratoryDataAnalysis/case-study.r
@@ -16,3 +16,29 @@ email %>%
   mutate(log_num_char = log(num_char)) %>%
   ggplot(aes(x = spam, y = log_num_char)) +
   geom_boxplot()
+
+# Compute center and spread for exclaim_mess by spam
+email %>%
+    group_by(spam) %>%
+    summarize(mean(exclaim_mess), sd(exclaim_mess))
+
+# Create plot for spam and exclaim_mess
+email %>%
+  mutate(log_exclaim_mess = log(exclaim_mess)) %>%
+  ggplot(aes(x = log_exclaim_mess)) +
+  geom_histogram() +
+  facet_wrap(~spam)
+
+# Create plot of proportion of spam by image
+email %>%
+  mutate(has_image = image > 0) %>%
+  ggplot(aes(x = has_image, fill = spam)) +
+  geom_bar(position = "fill")
+
+# Reorder levels
+email$number_reordered <- factor(email$number, levels = c("none", "small", "big"))
+
+# Construct plot of number_reordered
+ggplot(email, aes(x = number_reordered)) +
+  geom_bar() +
+  facet_wrap(~spam)
