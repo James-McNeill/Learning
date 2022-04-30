394d19f0bd36df03ae5f1a4fe1f0cf44e7b9de0a
James_McNeill <jmcneill06@outlook.com>
2022-04-30 16:37:09 +0100

    Update visualization.r

--- R/Projects/UnitedNationsVoting/visualization.r
+++ R/Projects/UnitedNationsVoting/visualization.r
@@ -17,3 +17,37 @@ ggplot(by_year, aes(year, percent_yes)) +
 ggplot(by_year, aes(year, percent_yes)) +
   geom_point() +
   geom_smooth()
+
+# Group by year and country: by_year_country
+by_year_country <- votes_processed %>%
+  group_by(year, country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Start with by_year_country dataset
+by_year_country <- votes_processed %>%
+  group_by(year, country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Print by_year_country
+by_year_country
+
+# Create a filtered version: UK_by_year
+UK_by_year <- by_year_country %>% filter(country %in% "United Kingdom")
+
+# Line plot of percent_yes over time for UK only
+ggplot(UK_by_year, aes(year, percent_yes)) +
+  geom_line()
+
+# Vector of four countries to examine
+countries <- c("United States", "United Kingdom",
+               "France", "India")
+
+# Filter by_year_country: filtered_4_countries
+filtered_4_countries <- by_year_country %>% filter(country %in% countries)
+
+# Line plot of % yes in four countries
+ggplot(filtered_4_countries, aes(year, percent_yes, color = country)) +
+  geom_line()
+
