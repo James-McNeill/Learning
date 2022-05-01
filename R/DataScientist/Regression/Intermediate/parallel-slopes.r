039520060ed9bb1288449a60cdca51e7250ab9a1
James_McNeill <jmcneill06@outlook.com>
2022-05-01 18:54:50 +0100

    Create parallel-slopes.r

---
+++ R/DataScientist/Regression/Intermediate/parallel-slopes.r
@@ -0,0 +1,4 @@
+# Parallel slopes
+
+# Extend your linear regression skills to "parallel slopes" regression, with one numeric and one categorical 
+# explanatory variable. This is the first step towards conquering multiple linear regression.
