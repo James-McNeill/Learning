86cfe37e33317292b54e3c609b8e342219144931
James_McNeill <jmcneill06@outlook.com>
2022-05-01 13:34:33 +0100

    Create model-fit.r

---
+++ R/DataScientist/Regression/model-fit.r
@@ -0,0 +1,4 @@
+# Assessing model fit
+
+# You’ll learn how to quantify how well a linear regression model fits, diagnose model problems using visualizations, 
+# and understand the leverage and influence of each observation used to create the model.
