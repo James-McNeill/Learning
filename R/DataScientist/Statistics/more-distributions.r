373d3e50c58adc744d19544e27c058c56d38ec03
James_McNeill <jmcneill06@outlook.com>
2022-05-01 10:06:07 +0100

    Update more-distributions.r

--- R/DataScientist/Statistics/more-distributions.r
+++ R/DataScientist/Statistics/more-distributions.r
@@ -1,2 +1,28 @@
 # More distributions and central limit theorem
 
+# Normal distribution
+# Probability of deal < 7500
+pnorm(7500, mean = 5000, sd = 2000)
+
+# Probability of deal > 1000
+pnorm(1000, mean = 5000, sd = 2000, lower.tail = FALSE)
+
+# Probability of deal between 3000 and 7000
+pnorm(7000, mean = 5000, sd = 2000) - pnorm(3000, mean = 5000, sd = 2000)
+
+# Calculate amount that 75% of deals will be more than
+qnorm(0.25, mean = 5000, sd = 2000)
+
+# Calculate new average amount
+new_mean <- 5000 * 1.2
+
+# Calculate new standard deviation
+new_sd <- 2000 * 1.3
+
+# Simulate 36 sales
+new_sales <- new_sales %>% 
+  mutate(amount = rnorm(36, mean = new_mean, sd = new_sd))
+
+# Create histogram with 10 bins
+ggplot(new_sales, aes(amount)) +
+  geom_histogram(bins = 10)
