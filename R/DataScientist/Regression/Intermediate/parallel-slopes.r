8f4988cf32019aa2d84c5f94079e30843888e89b
James_McNeill <jmcneill06@outlook.com>
2022-05-01 19:04:26 +0100

    Update parallel-slopes.r

--- R/DataScientist/Regression/Intermediate/parallel-slopes.r
+++ R/DataScientist/Regression/Intermediate/parallel-slopes.r
@@ -2,3 +2,44 @@
 
 # Extend your linear regression skills to "parallel slopes" regression, with one numeric and one categorical 
 # explanatory variable. This is the first step towards conquering multiple linear regression.
+
+# Fit a linear regr'n of price_twd_msq vs. n_convenience
+mdl_price_vs_conv <- lm(price_twd_msq ~ n_convenience, data = taiwan_real_estate)
+
+# See the result
+mdl_price_vs_conv
+
+# Fit a linear regr'n of price_twd_msq vs. house_age_years, no intercept
+mdl_price_vs_age <- lm(price_twd_msq ~ house_age_years + 0, data = taiwan_real_estate)
+
+# See the result
+mdl_price_vs_age
+
+# Fit a linear regr'n of price_twd_msq vs. n_convenience plus house_age_years, no intercept
+mdl_price_vs_both <- lm(price_twd_msq ~ n_convenience + house_age_years + 0, data = taiwan_real_estate)
+
+# See the result
+mdl_price_vs_both
+
+# Visualizing the numeric variable
+# Using taiwan_real_estate, plot price_twd_msq vs. n_convenience
+ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq)) +
+  # Add a point layer
+  geom_point() +
+  # Add a smooth trend line using linear regr'n, no ribbon
+  geom_smooth(method = "lm", se = FALSE)
+
+# Visualizing the categorical variable
+# Using taiwan_real_estate, plot price_twd_msq vs. house_age_years
+ggplot(taiwan_real_estate, aes(house_age_years, price_twd_msq)) +
+  # Add a box plot layer
+  geom_boxplot()
+
+# Visualizing parallel slopes
+# library(moderndive) provides the additional geom that can be used to view the parallel slopes created by the model for each category
+# Using taiwan_real_estate, plot price_twd_msq vs. n_convenience colored by house_age_years
+ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq, color = house_age_years)) +
+  # Add a point layer
+  geom_point() +
+  # Add parallel slopes, no ribbon
+  geom_parallel_slopes(se = FALSE)
