b4b2fcff944562e3eada459ed9f403ae580df796
James_McNeill <jmcneill06@outlook.com>
2022-05-01 09:06:06 +0100

    Update intro.r

--- R/DataScientist/Statistics/intro.r
+++ R/DataScientist/Statistics/intro.r
@@ -1,1 +1,26 @@
 # Intro to statistics
+
+# Mean and median
+food_consumption %>%
+  # Filter for Belgium and USA
+  filter(country %in% c("Belgium", "USA")) %>%
+  # Group by country
+  group_by(country) %>%
+  # Get mean_consumption and median_consumption
+  summarize(mean_consumption = mean(consumption),
+      median_consumption = median(consumption))
+
+# Mean vs median. Working with skewed data
+food_consumption %>%
+  # Filter for rice food category
+  filter(food_category == "rice") %>%
+  # Create histogram of co2_emission
+  ggplot(aes(co2_emission)) +
+    geom_histogram()
+
+food_consumption %>%
+  # Filter for rice food category
+  filter(food_category == "rice") %>% 
+  # Get mean_co2 and median_co2
+  summarize(mean_co2 = mean(co2_emission),
+            median_co2 = median(co2_emission))
