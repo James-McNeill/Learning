71fe92426366726893ec293934a81430655b5bd6
James_McNeill <jmcneill06@outlook.com>
2022-05-01 14:03:43 +0100

    Create logistic.r

---
+++ R/DataScientist/Regression/logistic.r
@@ -0,0 +1,4 @@
+# Logistic regression
+
+# Using real-world data, you’ll predict the likelihood of a customer closing their bank account as 
+# probabilities of success and odds ratios, and quantify model performance using confusion matrices.
