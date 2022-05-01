b26b58297ba4e1868eb621bebd40fd5d344cbffc
James_McNeill <jmcneill06@outlook.com>
2022-05-01 12:13:35 +0100

    Update linear.r

--- R/DataScientist/Regression/linear.r
+++ R/DataScientist/Regression/linear.r
@@ -1,2 +1,11 @@
 # Simple linear regression
 
+# View the data provided
+# Invoke a spreadsheet-style data viewer on a matrix-like R object
+View(taiwan_real_estate)
+
+# Visualising two variables
+# Add a linear trend line without a confidence ribbon
+ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq)) +
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm", se = FALSE)
