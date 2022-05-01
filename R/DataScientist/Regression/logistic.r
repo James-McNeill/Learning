d50023ebfd0a2e7d212025f7b979ae1787a2481f
James_McNeill <jmcneill06@outlook.com>
2022-05-01 14:13:10 +0100

    Update logistic.r

--- R/DataScientist/Regression/logistic.r
+++ R/DataScientist/Regression/logistic.r
@@ -2,3 +2,19 @@
 
 # Using real-world data, you’ll predict the likelihood of a customer closing their bank account as 
 # probabilities of success and odds ratios, and quantify model performance using confusion matrices.
+
+# Using churn, plot time_since_last_purchase
+ggplot(churn, aes(time_since_last_purchase)) +
+  # as a histogram with binwidth 0.25
+  geom_histogram(binwidth = 0.25) +
+  # faceted in a grid with has_churned on each row
+  facet_grid(vars(has_churned))
+
+# Using churn plot has_churned vs. time_since_first_purchase
+ggplot(churn, aes(time_since_first_purchase, has_churned)) +
+  # Make it a scatter plot
+  geom_point() +
+  # Add an lm trend line, no std error ribbon, colored red
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  # Add a glm trend line, no std error ribbon, binomial family
+  geom_smooth(method = "glm", se = FALSE, method.args = list(family = "binomial"))
