51f89f55b7c5f1e6b7993c1222413de86115b185
James_McNeill <jmcneill06@outlook.com>
2022-05-01 12:00:50 +0100

    Update correlation-experiments.r

--- R/DataScientist/Statistics/correlation-experiments.r
+++ R/DataScientist/Statistics/correlation-experiments.r
@@ -12,3 +12,22 @@ ggplot(world_happiness, aes(life_exp, happiness_score)) +
 
 # Correlation between life_exp and happiness_score
 cor(world_happiness$life_exp, world_happiness$happiness_score)
+
+# Transforming variables. Helps to resolve variables with skewed data
+# transformations can be: 1) log, 2) square root, and 3) reciprocal
+# Scatterplot of happiness_score vs. gdp_per_cap
+ggplot(world_happiness, aes(gdp_per_cap, happiness_score)) + geom_point()
+
+# Calculate correlation
+cor(world_happiness$gdp_per_cap, world_happiness$happiness_score)
+
+# Create log_gdp_per_cap column
+world_happiness <- world_happiness %>%
+  mutate(log_gdp_per_cap = log(gdp_per_cap))
+
+# Scatterplot of happiness_score vs. log_gdp_per_cap
+ggplot(world_happiness, aes(log_gdp_per_cap, happiness_score)) +
+  geom_point()
+
+# Calculate correlation
+cor(world_happiness$log_gdp_per_cap, world_happiness$happiness_score)
