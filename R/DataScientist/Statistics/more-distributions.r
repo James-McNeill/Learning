e1da7ae427b9aee83e16eb35f08137dc34878bfe
James_McNeill <jmcneill06@outlook.com>
2022-05-01 10:28:16 +0100

    Update more-distributions.r

--- R/DataScientist/Statistics/more-distributions.r
+++ R/DataScientist/Statistics/more-distributions.r
@@ -26,3 +26,57 @@ new_sales <- new_sales %>%
 # Create histogram with 10 bins
 ggplot(new_sales, aes(amount)) +
   geom_histogram(bins = 10)
+
+# Set seed to 104
+set.seed(104)
+
+# Sample 20 num_users from amir_deals and take mean
+sample(amir_deals$num_users, size = 20, replace = TRUE) %>%
+  mean()
+
+# Repeat the above 100 times. replicate can be used to repeat the process steps multiple times and return the summary statistic
+sample_means <- replicate(100, sample(amir_deals$num_users, size = 20, replace = TRUE) %>% mean())
+
+# Create data frame for plotting
+samples <- data.frame(mean = sample_means)
+
+# Histogram of sample means
+ggplot(samples, aes(mean)) +
+  geom_histogram(bins = 10)
+
+# The mean of means
+# Set seed to 321
+set.seed(321)
+
+# Take 30 samples of 20 values of num_users, take mean of each sample
+sample_means <- replicate(30, sample(all_deals$num_users, 20) %>% mean())
+
+# Calculate mean of sample_means
+mean(sample_means)
+
+# Calculate mean of num_users in amir_deals
+mean(amir_deals$num_users)
+
+# Poisson distribution
+# Probability of 5 responses. lambda = average within a defined time period
+dpois(5, lambda = 4)
+
+# Probability of 5 responses from coworker
+dpois(5, lambda = 5.5)
+
+# Probability of 2 or fewer responses
+ppois(2, lambda = 4)
+
+# Probability of > 10 responses
+ppois(10, lambda = 4, lower.tail = FALSE)
+
+# Exponential distribution. Continuous time of poisson distribution
+# Probability response takes < 1 hour
+pexp(1, rate = (1/2.5))
+
+# Probability response takes > 4 hours
+pexp(4, rate = (1 / 2.5), lower.tail = FALSE)
+
+# Probability response takes 3-4 hours
+pexp(4, rate = (1 / 2.5)) - pexp(3, rate = (1 / 2.5))
+
