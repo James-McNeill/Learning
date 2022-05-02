7db40bc8fd49864e707d74056c3f64d70f8c86e2
James_McNeill <jmcneill06@outlook.com>
2022-05-02 09:47:23 +0100

    Update parallel-slopes.r

--- R/DataScientist/Regression/Intermediate/parallel-slopes.r
+++ R/DataScientist/Regression/Intermediate/parallel-slopes.r
@@ -110,3 +110,20 @@ mdl_price_vs_age %>%
 mdl_price_vs_both %>% 
   glance() %>% 
   select(r.squared, adj.r.squared)
+
+# Comparing residual standard error
+mdl_price_vs_conv %>% 
+  # Get the model-level coefficients
+  glance() %>% 
+  # Pull out the RSE
+  pull(sigma)
+
+# Get the RSE for mdl_price_vs_age
+mdl_price_vs_age %>% 
+  glance() %>%
+  pull(sigma)
+
+# Get the RSE for mdl_price_vs_both
+mdl_price_vs_both %>% 
+  glance() %>%
+  pull(sigma)
