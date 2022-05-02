d86ee8ca11a91add241a4b6a5252fc2bcf625686
James_McNeill <jmcneill06@outlook.com>
2022-05-02 09:44:48 +0100

    Update parallel-slopes.r

--- R/DataScientist/Regression/Intermediate/parallel-slopes.r
+++ R/DataScientist/Regression/Intermediate/parallel-slopes.r
@@ -43,3 +43,70 @@ ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq, color = house_age_y
   geom_point() +
   # Add parallel slopes, no ribbon
   geom_parallel_slopes(se = FALSE)
+
+# Predicting with parallel slopes
+# Make a grid of explanatory data
+explanatory_data <- expand_grid(
+  # Set n_convenience to zero to ten
+  n_convenience = 0:10,
+  # Set house_age_years to the unique values of that variable. Take each value from the category
+  house_age_years = unique(taiwan_real_estate$house_age_years)
+)
+
+# Add predictions to the data frame
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_both, explanatory_data)
+  )
+
+# See the result
+prediction_data
+
+# Visualize
+taiwan_real_estate %>% 
+  ggplot(aes(n_convenience, price_twd_msq, color = house_age_years)) +
+  geom_point() +
+  geom_parallel_slopes(se = FALSE) +
+  # Add points using prediction_data, with size 5 and shape 15
+  geom_point(data = prediction_data, size = 5, shape = 15)
+
+# Manually calculate predictions
+# From previous step
+coeffs <- coefficients(mdl_price_vs_both)
+slope <- coeffs[1]
+intercept_0_15 <- coeffs[2]
+intercept_15_30 <- coeffs[3]
+intercept_30_45 <- coeffs[4]
+
+prediction_data <- explanatory_data %>% 
+  mutate(
+    # Consider the 3 cases to choose the intercept
+    intercept = case_when(
+      house_age_years == "0 to 15" ~ intercept_0_15,
+      house_age_years == "15 to 30" ~ intercept_15_30,
+      house_age_years == "30 to 45" ~ intercept_30_45
+    ),
+    
+    # Manually calculate the predictions
+    price_twd_msq = intercept + slope * n_convenience
+  )
+
+# See the results
+prediction_data
+
+# Comparing coefficients of determination
+mdl_price_vs_conv %>% 
+  # Get the model-level coefficients
+  glance() %>% 
+  # Select the coeffs of determination
+  select(r.squared, adj.r.squared)
+
+# Get the coeffs of determination for mdl_price_vs_age
+mdl_price_vs_age %>% 
+  glance() %>% 
+  select(r.squared, adj.r.squared)
+
+# Get the coeffs of determination for mdl_price_vs_both
+mdl_price_vs_both %>% 
+  glance() %>% 
+  select(r.squared, adj.r.squared)
