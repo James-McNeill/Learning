ce4b278da0586823fb2aa0952748cb8a89173a05
James_McNeill <jmcneill06@outlook.com>
2022-05-02 09:48:07 +0100

    Create interactions.r

---
+++ R/DataScientist/Regression/Intermediate/interactions.r
@@ -0,0 +1,5 @@
+# Interactions
+
+# Explore the effect of interactions between explanatory variables. Considering interactions allows 
+# for more realistic models that can have better predictive power. You'll also deal with Simpson's 
+# Paradox: a non-intuitive result that arises when you have multiple explanatory variables.
