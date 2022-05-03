d30269db0c058252f79965956eafc7ca1e2c5032
James_McNeill <jmcneill06@outlook.com>
2022-05-03 16:15:19 +0100

    Update multiple-logistic.r

--- R/DataScientist/Regression/Intermediate/multiple-logistic.r
+++ R/DataScientist/Regression/Intermediate/multiple-logistic.r
@@ -1,3 +1,64 @@
 # Multiple Logistic Regression
 
 # Extend your logistic regression skills to multiple explanatory variables. Understand the logistic distribution, which underpins this form of regression
+# Using churn, plot recency vs. length of relationship colored by churn status
+ggplot(churn, aes(time_since_first_purchase, time_since_last_purchase, color = has_churned)) +
+  # Make it a scatter plot, with transparency 0.5
+  geom_point(alpha = 0.5) +
+  # Use a 2-color gradient split at 0.5
+  scale_color_gradient2(midpoint = 0.5) +
+  # Use the black and white theme
+  theme_bw()
+
+# Fit a logistic regression of churn status vs. length of relationship, recency, and an interaction
+mdl_churn_vs_both_inter <- glm(
+    has_churned ~ time_since_first_purchase * time_since_last_purchase,
+    data = churn,
+    family = binomial
+)
+
+# See the result
+mdl_churn_vs_both_inter
+
+# Make a grid of explanatory data
+explanatory_data <- expand_grid(
+  # Set len. relationship to seq from -2 to 4 in steps of 0.1
+  time_since_first_purchase = seq(-2, 4, 0.1),
+  # Set recency to seq from -1 to 6 in steps of 0.1
+  time_since_last_purchase = seq(-1, 6, 0.1)
+)
+
+# See the result
+explanatory_data
+
+# Add a column of predictions using mdl_churn_vs_both_inter and explanatory_data with type response
+prediction_data <- explanatory_data %>%
+  mutate(
+    has_churned = predict(mdl_churn_vs_both_inter, explanatory_data, type = "response")
+  )
+
+# See the result
+prediction_data
+
+# Extend the plot
+ggplot(
+  churn, 
+  aes(time_since_first_purchase, time_since_last_purchase, color = has_churned)
+) +
+  geom_point(alpha = 0.5) +
+  scale_color_gradient2(midpoint = 0.5) +
+  theme_bw() +
+  # Add points from prediction_data with size 3 and shape 15
+  geom_point(data = prediction_data, size = 3, shape = 15)
+
+# Confusion matrix
+actual_response <- churn$has_churned
+predicted_response <- round(fitted(mdl_churn_vs_both_inter))
+outcomes <- table(predicted_response, actual_response)
+confusion <- conf_mat(outcomes)
+
+# "Automatically" plot the confusion matrix
+autoplot(confusion)
+
+# Get summary metrics
+summary(confusion, event = "second")
