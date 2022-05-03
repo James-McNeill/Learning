07a959ff4cf25be09afc50dbf3423b1aaa75c0b6
James_McNeill <jmcneill06@outlook.com>
2022-05-03 16:01:03 +0100

    Create multiple-logistic.r

---
+++ R/DataScientist/Regression/Intermediate/multiple-logistic.r
@@ -0,0 +1,3 @@
+# Multiple Logistic Regression
+
+# Extend your logistic regression skills to multiple explanatory variables. Understand the logistic distribution, which underpins this form of regression
