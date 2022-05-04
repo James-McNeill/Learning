775f646be3952d95ef65750516b511a4bfc7fc36
James_McNeill <jmcneill06@outlook.com>
2022-05-04 15:21:21 +0100

    Update knn.r

--- R/DataScientist/SupervisedLearning/Classification/knn.r
+++ R/DataScientist/SupervisedLearning/Classification/knn.r
@@ -2,3 +2,34 @@
 
 # As the kNN algorithm literally "learns by example" it is a case in point for starting to understand supervised machine learning. 
 # This chapter will introduce classification while working through the application of kNN to self-driving vehicle road sign recognition.
+
+# Load the 'class' package
+library(class)
+
+# Create a vector of labels
+sign_types <- signs$sign_type
+
+# Classify the next sign observed
+knn(train = signs[-1], test = next_sign, cl = sign_types) # next_sign holds the classification object
+
+# Exploring the traffic sign dataset
+# Examine the structure of the signs dataset
+str(signs)
+
+# Count the number of signs of each type
+table(signs$sign_type)
+
+# Check r10's average red level by sign type
+aggregate(r10 ~ sign_type, data = signs, mean)
+
+# Use kNN to identify the test road signs
+sign_types <- signs$sign_type
+signs_pred <- knn(train = signs[-1], test = test_signs[-1], cl = sign_types)
+
+# Create a confusion matrix of the predicted versus actual values
+signs_actual <- test_signs$sign_type
+table(signs_pred, signs_actual)
+
+# Compute the accuracy
+mean(signs_pred == signs_actual)
+
