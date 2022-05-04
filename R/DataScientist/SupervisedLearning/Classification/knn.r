a22255ac7ea8f840311afafce6a65be2dad17fbf
James_McNeill <jmcneill06@outlook.com>
2022-05-04 15:08:36 +0100

    Create knn.r

---
+++ R/DataScientist/SupervisedLearning/Classification/knn.r
@@ -0,0 +1,4 @@
+# Classification with Nearnest Neighbor (k-NN)
+
+# As the kNN algorithm literally "learns by example" it is a case in point for starting to understand supervised machine learning. 
+# This chapter will introduce classification while working through the application of kNN to self-driving vehicle road sign recognition.
