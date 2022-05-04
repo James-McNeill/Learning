878f9039015b1d6f2b1ce68187cb35dc31c49590
James_McNeill <jmcneill06@outlook.com>
2022-05-04 16:21:20 +0100

    Update naive-bayes.r

--- R/DataScientist/SupervisedLearning/Classification/naive-bayes.r
+++ R/DataScientist/SupervisedLearning/Classification/naive-bayes.r
@@ -1,3 +1,37 @@
 # Naive Bayes
 
 # Naive Bayes uses principles from the field of statistics to make predictions
+# Compute P(A) 
+p_A <- nrow(subset(where9am, location == "office")) / nrow(where9am)
+
+# Compute P(B)
+p_B <- nrow(subset(where9am, daytype == "weekday")) / nrow(where9am)
+
+# Compute the observed P(A and B)
+p_AB <- nrow(subset(where9am, daytype == "weekday" & location == "office")) / nrow(where9am)
+
+# Compute P(A | B) and print its value
+p_A_given_B <- p_AB / p_B
+p_A_given_B
+
+# A simple naive bayes location model
+# Load the naivebayes package
+library(naivebayes)
+
+# Build the location prediction model
+locmodel <- naive_bayes(location ~ daytype, data = where9am)
+
+# Predict Thursday's 9am location
+predict(locmodel, thursday9am)
+
+# Predict Saturdays's 9am location
+predict(locmodel, saturday9am)
+
+# Examine the location prediction model
+locmodel
+
+# Obtain the predicted probabilities for Thursday at 9am
+predict(locmodel, thursday9am, type = "prob")
+
+# Obtain the predicted probabilities for Saturday at 9am
+predict(locmodel, saturday9am, type = "prob")
