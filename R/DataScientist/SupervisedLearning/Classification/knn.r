c7df409c5ecc382953c094e146663bb50046ac44
James_McNeill <jmcneill06@outlook.com>
2022-05-04 15:43:58 +0100

    Update knn.r

--- R/DataScientist/SupervisedLearning/Classification/knn.r
+++ R/DataScientist/SupervisedLearning/Classification/knn.r
@@ -33,3 +33,36 @@ table(signs_pred, signs_actual)
 # Compute the accuracy
 mean(signs_pred == signs_actual)
 
+# Compute the accuracy of the baseline model (default k = 1)
+k_1 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types)
+mean(signs_actual == k_1)
+
+# Modify the above to set k = 7. This value showed the best accuracy score
+k_7 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k = 7)
+mean(signs_actual == k_7)
+
+# Set k = 15 and compare to the above
+k_15 <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k = 15)
+mean(signs_actual == k_15)
+
+# Seeing how the neighbors voted
+# Use the prob parameter to get the proportion of votes for the winning class
+sign_pred <- knn(train = signs[-1], test = signs_test[-1], cl = sign_types, k = 7, prob = TRUE)
+
+# Get the "prob" attribute from the predicted classes
+sign_prob <- attr(sign_pred, "prob")
+
+# Examine the first several predictions
+head(sign_pred)
+
+# Examine the proportion of votes for the winning class
+head(sign_prob)
+
+# Normalize data points
+# define a min-max normalize() function
+normalize <- function(x) {
+    return ((x - min(x)) / (max(x) - min(x)))
+  }
+
+# Normalize a variable. This output can be compared to the original version of the variable
+summary(normalize(signs$r1))
