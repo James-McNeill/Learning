66a194ce0eca8c7e032572731db9cce50029b8a5
James_McNeill <jmcneill06@outlook.com>
2022-05-05 12:42:14 +0100

    Update logistic-reg.r

--- R/DataScientist/SupervisedLearning/Classification/logistic-reg.r
+++ R/DataScientist/SupervisedLearning/Classification/logistic-reg.r
@@ -1,3 +1,42 @@
 # Logistic Regression
 
 # Logistic regression involves fitting a curve to numeric data to make predictions about binary events
+
+# Building a simple model
+# Examine the dataset to identify potential independent variables
+str(donors)
+
+# Explore the dependent variable
+table(donors$donated)
+
+# Build the donation model
+donation_model <- glm(donated ~ bad_address + interest_religion + interest_veterans, 
+                      data = donors, family = "binomial")
+
+# Summarize the model results
+summary(donation_model)
+
+# Estimate the donation probability
+donors$donation_prob <- predict(donation_model, donors, type = "response")
+
+# Find the donation probability of the average prospect
+mean(donors$donated)
+
+# Predict a donation if probability of donation is greater than average (0.0504). This value relates to the average proportion of donations
+donors$donation_pred <- ifelse(donors$donation_prob > 0.0504, 1, 0)
+
+# Calculate the model's accuracy
+mean(donors$donation_pred == donors$donated)
+
+# Model performance - ROC curve
+# Load the pROC package
+library(pROC)
+
+# Create a ROC curve
+ROC <- roc(donors$donated, donors$donation_prob)
+
+# Plot the ROC curve
+plot(ROC, col = "blue")
+
+# Calculate the area under the curve (AUC)
+auc(ROC)
