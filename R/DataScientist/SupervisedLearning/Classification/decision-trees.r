b98e9cd554a004ce4920755576ce3141a76a52f5
James_McNeill <jmcneill06@outlook.com>
2022-05-05 13:28:39 +0100

    Update decision-trees.r

--- R/DataScientist/SupervisedLearning/Classification/decision-trees.r
+++ R/DataScientist/SupervisedLearning/Classification/decision-trees.r
@@ -3,3 +3,29 @@
 # Classification trees use flowchart-like structures to make decisions. Because humans can readily understand these 
 # tree structures, classification trees are useful when transparency is needed, such as in loan approval. 
 # We'll use the Lending Club dataset to simulate this scenario
+
+# Simple model
+# Load the rpart package
+library(rpart)
+
+# Build a lending model predicting loan outcome versus loan amount and credit score
+loan_model <- rpart(outcome ~ loan_amount + credit_score, data = loans, method = "class", control = rpart.control(cp = 0))
+
+# Make a prediction for someone with good credit
+predict(loan_model, good_credit, type = "class")
+
+# Make a prediction for someone with bad credit
+predict(loan_model, bad_credit, type = "class")
+
+# Visualize the tree
+# Examine the loan_model object
+loan_model
+
+# Load the rpart.plot package
+library(rpart.plot)
+
+# Plot the loan_model with default settings
+rpart.plot(loan_model)
+
+# Plot the loan_model with customized settings
+rpart.plot(loan_model, type = 3, box.palette = c("red", "green"), fallen.leaves = TRUE)
