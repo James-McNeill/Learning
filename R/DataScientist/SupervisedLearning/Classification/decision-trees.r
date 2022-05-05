09f13d5fe8c75cf48569c2df8e57cdf65cbed92d
James_McNeill <jmcneill06@outlook.com>
2022-05-05 13:19:12 +0100

    Create decision-trees.r

---
+++ R/DataScientist/SupervisedLearning/Classification/decision-trees.r
@@ -0,0 +1,5 @@
+# Decision Trees
+
+# Classification trees use flowchart-like structures to make decisions. Because humans can readily understand these 
+# tree structures, classification trees are useful when transparency is needed, such as in loan approval. 
+# We'll use the Lending Club dataset to simulate this scenario
