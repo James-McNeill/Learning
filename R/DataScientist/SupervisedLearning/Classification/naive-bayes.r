1eb2cf706b9a9616ce0f17b1c12d6fa008814748
James_McNeill <jmcneill06@outlook.com>
2022-05-05 12:18:19 +0100

    Update naive-bayes.r

--- R/DataScientist/SupervisedLearning/Classification/naive-bayes.r
+++ R/DataScientist/SupervisedLearning/Classification/naive-bayes.r
@@ -33,5 +33,23 @@ locmodel
 # Obtain the predicted probabilities for Thursday at 9am
 predict(locmodel, thursday9am, type = "prob")
 
+# Build a NB model of location. Adding additional variables to the model
+locmodel <- naive_bayes(location ~ daytype + hourtype, data = locations)
+
+# Predict Brett's location on a weekday afternoon
+predict(locmodel, weekday_afternoon)
+
+# Predict Brett's location on a weekday evening
+predict(locmodel, weekday_evening)
 # Obtain the predicted probabilities for Saturday at 9am
 predict(locmodel, saturday9am, type = "prob")
+
+# Observe the predicted probabilities for a weekend afternoon
+predict(locmodel, weekend_afternoon, type = "prob")
+
+# Build a new model using the Laplace correction. The laplace parameter is used to assign a small prob value to each independent joint prob. This covers off the 
+# situation were a joint prob was never seen in the past but could happen in the future, so a small prob is required.
+locmodel2 <- naive_bayes(location ~ daytype + hourtype, data = locations, laplace = 1)
+
+# Observe the new predicted probabilities for a weekend afternoon
+predict(locmodel2, weekend_afternoon, type = "prob")
