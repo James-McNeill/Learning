0e89d06f62ae85001e694cc916ef3be4013088ba
James_McNeill <jmcneill06@outlook.com>
2022-05-06 15:02:15 +0100

    Create model-performance.r

---
+++ R/DataScientist/SupervisedLearning/Regression/model-performance.r
@@ -0,0 +1,15 @@
+# Model performance
+
+# Evaluating a model graphically
+
+# From previous step
+unemployment$predictions <- predict(unemployment_model)
+
+# Calculate residuals
+unemployment$residuals <-  unemployment$female_unemployment - unemployment$predictions
+
+# Fill in the blanks to plot predictions (on x-axis) versus the residuals
+ggplot(unemployment, aes(x = predictions, y = residuals)) + 
+  geom_pointrange(aes(ymin = 0, ymax = residuals)) + 
+  geom_hline(yintercept = 0, linetype = 3) + 
+  ggtitle("residuals vs. linear model prediction")
