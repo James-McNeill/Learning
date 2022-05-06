7ef4beae8ff31fd8c2757a71cc8eff10a77a669a
James_McNeill <jmcneill06@outlook.com>
2022-05-06 14:54:23 +0100

    Update intro.r

--- R/DataScientist/SupervisedLearning/Regression/intro.r
+++ R/DataScientist/SupervisedLearning/Regression/intro.r
@@ -28,3 +28,38 @@ broom::glance(unemployment_model)
 
 # Call wrapFTest() on unemployment_model to see the most relevant details
 sigr::wrapFTest(unemployment_model)
+
+# unemployment is available
+summary(unemployment)
+
+# newrates is available
+newrates
+
+# Predict female unemployment in the unemployment dataset
+unemployment$prediction <-  predict(unemployment_model, data = newrates)
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Make a plot to compare predictions to actual (prediction on x axis). 
+ggplot(unemployment, aes(x = prediction, y = female_unemployment)) + 
+  geom_point() +
+  geom_abline(color = "blue")
+
+# Predict female unemployment rate when male unemployment is 5%
+pred <- predict(unemployment_model, data.frame(male_unemployment = 5))
+pred
+
+# bloodpressure is available
+summary(bloodpressure)
+
+# Create the formula and print it
+fmla <- as.formula("blood_pressure ~ age + weight")
+fmla
+
+# Fit the model: bloodpressure_model
+bloodpressure_model <- lm(fmla, data = bloodpressure)
+
+# Print bloodpressure_model and call summary() 
+bloodpressure_model
+summary(bloodpressure_model)
