a0e1a0d86ee9d4589aed7f4916d712324886fd50
James_McNeill <jmcneill06@outlook.com>
2022-05-09 12:47:12 +0100

    Update tree-based.r

--- R/DataScientist/SupervisedLearning/Regression/tree-based.r
+++ R/DataScientist/SupervisedLearning/Regression/tree-based.r
@@ -1,2 +1,64 @@
 # The intuition behind tree-based methods
 
+# Random Forests
+# bikesJuly is available
+str(bikesJuly)
+
+# Random seed to reproduce results
+seed
+
+# The outcome column
+(outcome <- "cnt")
+
+# The input variables
+(vars <- c("hr", "holiday", "workingday", "weathersit", "temp", "atemp", "hum", "windspeed"))
+
+# Create the formula string for bikes rented as a function of the inputs
+(fmla <- paste(outcome, "~", paste(vars, collapse = " + ")))
+
+# Load the package ranger. Used to fit random forest models
+library(ranger)
+
+# Fit and print the random forest model
+(bike_model_rf <- ranger(fmla, # formula 
+                         bikesJuly, # data
+                         num.trees = 500, 
+                         respect.unordered.factors = "order", # allows the continuous variables to be ordered for the model bins
+                         seed = seed))
+
+# Predict
+# bikesAugust is available
+str(bikesAugust)
+
+# bike_model_rf is available
+bike_model_rf
+
+# Make predictions on the August data
+bikesAugust$pred <- predict(bike_model_rf, bikesAugust)$predictions
+
+# Calculate the RMSE of the predictions
+bikesAugust %>% 
+  mutate(residual = cnt - pred)  %>% # calculate the residual
+  summarize(rmse  = sqrt(mean(residual ^ 2)))      # calculate rmse
+
+# Plot actual outcome vs predictions (predictions on x-axis)
+ggplot(bikesAugust, aes(x = pred, y = cnt)) + 
+  geom_point() + 
+  geom_abline()
+
+# Visualize the predictions
+first_two_weeks <- bikesAugust %>% 
+  # Set start to 0, convert unit to days
+  mutate(instant = (instant - min(instant)) / 24) %>% 
+  # Gather cnt and pred into a column named value with key valuetype
+  gather(key = valuetype, value = value, cnt, pred) %>%
+  # Filter for rows in the first two
+  filter(instant < 14) 
+
+# Plot predictions and cnt by date/time 
+ggplot(first_two_weeks, aes(x = instant, y = value, color = valuetype, linetype = valuetype)) + 
+  geom_point() + 
+  geom_line() + 
+  scale_x_continuous("Day", breaks = 0:14, labels = 0:14) + 
+  scale_color_brewer(palette = "Dark2") + 
+  ggtitle("Predicted August bike rentals, Random Forest plot")
