c9ad203fd75728927112b8c9d8df7cec5593f7d7
James_McNeill <jmcneill06@outlook.com>
2022-05-10 13:56:17 +0100

    Update hierarchical-clustering.r

--- R/DataScientist/UnsupervisedLearning/hierarchical-clustering.r
+++ R/DataScientist/UnsupervisedLearning/hierarchical-clustering.r
@@ -5,3 +5,16 @@ hclust.out <- hclust(dist(x))
 
 # Inspect the result
 summary(hclust.out)
+
+# Dendrogram. Used to visualize the process of clustering datapoints
+plot(hclust.out)
+
+# Draw a line to cut by height on the plot
+abline(h = 6, col = "red")
+
+# Cutting the tree to return group values per row of the dataset
+# Cut by height
+cutree(hclust.out, h = 7)
+
+# Cut by number of clusters
+cutree(hclust.out, k = 3)
