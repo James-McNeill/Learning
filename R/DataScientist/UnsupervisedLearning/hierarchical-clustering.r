6c5f6e4803fc2f989b85eb185d3de3901c519606
James_McNeill <jmcneill06@outlook.com>
2022-05-10 13:50:24 +0100

    Update hierarchical-clustering.r

--- R/DataScientist/UnsupervisedLearning/hierarchical-clustering.r
+++ R/DataScientist/UnsupervisedLearning/hierarchical-clustering.r
@@ -1,2 +1,7 @@
 # Hierarchical Clustering
 
+# Create hierarchical clustering model: hclust.out. dist() is a matrix for euclidean distance between points
+hclust.out <- hclust(dist(x))
+
+# Inspect the result
+summary(hclust.out)
