d9a9a3d3af115524c00c4bb405b1a2ae5f0366ae
James_McNeill <jmcneill06@outlook.com>
2022-05-10 14:20:12 +0100

    Create PCA-dimensionality-reduction.r

---
+++ R/DataScientist/UnsupervisedLearning/PCA-dimensionality-reduction.r
@@ -0,0 +1,4 @@
+# Dimensionality reduction with PCA
+
+# Principal component analysis, or PCA, is a common approach to dimensionality reduction. Learn exactly what PCA does, 
+# visualize the results of PCA with biplots and scree plots, and deal with practical issues such as centering and scaling the data before performing PCA
