979ba5c42ba73f31ff51113aae7582604f072e6f
James_McNeill <jmcneill06@outlook.com>
2022-05-10 16:48:55 +0100

    Update PCA-dimensionality-reduction.r

--- R/DataScientist/UnsupervisedLearning/PCA-dimensionality-reduction.r
+++ R/DataScientist/UnsupervisedLearning/PCA-dimensionality-reduction.r
@@ -18,3 +18,26 @@ summary(pr.out)
 #   in terms of the original principal components
 # x: the value of each observation in the original dataset projected to the principal components
 # You can access these the same as other model components. For example, use pr.out$rotation to access the rotation component.
+
+# Visualize the two PC in a graph. The input features are put on top of the graph to show the direction that they follow. This helps to show how similar
+# features move in the same direction. Also it allows the user to understand if feature data point are far away
+biplot(pr.out)
+
+# Variance explained
+# Variability of each principal component: pr.var
+pr.var <- pr.out$sdev ^ 2
+
+# Variance explained by each principal component: pve
+pve <- pr.var / sum(pr.var)
+
+# Visualize variance explained
+# Plot variance explained for each principal component
+plot(pve, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
+
+# Plot cumulative proportion of variance explained. NOTE: When the number of principal components is equal to the number of data features the cumsum() will
+# accumulate to 1.
+plot(cumsum(pve), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
