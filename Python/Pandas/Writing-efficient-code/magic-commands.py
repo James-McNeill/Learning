21dc794edb9872688384db2e6aa00ce6aff87b1a
James_McNeill <jmcneill06@outlook.com>
2022-05-11 15:44:09 +0100

    Update magic-commands.py

--- Python/Pandas/Writing-efficient-code/magic-commands.py
+++ Python/Pandas/Writing-efficient-code/magic-commands.py
@@ -9,3 +9,12 @@
 
 # Have to inculde -f to show that it is a function to be reviewed, the function_name is then provided along with a version of what is required to run the function. 
 # Then this will test an implementation of the function"
+
+%mprun
+# Run a line by line review of the code to understand the memory overhead. For the cases that have been reviewed for profiling, 
+# the functions have been stored within Python modules (py files)
+
+# In the Ipython shell run
+pip install memory_profiler
+%load_ext memory_profiler
+%mprun -f function_name function_name(pparam1, kparam1)
