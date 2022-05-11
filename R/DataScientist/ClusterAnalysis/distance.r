cd22e2a67e97fe8a6b33b607e281978395fa5f64
James_McNeill <jmcneill06@outlook.com>
2022-05-11 13:09:39 +0100

    Update distance.r

--- R/DataScientist/ClusterAnalysis/distance.r
+++ R/DataScientist/ClusterAnalysis/distance.r
@@ -1,3 +1,26 @@
 # Calculating distance between observations
 
-# 
+# Calculate and plot distance between two players
+# Plot the positions of the players
+ggplot(two_players, aes(x = x, y = y)) + 
+  geom_point() +
+  # Assuming a 40x60 field
+  lims(x = c(-30,30), y = c(-20, 20))
+
+# Split the players data frame into two observations
+player1 <- two_players[1, ]
+player2 <- two_players[2, ]
+
+# Calculate and print their distance using the Euclidean Distance formula. The hypotenuse of a triangle
+player_distance <- sqrt( (player1$x - player2$x)^2 + (player1$y - player2$y)^2 )
+player_distance
+
+# Using dist() function
+# Note: The default distance calculation for the dist() function is Euclidean distance
+# Calculate the Distance Between two_players
+dist_two_players <- dist(two_players)
+dist_two_players
+
+# Calculate the Distance Between three_players
+dist_three_players <- dist(three_players)
+dist_three_players
