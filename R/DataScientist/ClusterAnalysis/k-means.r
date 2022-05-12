7696dd97389c0e1f6247520956d3f589e2e24643
James_McNeill <jmcneill06@outlook.com>
2022-05-12 10:55:29 +0100

    Update k-means.r

--- R/DataScientist/ClusterAnalysis/k-means.r
+++ R/DataScientist/ClusterAnalysis/k-means.r
@@ -1,3 +1,49 @@
 # K-means clustering
 
-# 
+# Review initial clusters
+# Build a kmeans model
+model_km2 <- kmeans(lineup, centers = 2)
+
+# Extract the cluster assignment vector from the kmeans model
+clust_km2 <- model_km2$cluster
+
+# Create a new data frame appending the cluster assignment
+lineup_km2 <- mutate(lineup, cluster = clust_km2)
+
+# Plot the positions of the players and color them using their cluster
+ggplot(lineup_km2, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
+
+# Adding an extra cluster
+# Build a kmeans model
+model_km3 <- kmeans(lineup, centers = 3)
+
+# Extract the cluster assignment vector from the kmeans model
+clust_km3 <- model_km3$cluster
+
+# Create a new data frame appending the cluster assignment
+lineup_km3 <- mutate(lineup, cluster = clust_km3)
+
+# Plot the positions of the players and color them using their cluster
+ggplot(lineup_km3, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
+
+# Many K's many models
+library(purrr)
+
+# Use map_dbl to run many models with varying value of k (centers). Creates the withinss for each model run
+tot_withinss <- map_dbl(1:10,  function(k){
+  model <- kmeans(x = lineup, centers = k)
+  model$tot.withinss
+})
+
+# Generate a data frame containing both k and tot_withinss
+elbow_df <- data.frame(
+  k = 1:10 ,
+  tot_withinss = tot_withinss
+)
+
+# Plot the elbow plot
+ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
+  geom_line() +
+  scale_x_continuous(breaks = 1:10)
