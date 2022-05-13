06cb7286a34677edb37253918ef12a01a981bb68
James_McNeill <jmcneill06@outlook.com>
2022-05-13 14:28:45 +0100

    Update cnn.py

--- Python/DL/PyTorch/cnn.py
+++ Python/DL/PyTorch/cnn.py
@@ -21,3 +21,36 @@ filters = torch.rand(6, 1, 3, 3)
 # Convolve the image with the filters
 output_feature = F.conv2d(image, filters, stride=1, padding=1)
 print(output_feature.shape)
+
+# Pooling operators
+# Aims to reduce the size of an image to a lower dimension / resolution without losing too much information. By reducing the image size this makes the
+# computations less intense.
+
+# Max pooling
+# Taking the maximum value from a section of the grid and keeping this value for the new grid of the original
+# Build a pooling operator with size `2`.
+max_pooling = torch.nn.MaxPool2d(2)
+
+# Apply the pooling operator
+output_feature = max_pooling(im)
+
+# Use pooling operator in the image
+output_feature_F = F.max_pool2d(im, 2)
+
+# print the results of both cases. Same results are returned for each method. Choice of which is to use is left to the user
+print(output_feature)
+print(output_feature_F)
+
+# Average pooling
+# Build a pooling operator with size `2`.
+avg_pooling = torch.nn.AvgPool2d(2)
+
+# Apply the pooling operator
+output_feature = avg_pooling(im)
+
+# Use pooling operator in the image
+output_feature_F = F.avg_pool2d(im, 2)
+
+# print the results of both cases
+print(output_feature)
+print(output_feature_F)
