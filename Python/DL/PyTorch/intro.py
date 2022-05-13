636f968582ccbe9539ded731c96a196b300ba3e2
James_McNeill <jmcneill06@outlook.com>
2022-05-13 10:52:34 +0100

    Update intro.py

--- Python/DL/PyTorch/intro.py
+++ Python/DL/PyTorch/intro.py
@@ -1,3 +1,46 @@
 # Introduction to PyTorch
 
 # Library is very similar to NumPy
+
+# Creating tensors
+# Import torch
+import torch
+
+# Create random tensor of size 3 by 3
+your_first_tensor = torch.rand(3, 3)
+
+# Calculate the shape of the tensor
+tensor_size = your_first_tensor.shape
+
+# Print the values of the tensor and its shape
+print(your_first_tensor)
+print(tensor_size)
+
+# Create a matrix of ones with shape 3 by 3
+tensor_of_ones = torch.ones(3, 3)
+
+# Create an identity matrix with shape 3 by 3
+identity_tensor = torch.eye(3)
+
+# Do a matrix multiplication of tensor_of_ones with identity_tensor
+matrices_multiplied = torch.matmul(tensor_of_ones, identity_tensor)
+print(matrices_multiplied)
+
+# Do an element-wise multiplication of tensor_of_ones with identity_tensor
+element_multiplication = tensor_of_ones * identity_tensor
+print(element_multiplication)
+
+# Forward pass
+# Initialize tensors x, y and z
+x = torch.rand(1000, 1000)
+y = torch.rand(1000, 1000)
+z = torch.rand(1000, 1000)
+
+# Multiply x with y
+q = torch.matmul(x, y)
+
+# Multiply elementwise z with q
+f = z * q
+
+mean_f = torch.mean(f)
+print(mean_f)
