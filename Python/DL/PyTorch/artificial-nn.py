132038cfeb76336321e23909bd63f3602b829f13
James_McNeill <jmcneill06@outlook.com>
2022-05-13 12:55:49 +0100

    Update artificial-nn.py

--- Python/DL/PyTorch/artificial-nn.py
+++ Python/DL/PyTorch/artificial-nn.py
@@ -101,3 +101,74 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
                                           shuffle=True, num_workers=0)
 testloader = torch.utils.data.DataLoader(testset, batch_size=32,
 										                      shuffle=False, num_workers=0) 
+
+# Inspecting the dataloaders
+# Compute the shape of the training set and testing set
+trainset_shape = trainloader.dataset.train_data.shape
+testset_shape = testloader.dataset.test_data.shape
+
+# Print the computed shapes
+print(trainset_shape, testset_shape)
+
+# Compute the size of the minibatch for training set and testing set
+trainset_batchsize = trainloader.batch_size
+testset_batchsize = testloader.batch_size
+
+# Print sizes of the minibatch
+print(trainset_batchsize, testset_batchsize)
+
+# Training Neural Networks
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# Define the class Net
+class Net(nn.Module):
+    def __init__(self):    
+    	# Define all the parameters of the net
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28 * 28 * 1, 200)
+        self.fc2 = nn.Linear(200, 10)
+
+    def forward(self, x):   
+    	# Do the forward pass
+        x = F.relu(self.fc1(x)) # uses the activation function in a functional way
+        x = self.fc2(x)
+        return x
+
+# Instantiate the Adam optimizer and Cross-Entropy loss function
+model = Net()   
+optimizer = optim.Adam(model.parameters(), lr=3e-4)	# used to optimize the network
+criterion = nn.CrossEntropyLoss()
+  
+for batch_idx, data_target in enumerate(train_loader):
+    data = data_target[0]
+    target = data_target[1]
+    data = data.view(-1, 28 * 28)
+    optimizer.zero_grad()
+
+    # Complete a forward pass
+    output = model(data)
+
+    # Compute the loss, gradients and change the weights
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step()
+	
+# Make predictions
+# Set the model in eval mode
+model.eval()
+
+for i, data in enumerate(test_loader, 0):
+    inputs, labels = data
+    
+    # Put each image into a vector
+    inputs = inputs.view(-1, 28*28)
+    
+    # Do the forward pass and get the predictions
+    outputs = model(inputs)
+    _, outputs = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (outputs == labels).sum().item()
+print('The testing set accuracy of the network is: %d %%' % (100 * correct / total))
