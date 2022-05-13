8d41f605b99de3a357dc9fe8da1f3f9e2da20392
James_McNeill <jmcneill06@outlook.com>
2022-05-13 15:46:16 +0100

    Update using-cnn.py

--- Python/DL/PyTorch/using-cnn.py
+++ Python/DL/PyTorch/using-cnn.py
@@ -48,3 +48,51 @@ val_loader = torch.utils.data.DataLoader(datasets.MNIST('mnist', download=True, 
                    transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])),
                    batch_size=64, shuffle=False, sampler=torch.utils.data.SubsetRandomSampler(indices[55000:60000]))
 
+# Regularization techniques
+# L2-regularization
+# Simply penalizes large weights, and thus enforces the network to use only small weights
+# Instantiate the network
+model = Net()
+
+# Instantiate the cross-entropy loss
+criterion = torch.nn.CrossEntropyLoss()
+
+# Instantiate the Adam optimizer
+optimizer = optim.Adam(model.parameters(), lr=3e-4, weight_decay=0.001)
+
+# Dropout
+# NN is forced to learn by dropping out a random proportion of nodes at each layer. Effective method to avoid overfitting with large NN.
+# When using smaller models, it does not make sense to use this feature too often.
+
+class Net(nn.Module):
+    def __init__(self):
+        
+        # Define all the parameters of the net
+        self.classifier = nn.Sequential(
+            nn.Linear(28*28, 200),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5), # 50% of the nodes are dropped from previous layer
+            nn.Linear(200, 500),
+            nn.ReLU(inplace=True),
+            nn.Linear(500, 10))
+        
+    def forward(self, x):
+    
+    	# Do the forward pass
+        return self.classifier(x)
+
+# Batch-normalization
+# Used to make the training of convolutional neural networks more efficient, while at the same time having regularization effects
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        # Implement the sequential module for feature extraction
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(2, 2), nn.ReLU(inplace=True), nn.BatchNorm2d(10),
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(2, 2), nn.ReLU(inplace=True), nn.BatchNorm2d(20))
+        
+        # Implement the fully connected layer for classification
+        self.fc = nn.Linear(in_features=7*7*20, out_features=10)
