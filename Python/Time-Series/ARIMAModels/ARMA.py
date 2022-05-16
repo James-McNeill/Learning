0f579712892154ba70960f6f6c495851a9ed6b12
James_McNeill <jmcneill06@outlook.com>
2022-05-16 13:39:14 +0100

    Update ARMA.py

--- Python/Time-Series/ARIMAModels/ARMA.py
+++ Python/Time-Series/ARIMAModels/ARMA.py
@@ -1,3 +1,29 @@
 # ARMA Models
 
-# 
+# Exploration
+# Import modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load in the time series
+candy = pd.read_csv('candy_production.csv', 
+            index_col='date',
+            parse_dates=True)
+
+# Plot and show the time series on axis ax1
+fig, ax1 = plt.subplots()
+candy.plot(ax=ax1)
+plt.show()
+
+# Train-Test split
+# Split the data into a train and test set
+candy_train = candy.loc[:'2006']
+candy_test = candy.loc['2007':]
+
+# Create an axis
+fig, ax = plt.subplots()
+
+# Plot the train and test sets on the axis ax
+candy_train.plot(ax=ax)
+candy_test.plot(ax=ax)
+plt.show()
