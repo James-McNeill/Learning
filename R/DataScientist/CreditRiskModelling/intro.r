56509bc5cbabe097220405070eee4ef67aab59e0
James_McNeill <jmcneill06@outlook.com>
2022-05-16 09:37:53 +0100

    Update intro.r

--- R/DataScientist/CreditRiskModelling/intro.r
+++ R/DataScientist/CreditRiskModelling/intro.r
@@ -38,3 +38,71 @@ new_data <- loan_data[-index_highage, ]
 # Make bivariate scatterplot of age and annual income
 plot(loan_data$age, loan_data$annual_inc, xlab = "Age", ylab = "Annual Income")
 
+# Missing data and coarse classification
+# Look at summary of loan_data
+summary(loan_data$int_rate)
+
+# Get indices of missing interest rates: na_index
+na_index <- which(is.na(loan_data$int_rate))
+
+# Remove observations with missing interest rates: loan_data_delrow_na
+loan_data_delrow_na <- loan_data[-na_index, ]
+
+# Make copy of loan_data
+loan_data_delcol_na <- loan_data
+
+# Delete interest rate column from loan_data_delcol_na
+loan_data_delcol_na$int_rate <- NULL
+
+# Replace missing data with median value
+# Compute the median of int_rate
+median_ir <- median(loan_data$int_rate, na.rm = TRUE)
+
+# Make copy of loan_data
+loan_data_replace <- loan_data
+
+# Replace missing interest rates with median
+loan_data_replace$int_rate[na_index] <- median_ir
+
+# Check if the NAs are gone
+summary(loan_data_replace$int_rate)
+
+# Coarse classification. Binning creates a segment for the missing values
+# Make the necessary replacements in the coarse classification example below 
+loan_data$ir_cat <- rep(NA, length(loan_data$int_rate))
+
+loan_data$ir_cat[which(loan_data$int_rate <= 8)] <- "0-8"
+loan_data$ir_cat[which(loan_data$int_rate > 8 & loan_data$int_rate <= 11)] <- "8-11"
+loan_data$ir_cat[which(loan_data$int_rate > 11 & loan_data$int_rate <= 13.5)] <- "11-13.5"
+loan_data$ir_cat[which(loan_data$int_rate > 13.5)] <- "13.5+"
+loan_data$ir_cat[which(is.na(loan_data$int_rate))] <- "Missing"
+
+loan_data$ir_cat <- as.factor(loan_data$ir_cat)
+
+# Look at your new variable using plot()
+plot(loan_data$ir_cat)
+
+# Data splitting and confusion matrices
+# Set seed of 567
+set.seed(567)
+
+# Store row numbers for training set: index_train
+# sample(parm1, parm2): parm1: vector from which the sample will be taken. parm2: number of items to choose
+index_train <- sample(1:nrow(loan_data), 2 / 3 * nrow(loan_data))
+
+# Create training set: training_set
+training_set <- loan_data[index_train, ]
+
+# Create test set: test_set
+test_set <- loan_data[-index_train, ]
+
+# Create confusion matrix
+conf_matrix <- table(test_set$loan_status, model_pred)
+
+# Compute classification accuracy
+accuracy <- sum(conf_matrix[1,1], conf_matrix[2,2]) / nrow(test_set)
+accuracy
+
+# Compute sensitivity
+sensitivity <- conf_matrix[2,2] / sum(conf_matrix[2,2], conf_matrix[2,1])
+sensitivity
