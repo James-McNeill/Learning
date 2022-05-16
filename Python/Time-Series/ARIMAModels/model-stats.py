93cf2f86e52d763c76bea13826fba21bb547df64
James_McNeill <jmcneill06@outlook.com>
2022-05-16 16:45:54 +0100

    Update model-stats.py

--- Python/Time-Series/ARIMAModels/model-stats.py
+++ Python/Time-Series/ARIMAModels/model-stats.py
@@ -1,3 +1,33 @@
 # The best of the best models
 
 # Understanding which elements to review and make the best model selection
+# ACF and PACF
+# Import
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+# Create figure
+fig, (ax1, ax2) = plt.subplots(2,1, figsize=(12,8))
+ 
+# Plot the ACF of df
+plot_acf(df, lags=10, zero=False, ax=ax1)
+
+# Plot the PACF of df
+plot_pacf(df, lags=10, zero=False, ax=ax2)
+
+plt.show()
+
+# Create figure
+fig, (ax1, ax2) = plt.subplots(2,1, figsize=(12,8))
+
+# Plot ACF and PACF
+plot_acf(earthquake, lags=10, zero=False, ax=ax1)
+plot_pacf(earthquake, lags=10, zero=False, ax=ax2)
+
+# Show plot
+plt.show()
+
+# Instantiate model
+model = SARIMAX(earthquake, order=(1,0,0))
+
+# Train model
+results = model.fit()
