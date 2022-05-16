c766736f12110f94be53fad97fc3870e9519cec2
James_McNeill <jmcneill06@outlook.com>
2022-05-16 12:20:24 +0100

    Update decision-tree.r

--- R/DataScientist/CreditRiskModelling/decision-tree.r
+++ R/DataScientist/CreditRiskModelling/decision-tree.r
@@ -99,3 +99,45 @@ ptree_loss_matrix <- prune(tree_loss_matrix, cp = 0.0012788)
 
 # Use prp() and argument extra = 1 to plot the pruned tree
 prp(ptree_loss_matrix, extra = 1)
+
+# Pruning trees with more options
+# set a seed and run the code to obtain a tree using weights, minsplit and minbucket
+set.seed(345)
+tree_weights <- rpart(loan_status ~ ., method = "class",
+                      data = training_set,
+                      weights = case_weights, # a vector with more weight applied to the default cases. default has value = 3 and non-default = 1
+                      control = rpart.control(minsplit = 5, minbucket = 2, cp = 0.001)) # minsplit: min number of splits, minbucket: min number of observations
+
+# Plot the cross-validated error rate for a changing cp
+plotcp(tree_weights)
+
+# Create an index for of the row with the minimum xerror
+index <- which.min(tree_weights$cp[ , "xerror"])
+
+# Create tree_min
+tree_min <- tree_weights$cp[index, "CP"]
+
+# Prune the tree using tree_min
+ptree_weights <- prune(tree_weights, cp = tree_min)
+
+# Plot the pruned tree using the rpart.plot()-package
+prp(ptree_weights, extra = 1)
+
+# Confusion matrix and accuracy
+# Make predictions for each of the pruned trees using the test set.
+pred_undersample <- predict(ptree_undersample, newdata = test_set,  type = "class")
+pred_prior <- predict(ptree_prior, newdata = test_set,  type = "class")
+pred_loss_matrix <- predict(ptree_loss_matrix, newdata = test_set,  type = "class")
+pred_weights <- predict(ptree_weights, newdata = test_set,  type = "class")
+
+# construct confusion matrices using the predictions.
+confmat_undersample <- table(test_set$loan_status, pred_undersample)
+confmat_prior <- table(test_set$loan_status, pred_prior)
+confmat_loss_matrix <- table(test_set$loan_status, pred_loss_matrix)
+confmat_weights <- table(test_set$loan_status, pred_weights)
+
+# Compute the accuracies. The loss matrix accuracy was very far from the other three, showing much weaker performance
+acc_undersample <- sum(diag(confmat_undersample)) / nrow(test_set)
+acc_prior <- sum(diag(confmat_prior)) / nrow(test_set)
+acc_loss_matrix <- sum(diag(confmat_loss_matrix)) / nrow(test_set)
+acc_weights <- sum(diag(confmat_weights)) / nrow(test_set)
