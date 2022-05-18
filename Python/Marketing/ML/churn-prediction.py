ec00d44ddbc0fa437a5aef707e64f73e4de11414
James_McNeill <jmcneill06@outlook.com>
2022-05-18 15:17:08 +0100

    Update churn-prediction.py

--- Python/Marketing/ML/churn-prediction.py
+++ Python/Marketing/ML/churn-prediction.py
@@ -74,3 +74,33 @@ for index in range(0, len(C)):
 col_names = ['C','Non-Zero Coeffs','Recall']
 print(pd.DataFrame(l1_metrics, columns=col_names))
 
+# Decision tree
+# Initialize decision tree classifier
+mytree = tree.DecisionTreeClassifier()
+
+# Fit the decision tree on training data
+mytree.fit(train_X, train_Y)
+
+# Predict churn labels on testing data
+pred_test_Y = mytree.predict(test_X)
+
+# Calculate accuracy score on testing data
+test_accuracy = accuracy_score(test_Y, pred_test_Y)
+
+# Print test accuracy
+print('Test accuracy:', round(test_accuracy, 4))
+
+# Identify optimal tree depth
+# Run a for loop over the range of depth list length
+for index in range(0, len(depth_list)):
+  # Initialize and fit decision tree with the `max_depth` candidate
+  mytree = DecisionTreeClassifier(max_depth=depth_list[index])
+  mytree.fit(train_X, train_Y)
+  # Predict churn on the testing data
+  pred_test_Y = mytree.predict(test_X)
+  # Calculate the recall score 
+  depth_tuning[index,1] = recall_score(test_Y, pred_test_Y)
+
+# Name the columns and print the array as pandas DataFrame
+col_names = ['Max_Depth','Recall']
+print(pd.DataFrame(depth_tuning, columns=col_names))
