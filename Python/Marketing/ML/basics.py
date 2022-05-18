398e36646829b4e262801e27866f4a212e5c59e4
James_McNeill <jmcneill06@outlook.com>
2022-05-18 09:15:58 +0100

    Update basics.py

--- Python/Marketing/ML/basics.py
+++ Python/Marketing/ML/basics.py
@@ -1,3 +1,36 @@
 # Machine learning for marketing basics
 
 # Investigate the data and prepare for modelling
+# Print the data types of telco_raw dataset
+print(telco_raw.dtypes)
+
+# Print the header of telco_raw dataset
+print(telco_raw.head())
+
+# Print the number of unique values in each telco_raw column
+print(telco_raw.nunique())
+
+# Store customerID and Churn column names
+custid = ['customerID']
+target = ['Churn']
+
+# Store categorical column names. Only reviewing this figure of unqiue values. This can be changed by the modeller
+categorical = telco_raw.nunique()[telco_raw.nunique() < 5].keys().tolist()
+
+# Remove target from the list of categorical variables
+categorical.remove(target[0])
+
+# Store numerical column names
+numerical = [x for x in telco_raw.columns if x not in custid + target + categorical]
+
+# Perform one-hot encoding to categorical variables 
+telco_raw = pd.get_dummies(data = telco_raw, columns = categorical, drop_first=True)
+
+# Initialize StandardScaler instance
+scaler = StandardScaler()
+
+# Fit and transform the scaler on numerical columns
+scaled_numerical = scaler.fit_transform(telco_raw[numerical])
+
+# Build a DataFrame from scaled_numerical
+scaled_numerical = pd.DataFrame(scaled_numerical, columns=numerical)
