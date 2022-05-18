0762de2ac70311bcd096217d9cb0fa8d4aeea831
James_McNeill <jmcneill06@outlook.com>
2022-05-18 16:03:26 +0100

    Update customer-segmention.py

--- Python/Marketing/ML/customer-segmention.py
+++ Python/Marketing/ML/customer-segmention.py
@@ -1,3 +1,30 @@
 # Customer segmentation
 
 # Performing unsupervised learning to segment customers
+
+# Explore customer product purchase dataset
+# Print the header of the `wholesale` dataset
+print(wholesale.head())
+
+# Plot the pairwise relationships between the variables
+sns.pairplot(wholesale, diag_kind='kde')
+
+# Display the chart
+plt.show()
+
+# Understand differences in variables
+# Create column names list and same length integer list
+x_names = wholesale.columns
+x_ix = np.arange(wholesale.shape[1])
+
+# Plot the averages data in gray and standard deviations in orange 
+plt.bar(x=x_ix-0.2, height=averages, color='grey', label='Average', width=0.4)
+plt.bar(x=x_ix+0.2, height=std_devs, color='orange', label='Standard Deviation', width=0.4)
+
+# Add x-axis labels and rotate
+plt.xticks(ticks=x_ix, labels=x_names, rotation=90)
+
+# Add the legend and display the chart
+plt.legend()
+plt.show()
+
