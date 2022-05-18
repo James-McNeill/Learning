10fc72db4b71a3ccd0c2ea3acd3fa9a10c8921d8
James_McNeill <jmcneill06@outlook.com>
2022-05-18 16:15:15 +0100

    Update customer-segmention.py

--- Python/Marketing/ML/customer-segmention.py
+++ Python/Marketing/ML/customer-segmention.py
@@ -28,3 +28,78 @@ plt.xticks(ticks=x_ix, labels=x_names, rotation=90)
 plt.legend()
 plt.show()
 
+# Unskew the variables
+# Define custom Box Cox transformation function
+def boxcox_df(x):
+    x_boxcox, _ = stats.boxcox(x)
+    return x_boxcox
+
+# Apply the function to the `wholesale` dataset
+wholesale_boxcox = wholesale.apply(boxcox_df, axis=0)
+
+# Plot the pairwise relationships between the transformed variables 
+sns.pairplot(wholesale_boxcox, diag_kind='kde')
+
+# Display the chart
+plt.show()
+
+# Normalize the variables
+# Once the variables are normalized then they can be used within the K-Means algorithm
+# Fit the initialized `scaler` instance on the Box-Cox transformed dataset
+scaler.fit(wholesale_boxcox)
+
+# Transform and store the scaled dataset as `wholesale_scaled`
+wholesale_scaled = scaler.transform(wholesale_boxcox)
+
+# Create a `pandas` DataFrame from the scaled dataset
+wholesale_scaled_df = pd.DataFrame(data=wholesale_scaled,
+                                       index=wholesale_boxcox.index,
+                                       columns=wholesale_boxcox.columns)
+
+# Print the mean and standard deviation for all columns
+print(wholesale_scaled_df.agg(['mean','std']).round())
+
+# Determine the optimal number of clusters
+# Create empty sse dictionary
+sse = {}
+
+# Fit KMeans algorithm on k values between 1 and 11
+for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, random_state=333)
+    kmeans.fit(wholesale_scaled_df)
+    sse[k] = kmeans.inertia_
+
+# Add the title to the plot
+plt.title('Elbow criterion method chart')
+
+# Create and display a scatter plot
+sns.pointplot(x=list(sse.keys()), y=list(sse.values()))
+plt.show()
+
+# Build segmentation with KMeans
+# Import `KMeans` module
+from sklearn.cluster import KMeans
+
+# Initialize `KMeans` with 4 clusters
+kmeans=KMeans(n_clusters=4, random_state=123)
+
+# Fit the model on the pre-processed dataset
+kmeans.fit(wholesale_scaled_df)
+
+# Assign the generated labels to a new column
+wholesale_kmeans4 = wholesale.assign(segment = kmeans.labels_)
+
+# Alternative segmentation with NMF
+# non-negative matrix factorization algorithm (NMF). It works well with sparse customer by product matrices that are typical in the e-commerce or retail space.
+# Import the non-negative matrix factorization module
+from sklearn.decomposition import NMF
+
+# Initialize NMF instance with 4 components
+nmf = NMF(4)
+
+# Fit the model on the wholesale sales data
+nmf.fit(wholesale)
+
+# Extract the components 
+components = pd.DataFrame(data=nmf.components_, columns=wholesale.columns)
+
