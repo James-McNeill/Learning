23ff8ed6ddabb5f6fbb1a4719860d4d43300feb8
James_McNeill <jmcneill06@outlook.com>
2022-05-18 09:27:30 +0100

    Update churn-prediction.py

--- Python/Marketing/ML/churn-prediction.py
+++ Python/Marketing/ML/churn-prediction.py
@@ -1,3 +1,30 @@
 # Churn prediction fundamentals
 # Reviewing Telco data
 
+# Explore data
+# Print the unique Churn values
+print(set(telcom['Churn']))
+
+# Calculate the ratio size of each churn group
+telcom.groupby(['Churn']).size() / telcom.shape[0] * 100
+
+# Import the function for splitting data to train and test
+from sklearn.model_selection import train_test_split
+
+# Split the data into train and test
+train, test = train_test_split(telcom, test_size = .25)
+
+# Store column names from `telcom` excluding target variable and customer ID
+cols = [col for col in telcom.columns if col not in custid + target]
+
+# Extract training features
+train_X = train[cols]
+
+# Extract training target
+train_Y = train[target]
+
+# Extract testing features
+test_X = test[cols]
+
+# Extract testing target
+test_Y = test[target]
