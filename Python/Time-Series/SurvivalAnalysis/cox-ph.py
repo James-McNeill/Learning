c72c5dd7aa10ead803febf2805271eebf5e18c35
James_McNeill <jmcneill06@outlook.com>
2022-05-19 14:44:50 +0100

    Update cox-ph.py

--- Python/Time-Series/SurvivalAnalysis/cox-ph.py
+++ Python/Time-Series/SurvivalAnalysis/cox-ph.py
@@ -1,3 +1,23 @@
 # Cox Proportional Hazards (PH) model
 
 # Model prison data with PH model
+# Import CoxPHFitter class
+from lifelines import CoxPHFitter
+
+# Instantiate CoxPHFitter class cph
+cph = CoxPHFitter()
+
+# Fit cph to data
+cph.fit(df=prison, duration_col="week", event_col="arrest")
+
+# Print model summary
+print(cph.summary)
+
+# Instantiate CoxPHFitter class
+custom_cph = CoxPHFitter()
+
+# Fit custom model
+custom_cph.fit(df=prison, duration_col="week", event_col="arrest", formula="fin + age + prio")
+
+# Print model summary
+print(custom_cph.summary)
