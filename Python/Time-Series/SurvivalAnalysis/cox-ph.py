13247f910d7f3c1c5c1d61c4ba2254b657c4efdc
James_McNeill <jmcneill06@outlook.com>
2022-05-19 15:33:39 +0100

    Update cox-ph.py

--- Python/Time-Series/SurvivalAnalysis/cox-ph.py
+++ Python/Time-Series/SurvivalAnalysis/cox-ph.py
@@ -21,3 +21,27 @@ custom_cph.fit(df=prison, duration_col="week", event_col="arrest", formula="fin 
 
 # Print model summary
 print(custom_cph.summary)
+
+# Instantiate CoxPHFitter class
+cph = CoxPHFitter()
+
+# Fit cph to data using all columns
+cph.fit(df=prison, duration_col="week", event_col="arrest")
+
+# Assign summary to summary_df
+summary_df = cph.summary
+
+# Create new column of survival time ratios
+summary_df["surv_ratio"] = 1 / np.exp(summary_df['coef'])
+
+# Print surv_ratio for prio
+print(summary_df.loc['prio', "surv_ratio"])
+
+# Plot partial effects
+cph.plot_partial_effects_on_outcome(covariates=["fin", "age", "wexp", "mar","paro", "prio"],
+         values=[[0, 35, 0, 1, 1, 3], [1, 22, 0, 0, 0, 0]])
+
+# Show plot
+plt.show()
+
+# Test the PH assumptions
