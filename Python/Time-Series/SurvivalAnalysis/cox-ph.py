0c1eff10d4bae3ecbb9b63be4be0716f9e14ff80
James_McNeill <jmcneill06@outlook.com>
2022-05-19 15:57:06 +0100

    Update cox-ph.py

--- Python/Time-Series/SurvivalAnalysis/cox-ph.py
+++ Python/Time-Series/SurvivalAnalysis/cox-ph.py
@@ -45,3 +45,43 @@ cph.plot_partial_effects_on_outcome(covariates=["fin", "age", "wexp", "mar","par
 plt.show()
 
 # Test the PH assumptions
+# Fit to fin == 0
+kmf.fit(durations=prison[prison["fin"] == 0]["week"], 
+        event_observed=prison[prison["fin"] == 0]["arrest"])
+
+# Plot survival curve for fin == 0
+ax = kmf.plot()
+
+# Fit to fin == 1 and plot on ax
+kmf.fit(durations=prison[prison["fin"] == 1]["week"], 
+        event_observed=prison[prison["fin"] == 1]["arrest"])
+kmf.plot(ax=ax)
+plt.show()
+
+# Test the PH assumption automatically
+# Check PH assumption
+print(cph.check_assumptions(training_df=prison, p_value_threshold=0.1))
+
+# Employee churn study
+# Instantiate a CoxPHFitter object cph
+cph = CoxPHFitter()
+
+# Fit cph on all covariates
+cph.fit(employees, 'years_at_company', event_col='attrition')
+
+# Select employees that have not churned
+current_employees = employees.loc[employees['attrition'] == 0]
+
+# Existing durations of employees that have not churned
+current_employees_last_obs = current_employees['years_at_company']
+
+# Predict survival function conditional on existing durations
+cph.predict_survival_function(current_employees, 
+                              conditional_after=current_employees_last_obs)
+
+# Predict median remaining times for current employees
+pred = cph.predict_median(current_employees, 
+                              conditional_after=current_employees_last_obs)
+
+# Print the smallest median remaining time
+print(min(pred))
