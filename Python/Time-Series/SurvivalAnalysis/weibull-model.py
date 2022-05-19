d237995801dbc13ae698d91784487f6a839c66a8
James_McNeill <jmcneill06@outlook.com>
2022-05-19 12:51:11 +0100

    Create weibull-model.py

---
+++ Python/Time-Series/SurvivalAnalysis/weibull-model.py
@@ -0,0 +1,6 @@
+# Weibull model
+# Parametric model used to fit a survival curve
+
+# Two key model inputs are k and lambda
+# k: determines the shape
+# lambda: determines the scale. relates to the point on the distribution were 63.2% of the population have experienced the event
