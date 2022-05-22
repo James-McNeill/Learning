f81c8481fd035759eeafc943d3e8fc603e07bbe1
James_McNeill <jmcneill06@outlook.com>
2022-05-22 13:49:07 +0100

    Create overview.py

---
+++ Python/Functions/overview.py
@@ -0,0 +1,62 @@
+# Inspect
+inspect.getdoc(function)
+# A neater version of the doc string with white spaces removed
+
+# Decorators - counter
+'''
+Count the number of times that a function is being called within the code. Could remove code that is not being called anymore.
+'''
+def counter(func):
+  def wrapper(*args, **kwargs):
+    wrapper.count += 1
+    # Call the function being decorated and return the result
+    return func(*args, **kwargs)
+  wrapper.count = 0
+  # Return the new decorated function
+  return wrapper
+
+# Decorate foo() with the counter() decorator
+@counter
+def foo():
+  print('calling foo()')
+  
+foo()
+foo()
+
+print('foo() was called {} times.'.format(foo.count))
+
+# Decorators - preserve metadata
+from functools import wraps
+
+def add_hello(func):
+  # Decorate wrapper() so that it keeps func()'s metadata
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    """Print 'hello' and then call the decorated function."""
+    print('Hello')
+    return func(*args, **kwargs)
+  return wrapper
+  
+@add_hello
+def print_sum(a, b):
+  """Adds two numbers and prints the sum"""
+  print(a + b)
+  
+print_sum(10, 20)
+print(print_sum.__doc__)
+# @wraps(func) inside decorator preserves the orginal functions metadata
+
+# Decorators - HTML example
+'''
+Create the opening and closing tags on the fly for a HTML file
+'''
+def html(open_tag, close_tag):
+  def decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+      msg = func(*args, **kwargs)
+      return '{}{}{}'.format(open_tag, msg, close_tag)
+    # Return the decorated function
+    return wrapper
+  # Return the decorator
+  return decorator
