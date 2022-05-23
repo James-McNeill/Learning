53786df210125f35a81f3048a5a214ab3670b2f9
James_McNeill <jmcneill06@outlook.com>
2022-05-23 15:36:08 +0100

    Create hypothesis-testing.py

---
+++ Python/DataScience/StatisticalThinking/hypothesis-testing.py
@@ -0,0 +1,89 @@
+# Intro to Hypothesis Testing
+
+'''
+Creating a permutation of the sample data. Re-organising the population to create new shares of the original sample proportions
+'''
+def permutation_sample(data1, data2):
+    """Generate a permutation sample from two data sets."""
+
+    # Concatenate the data sets: data
+    data = np.concatenate((data1, data2))
+
+    # Permute the concatenated array: permuted_data
+    permuted_data = np.random.permutation(data)
+
+    # Split the permuted array into two: perm_sample_1, perm_sample_2
+    perm_sample_1 = permuted_data[:len(data1)]
+    perm_sample_2 = permuted_data[len(data1):]
+
+    return perm_sample_1, perm_sample_2
+
+# show the permutations method in action with the actual data
+for _ in range(50):
+    # Generate permutation samples
+    perm_sample_1, perm_sample_2 = permutation_sample(rain_june, rain_november)
+
+
+    # Compute ECDFs
+    x_1, y_1 = ecdf(perm_sample_1)
+    x_2, y_2 = ecdf(perm_sample_2)
+
+    # Plot ECDFs of permutation sample
+    _ = plt.plot(x_1, y_1, marker='.', linestyle='none',
+                 color='red', alpha=0.02)
+    _ = plt.plot(x_2, y_2, marker='.', linestyle='none',
+                 color='blue', alpha=0.02)
+
+# Create and plot ECDFs from original data
+x_1, y_1 = ecdf(rain_june)
+x_2, y_2 = ecdf(rain_november)
+_ = plt.plot(x_1, y_1, marker='.', linestyle='none', color='red')
+_ = plt.plot(x_2, y_2, marker='.', linestyle='none', color='blue')
+
+# Label axes, set margin, and show plot
+plt.margins(0.02)
+_ = plt.xlabel('monthly rainfall (mm)')
+_ = plt.ylabel('ECDF')
+plt.show()
+
+
+# Creating a permutation of the sample data. Re-organising the population to create new shares of the original sample proportions
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+
+    return diff
+
+# Compute difference of mean impact force from experiment: empirical_diff_means
+empirical_diff_means = diff_of_means(force_a, force_b)
+
+# Draw 10,000 permutation replicates: perm_replicates
+perm_replicates = draw_perm_reps(force_a, force_b,
+                                 diff_of_means, size=10000)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= empirical_diff_means) / len(perm_replicates)
+
+# Print the result
+print('p-value =', p)
+
+# Reviewing the pearson correlation statistics
+# Compute observed correlation: r_obs
+r_obs = pearson_r(illiteracy, fertility)
+
+# Initialize permutation replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute illiteracy measurments: illiteracy_permuted
+    illiteracy_permuted = np.random.permutation(illiteracy)
+
+    # Compute Pearson correlation
+    perm_replicates[i] = pearson_r(illiteracy_permuted, fertility)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= r_obs) / len(perm_replicates)
+print('p-val =', p)
