b6d383de37513b594a82d21c56e8918c12e8d051
James_McNeill <jmcneill06@outlook.com>
2022-05-23 15:32:44 +0100

    Create param-est-by-optimization.py

---
+++ Python/DataScience/StatisticalThinking/param-est-by-optimization.py
@@ -0,0 +1,30 @@
+# Parameter estimation by optimization
+
+'''
+Linear regression analysis. The use of np.polyfit() allows the user to use the same method for any polynomial transformation 
+being performed. The third parameter relates to the degree that the polynomial is fitting.
+'''
+# Specify slopes to consider: a_vals
+a_vals = np.linspace(0, 0.1, 200)
+
+# Initialize sum of square of residuals: rss
+rss = np.empty_like(a_vals)
+
+# Compute sum of square of residuals for each value of a_vals
+for i, a in enumerate(a_vals):
+    rss[i] = np.sum((fertility - a * illiteracy - b)**2)
+
+# Plot the RSS
+plt.plot(a_vals, rss, '-')
+plt.xlabel('slope (children per woman / percent illiterate)')
+plt.ylabel('sum of square of residuals')
+
+plt.show()
+
+# Iterate through x,y pairs
+for x, y in zip(anscombe_x, anscombe_y):
+    # Compute the slope and intercept: a, b
+    a, b = np.polyfit(x, y, 1)
+
+    # Print the result
+    print('slope:', a, 'intercept:', b)
