8d61394a3b14d79997fc0826244aa9cb3218319d
James_McNeill <jmcneill06@outlook.com>
2022-05-23 15:14:24 +0100

    Create schedule-dag-run.py

---
+++ Python/Airflow/schedule-dag-run.py
@@ -0,0 +1,17 @@
+# Schedule a DAG to run
+
+'''
+The intervals can be assigned by using either Airflow (@hourly, @daily, @monthly) or cron task times (*****)
+'''
+# Update the scheduling arguments as defined
+default_args = {
+  'owner': 'Engineering',
+  'start_date': datetime(2019, 11, 1),
+  'email': ['airflowresults@datacamp.com'],
+  'email_on_failure': False,
+  'email_on_retry': False,
+  'retries': 3,
+  'retry_delay': timedelta(minutes=20)
+}
+
+dag = DAG('update_dataflows', default_args=default_args, schedule_interval='30 12 * * 3')
