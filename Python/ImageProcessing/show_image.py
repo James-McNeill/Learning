145f5947d6cc3d8b06a1625886ef646b0be4f607
James_McNeill <jmcneill06@outlook.com>
2022-05-23 15:21:22 +0100

    Update show_image.py

--- Python/ImageProcessing/show_image.py
+++ Python/ImageProcessing/show_image.py
@@ -1,3 +1,17 @@
+'''
+Working with image processing can be tricky. As colour images can take the form of 3-D shapes, greyscale images are seen as 2-D, 
+this creates the challenge or understanding how much extra information is contained within an image. To make it easier to switch 
+between image types the following function was shared:
+
+Pixels in an image range from 0 to 255. With 0 being pure black and 255 being pure white. The representation of these pixels 
+values can be analysed using histograms
+
+colours can be extracted from a 3d numpy array as follows:
+red = [:, :, 0]
+green = [:, :, 1]
+blue = [:, :, 2]
+'''
+
 # Show an image using matplotlib.pyplot
 import matplotlib.pyplot as plt
 
