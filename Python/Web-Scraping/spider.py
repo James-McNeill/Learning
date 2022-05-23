6de45fb50fdf5e61020e798c635427aa91fc4460
James_McNeill <jmcneill06@outlook.com>
2022-05-23 14:58:25 +0100

    Create spider.py

---
+++ Python/Web-Scraping/spider.py
@@ -0,0 +1,24 @@
+# Web scraping - Spider
+
+'''
+Creating a spider class to crawl on a website and extract HTML output. Scrapy allows the user to request data from the website
+'''
+
+# Import the scrapy library
+import scrapy
+
+# Create the Spider class
+class DCspider(scrapy.Spider):
+  name = 'dcspider'
+  # start_requests method
+  def start_requests( self ):
+    yield scrapy.Request( url = url_short, callback = self.parse )
+  # parse method
+  def parse( self, response ):
+    # Create an extracted list of course author names
+    author_names = response.css('p.course-block__author-name ::text').extract()
+    # Here we will just return the list of Authors
+    return author_names
+  
+# Inspect the spider
+inspect_spider(DCspider)
