1ffcc564ec4dd0a5244c6c31afd5a6b011d616e3
James_McNeill <jmcneill06@outlook.com>
2022-05-23 14:41:11 +0100

    Create bias-variance-tradeoff.py

---
+++ Python/ML-ModelValidation/bias-variance-tradeoff.py
@@ -0,0 +1,78 @@
+# Bias Variance trade-off
+
+# Compute the array containing the 10-folds CV MSEs
+MSE_CV_scores = - cross_val_score(dt, X_train, y_train, cv=10, 
+                       scoring='neg_mean_squared_error',
+                       n_jobs=-1)
+
+# Compute the 10-folds CV RMSE
+RMSE_CV = (MSE_CV_scores.mean())**(1/2)
+
+# Print RMSE_CV
+print('CV RMSE: {:.2f}'.format(RMSE_CV))
+
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Fit dt to the training set
+dt.fit(X_train, y_train)
+
+# Predict the labels of the training set
+y_pred_train = dt.predict(X_train)
+
+# Evaluate the training set RMSE of dt
+RMSE_train = (MSE(y_train, y_pred_train))**(1/2)
+
+# Print RMSE_train
+print('Train RMSE: {:.2f}'.format(RMSE_train))
+
+# Ensemble model
+'''
+Picking the best from each classifier. Each classifier reviews the best model. From their the models can be ensembled by 
+using a hard voting policy, whereby the majority decision is assigned to the prediction of a new label
+'''
+
+# Set seed for reproducibility
+SEED=1
+
+# Instantiate lr
+lr = LogisticRegression(random_state=SEED)
+
+# Instantiate knn
+knn = KNeighborsClassifer(n_neighbors=27)
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=0.13, random_state=SEED)
+
+# Define the list classifiers
+classifiers = [('Logistic Regression', lr), ('K Nearest Neighbours', knn), ('Classification Tree', dt)]
+
+# Iterate over the pre-defined list of classifiers
+for clf_name, clf in classifiers:    
+ 
+    # Fit clf to the training set
+    clf.fit(X_train, y_train)    
+   
+    # Predict y_pred
+    y_pred = clf.predict(X_test)
+    
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, y_pred) 
+   
+    # Evaluate clf's accuracy on the test set
+    print('{:s} : {:.3f}'.format(clf_name, accuracy))
+# Import VotingClassifier from sklearn.ensemble
+from sklearn.ensemble import VotingClassifier
+
+# Instantiate a VotingClassifier vc
+vc = VotingClassifier(estimators=classifiers)     
+
+# Fit vc to the training set
+vc.fit(X_train, y_train)   
+
+# Evaluate the test set predictions
+y_pred = vc.predict(X_test)
+
+# Calculate accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print('Voting Classifier: {:.3f}'.format(accuracy))
