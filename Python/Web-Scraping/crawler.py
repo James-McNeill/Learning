f45cd908c3808594d185155cc08000ca198f98b4
James_McNeill <jmcneill06@outlook.com>
2022-05-23 14:59:18 +0100

    Create crawler.py

---
+++ Python/Web-Scraping/crawler.py
@@ -0,0 +1,30 @@
+# Web scraping - crawler
+
+'''
+The links are generated within the first parse method. A list is generated from the response. The response.follow() method 
+then works to request data from each of the web links. Within the second parser method the course description text is requested
+'''
+
+# Import the scrapy library
+import scrapy
+
+# Create the Spider class
+class DCdescr( scrapy.Spider ):
+  name = 'dcdescr'
+  # start_requests method
+  def start_requests( self ):
+    yield scrapy.Request( url = url_short, callback = self.parse )
+  
+  # First parse method
+  def parse( self, response ):
+    links = response.css( 'div.course-block > a::attr(href)' ).extract()
+    # Follow each of the extracted links
+    for link in links:
+      yield response.follow(url = link, callback = self.parse_descr)
+      
+  # Second parsing method
+  def parse_descr( self, response ):
+    # Extract course description
+    course_descr = response.css( 'p.course__description::text' ).extract_first()
+    # For now, just yield the course description
+    yield course_descr
