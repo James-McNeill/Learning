4d6135cde387dd4e667088d15971166ef69facd7
James_McNeill <jmcneill06@outlook.com>
2022-05-24 09:57:31 +0100

    Create classifiers.py

---
+++ Python/DevelopingPackage/classifiers.py
@@ -0,0 +1,10 @@
+# Classifiers
+'''
+Adding a classifiers list to the setup.py file allows users to search for specific elements of the package.
+Full list of classifiers @ https://pypi.org/classifiers
+'''
+classifiers=[
+'Development Status :: 2 - Pre-Alpha',
+'Intended Audience :: Developers',
+'Programming Language :: Python :: 3
+]
