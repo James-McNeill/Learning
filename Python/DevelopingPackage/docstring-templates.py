32ec9ad6f0f446a6d2ad02ac5af5bce3713d7662
James_McNeill <jmcneill06@outlook.com>
2022-05-24 09:41:59 +0100

    Create docstring-templates.py

---
+++ Python/DevelopingPackage/docstring-templates.py
@@ -0,0 +1,14 @@
+# Docstring templates
+
+'''
+Adding template docstring that can be used to provide users with the information required to understand the python package/directory/module they are using
+Code is implemented within the terminal
+'''
+pyment -w -o numpydoc file.py
+# pyment : is the method
+# -w and -o : aim to overwrite the existing docstring structure
+# numpydoc : is a docstring format for setting out the commentary
+# file.py : location of the file being updated with the template docstring format
+
+# NOTE: the pyment method can be used to switch between different docstring methods, depending on what is used by the 
+# users creating the code. There are a few different conventions available
