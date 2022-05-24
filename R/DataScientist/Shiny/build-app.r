c2e35674de0341816cbaf139e1a9ca68871e9165
James_McNeill <jmcneill06@outlook.com>
2022-05-24 13:34:37 +0100

    Create build-app.r

---
+++ R/DataScientist/Shiny/build-app.r
@@ -0,0 +1,6 @@
+# Build Shiny Apps
+# It’s time to build your own Shiny apps. You’ll make several apps from scratch, including one that allows you to 
+# gather insights from the Mental Health in Tech Survey and another that uses recipe ingredients as its input to 
+# accurately categorize different cuisines of the world. Along the way, you’ll also learn about more advanced input 
+# and output widgets, such as input validation, word clouds, and interactive maps.
+
