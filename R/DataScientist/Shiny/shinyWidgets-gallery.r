8da24eed401d5134ab4aaf956b14cf667ed3310d
James_McNeill <jmcneill06@outlook.com>
2022-05-24 14:22:22 +0100

    Create shinyWidgets-gallery.r

---
+++ R/DataScientist/Shiny/shinyWidgets-gallery.r
@@ -0,0 +1,7 @@
+# The shinyWidgets gallery
+
+# Let's look at all the different input options that are already built for you by exploring the shinyWidgets package. 
+# It comes with a neat built-in function, shinyWidgetsGallery() that opens a pre-built Shiny app that allows you to explore 
+# these pre-built inputs and gives you the code for implementing them.
+
+# Navigate to the radioButtons section. What argument do you use in radioGroupButtons() to display the buttons vertically instead of horizontally?
