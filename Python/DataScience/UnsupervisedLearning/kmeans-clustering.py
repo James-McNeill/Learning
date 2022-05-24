1ceeca9ea5886b25738b76f120e22b6aafb24106
James_McNeill <jmcneill06@outlook.com>
2022-05-24 09:08:49 +0100

    Create kmeans-clustering.py

---
+++ Python/DataScience/UnsupervisedLearning/kmeans-clustering.py
@@ -0,0 +1,103 @@
+# KMeans clustering
+
+'''
+Create labels from the existing data points with no prior knowledge of the data. Then predict new labels for new data based on the model that was fit
+
+Create the model and visualise the clusters relative to the input data points
+'''
+# Import KMeans
+from sklearn.cluster import KMeans
+
+# Create a KMeans instance with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Fit model to points
+model.fit(points)
+
+# Determine the cluster labels of new_points: labels
+labels = model.predict(new_points)
+
+# Print cluster labels of new_points
+print(labels)
+
+# Import pyplot - visualise the cluster model
+import matplotlib.pyplot as plt
+
+# Assign the columns of new_points: xs and ys
+xs = new_points[:,0]
+ys = new_points[:,1]
+
+# Make a scatter plot of xs and ys, using labels to define the colors
+_ = plt.scatter(xs, ys, c=labels, alpha=0.5)
+
+# Assign the cluster centers: centroids
+centroids = model.cluster_centers_
+
+# Assign the columns of centroids: centroids_x, centroids_y
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1]
+
+# Make a scatter plot of centroids_x and centroids_y
+_ = plt.scatter(centroids_x, centroids_y, marker='D', s=50)
+plt.show()
+
+
+# Model performance
+'''
+Measuring the performance of the clustering by reviewing the inertia as a plot. Looking for the number of clusters were the 
+average inertia starts to decrease slowly. The inertia is the average distance of all data points assigned to a cluster relative 
+to the centroid. Having a lower value is better but make sure not to have too many clusters. This is the balance-tradeoff 
+that must to be kept in mind.
+
+Next steps are to begin using preprocessing steps to scale the data before clustering. By making use of standardization steps 
+the values can all be put in the same relative scale (mean=0, std=1) and the next phase of the pipeline, using the KMeans 
+algorithm can be processed in order.
+'''
+ks = range(1, 6)
+inertias = []
+
+for k in ks:
+    # Create a KMeans instance with k clusters: model
+    model = KMeans(n_clusters=k)
+    
+    # Fit model to samples
+    model.fit(samples)
+    
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+    
+# Plot ks vs inertias
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
+
+# Create a KMeans model with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Use fit_predict to fit model and obtain cluster labels: labels
+labels = model.fit_predict(samples)
+
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'varieties': varieties})
+
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'], df['varieties'])
+
+# Display ct
+print(ct)
+
+# Perform the necessary imports
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+# Create scaler: scaler
+scaler = StandardScaler()
+
+# Create KMeans instance: kmeans
+kmeans = KMeans(n_clusters=4)
+
+# Create pipeline: pipeline
+pipeline = make_pipeline(scaler, kmeans)
