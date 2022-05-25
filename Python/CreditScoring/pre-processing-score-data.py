0df6d7e1c479d314753aa93bafb34e465b08fa86
James_McNeill <jmcneill06@outlook.com>
2022-05-25 12:48:20 +0100

    Create pre-processing-score-data.py

---
+++ Python/CreditScoring/pre-processing-score-data.py
@@ -0,0 +1,5 @@
+# Working with JSON and YAML files to make the processing of input values unique
+# AIM was to have one source for the input and output table names
+
+# Also providing details on the scorecard buckets as well within one source
+
