fa9d6ffc3bd3909132ac8fc4c65ea786e646eabb
James_McNeill <jmcneill06@outlook.com>
2024-10-25 14:39:15 +0100

    Update aggregations.py

--- Python/Reporting/aggregations.py
+++ Python/Reporting/aggregations.py
@@ -1,1 +1,13 @@
+# Apply the mappings
+df.groupby(['var_x', 'var_y'], dropna=False).agg({
+        'ID': ['count'],
+        'summary_var_y': lambda x: x.isna().sum()
+    })
 
+# Reviewing the cases with null summary_var_y when DataFrames joined together
+df1 = (
+    df
+    .loc[df.summary_var_y.isnull()]
+)
+df1.shape
+df1.head()
