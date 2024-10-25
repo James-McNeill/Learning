622b3dd8334df42b614b84f1f056551f17c28172
James_McNeill <jmcneill06@outlook.com>
2024-10-25 14:34:19 +0100

    Update notebook-settings.py

--- Python/00_Repo/notebook-settings.py
+++ Python/00_Repo/notebook-settings.py
@@ -3,6 +3,9 @@
 pd.set_option('display.max_columns', 3000)
 pd.set_option('display.max_rows', 300)
 
+# Adjust options for displaying the float columns
+pd.options.display.float_format = '{:,.2f}'.format
+
 # Display all of the outputs from a notebook cell. This code will allow for all methods used within a cell block to be displayed instead of just the 
 # final one. For example; df.head(), df.info(), df.describe(), can all be run together
 from IPython.core.interactiveshell import InteractiveShell
